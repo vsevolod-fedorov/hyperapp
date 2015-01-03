f73119a5b433f5baec149d8bec8cba8ee6eaf0d9
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2015-01-03 11:10:45 +0300

    refactor server: extract fs and object modules, make Object

---
+++ object.py
@@ -0,0 +1,61 @@
+
+class Column(object):
+
+    def __init__( self, id, title ):
+        self.id = id
+        self.title = title
+
+    def as_json( self ):
+        return dict(
+            id=self.id,
+            title=self.title,
+            )
+
+
+class Element(object):
+
+    def __init__( self, key, row, commands ):
+        self.key = key
+        self.row = row  # value list
+        self.commands = commands
+
+    def as_json( self ):
+        return dict(
+            row=self.row,
+            commands=[cmd.as_json() for cmd in self.commands],
+            )
+
+
+class Command(object):
+
+    def __init__( self, id, text, desc, shortcut=None ):
+        self.id = id
+        self.text = text
+        self.desc = desc
+        self.shortcut = shortcut
+
+    def as_json( self ):
+        return dict(
+            id=self.id,
+            text=self.text,
+            desc=self.desc,
+            shortcut=self.shortcut,
+            )
+
+
+class Object(object):
+
+    def __init__( self, path ):
+        self.path = path
+
+    def get_elements( self, count=None, from_key=None ):
+        elements = self.get_all_elements()
+        from_idx = 0
+        if from_key is not None:
+            for idx, elt in enumerate(elements):
+                if elt.row[self.key_column_idx] == from_key:
+                    from_idx = idx + 1
+                    break
+            else:
+                print 'Warning: unknown "from_key" is requested: %r' % from_key
+        return elements[from_idx:from_idx + max(count or 0, MIN_ROWS_RETURNED)]
