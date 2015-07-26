f709c03977f8ee2c1dbab43b29491a84a2fbe732
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2015-07-26 12:06:01 +0300

    test_list: add size

--- server/test_list.py
+++ server/test_list.py
@@ -1,10 +1,12 @@
 from common.interface import Command, Column, StringFieldHandle, IntFieldHandle, FormField, FormHandle
 from common.interface.test_list import params_form_iface, test_list_iface
+from util import path_part_to_str
 from object import Object, ListObject
 from module import Module, ModuleCommand
 
 
 MODULE_NAME = 'test_list'
+DEFAULT_SIZE = 10000
 
 
 class ParamsForm(Object):
@@ -31,6 +33,7 @@ class ParamsForm(Object):
 
     def run_command_submit( self, request ):
         print 'submitted: ', `request.params.prefix`, `request.params.size`
+        return request.make_response_handle(TestList(request.params.size))
 
 
 class TestList(ListObject):
@@ -46,11 +49,18 @@ class TestList(ListObject):
         Column('field_3', 'Field #3'),
         ]
 
-    def __init__( self ):
+    @classmethod
+    def resolve( cls, path ):
+        size = path.pop_int()
+        path.check_empty()
+        return cls(size)
+
+    def __init__( self, size=DEFAULT_SIZE ):
         ListObject.__init__(self)
+        self.size = size
 
     def get_path( self ):
-        return module.make_path(self.class_name)
+        return module.make_path(self.class_name, path_part_to_str(self.size))
 
     def get_commands( self ):
         return [Command('params', 'Params', 'Edit params', 'Return')]
@@ -64,11 +74,14 @@ class TestList(ListObject):
         return request.make_response_handle(ParamsForm())
 
     def get_elements( self, count=None, from_key=None ):
-        start = from_key or 0
-        stop = start + max(count or 10, 10)
-        elements = [self.Element(idx, [str(idx), 'field1#%d' % idx, 'field2', 'field3'])
-                    for idx in xrange(start, stop)]
-        has_more = True
+        elements = []
+        start = min(self.size, from_key or 0)
+        stop = min(self.size, start + max(count or 10, 10))
+        print '--- get_elements', `self.size`, `count`, `from_key`, `start`, `stop`
+        for idx in xrange(start, stop):
+            element = self.Element(idx, [str(idx), 'field1#%d' % idx, 'field2', 'field3'])
+            elements.append(element)
+        has_more = stop < self.size
         return (elements, has_more)
 
     
@@ -79,11 +92,10 @@ class TestListModule(Module):
 
     def resolve( self, path ):
         class_name = path.pop_str()
-        path.check_empty()
         if class_name == ParamsForm.class_name:
             return ParamsForm()
         if class_name == TestList.class_name:
-            return TestList()
+            return TestList.resolve(path)
         path.raise_not_found()
 
     def get_commands( self ):
