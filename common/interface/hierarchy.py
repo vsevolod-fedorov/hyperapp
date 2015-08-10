05d0494ba5f6c60127f901919211e0a3725b5e59
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2015-08-10 09:29:44 +0300

    TRecord/THierarchy: remove registering classes; use_class/register_class/make_class methods

--- common/interface/hierarchy.py
+++ common/interface/hierarchy.py
@@ -10,24 +10,11 @@ class Class(object):
 
 class TClass(TRecord):
 
-    def __init__( self, hierarchy, id, base, fields, cls ):
-        TRecord.__init__(self, fields, cls)
+    def __init__( self, hierarchy, id, base, fields ):
+        TRecord.__init__(self, fields)
         self.hierarchy = hierarchy
         self.id = id
         self.base = base
-
-    def register_class( self, cls ):
-        self.cls = cls
-        self.hierarchy.cls2id[cls] = self.id
-
-    # need to pick base.cls here, not in __init__
-    # because base.register_class may be called after derived object is constructed
-    def get_class( self ):
-        if self.cls:
-            return self.cls
-        if self.base:
-            return self.base.get_class()
-        return None
 
     def get_fields( self ):
         if self.base:
@@ -43,16 +30,6 @@ class TClass(TRecord):
 
     def make_object( self ):
         return Class(self.id)
-
-    # only to be used as base for usual classes
-    def make_class( self ):
-        tclass = self
-        class ClassBase(object):
-            def __init__( self, *args, **kw ):
-                self._class_id = tclass.id
-                for name, val in tclass.adopt_args(args, kw).items():
-                    setattr(self, name, val)
-        return ClassBase
             
 
 class THierarchy(Type):
@@ -60,17 +37,14 @@ class THierarchy(Type):
     def __init__( self ):
         Type.__init__(self)
         self.registry = {}  # id -> TClass
-        self.cls2id = {}  # cls -> id
 
-    def register( self, id, fields=None, cls=None, base=None ):
+    def register( self, id, fields=None, base=None ):
         assert isinstance(id, basestring), repr(id)
         assert fields is None or is_list_inst(fields, Field), repr(fields)
         assert base is None or isinstance(base, TClass), repr(base)
         assert id not in self.registry, 'Class id is already registered: %r' % id
-        tclass = TClass(self, id, base, fields or [], cls)
+        tclass = TClass(self, id, base, fields or [])
         self.registry[id] = tclass
-        if cls:
-            self.cls2id[tclass.get_class()] = id
         return tclass
 
     def validate( self, path, obj ):
@@ -82,8 +56,6 @@ class THierarchy(Type):
         return self.registry[id]
 
     def resolve_obj( self, obj ):
-        id = self.cls2id.get(obj.__class__)
-        if id is None:
-            assert hasattr(obj, '_class_id'), repr(obj)  # not a TClass instance
-            id = obj._class_id
+        assert hasattr(obj, '_class_id'), repr(obj)  # not a TClass instance
+        id = obj._class_id
         return self.resolve(id)
