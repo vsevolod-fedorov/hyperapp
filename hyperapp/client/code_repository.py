20cbb944476a83641b93c9f207549c1c043c657c
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2015-10-04 12:29:26 +0300

    start code repository proxy

---
+++ hyperapp/client/code_repository.py
@@ -0,0 +1,42 @@
+# code repository proxy
+
+from ..common.interface.code_repository import code_repository_iface
+from .server import Server
+from .proxy_object import ObjRespHandler, ProxyObject
+
+
+class PacketRespHandler(ObjRespHandler):
+
+    def __init__( self, object, command_id, packet ):
+        ObjRespHandler.__init__(self, object, command_id)
+        self.packet = packet
+
+    def process_response( self, server, response ):
+        object = self.object()
+        if object:
+            object.process_response(server, response, self)
+
+
+class CodeRepositoryProxy(ProxyObject):
+
+    def __init__( self, server ):
+        path = ['code_repository', 'code_repository']
+        ProxyObject.__init__(self, server, path, code_repository_iface)
+
+    def get_modules_and_process_packet( self, packet ):
+        command_id = 'get_modules'
+        request = self.prepare_request(command_id, module_ids=packet.aux.requirements)
+        resp_handler = PacketRespHandler(self, command_id, packet)
+        self.resp_handlers.add(resp_handler)
+        self.server.execute_request(request, resp_handler)
+
+    def process_response( self, server, response, resp_handler, initiator_view=None ):
+        if resp_handler.command_id == 'get_modules':
+            self.process_get_modules_response(resp_handler.packet, response.result)
+        ProxyObject.process_response(self, server, response, resp_handler, initiator_view)
+
+    def process_get_modules_response( self, packet, result ):
+        print '*** process_get_modules_response', packet, result.modules
+
+
+code_repository = ProxyObject(Server(('localhost', 8888)))
