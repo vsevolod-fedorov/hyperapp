451ff1f019d9c1ef87d128e46107d3eb7e743139
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2015-11-01 11:44:11 +0300

    extract Visitor from RequirementsCollector

--- hyperapp/common/requirements_collector.py
+++ hyperapp/common/requirements_collector.py
@@ -1,11 +1,5 @@
-from .method_dispatch import method_dispatch
+from .visitor import Visitor
 from .interface import (
-    TPrimitive,
-    TOptional,
-    TRecord,
-    TSwitchedRec,
-    THierarchy,
-    TList,
     tObject,
     tProxyObject,
     tHandle,
@@ -13,53 +7,17 @@ from .interface import (
     )
 
 
-class RequirementsCollector(object):
+class RequirementsCollector(Visitor):
 
     def collect( self, t, value ):
         self.collected_requirements = set()
         self.dispatch(t, value)
         return list([registry, key] for registry, key in self.collected_requirements)
 
-    @method_dispatch
-    def dispatch( self, t, value ):
-        assert False, repr((t, value))  # Unknown type
-
-    @dispatch.register(TPrimitive)
-    def process_primitive( self, t, value ):
-        pass
-
-    @dispatch.register(TOptional)
-    def process_optional( self, t, value ):
-        if value is not None:
-            self.dispatch(t.type, value)
-
-    @dispatch.register(TRecord)
-    def process_record( self, t, value ):
-        fields = {}
-        for field in t.get_static_fields():
-            field_val = getattr(value, field.name)
-            self.dispatch(field.type, field_val)
-            fields[field.name] = field_val
-        if isinstance(t, TSwitchedRec):
-            field = t.get_dynamic_field(fields)
-            self.dispatch(field.type, getattr(value, field.name))
-            
-    @dispatch.register(THierarchy)
-    def process_hierarchy_obj( self, t, value ):
+    def visit_hierarchy_obj( self, t, value ):
         if t is tObject:
             self.collected_requirements.add(('object', value.objimpl_id))
             if tObject.isinstance(value, tProxyObject):
                 self.collected_requirements.add(('interface', value.iface))
         if t is tHandle and tHandle.isinstance(value, tViewHandle):
             self.collected_requirements.add(('handle', value.view_id))
-        tclass = t.resolve_obj(value)
-#        self.collected_requirements.add((t.hierarchy_id, tclass.id))
-        self.dispatch(tclass.get_trecord(), value)
-
-    def process_field( self, field, value ):
-        self.dispatch(field.type, getattr(value, field.name))
-
-    @dispatch.register(TList)
-    def process_list( self, t, value ):
-        for elt in value:
-            self.dispatch(t.element_type, elt)
---
+++ hyperapp/common/visitor.py
@@ -0,0 +1,53 @@
+from .method_dispatch import method_dispatch
+from .interface import (
+    TPrimitive,
+    TOptional,
+    TRecord,
+    TSwitchedRec,
+    THierarchy,
+    TList,
+    )
+
+
+class Visitor(object):
+
+    def visit_hierarchy_obj( self, t, value ):
+        pass
+
+    @method_dispatch
+    def dispatch( self, t, value ):
+        assert False, repr((t, value))  # Unknown type
+
+    @dispatch.register(TPrimitive)
+    def process_primitive( self, t, value ):
+        pass
+
+    @dispatch.register(TOptional)
+    def process_optional( self, t, value ):
+        if value is not None:
+            self.dispatch(t.type, value)
+
+    @dispatch.register(TRecord)
+    def process_record( self, t, value ):
+        fields = {}
+        for field in t.get_static_fields():
+            field_val = getattr(value, field.name)
+            self.dispatch(field.type, field_val)
+            fields[field.name] = field_val
+        if isinstance(t, TSwitchedRec):
+            field = t.get_dynamic_field(fields)
+            self.dispatch(field.type, getattr(value, field.name))
+            
+    @dispatch.register(THierarchy)
+    def process_hierarchy_obj( self, t, value ):
+        self.visit_hierarchy_obj(t, value)
+        tclass = t.resolve_obj(value)
+        self.dispatch(tclass.get_trecord(), value)
+
+    def process_field( self, field, value ):
+        self.dispatch(field.type, getattr(value, field.name))
+
+    @dispatch.register(TList)
+    def process_list( self, t, value ):
+        for elt in value:
+            self.dispatch(t.element_type, elt)
