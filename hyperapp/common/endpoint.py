e35e9c8b48597d684bbd4792761c452a90097add
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2015-11-29 12:13:09 +0300

    server: load identity, save endpoint

---
+++ hyperapp/common/endpoint.py
@@ -0,0 +1,20 @@
+import pickle
+from . identity import PublicKey
+
+
+class Endpoint(object):
+
+    @classmethod
+    def load_from_file( cls, fpath ):
+        with open(fpath, 'b') as f:
+            public_key_pem, routes = pickle.load(f)
+        return cls(PublicKey.from_pem(public_key_pem), routes)
+
+    def __init__( self, public_key, routes ):
+        assert isinstance(public_key, PublicKey), repr(public_key)
+        self.public_key = public_key
+        self.routes = routes
+
+    def save_to_file( self, fpath ):
+        with open(fpath, 'wb') as f:
+            pickle.dump((self.public_key.to_pem(), self.routes), f)
