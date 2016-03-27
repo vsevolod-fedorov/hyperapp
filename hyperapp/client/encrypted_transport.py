e4372c9b6921f14ca5d8f8b932a0ebe36587914e
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2016-03-27 12:07:20 +0300

    client: decode server packets

--- hyperapp/client/encrypted_transport.py
+++ hyperapp/client/encrypted_transport.py
@@ -1,5 +1,12 @@
-from ..common.htypes import tString, tBinary, Field, TRecord
-from ..common.encrypted_packet import tEncryptedPacket, tEncryptedInitialPacket, make_session_key, encrypt_initial_packet
+from PySide import QtCore
+from ..common.htypes import tServerPacket
+from ..common.encrypted_packet import (
+    tEncryptedPacket,
+    tEncryptedInitialPacket,
+    make_session_key,
+    encrypt_initial_packet,
+    decrypt_packet,
+    )
 from ..common.packet import AuxInfo, tPacket, Packet
 from ..common.transport_packet import tTransportPacket, encode_transport_packet, decode_transport_packet
 from ..common.packet_coders import packet_coders
@@ -47,6 +54,18 @@ class EncryptedTransport(Transport):
         connection.send_data(packet_data)
         return True
 
+    def process_packet( self, session_list, server_public_key, data ):
+        session = session_list.get_transport_session(TRANSPORT_ID)
+        assert session is not None  # must be created when sending request
+        encrypted_packet = packet_coders.decode(ENCODING, data, tEncryptedPacket)
+        packet_data = decrypt_packet(session.session_key, encrypted_packet)
+        packet = packet_coders.decode(ENCODING, packet_data, tPacket)
+        app = QtCore.QCoreApplication.instance()
+        app.response_mgr.process_packet(server_public_key, packet, self._decode_payload)
+
+    def _decode_payload( self, data ):
+        return packet_coders.decode(ENCODING, data, tServerPacket)
+    
     def _make_packet( self, session, server_public_key, payload, payload_type, aux_info ):
         if aux_info is None:
             aux_info = AuxInfo(requirements=[], modules=[])
