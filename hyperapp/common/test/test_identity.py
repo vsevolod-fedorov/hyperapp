03f0696a75ab72d7f4ae0a99fce40ed28ba8d289
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2016-05-02 11:00:01 +0300

    PublicKey.verity: use return value instead of exception

--- hyperapp/common/test/test_identity.py
+++ hyperapp/common/test/test_identity.py
@@ -1,14 +1,14 @@
 import unittest
-from hyperapp.common.identity import BadSignature, Identity
+from hyperapp.common.identity import Identity
 
 
 class IdentityTest(unittest.TestCase):
 
     def test_signature( self ):
-        identity = Identity.generate()
+        identity = Identity.generate(fast=True)
         message = 'test message'
         signature = identity.sign(message)
         public_key = identity.get_public_key()
-        public_key.verify(message, signature)
-        self.assertRaises(BadSignature, public_key.verify, message, signature + 'x')
-        self.assertRaises(BadSignature, public_key.verify, message + 'x', signature)
+        self.assertEqual(True, public_key.verify(message, signature))
+        self.assertEqual(False, public_key.verify(message, signature + 'x'))
+        self.assertEqual(False, public_key.verify(message + 'x', signature))
