ce928d4e773fa619c16591f5467d461db678edee
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2016-06-25 11:24:07 +0300

    restore element commands

--- hyperapp/client/cmd_pane.py
+++ hyperapp/client/cmd_pane.py
@@ -57,7 +57,7 @@ class View(QtGui.QDockWidget):
         for cmd in elt.commands:
             assert isinstance(cmd, Command), repr(cmd)
             button = self._make_button(cmd)
-            button.pressed.connect(lambda cmd=cmd: asyncio.async(cmd.run(elt.key)))
+            button.pressed.connect(lambda cmd=cmd: asyncio.async(cmd.run()))
             self.layout.addWidget(button)
             self.elts_buttons.append(button)
 
