63a992c3a84d7fe6ebddaa64648b8e2c60a0cca8
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2016-07-10 11:04:00 +0300

    resources type and registry

---
+++ hyperapp/client/resources_registry.py
@@ -0,0 +1,14 @@
+from ..common.htypes import tLocaleResources
+
+
+class ResourcesRegistry(object):
+
+    def __init__( self ):
+        self._registry = {}
+
+    def register( self, id, locale, resources ):
+        assert isisntance(resource, tLocaleResources), repr(resources)
+        self._registry[(id, locale)] = resources
+
+    def resolve( self, id, locale ):
+        return self._registry.get((id, locale))
