de5c5f12baec9a0ab6c1e0dc77b79b6216f8e129
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2016-08-11 09:36:46 +0300

    client: create type registry; register received type modules

---
+++ hyperapp/client/type_registry.py
@@ -0,0 +1,14 @@
+from ..common.htypes import tTypeModule
+
+
+class TypeRegistry(object):
+
+    def __init__( self ):
+        self._registry = {}  # module name -> tTypeModule
+
+    def register( self, type_module ):
+        assert isinstance(type_module, tTypeModule), repr(type_module)
+        self._registry[type_module.module_name] = type_module
+
+    def resolve( self, module_name ):
+        return self._registry[module_name]
