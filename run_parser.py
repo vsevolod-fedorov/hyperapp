efd8236ffcf10d0f679e3e70d5b9fb07e423f153
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2016-08-12 12:38:28 +0300

    start type module parser, using ply and python tokenizer

---
+++ run_parser.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import logging
+import argparse
+from hyperapp.common.type_module_parser import Lexer, parse_type_module
+
+
+def test_lex( fpaths ):
+    for fpath in fpaths:
+        print('%s:' % fpath)
+        lexer = Lexer()
+        with open(fpath) as f:
+            input = f.read()
+        lexer.input(input)
+        while True:
+            tok = lexer.token()
+            print(tok)
+            if not tok:
+                break
+
+def test_yacc( fpaths ):
+    for fpath in fpaths:
+        print('%s:' % fpath)
+        with open(fpath) as f:
+            input = f.read()
+        print('parsing:')
+        result = parse_type_module(input, debug=True)
+        print('result:', result)
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s  %(message)s')
+
+    parser = argparse.ArgumentParser(description='Hyperapp types file parser test')
+    parser.add_argument('command', choices=['lex', 'yacc'], help='What to test')
+    parser.add_argument('fpaths', nargs='+', help='Type files to parse')
+    args = parser.parse_args()
+
+    if args.command == 'lex':
+        test_lex(args.fpaths)
+    elif args.command == 'yacc':
+        test_yacc(args.fpaths)
+    
+main()
