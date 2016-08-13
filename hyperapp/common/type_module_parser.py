ecf11bebe1eaf851575a87ae6e23f787dabf19ac
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2016-08-13 09:07:48 +0300

    Lexer: detect&return BLOCK_BEGIN&BLOCK_END

--- hyperapp/common/type_module_parser.py
+++ hyperapp/common/type_module_parser.py
@@ -7,6 +7,8 @@ from .htypes import t_named, tTypeDef
 
 
 ENCODING = tokenize.ENCODING
+BLOCK_BEGIN = 'BLOCK_BEGIN'
+BLOCK_END = 'BLOCK_END'
 tok_name = dict(token.tok_name,
                 ENCODING='ENCODING')
 
@@ -18,10 +20,13 @@ token_types = [
     token.EQUAL,
     ]
 
-tokens = [tok_name[t] for t in token_types]
+tokens = [tok_name[t] for t in token_types] + [
+    BLOCK_BEGIN,
+    BLOCK_END,
+    ]    
 
 
-def p_module_1( p ):
+def p_module( p ):
     'module : ENCODING typedef_list eom'
     p[0] = p[2]
 
@@ -31,11 +36,11 @@ def p_eom_1( p ):
 def p_eom_2( p ):
     'eom : ENDMARKER'
 
-def p_typedef_list_multi( p ):
+def p_typedef_list_1( p ):
     'typedef_list : typedef_list NEWLINE typedef'
     p[0] = p[1] + [p[3]]
 
-def p_typedef_list_single( p ):
+def p_typedef_list_2( p ):
     'typedef_list : typedef'
     p[0] = [p[1]]
 
@@ -52,14 +57,29 @@ class Lexer(object):
 
     def input( self, input ):
         self._tokenizer = tokenize.tokenize(BytesIO(input.encode('utf-8')).readline)
+        self._tab_size = None
+        self._indent = 0
 
     def token( self ):
         try:
             tinfo = self._tokenizer.__next__()
         except StopIteration:
             return None
-        t = tok_name[tinfo.exact_type]
-        print('Lexer.token', t, tinfo)
+        if tinfo.type == token.INDENT:
+            if not self._tab_size:
+                # first indent found, this is file tab size
+                assert '\t' not in tinfo.string, 'Tab intention is not supported'
+                self._tab_size = len(tinfo.string)
+            assert len(tinfo.string) % self._tab_size == 0, 'Invalid indent: %r (detected tab size: %d)' % (token.string, self._tab_size)
+            assert len(tinfo.string)/self._tab_size == 1, 'Invalid indent: %r (detected tab size: %d)' % (token.string, self._tab_size)
+            self._indent += 1
+            t = BLOCK_BEGIN
+        elif tinfo.type == token.DEDENT:
+            self._indent -= 1
+            t = BLOCK_END
+        else:
+            t = tok_name[tinfo.exact_type]
+        ## print('Lexer.token', t, tinfo)
         tok = lex.LexToken()
         tok.type = t
         tok.value = tinfo.string
