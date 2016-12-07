9276e621a1706532de9d034f40b26f2666034429
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2016-12-07 12:40:58 +0300

    convert server_management to dynamic module

--- run_server.py
+++ run_server.py
@@ -6,13 +6,11 @@ from hyperapp.common.identity import Identity
 from hyperapp.server.services import Services
 from hyperapp.server.server import Server
 from hyperapp.server.tcp_server import TcpServer
-from hyperapp.server.server_management import get_management_url
 
 log = logging.getLogger(__name__)
 
 # self-registering modules:
 import hyperapp.server.ponyorm_module
-import hyperapp.server.server_management
 
 
 DEFAULT_ADDR = 'localhost:8888'
@@ -37,7 +35,7 @@ def main():
     services = Services()
     server = Server(identity, args.test_delay)
     tcp_server = TcpServer(services.remoting, server, host, port)
-    management_url = get_management_url(server.get_public_key())
+    management_url = services.modules.server_management.get_management_url(server.get_public_key())
     url_with_routes = management_url.clone_with_routes(tcp_server.get_routes())
     log.info('Management url: %s', url_with_routes.to_str())
     tcp_server.run()
