0d5772bd9ad757989844b0955003517821ccc5be
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2016-12-18 10:43:33 +0300

    remove global usage of core types for navigator module

--- hyperapp/client/navigator/__init__.py
+++ hyperapp/client/navigator/__init__.py
@@ -1,3 +1,10 @@
-from .htypes import item_type, state_type
+from .module import get_this_module, ThisModule
 from .navigator import register_views, View
 from .history_list import register_object_implementations
+
+
+def get_item_type():
+    return get_this_module().item_type
+
+def get_state_type():
+    return get_this_module().state_type
