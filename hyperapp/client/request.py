b028368591bd683e9e3c4bb35d74e79d2dacffd1
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2017-01-03 11:56:31 +0300

    wip: updating client test_real_request

--- hyperapp/client/request.py
+++ hyperapp/client/request.py
@@ -7,13 +7,14 @@ from ..common.identity import PublicKey
 
 class RequestBase(object):
 
-    def __init__( self, iface, path, command_id, params ):
+    def __init__( self, request_types, iface, path, command_id, params ):
         assert isinstance(iface, Interface), repr(iface)
         assert isinstance(command_id, str), repr(command_id)
         params_type = iface.get_request_params_type(command_id)
         if params is None:
             params = params_type()
         assert isinstance(params, params_type), repr(params)
+        self._request_types = request_types
         self.iface = iface
         self.path = path
         self.command_id = command_id
@@ -28,13 +29,13 @@ class ClientNotification(RequestBase):
 
 class Request(RequestBase):
 
-    def __init__( self, iface, path, command_id, request_id, params=None ):
+    def __init__( self, request_types, iface, path, command_id, request_id, params=None ):
         assert isinstance(request_id, str), repr(request_id)
-        RequestBase.__init__(self, iface, path, command_id, params)
+        RequestBase.__init__(self, request_types, iface, path, command_id, params)
         self.request_id = request_id
 
     def to_data( self ):
-        return tRequest(self.iface.iface_id, self.path, self.command_id, self.params, self.request_id)
+        return self._request_types.tRequest(self.iface.iface_id, self.path, self.command_id, self.params, self.request_id)
 
 
 class ResponseBase(object):
