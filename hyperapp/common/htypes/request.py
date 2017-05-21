1008c84de20734d8c7aa9c43ef97f3eb9d156571
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2017-05-21 12:39:19 +0300

    create exception hierarchy; add tError request types

--- hyperapp/common/htypes/request.py
+++ hyperapp/common/htypes/request.py
@@ -7,17 +7,19 @@ from .htypes import (
     tPath,
     tIfaceId,
     )
-from .hierarchy import THierarchy
+from .hierarchy import THierarchy, TExceptionHierarchy
 from .switched import tSwitched, TSwitchedRec
 
 
 def make_request_types():
     types = SimpleNamespace()
+
     types.tUpdate = TSwitchedRec(['iface'], fields=[
         Field('iface', tIfaceId),
         Field('path', tPath),
         Field('diff', tSwitched),
         ])
+
     types.tClientPacket = THierarchy('client_packet')
     types.tClientNotificationRec = TSwitchedRec(['iface', 'command_id'], fields=[
         Field('iface', tIfaceId),
@@ -29,6 +31,12 @@ def make_request_types():
     types.tRequest = types.tClientPacket.register('request', TSwitchedRec(base=types.tClientNotificationRec, fields=[
         Field('request_id', tString),
         ]))
+
+    types.tError = TExceptionHierarchy('error')
+    types.tErrorRoot = types.tError.register('root')
+    types.tClientError = types.tError.register('client_error', base=types.tErrorRoot)
+    types.tServerError = types.tError.register('server_error', base=types.tErrorRoot)
+
     types.tServerPacket = THierarchy('server_packet')
     types.tServerNotification = types.tServerPacket.register('notification', fields=[
         Field('updates', TList(types.tUpdate)),
@@ -42,6 +50,10 @@ def make_request_types():
                                 base=types.tResponseRec, fields=[
                                 Field('result', tSwitched),
                                 ])
+    types.tErrorResponseRec = TRecord(base=types.tResponseRec, fields=[
+                                Field('error', types.tError),
+                                ])
     types.tResponse = types.tServerPacket.register('response')
     types.tResultResponse = types.tServerPacket.register('result_response', types.tResultResponseRec, base=types.tResponse)
+    types.tErrorResponse = types.tServerPacket.register('error_response', types.tErrorResponseRec, base=types.tResponse)
     return types
