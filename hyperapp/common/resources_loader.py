4e6ab67bd27b1c9d36a03beafcd68493dafa7abd
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2017-06-25 11:33:13 +0300

    add error message resource type

--- hyperapp/common/resources_loader.py
+++ hyperapp/common/resources_loader.py
@@ -40,31 +40,37 @@ class ResourcesLoader(object):
         for section_type, item_elements in sections.items():
             for item_id, items in item_elements.items():
                 if section_type == 'commands':
-                    resource = self._dict2command_resource(items)
+                    resource = self._value2command_resource(items)
                     item_type = 'command'
                 elif section_type == 'columns':
-                    resource = self._dict2column_resource(items)
+                    resource = self._value2column_resource(items)
                     item_type = 'column'
                 elif section_type == 'param_editors':
-                    resource = self._dict2param_editor_resource(items)
+                    resource = self._value2param_editor_resource(items)
                     item_type = 'param_editor'
+                elif section_type == 'errors':
+                    resource = self._value2error_message_resource(items)
+                    item_type = 'error_message'
                 else:
                     assert False, 'Unknown resource section type: %r' % section_type
                 resource_id = [item_type, item_id]
                 yield self._resource_types.resource_rec(resource_id, resource)
 
-    def _dict2command_resource(self, d):
-        return self._resource_types.command_resource(is_default=d.get('is_default', False),
-                                                     text=d.get('text'),
-                                                     description=d.get('description') or d.get('text'),
-                                                     shortcuts=d.get('shortcuts', []))
+    def _value2command_resource(self, value):
+        return self._resource_types.command_resource(is_default=value.get('is_default', False),
+                                                     text=value.get('text'),
+                                                     description=value.get('description') or value.get('text'),
+                                                     shortcuts=value.get('shortcuts', []))
 
-    def _dict2column_resource(self, d):
-        return self._resource_types.column_resource(visible=d.get('visible', True),
-                                                    text=d.get('text'),
-                                                    description=d.get('description'))
+    def _value2column_resource(self, value):
+        return self._resource_types.column_resource(visible=value.get('visible', True),
+                                                    text=value.get('text'),
+                                                    description=value.get('description'))
 
-    def _dict2param_editor_resource(self, d):
+    def _value2param_editor_resource(self, value):
         decoder = DictDecoder()
-        param_editor = decoder.decode_dict(self._param_editor_types.param_editor, d)
+        param_editor = decoder.decode_dict(self._param_editor_types.param_editor, value)
         return self._param_editor_types.param_editor_resource(param_editor)
+
+    def _value2error_message_resource(self, value):
+        return self._resource_types.error_message_resource(message=value)
