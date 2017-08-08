9b561fc2083e5ca9b980e192888bfe503bfa4786
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2017-08-08 12:40:06 +0300

    update real client tests

--- hyperapp/common/request.py
+++ hyperapp/common/request.py
@@ -8,6 +8,12 @@ log = logging.getLogger(__name__)
 
 class Update(object):
 
+    @classmethod
+    def from_data(cls, iface_registry, update):
+        iface = iface_registry.resolve(update.iface_id)
+        diff = update.diff.decode(iface.diff_type)
+        return cls(iface, update.path, diff)
+
     def __init__(self, iface, path, diff):
         assert isinstance(iface, Interface), repr(iface)
         assert is_list_inst(path, str), repr(path)
