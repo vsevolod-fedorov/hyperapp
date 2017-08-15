4d77aa8c90c671d1e3f1a315c1dad573fe18ffdb
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2017-08-15 10:42:41 +0300

    run_test.sh script: add 'fast' parameter

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,13 @@
+#!/bin/bash -e
+
 DIR=$(dirname $0)
+
+if [ "$1" == "fast" ]; then
+	ARGS="-m not slow"
+	shift
+else
+	ARGS=""
+fi
+
 cd $DIR
-PYTHONPATH=$PYTHONPATH:$DIR ~/venv/bin/pytest --ignore dynamic_modules "$@"
+PYTHONPATH=$PYTHONPATH:$DIR ~/venv/bin/pytest --ignore dynamic_modules "$ARGS" "$@"
