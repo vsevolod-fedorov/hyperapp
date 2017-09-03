334dc68569aec7b7ead9426fb396f2ba3dbdb68f
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2017-09-03 11:50:21 +0300

    add test_slices_are_merged

--- hyperapp/client/test/test_proxy_list_object.py
+++ hyperapp/client/test/test_proxy_list_object.py
@@ -1,5 +1,6 @@
 import os.path
 import logging
+import uuid
 from unittest import mock
 import aiomock
 import asyncio
@@ -9,6 +10,7 @@ from hyperapp.common.htypes.list_interface import Column, ListInterface
 from hyperapp.common.list_object import Element, Slice
 from hyperapp.common.identity import PublicKey
 from hyperapp.common.services import ServicesBase
+from hyperapp.client.request import Request
 from hyperapp.client.server import Server
 from hyperapp.client.list_object import ListObserver
 from hyperapp.client.proxy_list_object import ProxyListObject
@@ -52,9 +54,11 @@ M6nlQe0Uf0B0rUKTvBnXI7Y5wrHnZkAHEvjOqj+RYiRD13f+yUOnpMaWB+s1GqGFeKiI58a13Vd/GBKU
 @pytest.fixture
 def server():
     public_key = PublicKey.from_pem(public_key_pem)
-    return aiomock.AIOMock(
+    server = aiomock.AIOMock(
         spec=Server,
         public_key=public_key)
+    server.send_notification.async_return_value = None
+    return server
 
 @pytest.fixture
 def proxy_list_object(services, server):
@@ -126,5 +130,54 @@ def test_fetch_cached_elements(proxy_list_object):
     assert slice.bof
     assert slice.eof
     assert slice.elements == elements
+    observer.process_fetch_result.assert_called_once_with(slice)
+    proxy_list_object.server.execute_request.assert_not_called()
+
+
+# two adjacent slices must be merged when fetch results are processed
+@pytest.mark.asyncio
+@asyncio.coroutine
+def test_slices_are_merged(services, proxy_list_object):
+    iface = proxy_list_object.iface
+
+    # fetch first 10 elements
+    fetch_result = iface.Contents(
+        slice=Slice(
+            sort_column_id='id',
+            from_key=None,
+            elements=[Element(i, iface.Row(i)) for i in range(10)],
+            bof=True,
+            eof=False,
+            ).to_data(iface))
+    response = mock.Mock(error=None, result=fetch_result)
+    proxy_list_object.server.execute_request.async_return_value = response
+    yield from proxy_list_object.fetch_elements('id', None, 0, 100)
+
+    # fetch next 10 elements
+    fetch_result = iface.Contents(
+        slice=Slice(
+            sort_column_id='id',
+            from_key=9,
+            elements=[Element(i, iface.Row(i)) for i in range(10, 20)],
+            bof=False,
+            eof=False,
+            ).to_data(iface))
+    response = mock.Mock(error=None, result=fetch_result)
+    proxy_list_object.server.execute_request.async_return_value = response
+    with mock.patch('uuid.uuid4') as uuid4:
+        uuid4.return_value = 'request#1'
+        yield from proxy_list_object.fetch_elements('id', 9, 0, 100)
+    proxy_list_object.server.execute_request.assert_called_with(
+        Request(services.types.packet, iface, ['test-path'], 'fetch_elements', 'request#1',
+                params=iface.get_command('fetch_elements').params_type('id', 9, 0, 100)))
+
+    # elements fetched by first two calls must be merged together
+    observer = mock.Mock(spec=ListObserver)
+    proxy_list_object.subscribe(observer)
+    proxy_list_object.server.reset_mock()
+    slice = yield from proxy_list_object.fetch_elements('id', None, 0, 100)
+    assert slice.bof
+    assert not slice.eof
+    assert slice.elements == [Element(i, iface.Row(i)) for i in range(20)]
     observer.process_fetch_result.assert_called_once_with(slice)
     proxy_list_object.server.execute_request.assert_not_called()
