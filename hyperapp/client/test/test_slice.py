7129ec1f1ff631ee2ed8e4a76576b8caa676fce8
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2017-09-24 11:00:49 +0300

    add tests for slices/chunks with order_key different from key

--- hyperapp/client/test/test_slice.py
+++ hyperapp/client/test/test_slice.py
@@ -15,19 +15,39 @@ def chunk(from_key, keys, bof=False, eof=False):
     elements = [element(key) for key in keys]
     return Chunk(sort_column_id='id', from_key=from_key, elements=elements, bof=bof, eof=eof)
 
+def ochunk(from_key, element_tuples, bof=False, eof=False):
+    elements = [element(key, order_key) for (key, order_key) in element_tuples]
+    return Chunk(sort_column_id='name', from_key=from_key, elements=elements, bof=bof, eof=eof)
+
 def slice(keys, bof=False, eof=False):
     key2element = {key: element(key) for key in keys}
     return Slice(key2element, sort_column_id='id', bof=bof, eof=eof, keys=list(keys))
 
+def oslice(element_tuples, bof=False, eof=False):
+    key2element = {key: element(key, order_key) for (key, order_key) in element_tuples}
+    keys = [key for (key, order_key) in element_tuples]
+    return Slice(key2element, sort_column_id='name', bof=bof, eof=eof, keys=keys)
+
 
 @pytest.mark.parametrize('slice, new_chunk, expected_new_slice', [
-    (slice([0, 1, 2, 3]), chunk(3, [4, 5]), slice([0, 1, 2, 3, 4, 5])),
-    (slice([0, 1, 2, 3, 4, 5]), chunk(1, [11, 12]), slice([0, 1, 11, 12])),  # old elements after from_key must be thrown away
+    (slice([1, 2, 3]), chunk(3, [4, 5]), slice([1, 2, 3, 4, 5])),
+    (slice([1, 2, 3, 4, 5]), chunk(1, [11, 12]), slice([1, 11, 12])),  # old elements after from_key must be thrown away
     (slice([10, 11, 12]), chunk(None, [1, 2, 3], bof=True), slice([1, 2, 3], bof=True)),  # bof chunk must replace all
     (slice([1]), chunk(1, [2, 3], eof=True), slice([1, 2, 3], eof=True)),  # eof must be recorded in slice
     (slice([1], eof=True), chunk(1, [2, 3], eof=False), slice([1, 2, 3], eof=False)),  # eof must be recorded in slice
     ])
 def test_new_chunk_should_be_properly_merged_in(slice, new_chunk, expected_new_slice):
+    slice.add_fetched_chunk(new_chunk)
+    assert slice == expected_new_slice
+
+@pytest.mark.parametrize('slice, new_chunk, expected_new_slice', [
+    (oslice([(21, 1), (12, 2), (43, 3)]), ochunk(43, [(34, 4), (15, 5)]), oslice([(21, 1), (12, 2), (43, 3), (34, 4), (15, 5)])),
+    (oslice([(21, 1), (12, 2), (43, 3), (34, 4), (15, 5)]), ochunk(21, [(38, 8), (19, 9)]), oslice([(21, 1), (38, 8), (19, 9)])),  # old elements after from_key must be thrown away
+    (oslice([(36, 6), (47, 7), (28, 8)]), ochunk(None, [(41, 1), (12, 2), (33, 3)], bof=True), oslice([(41, 1), (12, 2), (33, 3)], bof=True)),  # bof chunk must replace all
+    (oslice([(21, 1)]), ochunk(21, [(12, 2), (43, 3)], eof=True), oslice([(21, 1), (12, 2), (43, 3)], eof=True)),  # eof must be recorded in slice
+    (oslice([(21, 1)], eof=True), ochunk(21, [(12, 2), (43, 3)], eof=False), oslice([(21, 1), (12, 2), (43, 3)], eof=False)),  # eof must be recorded in slice
+    ])
+def test_new_chunk_should_be_properly_merged_in_ordered_slice(slice, new_chunk, expected_new_slice):
     slice.add_fetched_chunk(new_chunk)
     assert slice == expected_new_slice
 
@@ -40,6 +60,17 @@ def test_new_chunk_should_be_properly_merged_in(slice, new_chunk, expected_new_s
     (slice([1, 2, 3, 4], eof=True), ListDiff.add_one(element(5)), slice([1, 2, 3, 4, 5])),  # after last one and eof - append
     ])
 def test_diff_should_be_properly_merged_in(slice, diff, expected_new_slice):
+    slice.merge_in_diff(diff)
+    assert slice == expected_new_slice
+
+@pytest.mark.parametrize('slice, diff, expected_new_slice', [
+    (oslice([(21, 1), (12, 2), (34, 4), (15, 5)]), ListDiff.add_one(element(43, 3)), oslice([(21, 1), (12, 2), (43, 3), (34, 4), (15, 5)])),  # between existing ones
+    (oslice([(22, 2), (13, 3), (54, 4), (15, 5)]), ListDiff.add_one(element(21, 1)), oslice([(22, 2), (13, 3), (54, 4), (15, 5)])),  # before first one and not bof - ignore
+    (oslice([(22, 2), (13, 3), (54, 4), (15, 5)], bof=True), ListDiff.add_one(element(21, 1)), oslice([(21, 1), (22, 2), (13, 3), (54, 4), (15, 5)])),  # before first one and bof - insert
+    (oslice([(21, 1), (42, 2), (13, 3), (34, 4)]), ListDiff.add_one(element(15, 5)), oslice([(21, 1), (42, 2), (13, 3), (34, 4)])),  # after last one and not eof - ignore
+    (oslice([(21, 1), (42, 2), (13, 3), (34, 4)], eof=True), ListDiff.add_one(element(15, 5)), oslice([(21, 1), (42, 2), (13, 3), (34, 4), (15, 5)])),  # after last one and eof - append
+    ])
+def test_diff_should_be_properly_merged_in_ordered_slice(slice, diff, expected_new_slice):
     slice.merge_in_diff(diff)
     assert slice == expected_new_slice
 
