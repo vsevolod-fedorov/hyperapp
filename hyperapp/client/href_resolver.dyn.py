ec5407043165d133e532c1fc09260d3f8b467cfc
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2017-10-29 12:51:56 +0300

    create HRefObjectRegistry

--- hyperapp/client/href_resolver.dyn.py
+++ hyperapp/client/href_resolver.dyn.py
@@ -1,6 +1,8 @@
 import asyncio
 import os.path
+from ..common.interface import hyper_ref as href_types
 from ..common.url import UrlWithRoutes
+from .registry import Registry
 from .module import Module
 
 
@@ -18,6 +20,15 @@ class HRefResolver(object):
         return result.href_object
 
 
+class HRefObjectRegistry(Registry):
+
+    def resolve(self, href_object):
+        tclass = href_types.href_object.resolve_obj(href_object)
+        rec = self._resolve(tclass.id)
+        log.info('producing href object %r using %s(%s, %s)', tclass.id, rec.factory, rec.args, rec.kw)
+        return rec.factory(href_object, *rec.args, **rec.kw)
+
+
 class ThisModule(Module):
 
     def __init__(self, services):
@@ -28,3 +39,4 @@ class ThisModule(Module):
             url = UrlWithRoutes.from_str(services.iface_registry, f.read())
         proxy = services.proxy_factory.from_url(url)
         services.href_resolver = HRefResolver(proxy)
+        services.href_object_registry = HRefObjectRegistry()
