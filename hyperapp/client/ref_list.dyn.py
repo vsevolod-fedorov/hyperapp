c68ad64f634c9aa9e2cf6b85d118f227e2fc521a
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2017-11-12 12:59:55 +0300

    create RefListService, register RefListObject

--- hyperapp/client/ref_list.dyn.py
+++ hyperapp/client/ref_list.dyn.py
@@ -2,6 +2,7 @@ import asyncio
 from ..common.htypes import tInt, tString, Column, list_handle_type
 from ..common.interface import core as core_types
 from ..common.interface import ref_list as ref_list_types
+from ..common.url import Url
 from .module import Module
 from .list_object import ListObject
 
@@ -12,38 +13,34 @@ class RefListObject(ListObject):
 
     @classmethod
     def from_state(cls, state, service_registry):
-        fs_service = service_registry.resolve(state.fs_service)
-        return cls(fs_service, state.host, state.path)
+        ref_list_service = service_registry.resolve(state.ref_list_service)
+        return cls(ref_list_service, state.ref_list_id)
 
-    def __init__(self, fs_service, host, path):
+    def __init__(self, ref_list_service, ref_list_id):
         ListObject.__init__(self)
-        self._fs_service = fs_service
-        self._host = host
-        self._path = path
+        self._ref_list_service = ref_list_service
+        self._ref_list_id = ref_list_id
 
     def get_state(self):
-        return fs_types.fs_dir_object(self.objimpl_id, self._fs_service.to_data(), self._host, self._path)
+        return ref_list_types.ref_list_object(self.objimpl_id, self._ref_list_service.to_data(), self._ref_list_id)
 
     def get_title(self):
-        return '%s:%s' % (self._host, '/'.join(self._path))
+        return 'Ref List %s' % self._ref_list_id
 
     def get_commands(self):
         return ListObject.get_commands(self)
 
     def get_columns(self):
         return [
-            Column('key', is_key=True),
-            Column('ftype'),
-            Column('ftime', type=tInt),
-            Column('fsize', type=tInt),
+            Column('id', is_key=True),
             ]
 
     def get_key_column_id(self):
-        return 'key'
+        return 'id'
 
     @asyncio.coroutine
     def fetch_elements(self, sort_column_id, from_key, desc_count, asc_count):
-        chunk = yield from self._fs_service.fetch_dir_contents(
+        chunk = yield from self._ref_list_service.fetch_dir_contents(
             self._host, self._path, sort_column_id, from_key, desc_count, asc_count)
         elements = [Element(row.key, row, commands=None, order_key=getattr(row, sort_column_id))
                     for row in chunk.rows]
@@ -56,6 +53,27 @@ class RefListObject(ListObject):
         log.info('-- FsDirObject.process_diff self=%r diff=%r', id(self), diff)
 
 
+class RefListService(object):
+
+    @classmethod
+    def from_data(cls, service_object, iface_registry, proxy_factory):
+        service_url = Url.from_data(iface_registry, service_object.service_url)
+        service_proxy = proxy_factory.from_url(service_url)
+        return cls(service_proxy)
+
+    def __init__(self, service_proxy):
+        self._service_proxy = service_proxy
+
+    def to_data(self):
+        service_url = self._service_proxy.get_url()
+        return ref_list_types.ref_list_service(service_url.to_data())
+
+    @asyncio.coroutine
+    def get_ref_list(self, ref_list_id):
+        result = yield from self._service_proxy.get_ref_list(ref_list_id)
+        return result.chunk
+
+
 class ThisModule(Module):
 
     def __init__(self, services):
@@ -63,11 +81,13 @@ class ThisModule(Module):
         self._href_resolver = services.href_resolver
         self._service_registry = services.service_registry
         services.href_object_registry.register(ref_list_types.dynamic_ref_list.id, self.resolve_dynamic_ref_list_object)
+        services.service_registry.register(ref_list_types.ref_list_service.id, RefListService.from_data, services.iface_registry, services.proxy_factory)
+        services.objimpl_registry.register(RefListObject.objimpl_id, RefListObject.from_state, services.service_registry)
 
     @asyncio.coroutine
     def resolve_dynamic_ref_list_object(self, dynamic_ref_list):
-        service = yield from self._href_resolver.resolve_service_ref(dynamic_ref_list.ref_list_service)
-        object = ref_list_types.ref_list_object(RefListObject.objimpl_id, service, dynamic_ref_list.ref_list_id)
+        ref_list_service = yield from self._href_resolver.resolve_service_ref(dynamic_ref_list.ref_list_service)
+        object = ref_list_types.ref_list_object(RefListObject.objimpl_id, ref_list_service, dynamic_ref_list.ref_list_id)
         handle_t = list_handle_type(core_types, tString)
         sort_column_id = 'id'
         resource_id = ['client', 'ref_list']
