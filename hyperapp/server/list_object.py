8e2b63148ae512f930b1c1615fb47b6acf618156
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2017-12-17 11:59:36 +0300

    rows2fetched_chunk: add desc_count support

--- hyperapp/server/list_object.py
+++ hyperapp/server/list_object.py
@@ -5,7 +5,6 @@ MIN_ROWS_RETURNED = 100
 
 
 def rows2fetched_chunk(key_column_id, all_rows, fetch_request, Chunk):
-    assert fetch_request.desc_count == 0, repr(fetch_request.desc_count)  # Not yet supported
     sorted_rows = sorted(all_rows, key=attrgetter(fetch_request.sort_column_id))
     if fetch_request.from_key is None:
         idx = 0
@@ -15,10 +14,15 @@ def rows2fetched_chunk(key_column_id, all_rows, fetch_request, Chunk):
                 break
         else:
             idx = len(sorted_rows)
-    asc_count = fetch_request.asc_count
-    if asc_count < MIN_ROWS_RETURNED:
-        asc_count = MIN_ROWS_RETURNED
-    rows = sorted_rows[idx : idx+asc_count]
-    bof = idx == 0
-    eof = idx + asc_count >= len(sorted_rows)
+    start = max(0, idx - fetch_request.desc_count)
+    end = idx + fetch_request.asc_count
+    if end - start < MIN_ROWS_RETURNED:
+        end = start + MIN_ROWS_RETURNED
+        if end < idx + 1:
+            end = idx + 1
+    if end > len(sorted_rows):
+        end = len(sorted_rows)
+    rows = sorted_rows[start:end]
+    bof = start == 0
+    eof = end == len(sorted_rows)
     return Chunk(fetch_request.sort_column_id, fetch_request.from_key, rows, bof, eof)
