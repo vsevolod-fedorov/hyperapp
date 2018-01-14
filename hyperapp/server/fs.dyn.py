e45906d2ff73a823fe9b5f20b89b1f61bfa7c90a
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2018-01-14 11:47:08 +0300

    cleanup fs module: remove Dir and File objects

--- hyperapp/server/fs.dyn.py
+++ hyperapp/server/fs.dyn.py
@@ -2,116 +2,15 @@ import sys
 import os.path
 import stat
 from operator import itemgetter
-from ..common.htypes import Column
-from ..common.interface import core as core_types
 from ..common.interface import fs as fs_types
 from ..common.url import Url
 from .command import command
-from .object import Object, SmallListObject
-from .module import Module, ModuleCommand
+from .object import Object
+from .module import Module
 from .list_object import rows2fetched_chunk
 
 
 MODULE_NAME = 'file'
-
-
-class FsObject(SmallListObject):
-
-    def __init__(self, fspath):
-        SmallListObject.__init__(self, core_types)
-        self.fspath = os.path.abspath(fspath)
-
-    def get_path(self):
-        return this_module.make_path(self.fspath)
-
-
-class File(FsObject):
-
-    iface = fs_types.fs_file
-    objimpl_id = 'proxy_list'
-    default_sort_column_id = 'idx'
-
-    def get_commands(self):
-        return []
-
-    def fetch_all_elements(self, request):
-        return [self.Element(self.Row(idx, line)) for idx, line in enumerate(self._load_lines())]
-
-    def _load_lines(self, ofs=0):
-        with open(self.fspath) as f:
-            if ofs:
-                f.seek(ofs)
-            return f.readlines()
-
-
-class Dir(FsObject):
-
-    iface = fs_types.fs_dir
-    objimpl_id = 'proxy_list'
-    categories = [['initial', 'fs']]
-
-    def fetch_all_elements(self, request):
-        dirs  = []
-        files = []
-        try:
-            names = os.listdir(self.fspath)
-        except OSError:  # path may be invalid
-            names = []
-        for fname in names:
-            fname = fsname2uni(fname)
-            if fname[0] == '.': continue  # skip special and hidden names
-            fspath = os.path.join(self.fspath, fname)
-            finfo = self.get_file_info(fname, fspath)
-            if finfo['ftype'] == 'dir':
-                dirs.append(finfo)
-            else:
-                files.append(finfo)
-        def key(finfo):
-            return finfo['key']
-        return list(map(self.make_elt, sorted(dirs, key=key) + sorted(files, key=key)))
-
-    def get_file_info(self, fname, fspath):
-        try:
-            s = os.stat(fspath)
-        except OSError:
-            return dict(
-                key=fname,
-                ftype='file',
-                ftime=0,
-                fsize=0)
-        return dict(
-            key=fname,
-            ftype='dir' if os.path.isdir(fspath) else 'file',
-            ftime=s[stat.ST_MTIME],
-            fsize=s[stat.ST_SIZE],
-            )
- 
-    def make_elt(self, finfo):
-        row = self.Row(key=finfo['key'], ftype=finfo['ftype'], ftime=finfo['ftime'], fsize=finfo['fsize'])
-        return self.Element(row, commands=[self.command_open])
-
-    def get_handle(self, request):
-        return self.CategorizedListHandle(self.get(request))
-
-    @command('open', kind='element')
-    def command_open(self, request):
-        fname = request.params.element_key
-        fspath = os.path.join(self.fspath, fname)
-        return request.make_response_object(this_module.open(fspath))
-
-    @command('parent')
-    def command_parent(self, request):
-        fspath = self.get_parent_dir()
-        if fspath is None: return None
-        key = os.path.basename(self.fspath)
-        handle = self.CategorizedListHandle(this_module.open(fspath).get(request), key=key)
-        return request.make_response_handle(handle)
-
-    def get_parent_dir(self):
-        dir = os.path.dirname(self.fspath)
-        if dir == self.fspath:
-            return None  # already root
-        return dir
 
 
 class FsService(Object):
@@ -201,22 +100,7 @@ class ThisModule(Module):
         name = path.pop_str()
         if name == self._fs_service.class_name:
             return self._fs_service.resolve(path)
-        else:
-            return self.open(name)
-
-    def get_commands(self):
-        return [ModuleCommand('home', 'Home', 'Open home directory', ['Alt+H'], self.name)]
-
-    def run_command(self, request, command_id):
-        if command_id == 'home':
-            return request.make_response_object(self.open(os.path.expanduser('~')))
-        return Module.run_command(self, request, command_id)
-
-    def open(self, fspath):
-        if os.path.isdir(fspath):
-            return Dir(fspath)
-        else:
-            return File(fspath)
+        path.raise_not_found()
 
 
 if sys.platform == 'win32':
