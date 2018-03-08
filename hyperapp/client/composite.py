c312ff51c1d5e887d32fd4b99fecf06c4c8531ef
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2018-03-08 10:50:04 +0300

    base FormView on Composite; implement Composite.get_command_list to collect commands from children too

--- hyperapp/client/composite.py
+++ hyperapp/client/composite.py
@@ -6,11 +6,17 @@ from . import view
 
 class Composite(view.View):
 
-    def __init__(self, parent, children):
+    def __init__(self, parent, children=None):
         view.View.__init__(self, parent)
-        self._children = children
+        self._children = children or []
 
     def init(self, module_registry):
         view.View.init(self, module_registry)
         for child in self._children:
             child.init(module_registry)
+
+    def get_command_list(self, kinds=None):
+        command_list = super().get_command_list(kinds)
+        for child in self._children:
+            command_list += child.get_command_list(kinds)
+        return command_list
