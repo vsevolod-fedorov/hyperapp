62241d514f965786a01c704b53844d99bf3405e2
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2018-03-18 13:30:56 +0300

    Object.subscribe: add *args and **kw arguments, pass to ObjectObserver.object_changed method

--- hyperapp/client/util.py
+++ hyperapp/client/util.py
@@ -41,24 +41,6 @@ def start_thread(fn, *args, **kw):
     thread.start(QtCore.QThread.LowPriority)
 
 
-# calls 'on_remove' callback every time an item is removed
-# WARNING: uses undocumented WeakSet implementation details
-class WeakSetWithCallback(weakref.WeakSet):
-
-    def __init__(self, on_remove, data=None):
-        weakref.WeakSet.__init__(self, data)
-        self._orig_remove = self._remove
-        self._on_remove = on_remove
-
-        def remove(item, self_ref=weakref.ref(self)):
-            self = self_ref()
-            if self is None: return
-            self._orig_remove(item)
-            self._on_remove()
-
-        self._remove = remove
-
-
 # invoke_in_main_thread from:    
 # http://stackoverflow.com/questions/10991991/pyside-easier-way-of-updating-gui-from-another-thread
 
