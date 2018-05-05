1a157d0a5983fb256a9a5f93726be4beba5a6a29
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2018-05-05 09:08:41 +0300

    route -> address

--- hyperapp/server/transport/tcp.dyn.py
+++ hyperapp/server/transport/tcp.dyn.py
@@ -17,5 +17,5 @@ class ThisModule(Module):
 
     def __init__(self, services):
         Module.__init__(self, MODULE_NAME)
-        route = tcp_transport_types.route(LISTEN_HOST, LISTEN_PORT)
-        services.tcp_transport_ref = services.ref_registry.register_object(tcp_transport_types.route, route)
+        address = tcp_transport_types.address(LISTEN_HOST, LISTEN_PORT)
+        services.tcp_transport_ref = services.ref_registry.register_object(tcp_transport_types.address, address)
