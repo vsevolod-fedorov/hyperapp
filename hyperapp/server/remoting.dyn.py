ea10958c47aa4ab3f8bc108fb3798ae7997caa20
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2018-05-09 10:25:51 +0300

    server remoting: resolve and call servant method

--- hyperapp/server/remoting.dyn.py
+++ hyperapp/server/remoting.dyn.py
@@ -23,8 +23,15 @@ class ThisModule(ServerModule):
     def __init__(self, services):
         super().__init__(MODULE_NAME)
         services.service_registry = service_registry = ServiceRegistry()
-        services.transport_registry.register(href_types.service_request, self._process_request, service_registry)
+        services.transport_registry.register(href_types.service_request, self._process_request, services.types, service_registry)
 
-    def _process_request(self, request, service_registry):
+    def _process_request(self, request, types, service_registry):
+        iface = types.resolve(request.iface_full_type_name)
+        params_t = iface[request.command_id].request
+        params = request.params.decode(params_t)
         servant = service_registry.resolve(request.service_id)
+        request_util = None
+        method = getattr(servant, 'remote_' + request.command_id, None)
+        assert method, '%r does not implement method remote_%s' % (servant, request.command_id)
+        method(request_util, **params._asdict())
         assert 0, servant
