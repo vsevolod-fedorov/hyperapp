fbc289dc62e1e1f0b09459f7b4b69e87b60f554f
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2018-05-10 11:22:55 +0300

    create client remoting module

--- hyperapp/client/remoting_proxy.dyn.py
+++ hyperapp/client/remoting_proxy.dyn.py
@@ -1,41 +1,28 @@
-import uuid
+from .module import ClientModule
 
-from ..common.htypes import EncodableEmbedded
-from ..common.interface import hyper_ref as href_types
-from .module import Module
+
+MODULE_NAME = 'remoting_proxy'
 
 
 class ProxyMethod(object):
 
-    def __init__(self, ref_registry, transport, iface, service_id, command):
-        self._ref_registry = ref_registry
-        self._transport = transport
+    def __init__(self, remoting, transport_ref, iface, service_id, command):
+        self._remoting = remoting
+        self._transport_ref = transport_ref
         self._iface = iface
         self._service_id = service_id
         self._command = command
 
     async def __call__(self, *args, **kw):
-        if self._command.is_request:
-            request_id = str(uuid.uuid4())
-        else:
-            request_id = None
         params = self._command.request(*args, **kw)
-        request = href_types.service_request(
-            iface_full_type_name=self._iface.full_name,
-            service_id=self._service_id,
-            command_id=self._command.command_id,
-            request_id=request_id,
-            params=EncodableEmbedded(self._command.request, params),
-            )
-        request_ref = self._ref_registry.register_object(href_types.service_request, request)
-        self._transport.send(request_ref)
+        return await self._remoting.send_request(self._transport_ref, self._iface, self._service_id, self._command, params)
 
 
 class RemotingProxy(object):
 
-    def __init__(self, ref_registry, transport, iface, service_id):
-        self._ref_registry = ref_registry
-        self._transport = transport
+    def __init__(self, remoting, transport_ref, iface, service_id):
+        self._remoting = remoting
+        self._transport_ref = transport_ref
         self._iface = iface
         self._service_id = service_id
 
@@ -43,31 +30,24 @@ class RemotingProxy(object):
         command = self._iface.get(name)
         if not command:
             raise AttributeError(name)
-        return ProxyMethod(self._ref_registry, self._transport, self._iface, self._service_id, command)
+        return ProxyMethod(self._remoting, self._transport_ref, self._iface, self._service_id, command)
 
 
 class ProxyFactory(object):
 
-    def __init__(self, types, ref_registry, async_ref_resolver, transport_resolver):
+    def __init__(self, types, remoting, async_ref_resolver):
         self._types = types
-        self._ref_registry = ref_registry
+        self._remoting = remoting
         self._async_ref_resolver = async_ref_resolver
-        self._transport_resolver = transport_resolver
 
     async def from_ref(self, ref):
         service = await self._async_ref_resolver.resolve_ref_to_object(ref, expected_type='hyper_ref.service_ref')
         iface = self._types.resolve(service.iface_full_type_name)
-        transport = await self._transport_resolver.resolve(service.transport_ref)
-        return RemotingProxy(self._ref_registry, transport, iface, service.service_id)
+        return RemotingProxy(self._remoting, service.transport_ref, iface, service.service_id)
 
 
-class ThisModule(Module):
+class ThisModule(ClientModule):
 
     def __init__(self, services):
-        Module.__init__(self, services)
-        services.proxy_factory = ProxyFactory(
-            services.types,
-            services.ref_registry,
-            services.async_ref_resolver,
-            services.transport_resolver,
-            )
+        super().__init__(MODULE_NAME, services)
+        services.proxy_factory = ProxyFactory(services.types, services.remoting, services.async_ref_resolver)
