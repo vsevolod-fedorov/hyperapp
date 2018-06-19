ab9cb8e9fd6387429c6c7eba36dcf4936601a131
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2018-06-19 09:40:29 +0300

    client tcp transport: send peer_endpoints

--- hyperapp/client/transport/tcp.dyn.py
+++ hyperapp/client/transport/tcp.dyn.py
@@ -15,8 +15,9 @@ TCP_PACKET_ENCODING = 'cdr'
 
 class TcpProtocol(asyncio.Protocol):
 
-    def __init__(self, event_loop, ref_registry, ref_collector_factory, address):
+    def __init__(self, event_loop, ref_registry, endpoint_registry, ref_collector_factory, address):
         self._ref_registry = ref_registry
+        self._endpoint_registry = endpoint_registry
         self._ref_collector_factory = ref_collector_factory
         self._address = address
         self._recv_buf = b''
@@ -36,13 +37,19 @@ class TcpProtocol(asyncio.Protocol):
             self._recv_buf = self._recv_buf[packet_size:]
             self._log('consumed %d bytes, remained %d' % (packet_size, len(self._recv_buf)))
 
-    def send(self, ref):
-        assert isinstance(ref, href_types.ref), repr(ref)
+    def send(self, message_ref):
+        assert isinstance(message_ref, href_types.ref), repr(message_ref)
         ref_collector = self._ref_collector_factory()
-        bundle = ref_collector.make_bundle([ref])
+        peer_endpoints_ref = self._make_peer_endpoints_ref()
+        bundle = ref_collector.make_bundle([peer_endpoints_ref, message_ref])
         data = encode_tcp_packet(bundle, TCP_PACKET_ENCODING)
         self._log('sending data, size=%d' % len(data))
         self._asyncio_transport.write(data)
+
+    def _make_peer_endpoints_ref(self):
+        endpoint_ref_list = self._endpoint_registry.get_endpoint_ref_list()
+        peer_endpoints = tcp_transport_types.peer_endpoints(endpoint_ref_list=endpoint_ref_list)
+        return self._ref_registry.register_object(tcp_transport_types.peer_endpoints, peer_endpoints)
 
     def _log(self, message):
         log.info('tcp to %s:%d: %s', self._address.host, self._address.port, message)
@@ -61,13 +68,18 @@ class ThisModule(ClientModule):
         self._event_loop = services.event_loop
         self._address_to_protocol = {}  # tcp_transport_types.address -> TcpProtocol
         services.transport_registry.register(
-            tcp_transport_types.address, self._resolve_address, services.ref_registry, services.ref_collector_factory)
+            tcp_transport_types.address,
+            self._resolve_address,
+            services.ref_registry,
+            services.endpoint_registry,
+            services.ref_collector_factory,
+            )
 
-    async def _resolve_address(self, address, ref_registry, ref_collector_factory):
+    async def _resolve_address(self, address, ref_registry, endpoint_registry, ref_collector_factory):
         protocol = self._address_to_protocol.get(address)
         if protocol:
             return protocol
-        constructor = lambda: TcpProtocol(self._event_loop, ref_registry, ref_collector_factory, address)
+        constructor = lambda: TcpProtocol(self._event_loop, ref_registry, endpoint_registry, ref_collector_factory, address)
         asyncio_transport, protocol = await self._event_loop.create_connection(constructor, address.host, address.port)
         self._address_to_protocol[address] = protocol
         return protocol
