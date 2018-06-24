062c756585473d97bdaa05996c400a515cb8f77b
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2018-06-24 11:38:11 +0300

    wip: update client ref list

--- hyperapp/client/local_server_ref_list.dyn.py
+++ hyperapp/client/local_server_ref_list.dyn.py
@@ -1,7 +1,7 @@
 import os.path
 
 from ..common.interface import hyper_ref as href_types
-from ..common.local_server_paths import LOCAL_SERVER_REF_LIST_REF_PATH
+from ..common.local_server_paths import LOCAL_SERVER_REF_LIST_REF_PATH, load_bundle_from_file
 from .command import command
 from .module import ClientModule
 
@@ -11,10 +11,14 @@ class ThisModule(ClientModule):
     def __init__(self, services):
         super().__init__(services)
         self._handle_resolver = services.handle_resolver
-        ref_path = os.path.expanduser(LOCAL_SERVER_REF_LIST_REF_PATH)
-        with open(ref_path, 'rb') as f:
-            self._server_ref_list_ref = f.read()
+        self._local_server_ref = self._load_local_server_ref(services.ref_registry)
 
     @command('open_local_server')
     async def open_local_server(self):
         return (await self._handle_resolver.resolve(self._server_ref_list_ref))
+
+    def _load_local_server_ref(self, ref_registry):
+        bundle = load_bundle_from_file(LOCAL_SERVER_REF_LIST_REF_PATH)
+        ref_registry.register_bundle(bundle)
+        assert len(bundle.roots) == 1
+        return bundle.roots[0]
