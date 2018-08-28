d05171c3b8a9ec2b5f28d1facbb5eb22d3c8ad2c
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2018-08-28 13:27:04 +0300

    add method resolve_ref_to_object to common RefResolver; add server remoting_proxy

--- hyperapp/common/ref_resolver.dyn.py
+++ hyperapp/common/ref_resolver.dyn.py
@@ -1,20 +1,24 @@
 import logging
 
-from ..common.ref import ref_repr
-from ..common.local_server_paths import LOCAL_REF_RESOLVER_REF_PATH, save_bundle_to_file
+from .util import full_type_name_to_str
+from .htypes.packet_coders import packet_coders
+from .ref import ref_repr
 from .module import Module
 
 log = logging.getLogger(__name__)
 
 
 MODULE_NAME = 'ref_resolver'
-#REF_RESOLVER_CLASS_NAME = 'ref_resolver'
 
 
 class RefResolver(object):
 
-    def __init__(self):
+    def __init__(self, types):
+        self._types = types
         self._sources = []
+
+    def add_source(self, source):
+        self._sources.append(source)
 
     def resolve_ref(self, ref):
         log.debug('Resolving ref: %s', ref_repr(ref))
@@ -25,15 +29,17 @@ class RefResolver(object):
                 return capsule
         return None
 
-    def resolve_ref_recursive(self, rev):
-        pass
-
-    def add_source(self, source):
-        self._sources.append(source)
+    def resolve_ref_to_object(self, ref, expected_type=None):
+        capsule = self.resolve_ref(ref)
+        assert capsule is not None, 'Unknown ref: %s' % ref_repr(ref)
+        if expected_type:
+            assert full_type_name_to_str(capsule.full_type_name) == expected_type
+        t = self._types.resolve(capsule.full_type_name)
+        return packet_coders.decode(capsule.encoding, capsule.encoded_object, t)
 
 
 class ThisModule(Module):
 
     def __init__(self, services):
         super().__init__(MODULE_NAME)
-        services.ref_resolver = RefResolver()
+        services.ref_resolver = RefResolver(services.types)
