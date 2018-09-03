951ae406b48405b126be58f0edc78b1c847f27b6
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2018-09-03 12:32:30 +0300

    do not request routes every time

--- hyperapp/client/async_route_resolver.dyn.py
+++ hyperapp/client/async_route_resolver.dyn.py
@@ -28,8 +28,10 @@ class AsyncRouteResolver(object):
 
     async def resolve(self, endpoint_ref):
         transport_ref_set = self._route_resolver.resolve(endpoint_ref)
-        # Although we may have some transport refs now, they all may be unaccessible,
-        # so we need to collect async (remote) ones too.
+        # Although we may have some transport refs now, they all may be unaccessible.
+        # But we do not want to request routes again for every rpc call.
+        if transport_ref_set:
+            return transport_ref_set
         for source in self._async_source_list:
             transport_ref_set |= await source.resolve(endpoint_ref)
         return transport_ref_set
