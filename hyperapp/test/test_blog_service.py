a4d5a88ce52bd9ad3a3e1bc9286061820742d367
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2018-09-03 09:01:20 +0300

    test_blog_service: TEST_BLOG -> TEST_BLOG_ID

--- hyperapp/test/test_blog_service.py
+++ hyperapp/test/test_blog_service.py
@@ -18,7 +18,7 @@ from hyperapp.test.client_server_fixtures import (
 log = logging.getLogger(__name__)
 
 
-TEST_BLOG = 'test_blog_service.blog'
+TEST_BLOG_ID = 'test_blog_service.blog'
 
 
 type_module_list = [
@@ -78,7 +78,7 @@ def client_services(event_loop, queues, transport):
 
 
 async def pick_test_article(blog_service):
-    chunk = await blog_service.fetch_blog_contents(TEST_BLOG, sort_column_id='id', from_key=None, desc_count=0, asc_count=100)
+    chunk = await blog_service.fetch_blog_contents(TEST_BLOG_ID, sort_column_id='id', from_key=None, desc_count=0, asc_count=100)
     if not chunk.rows:
         pytest.skip('No test articles in blog_1')
     return chunk.rows[0]
@@ -95,30 +95,30 @@ class BlogObserver(object):
 
 async def blog_create_article(services, blog_service):
     observer = BlogObserver()
-    await blog_service.add_observer(TEST_BLOG, observer)
-    article_id = await blog_service.create_article(TEST_BLOG, 'title 1', 'text1 text1')
+    await blog_service.add_observer(TEST_BLOG_ID, observer)
+    article_id = await blog_service.create_article(TEST_BLOG_ID, 'title 1', 'text1 text1')
     assert article_id
     assert isinstance(article_id, int)
     blog_id, article = (await asyncio.wait_for(observer.article_added_future, timeout=3))
-    assert blog_id == TEST_BLOG
-    assert article == (await blog_service.get_blog_row(TEST_BLOG, article_id))
+    assert blog_id == TEST_BLOG_ID
+    assert article == (await blog_service.get_blog_row(TEST_BLOG_ID, article_id))
 
 async def blog_save_article(services, blog_service):
     article = await pick_test_article(blog_service)
     log.info('Saving article#%d', article.id)
-    await blog_service.save_article(TEST_BLOG, article.id, article.title, 'text2 text2')
+    await blog_service.save_article(TEST_BLOG_ID, article.id, article.title, 'text2 text2')
 
 async def blog_fetch_blog_contents(services, blog_service):
-    chunk = await blog_service.fetch_blog_contents(TEST_BLOG, sort_column_id='id', from_key=None, desc_count=0, asc_count=100)
+    chunk = await blog_service.fetch_blog_contents(TEST_BLOG_ID, sort_column_id='id', from_key=None, desc_count=0, asc_count=100)
 
 async def blog_get_blog_row(services, blog_service):
     article1 = await pick_test_article(blog_service)
     log.info('Requesting blog row for article#%d', article1.id)
-    article2 = await blog_service.get_blog_row(TEST_BLOG, article1.id)
+    article2 = await blog_service.get_blog_row(TEST_BLOG_ID, article1.id)
     assert article1.id == article2.id
 
 async def blog_get_article_ref_list(services, blog_service):
-    ref_list = await blog_service.get_article_ref_list(TEST_BLOG, article_id)
+    ref_list = await blog_service.get_article_ref_list(TEST_BLOG_ID, article_id)
 
 
 @pytest.fixture(params=[blog_create_article, blog_save_article, blog_fetch_blog_contents, blog_get_blog_row])
