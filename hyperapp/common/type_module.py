ac09d34d125178bdc3ea0265974a351fde82b3a0
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2018-11-04 10:32:12 +0300

    extract type_modle_loader module

--- hyperapp/common/type_module.py
+++ hyperapp/common/type_module.py
@@ -1,17 +1,9 @@
-import uuid
-
 from .htypes import (
     ref_t,
     TypeNamespace,
     TypeNameResolver,
     make_meta_type_registry,
-    tNamed,
-    t_named,
-    t_ref,
-    builtin_ref_t,
-    meta_ref_t,
     )
-from .mapper import Mapper
 
 
 class LocalTypeModule(object):
@@ -39,50 +31,6 @@ class LocalTypeModuleRegistry(object):
     def resolve(self, name):
         return self._registry.get(name)
 
-
-class TypeModuleToRefsMapper(Mapper):
-
-    def __init__(self, buildin_types_registry, ref_registry, local_name_dict):
-        self._buildin_types_registry = buildin_types_registry
-        self._ref_registry = ref_registry
-        self._local_name_dict = local_name_dict
-
-    def map_hierarchy_obj(self, tclass, value):
-        if tclass is tNamed:
-            return self._map_named_t(value)
-        return value
-
-    def _map_named_t(self, rec):
-        ref = self._local_name_dict.get(rec.name)
-        if ref:
-            return t_ref(ref)
-        t = self._buildin_types_registry.resolve(['basic', rec.name])
-        assert t, repr(rec.name)  # Unknown name, must be caught by type loader
-        return t_ref(self._make_builtin_type_ref(t))
-
-    def _make_builtin_type_ref(self, t):
-        rec = builtin_ref_t(t.full_name)
-        return self._ref_registry.register_object(rec)  # expected fail for duplicates; todo: move ref to Type
-
-
-def map_type_module_to_refs(buildin_types_registry, ref_registry, local_type_module_registry, module):
-    local_name_dict = {}  # name -> ref
-    for import_ in module.import_list:
-        imported_module = local_type_module_registry.resolve(import_.module_name)
-        local_name_dict[import_.name] = imported_module.resolve(import_.name)
-    local_type_module = LocalTypeModule()
-    mapper = TypeModuleToRefsMapper(buildin_types_registry, ref_registry, local_name_dict)
-    for typedef in module.typedefs:
-        t = mapper.map(typedef.type)
-        rec = meta_ref_t(
-            name=typedef.name,
-            random_salt=uuid.uuid4().bytes,
-            type=t,
-            )
-        ref = ref_registry.register_object(rec)
-        local_type_module.register(typedef.name, ref)
-        local_name_dict[typedef.name] = ref
-    return local_type_module
 
 def resolve_type_module(types, module):
     assert isinstance(types, TypeNamespace), repr(types)
