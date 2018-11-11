2223735a468630bcd3c52a8868434ccd5ba0e7e1
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2018-11-11 11:19:46 +0300

    add type resolver cache

--- hyperapp/common/type_resolver.py
+++ hyperapp/common/type_resolver.py
@@ -1,3 +1,4 @@
+import logging
 
 from .htypes import (
     builtin_ref_t,
@@ -5,16 +6,19 @@ from .htypes import (
     make_meta_type_registry,
     TypeRefResolver,
     )
+from .ref import ref_repr
 from .capsule_registry import CapsuleRegistry, CapsuleResolver
+
+log = logging.getLogger(__name__)
 
 
 class _TypeRefResolver(TypeRefResolver):
 
-    def __init__(self, type_capsule_resolver):
-        self._type_capsule_resolver = type_capsule_resolver
+    def __init__(self, type_resolver):
+        self._type_resolver = type_resolver
 
     def resolve(self, type_ref, name):
-        return self._type_capsule_resolver.resolve(type_ref.ref, name)
+        return self._type_resolver.resolve(type_ref.ref)
 
 
 class TypeResolver(object):
@@ -25,13 +29,21 @@ class TypeResolver(object):
         self._ref_resolver = ref_resolver
         self._type_capsule_registry = capsule_registry = CapsuleRegistry('type', types)
         self._type_capsule_resolver = type_capsule_resolver = CapsuleResolver(ref_resolver, capsule_registry)
-        self._type_ref_resolver = _TypeRefResolver(type_capsule_resolver)
+        self._type_ref_resolver = _TypeRefResolver(self)
         self._meta_type_registry = make_meta_type_registry()
+        self._ref2type_cache = {}  # we should resolve same ref to same instance, not a duplicate
         capsule_registry.register(meta_ref_t, self._resolve_meta_ref)
         capsule_registry.register(builtin_ref_t, self._resolve_builtin_ref)
 
     def resolve(self, type_ref):
-        return self._type_capsule_resolver.resolve(type_ref)
+        t = self._ref2type_cache.get(type_ref)
+        if t:
+            log.info('Resolve type %s -> (cached) %s', ref_repr(type_ref), t)
+            return t
+        t = self._type_capsule_resolver.resolve(type_ref)
+        self._ref2type_cache[type_ref] = t
+        log.info('Resolve type %s -> %s', ref_repr(type_ref), t)
+        return t
 
     def _resolve_meta_ref(self, ref, meta_ref, name=None):
         return self._meta_type_registry.resolve(self._type_ref_resolver, meta_ref.type, [meta_ref.name])
