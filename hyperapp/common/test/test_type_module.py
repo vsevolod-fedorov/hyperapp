845b27e9ac4f78bb2a1f3ae592ca1732ca7cfccd
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2018-11-11 11:20:07 +0300

    update type resolve test

--- hyperapp/common/test/test_type_module.py
+++ hyperapp/common/test/test_type_module.py
@@ -16,8 +16,7 @@ from hyperapp.common.htypes import (
     make_root_type_namespace,
     )
 from hyperapp.common import cdr_coders  # register codec
-from hyperapp.common.type_module_parser import load_type_module
-from hyperapp.common.type_module import LocalTypeModuleRegistry, resolve_type_module
+from hyperapp.common.type_module import LocalTypeModuleRegistry
 from hyperapp.common.builtin_types_registry import make_builtin_types_registry
 from hyperapp.common.ref_registry import RefRegistry
 from hyperapp.common.ref_resolver import RefResolver
@@ -26,37 +25,6 @@ from hyperapp.common.type_resolver import TypeResolver
 
 
 TEST_MODULES_DIR = Path(__file__).parent.resolve()
-
-
-def test_load_and_resolve():
-    types = make_root_type_namespace()
-    module_1 = load_type_module(types.builtins, 'test_module_1', TEST_MODULES_DIR / 'test_module_1.types')
-    ns = resolve_type_module(types, module_1)
-
-    assert tInt == ns.get('some_int')
-
-    assert ns.record_1 == TRecord([Field('int_field', tInt)])
-    assert ns.record_2 == TRecord([Field('int_field', tInt), Field('string_field', tString)])
-
-    assert 'object' in ns
-    object_t = ns.object
-
-    assert 'simple_class' in ns
-    simple_class = ns.simple_class
-    assert TClass(object_t, 'simple_2', TRecord([])) == simple_class
-
-    assert 'text_object' in ns
-    assert TClass(object_t, 'text_2', base=simple_class, trec=TRecord([Field('text', tString)])) == ns.text_object
-
-    assert [] == module_1.import_list
-
-    types[module_1.module_name] = ns
-    module_2 = load_type_module(types.builtins, 'test_module_2', TEST_MODULES_DIR / 'test_module_2.types')
-    assert tProvidedClass('object', 'text_object_2') in module_2.provided_classes
-
-    ns2 = resolve_type_module(types, module_2)
-
-    assert TOptional(TList(tBool)) == ns2.some_bool_list_opt
 
 
 def test_type_module_loader():
@@ -82,6 +50,25 @@ def test_type_resolver():
     ref_resolver.add_source(ref_registry)
 
     type_resolver = TypeResolver(types, builtin_types_registry, ref_resolver)
-    assert type_resolver.resolve(local_type_module_registry['test_module_1']['some_int']) == tInt
-    assert (type_resolver.resolve(local_type_module_registry['test_module_1']['record_1'])
-            == TRecord([Field('int_field', tInt)]))
+
+    def resolve(name):
+        return type_resolver.resolve(local_type_module_registry['test_module_1'][name])
+
+    assert resolve('some_int') == tInt
+    assert (resolve('record_1') == TRecord([Field('int_field', tInt)]))
+
+    assert resolve('some_int') == tInt
+
+    assert resolve('record_1') == TRecord([Field('int_field', tInt)])
+    assert resolve('record_2') == TRecord([Field('int_field', tInt), Field('string_field', tString)])
+
+    object_t = resolve('object')
+    simple_class = resolve('simple_class')
+
+    assert simple_class.hierarchy is object_t
+    assert simple_class == TClass(object_t, 'simple_2', TRecord([]))
+
+    assert resolve('text_object') == TClass(object_t, 'text_2', base=simple_class, trec=TRecord([Field('text', tString)]))
+
+    some_bool_list_opt = type_resolver.resolve(local_type_module_registry['test_module_1']['some_bool_list_opt'])
+    assert some_bool_list_opt == TOptional(TList(tBool))
