593e66eb00f71c95cb8eb650ac575e0ddaed6452
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2018-11-18 11:18:39 +0300

    Import dynamic code module from dynamic code module: load code module import refs

--- hyperapp/common/code_module.py
+++ hyperapp/common/code_module.py
@@ -14,12 +14,34 @@ type_import_t = TRecord([
     Field('type_ref', ref_t),
     ], full_name=['code_module', 'type_import'])
 
+code_import_t = TRecord([
+    Field('import_name', tString),
+    Field('code_module_ref', ref_t),
+    ], full_name=['code_module', 'code_import'])
+
 code_module_t = TRecord([
     Field('module_name', tString),
     Field('type_import_list', TList(type_import_t)),
+    Field('code_import_list', TList(code_import_t)),
     Field('source', tString),
     Field('file_path', tString),
     ], full_name=['code_module', 'code_module'])
+
+
+class LocalCodeModuleRegistry(object):
+
+    def __init__(self):
+        self._registry = {}  # module name -> ref_t
+
+    def register(self, code_module_name, code_module_ref):
+        assert isinstance(code_module_ref, ref_t), repr(code_module_ref)
+        self._registry[code_module_name] = code_module_ref
+
+    def resolve(self, name):
+        return self._registry.get(name)
+
+    def __getitem__(self, name):
+        return self._registry[name]
 
 
 def make_code_module_namespace():
--- hyperapp/common/code_module_loader.py
+++ hyperapp/common/code_module_loader.py
@@ -1,11 +1,13 @@
 import yaml
-from .code_module import type_import_t, code_module_t
+from .code_module import type_import_t, code_import_t, code_module_t
 
 
 class CodeModuleLoader(object):
 
-    def __init__(self, local_type_module_registry):
+    def __init__(self, ref_registry, local_type_module_registry, local_code_module_registry):
+        self._ref_registry = ref_registry
         self._local_type_module_registry = local_type_module_registry
+        self._local_code_module_registry = local_code_module_registry
 
     def load_code_module(self, module_name, file_path):
         info_path = file_path.with_suffix('.yaml')
@@ -20,4 +22,13 @@ class CodeModuleLoader(object):
                 type_ref = local_type_module.get(type_name)
                 assert type_ref, "%s: Unknown type: %s.%s" % (info_path, type_module_name, type_name)
                 type_import_list.append(type_import_t(type_module_name, type_name, type_ref))
-        return code_module_t(module_name, type_import_list, source, str(source_path))
+        code_import_list = []
+        for import_module_name in info['import'].get('code', []):
+            assert isinstance(import_module_name, str), (
+                '%s: string list is expected at import/code, but got: %r', file_path, import_module_name)
+            import_module_ref = self._local_code_module_registry[import_module_name]
+            code_import_list.append(code_import_t(import_module_name, import_module_ref))
+        code_module = code_module_t(module_name, type_import_list, [], source, str(source_path))
+        code_module_ref = self._ref_registry.register_object(code_module)
+        self._local_code_module_registry.register(module_name, code_module_ref)
+        return code_module
