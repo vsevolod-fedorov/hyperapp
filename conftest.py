bebe790c70c4c79c29bb9fcc2e3ea7563b0eb37c
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2018-12-09 10:45:40 +0300

    set root logger level to debug if capturing is turned off

--- conftest.py
+++ conftest.py
@@ -17,6 +17,8 @@ def pytest_configure(config):
         # when capturing stdout dump log to stdout too
         format = '%(asctime)s %(processName)-17s %(filename)-25s %(lineno)4d %(levelname)-8s %(message)s'
         logging.basicConfig(level=logging.DEBUG, format=format)
+    else:
+        logging.getLogger().setLevel(logging.DEBUG)
 
 
 @pytest.mark.hookwrapper
