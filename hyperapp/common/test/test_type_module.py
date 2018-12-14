fbd3e8ef6bb8caf749ab469f966f147e18ff0596
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2018-12-14 11:11:10 +0300

    update test_type_resolver

--- hyperapp/common/test/test_type_module.py
+++ hyperapp/common/test/test_type_module.py
@@ -53,19 +53,11 @@ def test_type_module_loader(type_resolver, ref_registry):
     loader.load_type_module('type_module_2', TEST_MODULES_DIR / 'type_module_2.types')
 
 
-def __test_type_resolver():
-    types = make_root_type_namespace()
-    builtin_types_registry = make_builtin_types_registry()
+def test_type_resolver(ref_resolver, type_resolver, ref_registry):
     local_type_module_registry = LocalTypeModuleRegistry()
-    ref_registry = RefRegistry(types)
-    loader = TypeModuleLoader(types.builtins, builtin_types_registry, ref_registry, local_type_module_registry)
+    loader = TypeModuleLoader(type_resolver, ref_registry, local_type_module_registry)
     loader.load_type_module('type_module_1', TEST_MODULES_DIR / 'type_module_1.types')
     loader.load_type_module('type_module_2', TEST_MODULES_DIR / 'type_module_2.types')
-
-    ref_resolver = RefResolver(types)
-    ref_resolver.add_source(ref_registry)
-
-    type_resolver = TypeResolver(types, builtin_types_registry, ref_resolver)
 
     def resolve_1(name):
         return type_resolver.resolve(local_type_module_registry['type_module_1'][name])
@@ -73,28 +65,29 @@ def __test_type_resolver():
     def resolve_2(name):
         return type_resolver.resolve(local_type_module_registry['type_module_2'][name])
 
-    assert resolve_1('some_int') == tInt
-    assert (resolve_1('record_1') == TRecord([Field('int_field', tInt)]))
+    assert resolve_1('some_int') is tInt
+    assert (resolve_1('record_1').match(TRecord([Field('int_field', tInt)])))
 
-    assert resolve_1('some_int') == tInt
+    assert resolve_1('some_int') is tInt
 
-    assert resolve_1('record_1') == TRecord([Field('int_field', tInt)])
-    assert resolve_1('record_2') == TRecord([Field('int_field', tInt), Field('string_field', tString)])
+    assert resolve_1('record_1').match(TRecord([Field('int_field', tInt)]))
+    assert resolve_1('record_2').match(TRecord([Field('int_field', tInt), Field('string_field', tString)]))
 
     object_t = resolve_1('object')
     simple_class = resolve_1('simple_class')
 
     assert simple_class.hierarchy is object_t
-    assert simple_class == TClass(object_t, 'simple_2', TRecord([]))
+    assert simple_class.match(TClass(object_t, 'simple_2', TRecord([])))
 
-    assert resolve_1('text_object') == TClass(object_t, 'text_2', base=simple_class, trec=TRecord([Field('text', tString)]))
+    assert resolve_1('text_object').match(
+        TClass(object_t, 'text_2', base=simple_class, trec=TRecord([Field('text', tString)])))
 
     some_bool_list_opt = resolve_2('some_bool_list_opt')
-    assert some_bool_list_opt == TOptional(TList(tBool))
+    assert some_bool_list_opt.match(TOptional(TList(tBool)))
 
     iface_a = resolve_1('test_iface_a')
     iface_b = resolve_2('test_iface_b')
     assert iface_b.base is iface_a
-    assert iface_b == Interface(['iface_b'], base=iface_a, commands=[
+    assert iface_b.match(Interface(['iface_b'], base=iface_a, commands=[
         NotificationCmd(['test_iface_b', 'keep_alive'], 'keep_alive', []),
-        ])
+        ]))
