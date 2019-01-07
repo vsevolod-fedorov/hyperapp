8dae9fe8c7333bbec7a0d6e2ed03a7434088ef64
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2019-01-07 13:31:07 +0300

    extract RegistryBase class

--- hyperapp/common/registry.py
+++ hyperapp/common/registry.py
@@ -9,7 +9,7 @@ class UnknownRegistryIdError(KeyError):
     pass
 
 
-class Registry(object):
+class RegistryBase(object):
 
     _Rec = namedtuple('_Rec', 'factory args kw')
 
@@ -19,17 +19,23 @@ class Registry(object):
     def id_to_str(self, id):
         return repr(id)
 
-    def register(self, id, factory, *args, **kw):
+    def is_registered(self, id):
+        return id in self._registry
+
+    def _register(self, id, factory, *args, **kw):
         log.info('%s: registering %s -> %s(*%r, **%r)',
                      self.__class__.__name__, self.id_to_str(id), factory, args, kw)
         assert id not in self._registry, repr(id)  # Duplicate id
         self._registry[id] = self._Rec(factory, args, kw)
-
-    def is_registered(self, id):
-        return id in self._registry
 
     def _resolve(self, id):
         try:
             return self._registry[id]
         except KeyError:
             raise UnknownRegistryIdError('Unknown id: %s' % self.id_to_str(id))
+
+
+class Registry(RegistryBase):
+
+    def register(self, id, factory, *args, **kw):
+        return super()._register(id, factory, *args, **kw)
