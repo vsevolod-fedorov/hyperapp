c57260ae69b3d8d2b909f58c88b98adda5cb0e45
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2019-01-13 12:18:05 +0300

    update navigator module, make it dynamic

---
+++ hyperapp/client/navigator/module.dyn.py
@@ -0,0 +1,21 @@
+from hyperapp.common.htypes import (
+    tInt,
+    tString,
+    TList,
+    Field,
+    TRecord,
+    )
+from hyperapp.client.module import ClientModule
+from .history_list import HistoryList
+from .navigator import NavigatorView
+
+
+MODULE_NAME = 'navigator'
+
+
+class ThisModule(ClientModule):
+
+    def __init__(self, services):
+        super().__init__(MODULE_NAME, services)
+        services.view_registry.register('navigator', NavigatorView.from_state, services.view_registry, self)
+        services.objimpl_registry.register(HistoryList.impl_id, HistoryList.from_state, self)
