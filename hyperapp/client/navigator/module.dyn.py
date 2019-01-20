327276cfeeab29756373bf1b3b4cb29852e4d4a5
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2019-01-20 11:00:54 +0300

    fix some errors in navigator modules

--- hyperapp/client/navigator/module.dyn.py
+++ hyperapp/client/navigator/module.dyn.py
@@ -17,5 +17,5 @@ class ThisModule(ClientModule):
 
     def __init__(self, services):
         super().__init__(MODULE_NAME, services)
-        services.view_registry.register('navigator', NavigatorView.from_state, services.view_registry, self)
+        services.view_registry.register('navigator', NavigatorView.from_state, services.view_registry)
         services.objimpl_registry.register(HistoryList.impl_id, HistoryList.from_state)
