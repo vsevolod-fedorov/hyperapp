e6cb646435e24e3d87656a1cff0125f58099ba11
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2019-01-27 14:19:37 +0300

    remove common command module and class

--- hyperapp/client/command.py
+++ hyperapp/client/command.py
@@ -1,6 +1,5 @@
 import logging
 import weakref
-from ..common.htypes import Field, TRecord
 from .command_class import Command, UnboundCommand
 from .error_handler_hook import get_handle_for_error
 
--- hyperapp/common/command.py
+++
@@ -1,6 +0,0 @@
-
-class Command(object):
-
-    def __init__(self, id):
-        assert isinstance(id, str), repr(id)
-        self.id = id
