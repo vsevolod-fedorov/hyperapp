122c56e6b55393af9946e6747ec20fca16ff27bb
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2019-02-17 10:50:51 +0300

    update mode_command module

--- hyperapp/client/mode_command.py
+++ hyperapp/client/mode_command.py
@@ -4,19 +4,19 @@ from .command import command
 
 class BoundModeCommand(BoundCommand):
 
-    def __init__(self, id, kind, resource_id, enabled, class_method, inst_wr, mode):
-        super().__init__(id, kind, resource_id, enabled, class_method, inst_wr)
+    def __init__(self, id, kind, resource_key, enabled, class_method, inst_wr, mode):
+        super().__init__(id, kind, resource_key, enabled, class_method, inst_wr)
         self.mode = mode
 
 
 class UnboundModeCommand(UnboundCommand):
 
-    def __init__(self, id, kind, resource_id, enabled, class_method, mode):
-        super().__init__(id, kind, resource_id, enabled, class_method)
+    def __init__(self, id, kind, resource_key, enabled, class_method, mode):
+        super().__init__(id, kind, resource_key, enabled, class_method)
         self._mode = mode
 
     def _bind(self, inst_wr, kind):
-        return BoundModeCommand(self.id, kind, self._resource_id, self.enabled, self._class_method, inst_wr, self._mode)
+        return BoundModeCommand(self.id, kind, self._resource_key, self.enabled, self._class_method, inst_wr, self._mode)
 
 
 class mode_command(command):
@@ -25,5 +25,5 @@ class mode_command(command):
         super().__init__(id, kind, enabled)
         self.mode = mode
 
-    def instantiate(self, wrapped_class_method, resource_id):
-        return UnboundModeCommand(self.id, self.kind, resource_id, self.enabled, wrapped_class_method, self.mode)
+    def instantiate(self, wrapped_class_method, resource_key):
+        return UnboundModeCommand(self.id, self.kind, resource_key, self.enabled, wrapped_class_method, self.mode)
