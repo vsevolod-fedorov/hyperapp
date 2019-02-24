a58026c8644618936aeb3d54ad60659becef849d
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2019-02-24 12:47:45 +0300

    deduce_value_type: return hierarchy, not tclass

--- hyperapp/common/htypes/deduce_value_type.py
+++ hyperapp/common/htypes/deduce_value_type.py
@@ -1,8 +1,11 @@
 from .htypes import TRecord
+from .hierarchy import TClass
 
 
 def deduce_value_type(value):
     t = getattr(value, 't', None)
     if isinstance(t, TRecord):
+        if isinstance(t, TClass):
+            return t.hierarchy
         return t
     assert False, 'Add type parameter for values of types others than TRecord or THierarchy: {!r} ({!r})'.format(value, t)
