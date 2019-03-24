ba02d83843580f24a86ef459e519c9b7b815b4b7
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2019-03-24 10:25:54 +0300

    rename tree view sample resource key

--- hyperapp/client/tree_view_sample.dyn.py
+++ hyperapp/client/tree_view_sample.dyn.py
@@ -67,6 +67,6 @@ class ThisModule(ClientModule):
     @command('open_tree_view_sample')
     async def open_tree_view_sample(self):
         object = htypes.core.object_base(SampleObject.impl_id)
-        resource_key = resource_key_t(__module_ref__, ['TreeView'])
+        resource_key = resource_key_t(__module_ref__, ['tree'])
         handle = htypes.tree_view.tree_handle('tree', object, resource_key)
         return handle
