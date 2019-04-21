4d809ca0af6be0724d80d9a22279cc1f868608b6
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2019-04-21 12:45:12 +0300

    Logger: implement context support

--- hyperapp/common/test/test_logger.py
+++ hyperapp/common/test/test_logger.py
@@ -1,6 +1,8 @@
+from contextlib import contextmanager
+
 import pytest
 
-from hyperapp.common.logger import log, set_log_storage
+from hyperapp.common.logger import log, logger_inited
 
 
 
@@ -12,18 +14,55 @@ class StubStorage:
     def add_entry(self, entry):
         self.entries.append(entry)
 
-
-@pytest.fixture(autouse=True)
-def storage():
-    storage = StubStorage()
-    set_log_storage(storage)
-    return storage
+    def close(self):
+        pass
 
 
-def test_log_entry(storage):
-    log.test_entry(foo='foo value', bar='bar value')
+@pytest.fixture
+def logger():
+
+    @contextmanager
+    def inited():
+        storage = StubStorage()
+        with logger_inited(storage):
+            yield storage
+
+    return inited
+
+
+def test_entry(logger):
+    with logger() as storage:
+        log.test_entry(foo='foo-value', bar='bar-value')
     assert storage.entries == [dict(
+        type='entry',
         name='test_entry',
-        foo='foo value',
-        bar='bar value',
+        foo='foo-value',
+        bar='bar-value',
         )]
+
+
+def test_context(logger):
+    with logger() as storage:
+        with log.test_context(foo='foo-value'):
+            log.test_entry(bar='bar-value')
+    assert storage.entries == [
+        dict(type='context-enter', name='test_context', foo='foo-value'),
+        dict(type='entry', name='test_entry', bar='bar-value'),
+        dict(type='context-exit', name='test_context'),
+        ]
+
+
+def test_nested_context(logger):
+    with logger() as storage:
+        with log.root_context(foo='foo'):
+            log.foo_entry(bar='bar')
+            with log.nested_context(bar='bar'):
+                log.inner_entry(foo='foo')
+    assert storage.entries == [
+        dict(type='context-enter', name='root_context', foo='foo'),
+        dict(type='entry', name='foo_entry', bar='bar'),
+        dict(type='context-enter', name='nested_context', bar='bar'),
+        dict(type='entry', name='inner_entry', foo='foo'),
+        dict(type='context-exit', name='nested_context'),
+        dict(type='context-exit', name='root_context'),
+        ]
--- run_client.py
+++ run_client.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from dateutil.tz import tzlocal
 
 from hyperapp.common.init_logging import init_logging
-from hyperapp.common.logger import log, set_log_storage, json_file_log_storage
+from hyperapp.common.logger import log, logger_inited, json_file_log_storage
 from hyperapp.client.application import Application
 
 
@@ -18,15 +18,14 @@ LOGS_DIR = Path('~/.local/share/hyperapp/client/logs').expanduser()
 
 def main():
     init_logging('client.yaml')
-    with json_file_log_storage(LOGS_DIR, datetime.now(tzlocal())) as log_storage:
-        set_log_storage(log_storage)
-        log.client_started()
+    with logger_inited(json_file_log_storage(LOGS_DIR, datetime.now(tzlocal()))):
+        with log.client_started():
 
-        parser = argparse.ArgumentParser(description='Hyperapp client')
-        args = parser.parse_args()
+            parser = argparse.ArgumentParser(description='Hyperapp client')
+            args = parser.parse_args()
 
-        app = Application(sys.argv)
-        app.exec_()
+            app = Application(sys.argv)
+            app.exec_()
 
 
 main()
