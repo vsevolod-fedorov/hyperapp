eadc85bfa7d62ce44239798b9bf05794daf58539
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2019-04-21 12:45:12 +0300

    Logger: implement context support

--- hyperapp/common/logger.py
+++ hyperapp/common/logger.py
@@ -1,47 +1,97 @@
 from contextlib import contextmanager
+import logging
 import json
 
+log = logging.getLogger(__name__)
 
-class LogFnAdapter:
+
+class _LogFnAdapter:
 
     def __init__(self):
         pass
 
     def __getattr__(self, name):
-        return LoggerAdapter(name)
+        return _LoggerAdapter(name)
 
 
-log = LogFnAdapter()
+log = _LogFnAdapter()
 
 
-class LoggerAdapter:
+class _LoggerAdapter:
 
     def __init__(self, entry_name):
         self._entry_name = entry_name
 
     def __call__(self, **kw):
         entry = dict(
+            type='entry',
             name=self._entry_name,
             **kw)
-        _storage.add_entry(entry)
+        _Logger.instance.add_entry(entry)
+        return _ContextAdapter()
 
 
-_storage = None
+class _ContextAdapter:
 
-def set_log_storage(storage):
-    global _storage
-    _storage = storage
+    def __init__(self):
+        pass
+
+    def __enter__(self):
+        _Logger.instance.push_context()
+
+    def __exit__(self, exc, value, tb):
+        _Logger.instance.exit_context()
+
+
+class _Logger:
+
+    instance = None
+
+    def __init__(self, storage):
+        self._storage = storage
+        self._pending_entry = None
+        self._context_name_stack = []
+
+    def add_entry(self, entry):
+        self.flush()
+        self._pending_entry = entry
+
+    def push_context(self):
+        assert self._pending_entry
+        entry = self._pending_entry
+        self._pending_entry = None
+        entry['type'] = 'context-enter'
+        self._store_entry(entry)
+        self._context_name_stack.append(entry['name'])
+
+    def exit_context(self):
+        self.flush()
+        self._store_entry(dict(type='context-exit', name=self._context_name_stack[-1]))
+        self._context_name_stack.pop()
+
+    def flush(self):
+        if self._pending_entry:
+            self._store_entry(self._pending_entry)
+            self._pending_entry = None
+
+    def _store_entry(self, entry):
+        self._storage.add_entry(entry)
 
 
 @contextmanager
-def json_file_log_storage(dir, start_time):
-    path = dir.joinpath(start_time.strftime('%Y-%m-%d-%H-%M-%S')).with_suffix('.json')
-    storage = JsonFileLogStorage(path)
-    yield storage
+def logger_inited(storage):
+    _Logger.instance = logger = _Logger(storage)
+    yield
+    logger.flush()
     storage.close()
 
+
+def json_file_log_storage(dir, start_time):
+    path = dir.joinpath(start_time.strftime('%Y-%m-%d-%H-%M-%S')).with_suffix('.json')
+    return _JsonFileLogStorage(path)
+
     
-class JsonFileLogStorage:
+class _JsonFileLogStorage:
 
     def __init__(self, path):
         path.parent.mkdir(parents=True, exist_ok=True)
