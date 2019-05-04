955f47c162c55ddd793d8bdc5536b8c744cee05c
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2019-05-04 12:26:46 +0300

    wip: json log storage: produce types and encode params

--- hyperapp/common/test/test_logger_json_storage.py
+++ hyperapp/common/test/test_logger_json_storage.py
@@ -1,13 +1,38 @@
 import logging
 
+import pytest
+
+from hyperapp.common.htypes import register_builtin_types
+from hyperapp.common.ref_resolver import RefResolver
+from hyperapp.common.type_resolver import TypeResolver
+from hyperapp.common.ref_registry import RefRegistry
 from hyperapp.common.logger import RecordKind, LogRecord
 from hyperapp.common.logger_json_storage import _RecordsToLineConverter
+from hyperapp.common import cdr_coders  # register codec
 
 _log = logging.getLogger(__name__)
 
 
-def test_record_to_line_converter():
-    storage = _RecordsToLineConverter()
+@pytest.fixture
+def ref_resolver():
+    return RefResolver()
+
+
+@pytest.fixture
+def type_resolver(ref_resolver):
+    return TypeResolver(ref_resolver)
+
+
+@pytest.fixture
+def ref_registry(ref_resolver, type_resolver):
+    registry = RefRegistry(type_resolver)
+    register_builtin_types(registry, type_resolver)
+    ref_resolver.add_source(registry)
+    return registry
+
+
+def test_record_to_line_converter(type_resolver, ref_registry):
+    storage = _RecordsToLineConverter(type_resolver, ref_registry)
     record = LogRecord(RecordKind.ENTER, [1, 2], 'context_enter', dict(num=123, name='sam'))
     for line in storage.record2lines(record):
         _log.info("storage line: %r", line)
