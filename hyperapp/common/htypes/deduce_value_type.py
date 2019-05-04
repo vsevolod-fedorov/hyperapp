b2f33c05f9fc0ec8c23f11d1724c5020658d13bb
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2019-05-04 12:23:42 +0300

    deduce_value_type: accept primitives; add special exception class

--- hyperapp/common/htypes/deduce_value_type.py
+++ hyperapp/common/htypes/deduce_value_type.py
@@ -1,11 +1,29 @@
-from .htypes import TRecord
+import datetime
+
+from .htypes import tString, tBinary, tInt, tBool, tDateTime, TRecord
 from .hierarchy import TClass
 
 
+class DeduceTypeError(RuntimeError):
+    pass
+
+
+_primitive_types = {
+    str: tString,
+    bytes: tBinary,
+    int: tInt,
+    bool: tBool,
+    datetime.datetime: tDateTime,
+    }
+
+
 def deduce_value_type(value):
+    t = _primitive_types.get(type(value))
+    if t:
+        return t
     t = getattr(value, 't', None)
     if isinstance(t, TRecord):
         if isinstance(t, TClass):
             return t.hierarchy
         return t
-    assert False, 'Add type parameter for values of types others than TRecord or THierarchy: {!r} ({!r})'.format(value, t)
+    raise DeduceTypeError("Unable to deduce type for {!r} ({!r}). Use explicit type parameter.".format(value, t))
