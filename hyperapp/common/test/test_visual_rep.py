1810306e48e18929ce401a0a28d760b0dfc8a199
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2019-05-11 11:45:55 +0300

    add test_visual_rep

---
+++ hyperapp/common/test/test_visual_rep.py
@@ -0,0 +1,53 @@
+from collections import OrderedDict
+import logging
+
+from hyperapp.common.htypes import (
+    tInt,
+    tString,
+    tBool,
+    TOptional,
+    TRecord,
+    TList,
+    THierarchy,
+    TClass,
+    )
+from hyperapp.common.visual_rep import VisualRepEncoder
+
+log = logging.getLogger(__name__)
+
+
+def test_visual_rep():
+    rec_1_t = TRecord('record_1', OrderedDict([
+        ('int_field', tInt),
+        ('opt_string_field', TOptional(tString)),
+        ]))
+    rec_1a = rec_1_t(123, 'abc')
+    rep = VisualRepEncoder().encode(rec_1a, rec_1_t)
+    rep.pprint(log.info)
+    rec_1b = rec_1_t(123, None)
+    rep = VisualRepEncoder().encode(rec_1b, rec_1_t)
+    rep.pprint(log.info)
+
+    rec_2_t = TRecord('record_2', OrderedDict([
+        ('rec_1_list', TList(rec_1_t)),
+        ]))
+    rec_2a = rec_2_t([rec_1_t(123, 'abc'), rec_1_t(456, None)])
+    rep = VisualRepEncoder().encode(rec_2a, rec_2_t)
+    rep.pprint(log.info)
+    rec_2b = rec_2_t([])
+    rep = VisualRepEncoder().encode(rec_2b, rec_2_t)
+    rep.pprint(log.info)
+
+    hierarchy = THierarchy('test_hierarchy')
+    class_1 = TClass(hierarchy, 'class_1', OrderedDict([
+        ('bool_opt', TOptional(tBool)),
+        ]))
+    class_2 = TClass(hierarchy, 'class_1', OrderedDict([
+        ('int_list', TList(tInt)),
+        ]), base=class_1)
+    value_1 = class_1(True)
+    rep = VisualRepEncoder().encode(value_1, hierarchy)
+    rep.pprint(log.info)
+    value_2 = class_2(None, [1, 2, 3])
+    rep = VisualRepEncoder().encode(value_2, hierarchy)
+    rep.pprint(log.info)
