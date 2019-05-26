a8de4cfc8715a2c558beef2c3c857552cde608c9
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2019-05-26 11:59:57 +0300

    update run_server.sh: autoreload.py now takes mask/dir parameter

--- run_server.sh
+++ run_server.sh
@@ -11,4 +11,4 @@ killall python3 || true
 # using python autoreload script:
 #   https://gist.github.com/jmingtan/1171288
 
-$HOME/bin/autoreload.py -f '*.types' -f '*.yaml' -f '*.py'  ./run_server.py server.identity.pem
+$HOME/bin/autoreload.py -f '*.types' -f '*.yaml' -f '*.py'  . ./run_server.py server.identity.pem
