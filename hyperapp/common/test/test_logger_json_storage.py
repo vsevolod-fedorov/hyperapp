3b4ec0785f82bf169c74331f413ee81640b65e8b
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2019-06-02 10:26:12 +0300

    Update logger json encoder

--- hyperapp/common/test/test_logger_json_storage.py
+++ hyperapp/common/test/test_logger_json_storage.py
@@ -1,9 +1,10 @@
 import logging
+from types import SimpleNamespace
 
 import pytest
 
 from hyperapp.common.util import flatten
-from hyperapp.common.htypes import ref_t, register_builtin_types
+from hyperapp.common.htypes import tInt, tString, meta_ref_t, t_ref, t_field_meta, t_record_meta, ref_t, register_builtin_types
 from hyperapp.common.ref_resolver import RefResolver
 from hyperapp.common.type_resolver import TypeResolver
 from hyperapp.common.ref_registry import RefRegistry
@@ -33,8 +34,8 @@ def ref_registry(ref_resolver, type_resolver):
 
 
 @pytest.fixture
-def encoder(type_resolver, ref_registry):
-    return _RecordsJsonEncoder(type_resolver, ref_registry)
+def encoder(ref_resolver, type_resolver):
+    return _RecordsJsonEncoder(ref_resolver, type_resolver)
 
 
 @pytest.fixture
@@ -42,9 +43,24 @@ def decoder(type_resolver, ref_registry):
     return _RecordsJsonDecoder(type_resolver, ref_registry)
 
 
-def test_primitives(encoder, decoder):
-    record_1 = LogRecord(RecordKind.ENTER, [1, 2], None, 'context_enter', dict(num=123, name='sam'))
-    record_2 = LogRecord(RecordKind.ENTER, [1, 3], None, 'context_enter', dict(num=456, name='mike'))
+@pytest.fixture
+def types(type_resolver, ref_registry):
+    primitive_params = type_resolver.register_type(ref_registry, meta_ref_t('primitive_params', t_record_meta([
+        t_field_meta('num', t_ref(type_resolver.reverse_resolve(tInt))),
+        t_field_meta('name', t_ref(type_resolver.reverse_resolve(tString))),
+        ]))).t
+    ref_params = type_resolver.register_type(ref_registry, meta_ref_t('primitive_params', t_record_meta([
+        t_field_meta('test_ref', t_ref(type_resolver.reverse_resolve(ref_t))),
+        ]))).t
+    return SimpleNamespace(
+        primitive_params=primitive_params,
+        ref_params=ref_params,
+        )
+
+
+def test_primitives(encoder, decoder, types):
+    record_1 = LogRecord(RecordKind.ENTER, [1, 2], None, 'context_enter', types.primitive_params(num=123, name='sam'))
+    record_2 = LogRecord(RecordKind.ENTER, [1, 3], None, 'context_enter', types.primitive_params(num=456, name='mike'))
     line_list = list(encoder.record2lines(record_1)) + list(encoder.record2lines(record_2))
     for line in line_list:
         _log.info("storage line: %r", line)
@@ -56,9 +72,9 @@ def test_primitives(encoder, decoder):
     assert records[1].params.name == 'mike'
 
 
-def test_ref(encoder, decoder):
+def test_ref(encoder, decoder, types):
     test_ref = ref_t('sha-whatever', b'some-hash')
-    record = LogRecord(RecordKind.ENTER, [1, 2], None, 'context_enter', dict(test_ref=test_ref))
+    record = LogRecord(RecordKind.ENTER, [1, 2], None, 'context_enter', types.ref_params(test_ref=test_ref))
     line_list = list(encoder.record2lines(record))
     for line in line_list:
         _log.info("storage line: %r", line)
