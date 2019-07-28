3cc0c27a72f36cf9bf4e2ea1a54b3552c1be2976
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2019-07-28 12:31:33 +0300

    Implement element commands

--- hyperapp/client/layout_manager.dyn.py
+++ hyperapp/client/layout_manager.dyn.py
@@ -19,6 +19,16 @@ _log = logging.getLogger(__name__)
 MODULE_NAME = 'layout_manager'
 
 
+class _CurrentItemObserver:
+
+    def __init__(self, layout_manager, object):
+        self._layout_manager = layout_manager
+        self._object = object
+
+    def current_changed(self, current_item_key):
+        self._layout_manager.update_element_commands(self._object, current_item_key)
+
+
 class LayoutManager(object):
 
     def __init__(self, resource_resolver, module_command_registry, objimpl_registry):
@@ -27,6 +37,8 @@ class LayoutManager(object):
         self._objimpl_registry = objimpl_registry
         self._cmd_pane = self._construct_cmd_pane()
         self._dir_buttons = []
+        self._element_buttons = []
+        self._current_item_observer = None
 
     def build_default_layout(self, app):
         text_object = TextObject('hello')
@@ -73,12 +85,26 @@ class LayoutManager(object):
             text = command.id
             button = QtGui.QPushButton(text, focusPolicy=QtCore.Qt.NoFocus)
             button.pressed.connect(partial(asyncio.ensure_future, self._run_command(command)))
-            self._cmd_pane.widget().layout().addWidget(button)
+            layout = self._cmd_pane.widget().layout()
+            layout.insertWidget(len(self._dir_buttons), button)  # must be inserted before spacing
             self._dir_buttons.append(button)
 
-    async def _run_command(self, command):
+    def update_element_commands(self, object, current_item_key):
+        _log.debug('Update element commands for item %r', current_item_key)
+        for button in self._element_buttons:
+            button.deleteLater()
+        self._element_buttons.clear()
+        for command in object.get_item_command_list(current_item_key):
+            text = command.id
+            button = QtGui.QPushButton(text, focusPolicy=QtCore.Qt.NoFocus)
+            button.pressed.connect(partial(asyncio.ensure_future, self._run_command(command, current_item_key)))
+            layout = self._cmd_pane.widget().layout()
+            layout.addWidget(button)
+            self._element_buttons.append(button)
+
+    async def _run_command(self, command, *args, **kw):
         _log.info('Run command: %r', command.id)
-        state = await command.run()
+        state = await command.run(*args, **kw)
         object = await self._objimpl_registry.resolve_async(state)
         assert isinstance(object, ListObject), repr(object)
         locale = 'en'
@@ -91,6 +117,8 @@ class LayoutManager(object):
         old_widget.deleteLater()
         tab_view.insertTab(0, list_view, list_view.get_title())
         self._update_dir_buttons(object)
+        self._current_item_observer = observer = _CurrentItemObserver(self, object)
+        list_view.add_observer(observer)
 
 
 class ThisModule(ClientModule):
