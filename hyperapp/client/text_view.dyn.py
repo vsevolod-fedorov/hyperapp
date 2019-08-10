e60f687d5f59f7680140782c5033b5e3606a521c
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2019-08-10 12:12:30 +0300

    Add view_opener; update wiki ref opening

--- hyperapp/client/text_view.dyn.py
+++ hyperapp/client/text_view.dyn.py
@@ -15,10 +15,11 @@ log = logging.getLogger(__name__)
 
 class TextView(View, QtGui.QTextBrowser):
 
-    def __init__(self, object):
+    def __init__(self, view_opener, object):
         QtGui.QTextBrowser.__init__(self)
         View.__init__(self)
         self.setOpenLinks(False)
+        self._view_opener = view_opener
         self.object = object
         self.setHtml(self.text2html(object.text or ''))
         self.anchorClicked.connect(self.on_anchor_clicked)
@@ -38,9 +39,9 @@ class TextView(View, QtGui.QTextBrowser):
         asyncio.ensure_future(self.open_url(url))
 
     async def open_url(self, url):
-        handle = await self.object.open_ref(url.path())
-        if handle:
-            self.open(handle)
+        rec = await self.object.open_ref(url.path())
+        if rec is not None:
+            await self._view_opener.open_rec(rec)
 
     def object_changed(self):
         self.setHtml(self.text2html(self.object.text))
@@ -54,9 +55,10 @@ class ThisModule(ClientModule):
 
     def __init__(self, module_name, services):
         super().__init__(module_name, services)
+        self._view_opener = services.view_opener
         services.view_registry.register_view_producer(self._produce_view)
 
     def _produce_view(self, type_ref, object, observer):
         if not isinstance(object, TextObject):
             raise NotApplicable(object)
-        return TextView(object)
+        return TextView(self._view_opener, object)
