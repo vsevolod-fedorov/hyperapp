e0e898584d76df37524f2507337feea0c4daa94d
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2019-08-25 13:35:13 +0300

    Move to asyncqt

--- hyperapp/client/async_application.py
+++ hyperapp/client/async_application.py
@@ -1,6 +1,8 @@
 # qt application with async loop support
 
 import asyncio
+
+from asyncqt import QEventLoop
 from PySide2 import QtCore, QtWidgets
 
 
@@ -8,8 +10,9 @@ class AsyncApplication(QtWidgets.QApplication):
 
     def __init__(self, sys_argv=None):
         super().__init__(sys_argv or [])
-        self.event_loop = asyncio.get_event_loop()
+        self.event_loop = QEventLoop(self)
         self.event_loop.set_debug(True)
+        asyncio.set_event_loop(self.event_loop)
 
     def stop_loop(self):
         asyncio.ensure_future(self._stop_loop_async())  # call it async to allow all pending tasks to complete
@@ -17,15 +20,7 @@ class AsyncApplication(QtWidgets.QApplication):
     async def _stop_loop_async(self):
         self.event_loop.stop()
 
-    # process qt events while inside asyncio loop
-    def _process_events_and_repeat(self):
-        while self.hasPendingEvents():
-            self.processEvents()
-        self.sendPostedEvents(None, 0)
-        self.event_loop.call_later(0.01, self._process_events_and_repeat)
-
-    def exec_(self):
-        self.event_loop.call_soon(self._process_events_and_repeat)
+    def run_event_loop(self):
         try:
             self.event_loop.run_forever()
         finally:
