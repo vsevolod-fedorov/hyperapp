ee1fe1a96e84d2b2eb9523a8b1b2cee238c3f94a
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2019-10-06 13:05:12 +0300

    cleanup

--- hyperapp/client/default_state_builder.dyn.py
+++ hyperapp/client/default_state_builder.dyn.py
@@ -22,17 +22,3 @@ class ThisModule(ClientModule):
             size=htypes.window.size(1000, 800),
             pos=htypes.window.pos(500, 100))
         return [window_state]
-
-    # def _build_default_state(self):
-    #     text_object_state = htypes.text.text('text', 'hello')
-    #     text_handle = htypes.text.text_view('text_view', text_object_state)
-    #     navigator_state = htypes.navigator.state(
-    #         view_id='navigator',
-    #         history=[htypes.navigator.item('sample text', text_handle)],
-    #         current_pos=0)
-    #     tabs_state = htypes.tab_view.tab_view(tabs=[navigator_state], current_tab=0)
-    #     window_state = htypes.window.window(
-    #         tab_view=tabs_state,
-    #         size=htypes.window.size(1000, 800),
-    #         pos=htypes.window.pos(500, 100))
-    #     return [window_state]
