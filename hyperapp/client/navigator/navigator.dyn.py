608b56b524740c128ac9861a506fe8f2d555fbb5
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2019-10-27 10:32:37 +0300

    Implement navigator view opening; cleanup tab_view

--- hyperapp/client/navigator/navigator.dyn.py
+++ hyperapp/client/navigator/navigator.dyn.py
@@ -16,6 +16,8 @@ class ThisModule(ClientModule):
 
     def __init__(self, module_name, services):
         super().__init__(module_name, services)
+        self._object_registry = services.object_registry
+        self._view_producer_registry = services.view_producer_registry
         self._module_command_registry = services.module_command_registry
         services.view_registry.register_type(
             htypes.navigator.navigator,
@@ -25,27 +27,29 @@ class ThisModule(ClientModule):
             services.view_producer_registry,
             )
 
-    async def _resolve_data(self, state, command_registry, async_ref_resolver, object_registry, view_producer_registry):
+    async def _resolve_data(self, state, command_registry, view_opener, async_ref_resolver, object_registry, view_producer_registry):
         piece = await async_ref_resolver.resolve_ref_to_object(state.current_piece_ref)
         object = object_registry.resolve(piece)
-        command_registry.set_commands('global', list(self._get_global_commands()))
-        command_registry.set_commands('object', list(self._get_object_commands(object)))
+        command_registry.set_commands('global', list(self._get_global_commands(view_opener)))
+        command_registry.set_commands('object', list(self._get_object_commands(view_opener, object)))
         return (await view_producer_registry.produce_view(piece, object))
 
-    def _get_global_commands(self):
+    def _get_global_commands(self, view_opener):
         for command in self._module_command_registry.get_all_commands():
-            yield FreeFnCommand.from_command(command, partial(self._run_command, command))
+            yield FreeFnCommand.from_command(command, partial(self._run_command, view_opener, command))
 
-    def _get_object_commands(self, object):
+    def _get_object_commands(self, view_opener, object):
         for command in object.get_command_list():
             if command.kind != 'object':
                 continue
-            yield FreeFnCommand.from_command(command, partial(self._run_command, command))
+            yield FreeFnCommand.from_command(command, partial(self._run_command, view_opener, command))
 
-    async def _run_command(self, command):
+    async def _run_command(self, view_opener, command):
         piece = await command.run()
         if piece is not None:
-            await self._open(piece)
+            view = await self._make_view(piece)
+            view_opener.open(view)
 
-    async def _open(self, piece):
-        assert 0  # todo
+    async def _make_view(self, piece):
+        object = await self._object_registry.resolve_async(piece)
+        return (await self._view_producer_registry.produce_view(piece, object))
--- hyperapp/client/tab_view.dyn.py
+++ hyperapp/client/tab_view.dyn.py
@@ -11,114 +11,52 @@ from .view import View
 log = logging.getLogger(__name__)
 
 
-class TabView(QtWidgets.QTabWidget, View):
+class _ViewOpener:
 
-    @staticmethod    
-    def map_current(state, mapper):
-        idx = state.current_tab
-        return htypes.tab_view.tab_view(state.tabs[:idx] + [mapper(state.tabs[idx])] + state.tabs[idx+1:], idx)
+    def __init__(self, tab_index):
+        self._tab_view = None
+        self._tab_index = tab_index
+
+    def set_tab_view(self, tab_view):
+        self._tab_view = tab_view
+
+    def open(self, view):
+        self._tab_view._replace_view(self._tab_index, view)
+
+
+class TabView(QtWidgets.QTabWidget, View):
 
     @classmethod
     async def from_data(cls, state, command_registry, view_resolver):
-        children = [await view_resolver.resolve(tab_ref, command_registry)
-                    for tab_ref in state.tabs]
-        return cls(children, state.current_tab)
+        children = []
+        opener_list = []
+        for idx, tab_ref in enumerate(state.tabs):
+            opener = _ViewOpener(idx)
+            child = await view_resolver.resolve(tab_ref, command_registry, opener)
+            opener_list.append(opener)
+            children.append(child)
+        tab_view = cls(children, state.current_tab)
+        for opener in opener_list:
+            opener.set_tab_view(tab_view)
+        return tab_view
 
     def __init__(self, children, current_tab):
         QtWidgets.QTabWidget.__init__(self)
         View.__init__(self)
         self.tabBar().setFocusPolicy(QtCore.Qt.NoFocus)
         self.setElideMode(QtCore.Qt.ElideMiddle)
-        self.currentChanged.connect(self._on_current_changed)
         for view in children:
             self.addTab(view, view.get_title())
         self.setCurrentIndex(current_tab)
 
-    def get_state(self):
-        return htypes.tab_view.tab_view([view.get_state() for view in self._children], self.currentIndex())
-
-    def get_current_child(self):
-        idx = self.currentIndex()
-        if idx == -1 or idx >= len(self._children): return None  # changing right now
-        return self._children[idx]
-
-    def view_changed(self, child):
-        idx = self.currentIndex()
-        if idx == -1: return  # constructing right now
-        if idx >= len(self._children) or child is not self._children[idx]:
-            return  # this child may be deleted right now
-        w = self.widget(idx)
-        if child is not self._children[idx] or w is not child.get_widget():
-            if DEBUG_FOCUS: log.info('*** tab_view.view_changed: replacing tab self=%r idx=%r child=%r', self, idx, child)
-            self.removeTab(idx)
-            w.deleteLater()
-            self.insertTab(idx, child.get_widget(), child.get_title())
-            self.setCurrentIndex(idx)
-            child.ensure_has_focus()
-        self.setTabText(idx, child.get_title())
-        View.view_changed(self)  # notify parents
-
-    def open(self, handle):
-        idx = self.currentIndex()
-        self._children[idx].open(handle)
-
-    def _on_current_changed(self, idx):
-        View.view_changed(self)
+    def _replace_view(self, idx, view):
+        old_widget = self.widget(idx)
+        self.removeTab(idx)
+        old_widget.deleteLater()
+        self.insertTab(idx, view.get_widget(), view.get_title())
 
     def setVisible(self, visible):
-        if DEBUG_FOCUS: log.info('*** tab_view.setVisible self=%r visible=%r current-tab#=%d', self, visible, self.currentIndex())
         QtWidgets.QTabWidget.setVisible(self, visible)
-
-    @command('duplicate_tab')
-    async def duplicate_tab(self):
-        idx = self.currentIndex()
-        state = self._children[idx].get_state()
-        new_view = await self._view_registry.resolve_async(self._locale, state, self)
-        self._insert_tab(idx + 1, new_view)
-        self._parent().view_changed(self)
-
-    @command('close_tab')
-    def close_tab(self):
-        if len(self._children) == 1: return  # never close last tab
-        idx = self.currentIndex()
-        self._remove_tab(idx)
-        if idx >= len(self._children):
-            idx -= 1
-        View.view_changed(self)  # notify parents
-
-    @command('split_horizontally')
-    async def split_horizontally(self):
-        await self._map_current(splitter.split(splitter.horizontal))
-
-    @command('split_vertically')
-    async def split_vertically(self):
-        await self._map_current(splitter.split(splitter.vertical))
-
-    @command('unsplit')
-    async def unsplit(self):
-        await self._map_current(splitter.unsplit)
-
-    async def _map_current(self, mapper):
-        idx = self.currentIndex()
-        state = mapper(self._children[idx].get_state())
-        if not state: return
-        self._remove_tab(idx)
-        child = await self._view_registry.resolve_async(self._locale, state, self)
-        self._insert_tab(idx, child)
-        child.ensure_has_focus()
-        View.view_changed(self)  # notify parents
-
-    def _remove_tab(self, idx):
-        w = self.widget(idx)
-        # remove child first so later view_changed calls from child being deleted may be ignored:
-        del self._children[idx]
-        self.removeTab(idx)
-        w.deleteLater()
-
-    def _insert_tab(self, idx, child):
-        self.insertTab(idx, child.get_widget(), child.get_title())
-        self._children.insert(idx, child)
-        self.setCurrentIndex(idx)
 
 
 class ThisModule(ClientModule):
