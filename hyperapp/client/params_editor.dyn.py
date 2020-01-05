0db689a774a49cb4788eece5a173aacd2a96e979
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2020-01-05 12:34:28 +0300

    Implement minimal params editor record object

--- hyperapp/client/params_editor.dyn.py
+++ hyperapp/client/params_editor.dyn.py
@@ -3,10 +3,28 @@ from collections import OrderedDict
 from hyperapp.client.module import ClientModule
 
 from . import htypes
+from .record_object import RecordObject
 
 
-class ParamsEditor:
-    pass
+class ParamsEditor(RecordObject):
+
+    @classmethod
+    async def from_data(cls, state, async_ref_resolver):
+        target_piece = await async_ref_resolver.resolve_ref_to_object(state.target_piece_ref)
+        fields = [(name, await async_ref_resolver.resolve_ref_to_object(piece_ref))
+                  for name, piece_ref in state.fields]
+        return cls(target_piece, state.target_command_id, fields)
+
+    def __init__(self, target_piece, target_command_id, fields):
+        self._target_piece = target_piece
+        self._target_command_id = target_command_id
+        self._fields = fields
+
+    def get_title(self):
+        return f"Parameters for {self._target_command_id}"
+
+    def get_fields(self):
+        return OrderedDict(self._fields)
 
 
 class ThisModule(ClientModule):
@@ -15,6 +33,8 @@ class ThisModule(ClientModule):
         super().__init__(module_name, services)
         self._ref_registry = services.ref_registry
         services.params_editor = self._open_params_editor
+        services.object_registry.register_type(
+            htypes.params_editor.params_editor, ParamsEditor.from_data, services.async_ref_resolver)
 
     def _open_params_editor(self, piece, command, args, kw):
         bound_arguments = command.bound_arguments(*args, **kw)
