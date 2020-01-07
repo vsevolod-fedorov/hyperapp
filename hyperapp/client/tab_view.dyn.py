67802b6f394431ee3ba134ff2af31c064ced662c
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2020-01-07 10:35:26 +0300

    Reimplement TabView.get_current_child method

--- hyperapp/client/tab_view.dyn.py
+++ hyperapp/client/tab_view.dyn.py
@@ -190,11 +190,7 @@ class TabView(QtWidgets.QTabWidget, View):
         QtWidgets.QTabWidget.setVisible(self, visible)
 
     def get_current_child(self):
-        tab_idx = self.currentIndex()
-        if tab_idx == -1:
-            return None
-        else:
-            return self.widget(tab_idx)
+        return self.currentWidget()
 
     def _replace_tab(self, tab_idx, view):
         old_widget = self.widget(tab_idx)
