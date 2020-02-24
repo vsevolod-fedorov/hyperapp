9435cfb70211878d69bbfcfc234cc40efeb8d54b
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2020-02-24 10:36:29 +0300

    Migrate record view to new layout

--- hyperapp/client/record_view.dyn.py
+++ hyperapp/client/record_view.dyn.py
@@ -1,24 +1,56 @@
 from PySide2 import QtCore, QtWidgets
 
 from hyperapp.client.module import ClientModule
+
 from . import htypes
+from .layout import RootVisualItem, Layout
 from .view_registry import NotApplicable
 from .record_object import RecordObject
-from .layout_registry import LayoutViewProducer
 
 
 class RecordView(QtWidgets.QWidget):
 
     @classmethod
-    async def make(cls, object_registry, view_producer, layout_resolver, object, observer, layout=None):
+    async def make(cls, object_registry, view_producer_registry, object, command_hub, piece_opener, fields=None):
         view = cls(object)
-        await view._construct(object_registry, view_producer, layout_resolver, observer, layout)
+        await view._async_init(object_registry, view_producer_registry, command_hub, piece_opener, fields)
         return view
 
     def __init__(self, object):
         super().__init__()
         self._object = object
-        self._field_views = None
+
+    async def _async_init(self, object_registry, view_producer_registry, command_hub, piece_opener, fields):
+        # if fields:
+        #     field_to_layout_ref = {field.field_id: field.layout_ref for field in fields}
+        # else:
+        #     field_to_layout_ref = {}
+        qt_layout = QtWidgets.QVBoxLayout()
+        has_expandable_field = False
+        self._field_views = []
+        for field_id, field_piece in self._object.get_fields().items():
+            # layout_ref = field_to_layout_ref.get(field_id)
+            layout = None  # todo
+            field_view = await self._construct_field_view(
+                object_registry, view_producer_registry, qt_layout, field_id, field_piece, command_hub, piece_opener, layout)
+            if field_view.sizePolicy().verticalPolicy() & QtWidgets.QSizePolicy.ExpandFlag:
+                has_expandable_field = True
+            self._field_views.append(field_view)
+        if not has_expandable_field:
+            qt_layout.addStretch()
+        self.setLayout(qt_layout)
+
+    async def _construct_field_view(
+            self, object_registry, view_producer_registry, qt_layout, field_id, piece, command_hub, piece_opener, layout):
+        object = await object_registry.resolve_async(piece)
+        layout = await view_producer_registry.produce_layout(piece, object, command_hub, piece_opener)
+        view = await layout.create_view()
+        label = QtWidgets.QLabel(field_id)
+        label.setBuddy(view)
+        qt_layout.addWidget(label)
+        qt_layout.addWidget(view)
+        qt_layout.addSpacing(10)
+        return view
 
     def get_title(self):
         return self._object.get_title()
@@ -34,49 +66,26 @@ class RecordView(QtWidgets.QWidget):
         if visible:
             self._field_views[0].setFocus()
 
-    async def _construct(self, object_registry, view_producer, layout_resolver, observer, layout):
-        if layout:
-            field_to_layout_ref = {field.field_id: field.layout_ref for field in layout.fields}
-        else:
-            field_to_layout_ref = {}
-        qt_layout = QtWidgets.QVBoxLayout()
-        has_expandable_field = False
-        self._field_views = []
-        for field_id, field_rec in self._object.get_fields().items():
-            layout_ref = field_to_layout_ref.get(field_id)
-            if layout_ref:
-                producer = await layout_resolver.resolve(layout_ref)
-            else:
-                producer = view_producer
-            field_view = await self._construct_field_view(object_registry, producer, qt_layout, field_id, field_rec, observer, layout)
-            if field_view.sizePolicy().verticalPolicy() & QtWidgets.QSizePolicy.ExpandFlag:
-                has_expandable_field = True
-            self._field_views.append(field_view)
-        if not has_expandable_field:
-            qt_layout.addStretch()
-        self.setLayout(qt_layout)
 
-    async def _construct_field_view(self, object_registry, producer, qt_layout, field_id, field_rec, observer, layout):
-        field_object = await object_registry.resolve_async(field_rec)
-        field_view = await producer.produce_view(field_rec, field_object, observer)
-        label = QtWidgets.QLabel(field_id)
-        label.setBuddy(field_view)
-        qt_layout.addWidget(label)
-        qt_layout.addWidget(field_view)
-        qt_layout.addSpacing(10)
-        return field_view
+class RecordViewLayout(Layout):
 
-
-class RecordViewProducer(LayoutViewProducer):
-
-    def __init__(self, layout, object_registry, view_producer, layout_resolver):
-        self._layout = layout
+    def __init__(self, object_registry, view_producer_registry, object, path, command_hub, piece_opener, fields=None):
+        super().__init__(path)
+        self._object = object
         self._object_registry = object_registry
-        self._view_producer = view_producer
-        self._layout_resolver = layout_resolver
+        self._view_producer_registry = view_producer_registry
+        self._command_hub = command_hub
+        self._piece_opener = piece_opener
+        self._fields = fields  # htypes.record_view.record_field list
 
-    async def produce_view(self, piece, object, observer=None):
-        return (await RecordView.make(self._object_registry, self._view_producer, self._layout_resolver, object, observer, self._layout))
+    def get_view_ref(self):
+        assert 0  # todo
+
+    async def create_view(self):
+        return (await RecordView.make(self._object_registry, self._view_producer_registry, self._object, self._command_hub, self._piece_opener, self._fields))
+
+    async def visual_item(self):
+        return RootVisualItem('RecordView')  # todo: add fields children
 
 
 class ThisModule(ClientModule):
@@ -84,13 +93,10 @@ class ThisModule(ClientModule):
     def __init__(self, module_name, services):
         super().__init__(module_name, services)
         self._object_registry = services.object_registry
-        self._view_producer = services.view_producer
-        self._layout_resolver = services.layout_resolver
+        self._view_producer_registry = services.view_producer_registry
         services.view_producer_registry.register_view_producer(self._produce_view)
-        services.layout_registry.register_type(
-            htypes.record_view.record_view_layout, RecordViewProducer, services.object_registry, services.view_producer, services.layout_resolver)
 
     async def _produce_view(self, piece, object, command_hub, piece_opener):
         if not isinstance(object, RecordObject):
             raise NotApplicable(object)
-        return (await RecordView.make(self._object_registry, self._view_producer, self._layout_resolver, object, observer))
+        return RecordViewLayout(self._object_registry, self._view_producer_registry, object, [], command_hub, piece_opener)
