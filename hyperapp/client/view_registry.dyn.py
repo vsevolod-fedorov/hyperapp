a915e53c85d5b6dde6d25c076e13206d2d0361b2
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2020-05-31 11:04:58 +0300

    AvailableObjectLayouts: implement resolving layout_rec for association

--- hyperapp/client/view_registry.dyn.py
+++ hyperapp/client/view_registry.dyn.py
@@ -37,20 +37,22 @@ class AvailableObjectLayouts:
 
 class ObjectLayoutProducer:
 
-    def __init__(self, default_object_layouts, object_layout_association, object_layout_registry):
+    def __init__(self, async_ref_resolver, default_object_layouts, object_layout_association, object_layout_registry):
+        self._async_ref_resolver = async_ref_resolver
         self._default_object_layouts = default_object_layouts
         self._object_layout_association = object_layout_association
         self._object_layout_registry = object_layout_registry
 
     async def produce_layout(self, object, command_hub, piece_opener):
-        layout_ref = None
+        layout_rec = None
         for category in reversed(object.category_list):
             try:
                 layout_ref = self._object_layout_association[category]
-                assert 0  # todo: resolve to layout_rec
             except KeyError:
-                pass
-        if not layout_ref:
+                continue
+            layout_rec = await self._async_ref_resolver.resolve_ref_to_object(layout_ref)
+            break
+        if not layout_rec:
             for category in reversed(object.category_list):
                 name_list = self._default_object_layouts.category_name_list(category)
                 if name_list:
@@ -77,4 +79,4 @@ class ThisModule(ClientModule):
         services.object_layout_registry = view_registry = AsyncCapsuleRegistry('object_layout', services.type_resolver)
         services.object_layout_resolver = view_resolver = AsyncCapsuleResolver(services.async_ref_resolver, services.object_layout_registry)
         services.object_layout_producer = ObjectLayoutProducer(
-            services.default_object_layouts, services.object_layout_association, services.object_layout_registry)
+            services.async_ref_resolver, services.default_object_layouts, services.object_layout_association, services.object_layout_registry)
