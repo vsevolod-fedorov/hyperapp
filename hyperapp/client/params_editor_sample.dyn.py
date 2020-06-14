66b44708580ac2691c708ff95db500e51af1d881
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2020-06-14 11:00:17 +0300

    Cleanup param editor sample: remove view chooser

--- hyperapp/client/params_editor_sample.dyn.py
+++ hyperapp/client/params_editor_sample.dyn.py
@@ -7,7 +7,6 @@ from hyperapp.client.module import ClientModule
 from . import htypes
 from .column import Column
 from .simple_list_object import SimpleListObject
-from .view_chooser import LayoutRecMakerField
 
 
 Item = namedtuple('Item', 'id name')
@@ -36,14 +35,12 @@ class SampleList(SimpleListObject):
         return [
             Item('single_simple_str', 'Single simple str'),
             Item('two_simple_strings', 'Two simple strings'),
-            Item('view_chooser', 'View chooser'),
             ]
 
     def get_item_command_list(self, item_key):
         command_map = dict(
             single_simple_str=self._test_single_simple_str,
             two_simple_strings=self._test_two_simple_str,
-            view_chooser=self._test_view_chooser,
             )
         try:
             return [command_map[item_key]]
@@ -58,12 +55,6 @@ class SampleList(SimpleListObject):
     @command('test_two_simple_str', kind='element')
     def _test_two_simple_str(self, item_key, str_param_1: str, str_param_2: str):
         text = f"Opened item {item_key}: {str_param_1!r},  {str_param_2!r}"
-        return htypes.text.text(text)
-
-    # todo: remove optionality when predefined params are supported by param editor
-    @command('test_view_chooser', kind='element')
-    def _test_view_chooser(self, item_key, view: LayoutRecMakerField):
-        text = f"Opened item {item_key}: {ref_repr(view)}"
         return htypes.text.text(text)
 
 
