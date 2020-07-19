26183dabde91d0c2b40b8d6a692452e3f754d877
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2020-07-19 13:32:34 +0300

    Add custom async exception handler

--- hyperapp/client/async_application.py
+++ hyperapp/client/async_application.py
@@ -1,9 +1,13 @@
 # qt application with async loop support
 
 import asyncio
+import logging
+import traceback
 
 from asyncqt import QEventLoop
 from PySide2 import QtCore, QtWidgets
+
+_log = logging.getLogger(__name__)
 
 
 class AsyncApplication(QtWidgets.QApplication):
@@ -12,6 +16,7 @@ class AsyncApplication(QtWidgets.QApplication):
         super().__init__(sys_argv or [])
         self.event_loop = QEventLoop(self)
         self.event_loop.set_debug(True)
+        self.event_loop.set_exception_handler(self._async_exception_handler)
         asyncio.set_event_loop(self.event_loop)
 
     def stop_loop(self):
@@ -25,3 +30,9 @@ class AsyncApplication(QtWidgets.QApplication):
             self.event_loop.run_forever()
         finally:
             self.event_loop.close()
+
+    def _async_exception_handler(self, loop, context):
+        exception = context['exception']
+        exception_tb = traceback.format_exception(exception.__class__, exception, exception.__traceback__)
+        source_tb = traceback.StackSummary.from_list(context['source_traceback']).format()
+        _log.error("%s\n%s\nSource traceback:\n%s", context['message'], ''.join(exception_tb), ''.join(source_tb))
