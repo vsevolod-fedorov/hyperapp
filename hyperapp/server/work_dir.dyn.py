fc0467697b0a04a1f4ee36b9d1202e1a86961693
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2020-12-13 11:40:37 +0300

    work_dir server module

---
+++ hyperapp/server/work_dir.dyn.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+from hyperapp.common.module import Module
+
+
+class ThisModule(Module):
+
+    def __init__(self, module_name, services):
+        super().__init__(module_name)
+        services.work_dir = Path.home() / '.cache' / 'hyperapp' / 'server'
