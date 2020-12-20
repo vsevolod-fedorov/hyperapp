5d299757161b48e03c8e7e1d7067aa5851e1570e
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2020-12-20 12:47:09 +0300

    test_send_subprocess_parcel: pass master_peer_ref to send_subprocess_parcel module

--- hyperapp/sync/transport/test/send_subprocess_parcel.dyn.py
+++ hyperapp/sync/transport/test/send_subprocess_parcel.dyn.py
@@ -1,3 +1,5 @@
+from hyperapp.common.htypes import ref_t
+from hyperapp.common.htypes.packet_coders import packet_coders
 from hyperapp.common.module import Module
 
 from .rsa_identity import RsaIdentity
@@ -7,5 +9,6 @@ class ThisModule(Module):
 
     def __init__(self, module_name, services, config):
         super().__init__(module_name)
+        master_peer_ref = packet_coders.decode('cdr', config['master_peer_ref_cdr'], ref_t)
         my_identity = RsaIdentity.generate(fast=True)
         services.transport.send(None)
--- hyperapp/sync/transport/test/test_sync_transport.py
+++ hyperapp/sync/transport/test/test_sync_transport.py
@@ -1,5 +1,6 @@
 import pytest
 
+from hyperapp.common.htypes.packet_coders import packet_coders
 from hyperapp.common import cdr_coders  # self-registering
 
 
@@ -27,6 +28,7 @@ def test_send_subprocess_parcel(services):
     rsa_identity_module = services.name2module['common.remoting.rsa_identity']
     my_identity = rsa_identity_module.RsaIdentity.generate(fast=True)
     my_peer_ref = services.ref_registry.distil(my_identity.peer.piece)
+    my_peer_ref_cdr = packet_coders.encode('cdr', my_peer_ref)
 
     subprocess = services.subprocess(
         'test_subprocess',
@@ -39,7 +41,12 @@ def test_send_subprocess_parcel(services):
             'sync.transport.transport',
             'sync.transport.test.send_subprocess_parcel',
             ],
+        config = {
+            'sync.transport.test.send_subprocess_parcel': {'master_peer_ref_cdr': my_peer_ref_cdr},
+            },
         master_peer_ref_list=[my_peer_ref],
         )
-    with subprocess:
-        pass
+    with pytest.raises(AssertionError) as excinfo:
+        with subprocess:
+            pass
+    assert str(excinfo.value) == 'todo'
