58740071229e37d433de66e99d516ade43466a44
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2020-12-20 13:52:45 +0300

    Minor line reordering

--- hyperapp/sync/transport/test/send_subprocess_parcel.dyn.py
+++ hyperapp/sync/transport/test/send_subprocess_parcel.dyn.py
@@ -10,8 +10,8 @@ class ThisModule(Module):
     def __init__(self, module_name, services, config):
         super().__init__(module_name)
         master_peer_bundle = packet_coders.decode('cdr', config['master_peer_bundle_cdr'], bundle_t)
-        master_peer_ref = master_peer_bundle.roots[0]
         services.unbundler.register_bundle(master_peer_bundle)
+        master_peer_ref = master_peer_bundle.roots[0]
         master_peer = services.peer_registry.invite(master_peer_ref)
         my_identity = RsaIdentity.generate(fast=True)
         bundle = bundle_t(roots=[], capsule_list=[], route_list=[])
