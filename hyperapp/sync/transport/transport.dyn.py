26fcebfd397b244e6849b18cfe951ed964eac370
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2020-12-27 11:41:42 +0300

    Implement Transport.send method

--- hyperapp/sync/transport/transport.dyn.py
+++ hyperapp/sync/transport/transport.dyn.py
@@ -18,13 +18,16 @@ class RouteAssociationRegistry:
 
 class Transport:
 
-    def __init__(self, route_registry, route_a9n_registry):
+    def __init__(self, ref_registry, route_registry, route_a9n_registry):
+        self._ref_registry = ref_registry
         self._route_registry = route_registry
         self._route_a9n_registry = route_a9n_registry
 
     def send(self, parcel):
-        raise NotImplementedError('todo')
-        # route_list = self._route_a9n_registry.get(parcel)
+        receiver_peer_ref = self._ref_registry.distil(parcel.receiver_peer.piece)
+        route_list = self._route_a9n_registry.get(receiver_peer_ref)
+        route, *_ = route_list
+        route.send(parcel)
 
 
 class ThisModule(Module):
@@ -33,4 +36,4 @@ class ThisModule(Module):
         super().__init__(module_name)
         services.route_registry = CodeRegistry('route', services.ref_resolver, services.types)
         services.route_a9n_registry = RouteAssociationRegistry()
-        services.transport = Transport(services.route_registry, services.route_a9n_registry)
+        services.transport = Transport(services.ref_registry, services.route_registry, services.route_a9n_registry)
