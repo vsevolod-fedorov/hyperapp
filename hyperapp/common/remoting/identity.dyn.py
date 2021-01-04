8977c31fde68415c1a838aed909f2bbd6dc09872
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-01-04 11:07:54 +0300

    Add RsaSignature, signature_registry and RsaParcel.sender

--- hyperapp/common/remoting/identity.dyn.py
+++ hyperapp/common/remoting/identity.dyn.py
@@ -8,4 +8,5 @@ class ThisModule(Module):
         super().__init__(module_name)
         services.identity_registry = CodeRegistry('identity', services.ref_resolver, services.types)
         services.peer_registry = CodeRegistry('peer', services.ref_resolver, services.types)
+        services.signature_registry = CodeRegistry('signature', services.ref_resolver, services.types)
         services.parcel_registry = CodeRegistry('parcel', services.ref_resolver, services.types)
