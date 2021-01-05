1a4f4669015a31fd34f4f0edd4f16032db24bfe6
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-01-05 11:37:36 +0300

    test subprocess echo: send response from subprocess

--- hyperapp/sync/transport/test/subprocess_echo.dyn.py
+++ hyperapp/sync/transport/test/subprocess_echo.dyn.py
@@ -3,6 +3,28 @@ from hyperapp.common.htypes.packet_coders import packet_coders
 from hyperapp.common.module import Module
 
 
+
+class Endpoint:
+
+    def __init__(self, ref_registry, unbundler, ref_collector_factory, transport, my_identity):
+        self._ref_registry = ref_registry
+        self._unbundler = unbundler
+        self._ref_collector_factory = ref_collector_factory
+        self._transport = transport
+        self._my_identity = my_identity
+
+    def process(self, parcel):
+        bundle = self._my_identity.decrypt_parcel(parcel)
+        self._unbundler.register_bundle(bundle)
+
+        my_peer_ref = self._ref_registry.distil(self._my_identity.peer.piece)
+        ref_collector = self._ref_collector_factory()
+        resp_bundle = ref_collector.make_bundle([*bundle.roots, my_peer_ref])
+        resp_parcel = parcel.sender.make_parcel(resp_bundle, self._my_identity)
+        self._transport.send(resp_parcel)
+
+
+
 class ThisModule(Module):
 
     def __init__(self, module_name, services, config):
@@ -15,6 +37,10 @@ class ThisModule(Module):
 
         my_identity = services.generate_rsa_identity(fast=True)
         my_peer_ref = services.ref_registry.distil(my_identity.peer.piece)
+
+        endpoint = Endpoint(
+            services.ref_registry, services.unbundler, services.ref_collector_factory, services.transport, my_identity)
+        services.endpoint_registry.register(my_peer_ref, endpoint)
 
         ref_collector = services.ref_collector_factory()
         bundle = ref_collector.make_bundle([my_peer_ref])
