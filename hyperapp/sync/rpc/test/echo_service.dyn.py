c7b9a7288caed97c68ed72a53dd5bcf2279d2750
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-01-06 10:31:51 +0300

    Start test_echo; add rpc types

---
+++ hyperapp/sync/rpc/test/echo_service.dyn.py
@@ -0,0 +1,17 @@
+from hyperapp.common.htypes import bundle_t
+from hyperapp.common.htypes.packet_coders import packet_coders
+from hyperapp.common.module import Module
+
+
+class ThisModule(Module):
+
+    def __init__(self, module_name, services, config):
+        super().__init__(module_name)
+
+        master_peer_bundle = packet_coders.decode('cdr', config['master_peer_bundle_cdr'], bundle_t)
+        services.unbundler.register_bundle(master_peer_bundle)
+        master_peer_ref = master_peer_bundle.roots[0]
+        master_peer = services.peer_registry.invite(master_peer_ref)
+
+        my_identity = services.generate_rsa_identity(fast=True)
+        my_peer_ref = services.ref_registry.distil(my_identity.peer.piece)
