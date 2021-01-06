3132dae7926907b6a9957fe2384f9cb7f98eb650
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-01-06 11:37:08 +0300

    Start rpc Proxy

--- hyperapp/sync/rpc/test/echo_service.dyn.py
+++ hyperapp/sync/rpc/test/echo_service.dyn.py
@@ -2,6 +2,8 @@ from hyperapp.common.htypes import bundle_t
 from hyperapp.common.htypes.packet_coders import packet_coders
 from hyperapp.common.module import Module
 
+from . import htypes
+
 
 class ThisModule(Module):
 
@@ -11,7 +13,18 @@ class ThisModule(Module):
         master_service_bundle = packet_coders.decode('cdr', config['master_service_bundle_cdr'], bundle_t)
         services.unbundler.register_bundle(master_service_bundle)
         master_service_ref = master_service_bundle.roots[0]
-        master_service = services.types.resolve_ref(master_service_ref)
+        master_service = services.types.resolve_ref(master_service_ref).value
 
         my_identity = services.generate_rsa_identity(fast=True)
         my_peer_ref = services.ref_registry.distil(my_identity.peer.piece)
+
+        echo_iface_ref = services.types.reverse_resolve(htypes.echo.echo_iface)
+        echo_service = htypes.rpc.endpoint(
+            peer_ref=my_peer_ref,
+            iface_ref=echo_iface_ref,
+            object_id='echo',
+            )
+        echo_service_ref = services.ref_registry.distil(echo_service)
+
+        proxy = services.rpc_proxy(master_service)
+        proxy.run(echo_service_ref)
