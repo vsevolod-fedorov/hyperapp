694e105633aacbdbdcf9a0610c51dbb86318e9f3
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-01-24 13:17:13 +0300

    Wip: handle and wait for response

--- hyperapp/sync/rpc/rpc_endpoint.dyn.py
+++ hyperapp/sync/rpc/rpc_endpoint.dyn.py
@@ -1,3 +1,5 @@
+import logging
+import threading
 from collections import namedtuple
 from functools import partial
 
@@ -5,6 +7,8 @@ from hyperapp.common.code_registry import CodeRegistry
 from hyperapp.common.module import Module
 
 from . import htypes
+
+log = logging.getLogger(__name__)
 
 
 RpcRequest = namedtuple('RpcRequest', 'receiver_identity sender')
@@ -18,16 +22,31 @@ class RpcEndpoint:
         self._peer_registry = peer_registry
         self._transport = transport
         self._servant_by_id = {}
+        self._result_by_request_id = {}
+        self._response_lock = threading.Lock()
+        self._response_available = threading.Condition(self._response_lock)
         self._message_registry = registry = CodeRegistry('rpc_message', web, types)
         registry.register_actor(htypes.rpc.request, self._handle_request)
+        registry.register_actor(htypes.rpc.response, self._handle_response)
 
     def register_servant(self, object_id, servant):
         self._servant_by_id[object_id] = servant
 
+    def wait_for_response(self, request_id):
+        log.info("Wait for rpc response: %s", request_id)
+        with self._response_lock:
+            while True:
+                try:
+                    return self._result_by_request_id.pop(request_id)
+                except KeyError:
+                    self._response_available.wait()
+
     def process(self, request):
+        log.info("Received rpc message: %s", request)
         self._message_registry.invite(request.ref_list[0], request)
 
     def _handle_request(self, request, transport_request):
+        log.info("Process rpc request: %s", request)
         receiver_identity = transport_request.receiver_identity
         sender = self._peer_registry.invite(request.sender_peer_ref)
         iface = self._types.resolve(request.iface_ref)
@@ -55,6 +74,13 @@ class RpcEndpoint:
             )
         response_ref = self._mosaic.put(response)
         self._transport.send(sender, receiver_identity, [response_ref])
+
+    def _handle_response(self, response, transport_request):
+        log.info("Process rpc response: %s", response)
+        result = self._types.resolve_ref(response.result_ref).value
+        with self._response_lock:
+            self._result_by_request_id[response.request_id] = result
+            self._response_available.notify_all()
 
 
 class ThisModule(Module):
