997483fb1099619a03821ea4ca2ba839a8ef8d53
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-01-24 11:05:51 +0300

    Add RpcRequest param for servant; invite sender peer

--- hyperapp/sync/rpc/rpc_endpoint.dyn.py
+++ hyperapp/sync/rpc/rpc_endpoint.dyn.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from functools import partial
 
 from hyperapp.common.code_registry import CodeRegistry
@@ -6,10 +7,14 @@ from hyperapp.common.module import Module
 from . import htypes
 
 
+RpcRequest = namedtuple('RpcRequest', 'sender')
+
+
 class RpcEndpoint:
 
-    def __init__(self, web, types):
+    def __init__(self, web, types, peer_registry):
         self._types = types
+        self._peer_registry = peer_registry
         self._servant_by_id = {}
         self._message_registry = registry = CodeRegistry('rpc_message', web, types)
         registry.register_actor(htypes.rpc.request, self._handle_request)
@@ -21,10 +26,14 @@ class RpcEndpoint:
         self._message_registry.invite(request.ref_list[0])
 
     def _handle_request(self, request):
+        sender = self._peer_registry.invite(request.sender_peer_ref)
         servant = self._servant_by_id[request.object_id]
         params = self._types.resolve_ref(request.params_ref).value
         method = getattr(servant, request.method_name)
-        result = method(**params._asdict())
+        result = method(
+            RpcRequest(sender),
+            **params._asdict(),
+            )
         # todo: send response with result or exception
 
 
@@ -32,4 +41,4 @@ class ThisModule(Module):
 
     def __init__(self, module_name, services, config):
         super().__init__(module_name)
-        services.rpc_endpoint_factory = partial(RpcEndpoint, services.web, services.types)
+        services.rpc_endpoint_factory = partial(RpcEndpoint, services.web, services.types, services.peer_registry)
