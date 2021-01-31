edbf33c59b968848069e85ae3443a6a0ac5e6a6e
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-01-31 11:06:03 +0300

    Endpoint: add thread pool

--- hyperapp/sync/transport/endpoint.dyn.py
+++ hyperapp/sync/transport/endpoint.dyn.py
@@ -1,6 +1,10 @@
+import logging
 from collections import namedtuple
+from concurrent.futures import ThreadPoolExecutor
 
 from hyperapp.common.module import Module
+
+log = logging.getLogger(__name__)
 
 
 Request = namedtuple('Request', 'receiver_identity sender ref_list')
@@ -8,8 +12,9 @@ Request = namedtuple('Request', 'receiver_identity sender ref_list')
 
 class LocalRoute:
 
-    def __init__(self, unbundler, identity, endpoint):
+    def __init__(self, unbundler, thread_pool, identity, endpoint):
         self._unbundler = unbundler
+        self._thread_pool = thread_pool
         self._identity = identity
         self._endpoint = endpoint
 
@@ -17,19 +22,20 @@ class LocalRoute:
         bundle = self._identity.decrypt_parcel(parcel)
         self._unbundler.register_bundle(bundle)
         request = Request(self._identity, parcel.sender, bundle.roots)
-        self._endpoint.process(request)
+        self._thread_pool.submit(self._endpoint.process, request)
 
 
 class EndpointRegistry:
 
-    def __init__(self, mosaic, unbundler, route_table):
+    def __init__(self, mosaic, unbundler, route_table, thread_pool):
         self._mosaic = mosaic
         self._unbundler = unbundler
         self._route_table = route_table
+        self._thread_pool = thread_pool
 
     def register(self, identity, endpoint):
         peer_ref = self._mosaic.put(identity.peer.piece)
-        route = LocalRoute(self._unbundler, identity, endpoint)
+        route = LocalRoute(self._unbundler, self._thread_pool, identity, endpoint)
         self._route_table.add_route(peer_ref, route)
 
 
@@ -37,5 +43,11 @@ class ThisModule(Module):
 
     def __init__(self, module_name, services, config):
         super().__init__(module_name)
-        services.endpoint_registry = EndpointRegistry(services.mosaic, services.unbundler, services.route_table)
+        self._thread_pool = ThreadPoolExecutor(max_workers=5)
+        services.on_stop.append(self.stop)
+        services.endpoint_registry = EndpointRegistry(services.mosaic, services.unbundler, services.route_table, self._thread_pool)
 
+    def stop(self):
+        log.info("Shutdown endpoint thread pool")
+        self._thread_pool.shutdown()
+        log.info("Endpoint thread pool is shut down")
