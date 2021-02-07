1aaae59594dc742fda32f5fc749e280d1ad6d149
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-02-07 12:58:41 +0300

    Cleanup some common modules

--- hyperapp/common/object_path_collector.py
+++
@@ -1,9 +0,0 @@
-from .visitor import Visitor
-
-
-class ObjectPathCollector(Visitor):
-
-    def collect(self, t, value):
-        self.collected_paths = set()
-        self.visit(t, value)
-        return [list(path) for path in self.collected_paths]
