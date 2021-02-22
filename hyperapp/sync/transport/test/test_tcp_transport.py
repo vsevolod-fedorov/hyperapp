e81a5290b764dc1803f5e4fb3fc576194c513348
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-02-22 11:32:06 +0300

    test_tcp_transport: register endpoint, use send_subprocess_parcel module

--- hyperapp/sync/transport/test/test_tcp_transport.py
+++ hyperapp/sync/transport/test/test_tcp_transport.py
@@ -1,7 +1,9 @@
 import logging
+import queue
 
 import pytest
 
+from hyperapp.common.htypes.packet_coders import packet_coders
 from hyperapp.common import cdr_coders  # self-registering
 
 log = logging.getLogger(__name__)
@@ -13,6 +15,7 @@ pytest_plugins = ['hyperapp.common.test.services']
 def type_module_list():
     return [
         'rsa_identity',
+        'transport',
         'tcp_transport',
         ]
 
@@ -29,11 +32,68 @@ def code_module_list():
         'sync.async_stop',
         'sync.transport.transport',
         'sync.transport.endpoint',
+        'sync.subprocess_connection',
+        'sync.subprocess',
         'sync.transport.tcp',
         ]
 
 
+class Endpoint:
+
+    def __init__(self, request_queue):
+        self._request_queue = request_queue
+
+    def process(self, request):
+        self._request_queue.put(request)
+
+
 def test_tcp_transport(services):
+    master_identity = services.generate_rsa_identity(fast=True)
+    master_peer_ref = services.mosaic.put(master_identity.peer.piece)
+
+    request_queue = queue.Queue()
+    services.endpoint_registry.register(master_identity, Endpoint(request_queue))
+
     server = services.tcp_server(('localhost', 0))
-    route = server.route
-    log.info("Route: %r", route)
+    log.info("Tcp route: %r", server.route)
+    services.route_table.add_route(master_peer_ref, server.route)
+
+    ref_collector = services.ref_collector_factory()
+    master_peer_bundle = ref_collector.make_bundle([master_peer_ref])
+    master_peer_bundle_cdr = packet_coders.encode('cdr', master_peer_bundle)
+
+    master_peer_ref_cdr_list = [packet_coders.encode('cdr', master_peer_ref)]
+
+    subprocess = services.subprocess(
+        'subprocess',
+        type_module_list=[
+            'rsa_identity',
+            'transport',
+            'tcp_transport',
+            ],
+        code_module_list=[
+            'common.visitor',
+            'common.ref_collector',
+            'common.unbundler',
+            'transport.identity',
+            'transport.rsa_identity',
+            'sync.async_stop',
+            'sync.transport.transport',
+            'sync.subprocess_connection',
+            'sync.subprocess_child',
+            'sync.transport.tcp',
+            'sync.transport.test.send_subprocess_parcel',
+            ],
+        config = {
+            'sync.transport.test.send_subprocess_parcel': {'master_peer_bundle_cdr': master_peer_bundle_cdr},
+            },
+        )
+    try:
+        with subprocess:
+            log.info("Waiting for request.")
+            request = request_queue.get(timeout=5)
+            log.info("Got request.")
+            assert request.receiver_identity.piece == master_identity.piece
+        log.info("Subprocess is finished.")
+    finally:
+        log.info("Subprocess is exited.")
