1b2cd60d5835f8e767728574fa5df2ee2a9f2f7a
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-02-23 15:15:36 +0300

    Move to python 3.9: Add hack/fix for libxcb-util qt but

--- run_client.sh
+++ run_client.sh
@@ -4,6 +4,11 @@ venv=${VENV:-$HOME/venv}
 
 . $venv/bin/activate
 
+# Hack/fix for https://bugreports.qt.io/browse/QTCREATORBUG-24967
+# ~/.venv-hyperapp-3.9.2/lib/python3.9/site-packages/PySide2/Qt/plugins/platforms$
+# > ln -s /usr/lib/x86_64-linux-gnu/libxcb-util.so.0.0.0 libxcb-util.so.1
+export LD_LIBRARY_PATH="$HOME/.venv-hyperapp-3.9.2/lib/python3.9/site-packages/PySide2/Qt/plugins/platforms"
+
 export LOG_CFG=client-file
 
 rm /tmp/client*.log || true
--- run_tests.sh
+++ run_tests.sh
@@ -12,6 +12,11 @@ fi
 
 FILTER="$DIR/scripts/log-sort.py"
 
+# Hack/fix for https://bugreports.qt.io/browse/QTCREATORBUG-24967
+# ~/.venv-hyperapp-3.9.2/lib/python3.9/site-packages/PySide2/Qt/plugins/platforms$
+# > ln -s /usr/lib/x86_64-linux-gnu/libxcb-util.so.0.0.0 libxcb-util.so.1
+export LD_LIBRARY_PATH="$HOME/.venv-hyperapp-3.9.2/lib/python3.9/site-packages/PySide2/Qt/plugins/platforms"
+
 cd $DIR
 
 export PYTHONPATH=$PYTHONPATH:$DIR
