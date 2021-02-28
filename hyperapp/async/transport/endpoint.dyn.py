899ed79ba9dbd6124055609a06f13a0ce1ff590e
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-02-28 12:57:04 +0300

    Add async transport and endpoint modules

---
+++ hyperapp/async/transport/endpoint.dyn.py
@@ -0,0 +1,50 @@
+import logging
+from collections import namedtuple
+from concurrent.futures import ThreadPoolExecutor
+
+from hyperapp.common.ref import ref_repr
+from hyperapp.common.module import Module
+
+log = logging.getLogger(__name__)
+
+
+Request = namedtuple('Request', 'receiver_identity sender ref_list')
+
+
+class LocalRoute:
+
+    def __init__(self, unbundler, identity, endpoint):
+        self._unbundler = unbundler
+        self._identity = identity
+        self._endpoint = endpoint
+
+    @property
+    def piece(self):
+        return None
+
+    async def send(self, parcel):
+        bundle = self._identity.decrypt_parcel(parcel)
+        self._unbundler.register_bundle(bundle)
+        request = Request(self._identity, parcel.sender, bundle.roots)
+        await self._endpoint.process(request)
+
+
+class EndpointRegistry:
+
+    def __init__(self, mosaic, unbundler, route_table):
+        self._mosaic = mosaic
+        self._unbundler = unbundler
+        self._route_table = route_table
+
+    def register(self, identity, endpoint):
+        peer_ref = self._mosaic.put(identity.peer.piece)
+        log.info("Local peer %s: %s", ref_repr(peer_ref), endpoint)
+        route = LocalRoute(self._unbundler, identity, endpoint)
+        self._route_table.add_route(peer_ref, route)
+
+
+class ThisModule(Module):
+
+    def __init__(self, module_name, services, config):
+        super().__init__(module_name)
+        services.endpoint_registry = EndpointRegistry(services.mosaic, services.unbundler, services.route_table)
