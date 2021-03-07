8aebdde7cf0f5d8bcdf55ce97ebeee4cf7467d29
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-03-07 12:51:04 +0300

    Update client after enum_modules_method change

--- hyperapp/client/module_command_registry.dyn.py
+++ hyperapp/client/module_command_registry.dyn.py
@@ -8,13 +8,13 @@ class ModuleCommandRegistry:
         self._module_registry = module_registry
 
     def get_all_commands(self):
-        return flatten(method() for method in
+        return flatten(method() for module, method in
                        self._module_registry.enum_modules_method('get_command_list'))
 
     def get_all_object_commands(self, object):
         # todo: command.clone is gone, use special wrapper command class or think about another way
         return [command.clone(args=(object,)) for command in
-                flatten(method(object) for method in
+                flatten(method(object) for module, method in
                         self._module_registry.enum_modules_method('get_object_command_list'))]
         
 class ThisModule(ClientModule):
