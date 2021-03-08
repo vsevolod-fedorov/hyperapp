04f68f003a37e67b3023e7de44e59e348dc42e07
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-03-08 13:58:34 +0300

    Call event_loop set_debug where it is created

--- hyperapp/async/event_loop.dyn.py
+++ hyperapp/async/event_loop.dyn.py
@@ -11,4 +11,5 @@ class ThisModule(Module):
     def __init__(self, module_name, services, config):
         super().__init__(module_name)
         services.event_loop = event_loop = asyncio.new_event_loop()
+        event_loop.set_debug(True)
         asyncio.set_event_loop(event_loop)  # Should be set before any asyncio objects created.
