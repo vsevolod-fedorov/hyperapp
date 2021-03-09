6209dff715d738f2dd4ea6779eec69e82fa7b5ae
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-03-09 11:38:25 +0300

    Add qt application even loop

--- hyperapp/async/test/test_event_loop.py
+++ hyperapp/async/test/test_event_loop.py
@@ -10,16 +10,25 @@ log = logging.getLogger(__name__)
 pytest_plugins = ['hyperapp.common.test.services']
 
 
+@pytest.fixture(params=['native', 'qt'])
+def event_loop_module(request):
+    if request.param == 'native':
+        return 'async.event_loop'
+    if request.param == 'qt':
+        return 'async.ui.qt.application'
+    assert False, request.param
+
+
 @pytest.fixture
-def code_module_list():
+def code_module_list(event_loop_module):
     return [
-        'async.event_loop',
+        event_loop_module,
         'async.async_main',
         'async.test.module_async_init_close',
         ]
 
 
-def test_native_event_loop(services):
+def test_event_loop(services):
     assert services.test_init_event.wait(timeout=1)
     services.stop()
     assert services.test_stop_event.wait(timeout=1)
