c3636a02ef62d213228431dc242b1969d2faf262
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-03-13 12:34:42 +0300

    async_web, object_registry: do not use ClientModule

--- hyperapp/client/object_registry.dyn.py
+++ hyperapp/client/object_registry.dyn.py
@@ -1,9 +1,10 @@
+from hyperapp.common.module import Module
+
 from .code_registry import CodeRegistry
-from .module import ClientModule
 
 
-class ThisModule(ClientModule):
+class ThisModule(Module):
 
     def __init__(self, module_name, services, config):
-        super().__init__(module_name, services)
+        super().__init__(module_name)
         services.object_registry = CodeRegistry('object', services.async_web, services.types)
