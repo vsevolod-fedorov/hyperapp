d5b4820adfdb24834fe4a33f06810772980c2db5
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-03-21 12:46:58 +0300

    Create run_server

---
+++ hyperapp/common/local_server.dyn.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+from hyperapp.common.module import Module
+
+
+class ThisModule(Module):
+
+    def __init__(self, module_name, services, config):
+        super().__init__(module_name)
+        services.local_server_ref = services.file_bundle(Path.home() / '.local/share/hyperapp/server')
