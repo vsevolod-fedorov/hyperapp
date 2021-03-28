6248b8aa0f91b84bd990a21df87461e456a3bbae
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-03-28 11:48:21 +0300

    Add identity and tcp_server server modules

---
+++ hyperapp/server/tcp_server.dyn.py
@@ -0,0 +1,15 @@
+import logging
+
+from hyperapp.common.module import Module
+
+log = logging.getLogger(__name__)
+
+
+class ThisModule(Module):
+
+    def __init__(self, module_name, services, config):
+        super().__init__(module_name)
+        server_peer_ref = services.mosaic.put(services.server_identity.peer.piece)
+        self._tcp_server = server = services.tcp_server()
+        services.route_table.add_route(server_peer_ref, server.route)
+        log.info("Server tcp route: %r", server.route)
