8ef7af7410ea00a95de449b508dc4caf3a2731be
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-03-28 13:23:05 +0300

    Add client local_server module

---
+++ hyperapp/client/local_server.dyn.py
@@ -0,0 +1,13 @@
+from .object_command import command
+from .module import ClientModule
+
+
+class ThisModule(ClientModule):
+
+    def __init__(self, module_name, services, config):
+        super().__init__(module_name, services)
+        self._local_server_ref = services.local_server_ref
+
+    @command('open_local_server')
+    async def open_local_server(self):
+        return self._local_server_ref.load_piece()
