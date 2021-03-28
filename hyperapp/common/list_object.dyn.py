ed26f06ee78f0c1b0e69ddd6cb5ee25bd502bcc7
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-03-28 11:00:58 +0300

    Refactor: add list_object module

---
+++ hyperapp/common/list_object.dyn.py
@@ -0,0 +1,32 @@
+from hyperapp.common.htypes import (
+    field_mt,
+    record_mt,
+    request_mt,
+    interface_mt,
+    name_wrapped_mt,
+    )
+
+
+def list_row_ref(mosaic, list_ot, name):
+    field_list = [
+        field_mt(column.id, column.type_ref)
+        for column in list_ot.column_list
+        ]
+    row_mt = record_mt(None, field_list)
+    row_ref = mosaic.put(row_mt)
+    named_row_ref = mosaic.put(name_wrapped_mt(f'{name}_row', row_ref))
+    return named_row_ref
+
+
+def list_row_t(mosaic, types, list_ot, name):
+    named_row_ref = list_row_ref(mosaic, list_ot, name)
+    return types.resolve(named_row_ref)
+
+
+def list_interface_ref(mosaic, list_ot, name):
+    named_row_ref = list_row_ref(mosaic, list_ot, name)
+    rows_field = field_mt('rows', named_row_ref)
+    get_method_ref = mosaic.put(request_mt('get', [], [rows_field]))
+    interface_ref = mosaic.put(interface_mt(None, [get_method_ref]))
+    named_interface_ref = mosaic.put(name_wrapped_mt(f'{name}_interface', interface_ref))
+    return named_interface_ref
