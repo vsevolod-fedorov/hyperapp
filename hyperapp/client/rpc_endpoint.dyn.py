533d3cc3e03f7a9d369d995dc69a19a012bb9aec
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-04-11 10:07:15 +0300

    Add client rpc_endpoint

---
+++ hyperapp/client/rpc_endpoint.dyn.py
@@ -0,0 +1,9 @@
+from hyperapp.common.module import Module
+
+
+class ThisModule(Module):
+
+    def __init__(self, module_name, services, config):
+        super().__init__(module_name)
+        services.client_rpc_endpoint = services.async_rpc_endpoint()
+        services.async_endpoint_registry.register(services.client_identity, services.client_rpc_endpoint)
