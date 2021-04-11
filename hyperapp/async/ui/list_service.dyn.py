c49dad2e4ca514b5d80697c0d02562fae88666c7
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-04-11 10:45:46 +0300

    Move client_rpc_endpoint creation to async_init

--- hyperapp/async/ui/list_service.dyn.py
+++ hyperapp/async/ui/list_service.dyn.py
@@ -65,6 +65,8 @@ class ThisModule(Module):
 
     def __init__(self, module_name, services, config):
         super().__init__(module_name)
+
+    async def async_init(self, services):
         services.object_registry.register_actor(
             list_service_t, ListService.from_piece,
             services.client_identity, services.mosaic, services.types, services.client_rpc_endpoint, services.async_rpc_proxy)
