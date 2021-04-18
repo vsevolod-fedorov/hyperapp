d72bc659644405fa52824a2abefc8542104dfd27
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-04-18 11:02:49 +0300

    Add rpc element command

--- hyperapp/async/ui/command_registry.dyn.py
+++ hyperapp/async/ui/command_registry.dyn.py
@@ -7,4 +7,4 @@ class ThisModule(Module):
 
     def __init__(self, module_name, services, config):
         super().__init__(module_name)
-        services.command_registry = CodeRegistry('rpc_message', services.web, services.types)
+        services.command_registry = CodeRegistry('rpc_message', services.async_web, services.types)
