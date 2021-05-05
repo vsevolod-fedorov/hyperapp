57157621facdee4605e1b2323e2a6f1e1dc6289d
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-05-05 11:24:47 +0300

    Rename record_type_field -> field

--- hyperapp/client/tree_view_sample.dyn.py
+++ hyperapp/client/tree_view_sample.dyn.py
@@ -125,8 +125,8 @@ class ThisModule(ClientModule):
         sample_article_type = htypes.tree_view_sample.tree_view_sample_article_type(
             command_list=(),
             field_type_list=(
-                htypes.record_object.record_type_field('title', services.mosaic.put(StringObject.type)),
-                htypes.record_object.record_type_field('text', services.mosaic.put(StringObject.type)),
+                htypes.record_object.field('title', services.mosaic.put(StringObject.type)),
+                htypes.record_object.field('text', services.mosaic.put(StringObject.type)),
                 ),
             )
         sample_tree_type = htypes.tree_view_sample.tree_view_sample_object_type(
