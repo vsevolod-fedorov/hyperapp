d587a48083a041abced055044a8d65fceaee1276
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-05-05 10:12:55 +0300

    Server identity: store to a file

--- hyperapp/server/identity.dyn.py
+++ hyperapp/server/identity.dyn.py
@@ -1,9 +1,25 @@
+import logging
+from pathlib import Path
+
 from hyperapp.common.module import Module
+
+log = logging.getLogger(__name__)
 
 
 class ThisModule(Module):
 
     def __init__(self, module_name, services, config):
         super().__init__(module_name)
-        # todo: store&load to/from file, remove fast=True.
-        services.server_identity = services.generate_rsa_identity(fast=True)
+        services.server_identity = self._produce_identity(services)
+
+    def _produce_identity(self, services):
+        bundle = services.file_bundle(Path.home() / '.local/share/hyperapp/server/identity.json')
+        try:
+            identity = services.identity_registry.animate(bundle.load_piece())
+            log.info("Server identity: loaded from %s", bundle.path)
+            return identity
+        except FileNotFoundError:
+            identity = services.generate_rsa_identity()
+            bundle.save_piece(identity.piece)
+            log.info("Server identity: generated new; saved to %s", bundle.path)
+            return identity
