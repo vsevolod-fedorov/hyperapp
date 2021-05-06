2fca8ac45d89a890d18931aaaf922006b55a62f8
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-05-06 10:33:00 +0300

    Add record service: return it from servant

---
+++ hyperapp/common/record.dyn.py
@@ -0,0 +1,20 @@
+from hyperapp.common.htypes import (
+    field_mt,
+    record_mt,
+    name_wrapped_mt,
+    )
+
+
+def make_record_ref(mosaic, record_field_list):
+    field_list = [
+        field_mt(field.id, field.type_ref)
+        for field in record_field_list
+        ]
+    record = record_mt(None, field_list)
+    record_ref = mosaic.put(record)
+    return mosaic.put(name_wrapped_mt('record_service_record', record_ref))
+
+
+def record_t(mosaic, types, record_field_list):
+    record_ref = make_record_ref(mosaic, record_field_list)
+    return types.resolve(record_ref)
