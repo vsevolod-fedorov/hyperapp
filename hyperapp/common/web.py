ec89122030c43c44df7fcda907b939e3af6d8470
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-05-06 11:48:27 +0300

    Cleanup: web: remove ref_repf usages

--- hyperapp/common/web.py
+++ hyperapp/common/web.py
@@ -1,14 +1,12 @@
 import logging
 
-from .ref import ref_repr
-
 _log = logging.getLogger(__name__)
 
 
 class RefResolveFailure(Exception):
 
     def __init__(self, ref):
-        super().__init__(f"Failed to resolve ref {ref_repr(ref)}")
+        super().__init__(f"Failed to resolve ref {ref}")
 
 
 class Web(object):
@@ -20,10 +18,10 @@ class Web(object):
         self._sources.append(source)
 
     def pull(self, ref):
-        _log.debug('Resolving ref: %s', ref_repr(ref))
+        _log.debug('Resolving ref: %s', ref)
         for source in self._sources:
             capsule = source.pull(ref)
             if capsule:
-                _log.debug('Ref %s is resolved to capsule, type %s', ref_repr(ref), ref_repr(capsule.type_ref))
+                _log.debug('Ref %s is resolved to capsule, type %s', ref, capsule.type_ref)
                 return capsule
         raise RefResolveFailure(ref)
