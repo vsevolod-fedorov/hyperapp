9d79f4ef9a51c170a9345fdbbd737dbbd19742d6
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-05-07 10:41:25 +0300

    Move service types to type module

--- hyperapp/client/record_service.dyn.py
+++ hyperapp/client/record_service.dyn.py
@@ -1,6 +1,6 @@
-from hyperapp.common.htypes import record_service_t
 from hyperapp.common.module import Module
 
+from . import htypes
 from .record_service import RecordService
 
 
@@ -8,6 +8,6 @@ class ThisModule(Module):
 
     async def async_init(self, services):
         services.object_registry.register_actor(
-            record_service_t, RecordService.from_piece,
+            htypes.service.record_service, RecordService.from_piece,
             services.client_identity, services.mosaic, services.object_animator, services.command_registry,
             services.client_rpc_endpoint, services.async_rpc_proxy)
