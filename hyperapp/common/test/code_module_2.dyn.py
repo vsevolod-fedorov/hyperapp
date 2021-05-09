8d9f84d7c548c5b7289b864e9e70b411f36d5f0c
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-05-09 11:44:26 +0300

    Update type and code module tests: remove aliases

--- hyperapp/common/test/code_module_2.dyn.py
+++ hyperapp/common/test/code_module_2.dyn.py
@@ -4,7 +4,7 @@ from . import htypes
 from .code_module_1 import module_1_var
 from . import code_module_1
 
-print('type_module_2.some_bool_list_opt =', htypes.type_module_2.some_bool_list_opt)
+print('type_module_2.some_bool_list_opt =', htypes.type_module_2.record_3)
 print('code_module_1 =', code_module_1)
 print('module_1_var =', module_1_var)
 
