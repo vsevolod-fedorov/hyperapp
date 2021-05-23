5bc449cf485fc86f03d54816bb489c0f82cda8ae
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-05-23 12:31:57 +0300

    Code module loader: load all modules from a directory

--- hyperapp/common/test/test_code_module.py
+++ hyperapp/common/test/test_code_module.py
@@ -3,7 +3,7 @@ import pytest
 
 from hyperapp.common.code_module import code_module_t
 from hyperapp.common.type_module_loader import TypeModuleLoader
-from hyperapp.common.code_module import LocalCodeModuleRegistry, register_code_module_types
+from hyperapp.common.code_module import register_code_module_types
 from hyperapp.common.code_module_loader import CodeModuleLoader
 from hyperapp.common.code_module_importer import CodeModuleImporter
 from hyperapp.common import cdr_coders  # register codec
@@ -13,12 +13,7 @@ from hyperapp.common.visual_rep import pprint
 pytest_plugins = ['hyperapp.common.htypes.test.fixtures']
 
 
-TEST_MODULES_DIR = Path(__file__).parent.resolve()
-
-
-@pytest.fixture
-def local_code_module_registry():
-    return LocalCodeModuleRegistry()
+TEST_DIR = Path(__file__).parent.resolve()
 
 
 @pytest.fixture
@@ -32,16 +27,22 @@ def local_type_module_registry(type_module_loader):
 
 
 @pytest.fixture
-def code_module_loader(mosaic, local_type_module_registry, local_code_module_registry):
-    return CodeModuleLoader(mosaic, local_type_module_registry, local_code_module_registry)
+def code_module_loader(mosaic, local_type_module_registry):
+    return CodeModuleLoader(mosaic, local_type_module_registry)
 
 
-def test_code_module_load(type_module_loader, code_module_loader):
-    type_module_loader.load_type_modules(TEST_MODULES_DIR / 'test_type_modules')
+def test_code_module_load(mosaic, type_module_loader, code_module_loader):
+    type_module_loader.load_type_modules(TEST_DIR / 'test_type_modules')
 
-    code_module = code_module_loader.load_code_module(TEST_MODULES_DIR / 'code_module_1')
-    assert isinstance(code_module, code_module_t)
-    pprint(code_module, title='Loaded code module')
+    registry = code_module_loader.load_code_modules(TEST_DIR / 'test_code_modules')
+    for module_name, code_module_ref in registry.items():
+        code_module = mosaic.resolve_ref(code_module_ref).value
+        assert isinstance(code_module, code_module_t)
+        pprint(code_module, title=f"Loaded code module: {module_name!r}")
+    assert set(registry) == {
+        'subdir.code_module_1',
+        'code_module_2',
+        }
 
 
 @pytest.fixture
@@ -52,16 +53,16 @@ def code_module_importer(web, mosaic, types):
 
 
 def test_code_module_import(mosaic, type_module_loader, code_module_loader, code_module_importer):
-    type_module_loader.load_type_modules(TEST_MODULES_DIR / 'test_type_modules')
-    code_module = code_module_loader.load_code_module(TEST_MODULES_DIR / 'code_module_1')
+    type_module_loader.load_type_modules(TEST_DIR / 'test_type_modules')
+    code_module = code_module_loader.load_code_module(TEST_DIR / 'code_module_1')
     code_module_ref = mosaic.put(code_module)
     code_module_importer.import_code_module(code_module_ref)
 
 
 def test_code_module_import_from_code_module(mosaic, type_module_loader, code_module_loader, code_module_importer):
-    type_module_loader.load_type_modules(TEST_MODULES_DIR / 'test_type_modules')
-    code_module_1 = code_module_loader.load_code_module(TEST_MODULES_DIR / 'code_module_1')
-    code_module_2 = code_module_loader.load_code_module(TEST_MODULES_DIR / 'code_module_2')
+    type_module_loader.load_type_modules(TEST_DIR / 'test_type_modules')
+    code_module_1 = code_module_loader.load_code_module(TEST_DIR / 'code_module_1')
+    code_module_2 = code_module_loader.load_code_module(TEST_DIR / 'code_module_2')
     code_module_1_ref = mosaic.put(code_module_1)
     code_module_2_ref = mosaic.put(code_module_2)
 
