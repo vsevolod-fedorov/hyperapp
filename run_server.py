2038e7f0161b2786d1134d02311a7665cf4914bc
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-05-30 10:13:04 +0300

    Add some code requires; update server

--- run_server.py
+++ run_server.py
@@ -12,30 +12,9 @@ log = logging.getLogger(__name__)
 
 
 code_module_list = [
-    'common.dict_coders',
-    'common.visitor',
-    'common.ref_collector',
-    'common.unbundler',
-    'common.file_bundle',
-    'common.local_server',
-    'common.list',
-    'common.tree',
-    'common.record',
-    'transport.identity',
+    'common.dict_coders',  # Load json coders.
     'transport.rsa_identity',
-    'transport.route_table',
-    'transport.tcp',
-    'sync.failure',
-    'sync.transport.route_table',
-    'sync.transport.transport',
-    'sync.transport.endpoint',
-    'sync.transport.tcp',
-    'sync.rpc.rpc_proxy',
-    'sync.rpc.rpc_endpoint',
-    'server.identity',
     'server.tcp_server',
-    'server.rpc_endpoint',
-    'server.server_ref_list',
     'server.sample_list',
     'server.sample_tree',
     ]
