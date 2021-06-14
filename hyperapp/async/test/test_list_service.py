8525869d24067c4213fbe5f7d4ba74b61d22567b
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-06-14 10:06:23 +0300

    Update test_list_service.py; Add some require/provide

--- hyperapp/async/test/test_list_service.py
+++ hyperapp/async/test/test_list_service.py
@@ -1,5 +1,6 @@
 import logging
 import threading
+from pathlib import Path
 
 import pytest
 
@@ -20,24 +21,15 @@ pytest_plugins = ['hyperapp.common.test.services']
 @pytest.fixture
 def code_module_list():
     return [
-        'common.visitor',
         'common.ref_collector',
-        'common.unbundler',
         'common.list',
-        'transport.identity',
         'transport.rsa_identity',
-        'transport.route_table',
-        'transport.tcp',
-        'sync.work_dir',
-        'sync.failure',
         'sync.transport.route_table',
-        'sync.transport.transport',
         'sync.transport.endpoint',
-        'sync.subprocess_connection',
-        'sync.subprocess',
         'sync.transport.tcp',
         'sync.rpc.rpc_proxy',
         'sync.rpc.rpc_endpoint',
+        'sync.subprocess',
         ]
 
 
@@ -101,50 +93,15 @@ def test_list_service(services, htypes, code):
 
     subprocess = services.subprocess(
         'subprocess',
+        additional_code_module_dirs=[Path(__file__).parent],
         code_module_list=[
-            'common.visitor',
-            'common.ref_collector',
-            'common.unbundler',
-            'common.weak_key_dictionary_with_callback',
-            'common.list',
-            'common.resource_key',
-            'transport.identity',
-            'transport.rsa_identity',
-            'transport.route_table',
-            'transport.tcp',
-            'sync.failure',
-            'sync.transport.route_table',
-            'sync.transport.transport',
-            'sync.transport.tcp',  # Provide tcp route for sync route table
-            'sync.transport.endpoint',
-            'sync.subprocess_connection',
-            'sync.subprocess_child',
             'async.event_loop',
             'async.async_main',
-            'async.ui.commander',
-            'async.ui.module',
-            'async.async_web',
-            'async.async_registry',
-            'async.code_registry',
-            'async.transport.route_table',
-            'async.transport.transport',
-            'async.transport.endpoint',
             'async.transport.tcp',
-            'async.rpc.rpc_proxy',
-            'async.rpc.rpc_endpoint',
-            'async.ui.object',
-            'async.ui.record_object',
-            'async.ui.object_registry',
-            'async.ui.object',
-            'async.ui.command_registry',
-            'async.ui.column',
-            'async.ui.list_object',
-            'async.ui.simple_list_object',
-            'async.ui.list_service',
-            'async.test.list_service_client',
+            'list_service_client',
             ],
         config = {
-            'async.test.list_service_client': {'list_service_bundle_cdr': list_service_bundle_cdr},
+            'list_service_client': {'list_service_bundle_cdr': list_service_bundle_cdr},
             },
         )
     with subprocess:
