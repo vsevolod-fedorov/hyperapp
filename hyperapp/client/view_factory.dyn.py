c685d0e1e6250efbcf9f4887a8b6cb2632817594
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-06-20 12:28:02 +0300

    Start views based on lcs - layered configuration sheet

---
+++ hyperapp/client/view_factory.dyn.py
@@ -0,0 +1,19 @@
+from .module import ClientModule
+
+
+class ViewFactory:
+
+    def __init__(self, lcs, view_registry):
+        self._lcs = lcs
+        self._view_registry = view_registry
+
+    async def create_view(self, object):
+        piece = self._lcs.resolve(object.dir_list)
+        return await self._view_registry.animate(piece, object)
+
+
+class ThisModule(ClientModule):
+
+    def __init__(self, module_name, services, config):
+        super().__init__(module_name, services, config)
+        services.view_factory = ViewFactory(services.lcs, services.view_registry)
