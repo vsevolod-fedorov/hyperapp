025b5e921087ffe14f90c013569c50cb3b3557d4
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-06-25 11:02:39 +0300

    LCSheet: add get_first, remove get_opt

--- hyperapp/common/layered_config_sheet.dyn.py
+++ hyperapp/common/layered_config_sheet.dyn.py
@@ -24,20 +24,20 @@ class LCSheet:
         for dir in dir_list:
             yield from self._dir_to_piece[tuple(dir)]
 
-    def get(self, dir_list):
-        for dir in reversed(dir_list):
-            piece_list = self._dir_to_piece[tuple(dir)]
-            if len(piece_list) > 1:
-                raise RuntimeError(f"More than one value is registered for {dir}")
-            if piece_list:
-                return piece_list[0]
-        raise KeyError(f"No dir among {dir_list} is registered at LCS")
+    def get(self, dir):
+        piece_list = self._dir_to_piece[tuple(dir)]
+        if len(piece_list) > 1:
+            raise RuntimeError(f"More than one value is registered for {dir}")
+        if piece_list:
+            return piece_list[0]
+        return None
 
-    def get_opt(self, dir_list):
-        try:
-            return self.get(dir_list)
-        except KeyError:
-            return None
+    def get_first(self, dir_list):
+        for dir in reversed(dir_list):
+            value = self.get(dir)
+            if value is not None:
+                return value
+        raise KeyError(f"No dir among {dir_list} is registered at LCS")
 
 
 class ThisModule(ClientModule):
