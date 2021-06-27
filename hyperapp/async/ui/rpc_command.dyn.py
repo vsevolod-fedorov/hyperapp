5b0e9f10fb7674fbbf7e1b78f226412252c115dc
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-06-27 10:49:00 +0300

    Update RpcElementCommand: remove 'id', update 'run' method arguments

--- hyperapp/async/ui/rpc_command.dyn.py
+++ hyperapp/async/ui/rpc_command.dyn.py
@@ -26,7 +26,7 @@ def element_command_interface_ref(mosaic, types, key_type_ref, method_name):
 class RpcElementCommand:
 
     @classmethod
-    async def from_piece(cls, piece, command_id, mosaic, types, web, rpc_endpoint, async_rpc_proxy, identity):
+    async def from_piece(cls, piece, mosaic, types, web, rpc_endpoint, async_rpc_proxy, identity):
         interface_ref = element_command_interface_ref(mosaic, types, piece.key_type_ref, piece.method_name)
         service = htypes.rpc.endpoint(
             peer_ref=piece.peer_ref,
@@ -34,30 +34,23 @@ class RpcElementCommand:
             object_id=piece.object_id,
             )
         proxy = async_rpc_proxy(identity, rpc_endpoint, service)
-        return cls(web, command_id, piece.key_type_ref, piece.method_name, piece.peer_ref, piece.object_id, proxy)
+        return cls(web, piece.key_type_ref, piece.method_name, piece.peer_ref, piece.object_id, proxy)
 
-    def __init__(self, web, id, key_type_ref, method_name, peer_ref, object_id, proxy):
+    def __init__(self, web, key_type_ref, method_name, peer_ref, object_id, proxy):
         self._web = web
-        self.id = id
         self._key_type_ref = key_type_ref
         self._method_name = method_name
         self._peer_ref = peer_ref
         self._object_id = object_id
         self._proxy = proxy
 
-    def is_enabled(self):
-        return True
+    @property
+    def name(self):
+        return self._method_name
 
     @property
     def kind(self):
-        return 'element'
-
-    @property
-    def resource_key(self):
-        cls = self.__class__
-        class_name = cls.__name__
-        module_name = cls.__module__
-        return module_resource_key(module_name, [class_name, 'command', self.id])
+        return 'object'
 
     @property
     def piece(self):
@@ -68,9 +61,9 @@ class RpcElementCommand:
             object_id=self._object_id,
             )
 
-    async def run(self, item_key):
+    async def run(self, object, view_state):
         method = getattr(self._proxy, self._method_name)
-        response = await method(item_key)
+        response = await method(view_state.current_key)
         return await self._web.summon_opt(response.piece_ref)
 
 
