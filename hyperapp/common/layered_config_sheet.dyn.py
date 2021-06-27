552503114b5058a0814d9043017c99d026ba8310
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-06-27 09:37:48 +0300

    Extract (split) plcs - persistent layered configuration sheet

--- hyperapp/common/layered_config_sheet.dyn.py
+++ hyperapp/common/layered_config_sheet.dyn.py
@@ -13,41 +13,24 @@ lcs_path = Path('~/.local/share/hyperapp/client/lcs.cdr').expanduser()
 
 class LCSheet:
 
-    def __init__(self, mosaic, web, bundle):
+    def __init__(self, name, mosaic):
+        self._name = name
         self._mosaic = mosaic
-        self._bundle = bundle
         self._dir_to_value_list = defaultdict(list)
-        try:
-            storage = bundle.load_piece()
-        except FileNotFoundError:
-            pass
-        else:
-            for rec in storage.record_list:
-                value = web.summon(rec.value_ref)
-                log.info("LCS: loaded %s -> %s", rec.dir, value)
-                self._add(rec.dir, value)
 
     def _save(self):
-        rec_list = []
-        for dir, value_list in self._dir_to_value_list.items():
-            for value in value_list:
-                rec = htypes.layered_config_sheet.lcs_record(
-                    dir=dir,
-                    value_ref=self._mosaic.put(value),
-                    )
-                rec_list.append(rec)
-        self._bundle.save_piece(htypes.layered_config_sheet.lcs_storage(rec_list))
+        pass
 
     def _add(self, dir, piece):
         self._dir_to_value_list[tuple(dir)].append(piece)
 
     def add(self, dir, piece):
-        log.info("LCS: add %s -> %s", dir, piece)
+        log.info("%s: add %s -> %s", self._name, dir, piece)
         self._add(dir, piece)
         self._save()
 
     def set(self, dir, piece):
-        log.info("LCS: set %s -> %s", dir, piece)
+        log.info("%s: set %s -> %s", self._name, dir, piece)
         self._dir_to_value_list[tuple(dir)] = [piece]
         self._save()
 
@@ -71,9 +54,41 @@ class LCSheet:
         raise KeyError(f"No dir among {dir_list} is registered at LCS")
 
 
+class PersistentLCSheet(LCSheet):
+
+    def __init__(self, name, mosaic, web, bundle):
+        super().__init__(name, mosaic)
+        self._web = web
+        self._bundle = bundle
+        self._load()
+
+    def _load(self):
+        try:
+            storage = self._bundle.load_piece()
+        except FileNotFoundError:
+            pass
+        else:
+            for rec in storage.record_list:
+                value = self._web.summon(rec.value_ref)
+                log.info("%s: loaded %s -> %s", self._name, rec.dir, value)
+                self._add(rec.dir, value)
+
+    def _save(self):
+        rec_list = []
+        for dir, value_list in self._dir_to_value_list.items():
+            for value in value_list:
+                rec = htypes.layered_config_sheet.lcs_record(
+                    dir=dir,
+                    value_ref=self._mosaic.put(value),
+                    )
+                rec_list.append(rec)
+        self._bundle.save_piece(htypes.layered_config_sheet.lcs_storage(rec_list))
+
+
 class ThisModule(ClientModule):
 
     def __init__(self, module_name, services, config):
         super().__init__(module_name, services, config)
         bundle = services.file_bundle(lcs_path, encoding='cdr')
-        services.lcs = LCSheet(services.mosaic, services.web, bundle)
+        services.plcs = PersistentLCSheet('PLCS', services.mosaic, services.web, bundle)
+        services.lcs = LCSheet('LCS', services.mosaic)
