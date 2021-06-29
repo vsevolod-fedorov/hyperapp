5755542b7339318069975fa958af27f455a64525
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-06-29 10:14:49 +0300

    Merge plcs and lcs back; add 'save' parameter

--- hyperapp/async/ui/command_list.dyn.py
+++ hyperapp/async/ui/command_list.dyn.py
@@ -23,10 +23,10 @@ ViewItem = namedtuple('Item', 'name path shortcut')
 
 class CommandList(SimpleListObject):
 
-    def __init__(self, mosaic, plcs):
+    def __init__(self, mosaic, lcs):
         super().__init__()
         self._mosaic = mosaic
-        self._plcs = plcs
+        self._lcs = lcs
         self._command_by_name = None
         self._command_shortcut_d_ref = mosaic.put(htypes.command.command_shortcut_d())
 
@@ -52,7 +52,7 @@ class CommandList(SimpleListObject):
 
     def _command_shortcut(self, command):
         command_ref = self._mosaic.put(command.piece)
-        return self._plcs.get([command_ref, self._command_shortcut_d_ref])
+        return self._lcs.get([command_ref, self._command_shortcut_d_ref])
 
     @command
     async def set_key(self, current_key):
@@ -68,7 +68,7 @@ class CommandList(SimpleListObject):
         log.info("Set shortcut for command %s: %r", command_name, shortcut)
         command = self._command_by_name[command_name]
         command_ref = self._mosaic.put(command.piece)
-        self._plcs.set([command_ref, self._command_shortcut_d_ref], shortcut)
+        self._lcs.set([command_ref, self._command_shortcut_d_ref], shortcut, save=True)
         await self.update()
 
     @command
@@ -87,15 +87,15 @@ class CommandList(SimpleListObject):
 class ObjectCommandList(CommandList):
 
     @classmethod
-    async def from_piece(cls, piece, mosaic, async_web, plcs, object_animator, object_commands_factory):
+    async def from_piece(cls, piece, mosaic, async_web, lcs, object_animator, object_commands_factory):
         object = await object_animator.invite(piece.piece_ref)
         view_state = await async_web.summon(piece.view_state_ref)
-        self = cls(mosaic, plcs, object, view_state)
+        self = cls(mosaic, lcs, object, view_state)
         await self._async_init(object_commands_factory)
         return self
 
-    def __init__(self, mosaic, plcs, object, view_state):
-        super().__init__(mosaic, plcs)
+    def __init__(self, mosaic, lcs, object, view_state):
+        super().__init__(mosaic, lcs)
         self._object = object
         self._view_state = view_state
 
@@ -123,11 +123,11 @@ class ObjectCommandList(CommandList):
 class GlobalCommandList(CommandList):
 
     @classmethod
-    async def from_piece(cls, piece, mosaic, async_web, plcs, global_command_list, layout_manager):
-        return cls(mosaic, plcs, global_command_list, layout_manager)
+    async def from_piece(cls, piece, mosaic, async_web, lcs, global_command_list, layout_manager):
+        return cls(mosaic, lcs, global_command_list, layout_manager)
 
-    def __init__(self, mosaic, plcs, global_command_list, layout_manager):
-        super().__init__(mosaic, plcs)
+    def __init__(self, mosaic, lcs, global_command_list, layout_manager):
+        super().__init__(mosaic, lcs)
         self._layout_manager = layout_manager
         self._command_by_name = {
             command.name: command
@@ -150,15 +150,15 @@ class GlobalCommandList(CommandList):
 class ViewCommandList(CommandList):
 
     @classmethod
-    async def from_piece(cls, piece, mosaic, async_web, plcs, layout_manager):
-        self = cls(mosaic, plcs, layout_manager)
+    async def from_piece(cls, piece, mosaic, async_web, lcs, layout_manager):
+        self = cls(mosaic, lcs, layout_manager)
         # When this object is current on client start, layout_manager is not yet fully constructed.
         # Postpone it's usage until layout_manager.root_layout is set.
         asyncio.get_event_loop().call_soon(self._post_init)
         return self
 
-    def __init__(self, mosaic, plcs, layout_manager):
-        super().__init__(mosaic, plcs)
+    def __init__(self, mosaic, lcs, layout_manager):
+        super().__init__(mosaic, lcs)
         self._layout_manager = layout_manager
         self._path_by_name = None
 
@@ -212,7 +212,7 @@ class ThisModule(ClientModule):
             ObjectCommandList.from_piece,
             services.mosaic,
             services.async_web,
-            services.plcs,
+            services.lcs,
             services.object_animator,
             services.object_commands_factory,
             )
@@ -221,7 +221,7 @@ class ThisModule(ClientModule):
             GlobalCommandList.from_piece,
             services.mosaic,
             services.async_web,
-            services.plcs,
+            services.lcs,
             services.global_command_list,
             services.layout_manager,
             )
@@ -230,7 +230,7 @@ class ThisModule(ClientModule):
             ViewCommandList.from_piece,
             services.mosaic,
             services.async_web,
-            services.plcs,
+            services.lcs,
             services.layout_manager,
             )
         object_commands_d_ref = services.mosaic.put(htypes.command.object_commands_d())
