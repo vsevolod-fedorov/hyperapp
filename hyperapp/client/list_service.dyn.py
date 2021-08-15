5341c8c9502094b9906cb7aa04691db1208ac662
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-08-15 11:13:25 +0300

    Add custom dirs to service

--- hyperapp/client/list_service.dyn.py
+++ hyperapp/client/list_service.dyn.py
@@ -9,5 +9,5 @@ class ThisModule(Module):
     async def async_init(self, services):
         services.object_registry.register_actor(
             htypes.service.list_service, ListService.from_piece,
-            services.client_identity, services.mosaic, services.types, services.command_registry,
+            services.client_identity, services.mosaic, services.types, services.async_web, services.command_registry,
             services.client_rpc_endpoint, services.async_rpc_proxy)
