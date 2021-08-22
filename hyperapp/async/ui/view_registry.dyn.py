7e890e658e6675b11c1ee9ea503339da716a3668
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-08-22 10:47:16 +0300

    Cleanup: remove available_view_registry

--- hyperapp/async/ui/view_registry.dyn.py
+++ hyperapp/async/ui/view_registry.dyn.py
@@ -1,19 +1,15 @@
 import logging
-import weakref
-from collections import defaultdict, namedtuple
 
-from .async_registry import run_awaitable_factory
+from hyperapp.common.module import Module
+
 from .code_registry import CodeRegistry
-from .module import ClientModule
 
 _log = logging.getLogger(__name__)
 
 
-class ThisModule(ClientModule):
+class ThisModule(Module):
 
     def __init__(self, module_name, services, config):
         super().__init__(module_name, services, config)
-        # todo: rename view to layout
-        services.available_view_registry = {}  # id -> view ref, views available to add to layout
         services.view_registry = CodeRegistry('view', services.async_web, services.types)
         services.object_layout_registry = CodeRegistry('object_layout', services.async_web, services.types)
