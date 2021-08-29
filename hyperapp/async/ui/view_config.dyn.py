e29a75b7883707340f2dde8624d7b4a59fbcda1d
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-08-29 11:27:35 +0300

    View config: add view_dir_to_config registry; implement open command

--- hyperapp/async/ui/view_config.dyn.py
+++ hyperapp/async/ui/view_config.dyn.py
@@ -8,8 +8,15 @@ from .object_command import Command
 log = logging.getLogger(__name__)
 
 
-def open_view_config(piece):
-    assert 0, 'todo'
+def open_view_config(object, view_dir_to_config):
+    for dir in object.dir_list:
+        try:
+            config_editor_factory = view_dir_to_config[tuple(dir)]
+        except KeyError:
+            continue
+        else:
+            return config_editor_factory(object)
+    raise RuntimeError(f"No view config editor is registered for any of object {object} dirs: {object.dir_list}")
 
     
 class ThisModule(Module):
@@ -17,8 +24,10 @@ class ThisModule(Module):
     def __init__(self, module_name, services, config):
         super().__init__(module_name, services, config)
 
+        services.view_dir_to_config = {}
+
         services.command_registry.register_actor(
-            htypes.view_config.open_view_config_command, Command.from_fn(self.name, open_view_config))
+            htypes.view_config.open_view_config_command, Command.from_fn(self.name, open_view_config), services.view_dir_to_config)
         services.lcs.add(
             [htypes.object.object_d(), htypes.command.object_commands_d()],
             htypes.view_config.open_view_config_command(),
