5a50540690e3cb6e68d7a9de2efc9a9f11e3a0a1
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-08-29 13:19:53 +0300

    RecordViewConfig: create and register object

--- client.py
+++ client.py
@@ -48,6 +48,7 @@ code_module_list = [
     'async.ui.record_field_list',
     'async.ui.view_config',
     'async.ui.object_view_config',
+    'async.ui.record_view_config',
     'async.ui.available_view_list',
     'async.ui.selector',
     'async.ui.qt.selector_view',
---
+++ hyperapp/async/ui/record_view_config.dyn.py
@@ -0,0 +1,78 @@
+from hyperapp.common.module import Module
+
+from . import htypes
+from .command import command
+from .object_view_config import ObjectViewConfig
+
+
+class RecordViewConfig(ObjectViewConfig):
+
+    dir_list = [
+        *ObjectViewConfig.dir_list,
+        [htypes.record_view_config.record_view_config_d()],
+        ]
+
+    @classmethod
+    async def from_piece(cls, piece, mosaic, async_web, object_factory, view_producer):
+        object = await object_factory.invite(piece.piece_ref)
+        view_state = await async_web.summon(piece.view_state_ref)
+        origin_dir = [
+            await async_web.summon(ref)
+            for ref in piece.origin_dir
+            ]
+
+        dir, view_piece = view_producer.pick_view_piece(object, [origin_dir])
+
+        fields_pieces = {
+            'title': object.title,
+            'dir': '/'.join(str(p) for p in dir),
+            'view': str(view_piece),
+            'commands': htypes.command_list.object_command_list(piece.piece_ref, piece.view_state_ref),
+            }
+
+        self = cls(mosaic, object, view_state, origin_dir)
+        await self.async_init(object_factory, fields_pieces)
+        return self
+
+    @property
+    def title(self):
+        return f"View config for record: {self._object.title}"
+
+    @property
+    def piece(self):
+        return htypes.record_view_config.record_view_config(
+            piece_ref=self._mosaic.put(self._object.piece),
+            view_state_ref=self._mosaic.put(self._view_state),
+            origin_dir=[
+                self._mosaic.put(piece)
+                for piece in self._origin_dir
+                ],
+            )
+
+
+class ThisModule(Module):
+
+    def __init__(self, module_name, services, config):
+        super().__init__(module_name, services, config)
+
+        self._mosaic = services.mosaic
+
+        services.object_registry.register_actor(
+            htypes.record_view_config.record_view_config,
+            RecordViewConfig.from_piece,
+            services.mosaic,
+            services.async_web,
+            services.object_factory,
+            services.view_producer,
+            )
+        services.view_dir_to_config[(htypes.record_object.record_object_d(),)] = self.config_editor_for_record_object
+
+    async def config_editor_for_record_object(self, object, view_state, origin_dir):
+        return htypes.record_view_config.record_view_config(
+            piece_ref=self._mosaic.put(object.piece),
+            view_state_ref=self._mosaic.put(view_state),
+            origin_dir=[
+                self._mosaic.put(piece)
+                for piece in origin_dir
+                ],
+            )
