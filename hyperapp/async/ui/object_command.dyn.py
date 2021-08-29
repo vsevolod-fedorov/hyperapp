829f3d057bfb9f3a5a1fa0fe857de756ba963966
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-08-29 11:02:34 +0300

    Object Command: pass only wanted params

--- hyperapp/async/ui/object_command.dyn.py
+++ hyperapp/async/ui/object_command.dyn.py
@@ -54,20 +54,31 @@ class Command:
     def from_fn(cls, module_name, fn, name=None):
         if not name:
             name = fn.__name__
+        wanted_params = {
+            name for name
+            in inspect.signature(fn).parameters
+            if name != 'self'
+            }
 
         def from_piece(piece):
-            return cls(module_name, name, fn)
+            return cls(module_name, name, fn, wanted_params)
 
         return from_piece
 
-    def __init__(self, module_name, name, fn):
+    def __init__(self, module_name, name, fn, wanted_params):
         self._module_name = module_name
         self.name = name
         self._fn = fn
+        self._wanted_params = wanted_params
 
     @property
     def dir(self):
         return [htypes.command.context_object_command_d(self._module_name, self.name)]
 
     async def run(self, object, view_state, origin_dir):
-        return await self._fn(object, view_state, origin_dir)
+        kw = {}
+        if 'view_state' in self._wanted_params:
+            kw['view_state'] = view_state
+        if 'origin_dir' in self._wanted_params:
+            kw['origin_dir'] = origin_dir
+        return await self._fn(object, **kw)
