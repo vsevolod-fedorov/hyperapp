8d11df67ac15994fcec9a85fea0ad749ade11d70
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-09-01 12:04:01 +0300

    Construct windows only after all modules are registered

--- hyperapp/async/ui/application_state.dyn.py
+++ hyperapp/async/ui/application_state.dyn.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 
 from hyperapp.common.module import Module
@@ -9,30 +10,24 @@ class ThisModule(Module):
 
     def __init__(self, module_name, services, config):
         super().__init__(module_name, services, config)
-        self._mosaic = services.mosaic
-        self._stop_event = services.async_stop_event
-        self._async_web = services.async_web
-        self._default_state_builder = services.default_state_builder
         self._state_storage = services.application_state_storage
-        self._view_registry = services.view_registry
         self._root_view = None
 
     async def async_init(self, services):
-        self._root_view = await self._load_state()
-        services.root_view = self._root_view
-
-    async def async_stop(self):
-        self._save_state()
-
-    async def _load_state(self):
         log.info("Load application state.")
         state = self._state_storage.load_state()
         if state is None:
-            state = self._default_state_builder()
-        return await self._view_registry.animate(state)
+            state = services.default_state_builder()
+        self._root_view = await services.view_registry.animate(state)
+        services.root_view = self._root_view
+        # Construct windows only after all modules are (async) inited - registered all their actors.
+        asyncio.get_event_loop().create_task(self._open(state))
 
-    def _save_state(self):
-        log.info("Save application state.")
+    async def _open(self, state):
+        await self._root_view.open(state)
+
+    async def async_stop(self):
         if self._root_view:  # Services init failed before layout constructed?
+            log.info("Save application state.")
             state = self._root_view.state
             self._state_storage.save_state(state)
