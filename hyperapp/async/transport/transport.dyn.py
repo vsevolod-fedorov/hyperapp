47d2b441d1192bfc9ee407eabb8b304ef0e87ffc
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-09-06 10:14:06 +0300

    Async transport: remove explicit ref_repr calls

--- hyperapp/async/transport/transport.dyn.py
+++ hyperapp/async/transport/transport.dyn.py
@@ -1,6 +1,5 @@
 import logging
 
-from hyperapp.common.ref import ref_repr
 from hyperapp.common.module import Module
 
 log = logging.getLogger(__name__)
@@ -18,13 +17,13 @@ class Transport:
         receiver_peer_ref = self._mosaic.put(parcel.receiver.piece)
         route_list = self._route_table.peer_route_list(receiver_peer_ref)
         if not route_list:
-            raise RuntimeError(f"No route for peer {ref_repr(receiver_peer_ref)}")
+            raise RuntimeError(f"No route for peer {receiver_peer_ref}")
         route, *_ = [route for route in route_list if route.available]
         log.info("Send parcel %s by route %s", parcel, route)
         await route.send(parcel)
 
     async def send(self, receiver, sender_identity, ref_list):
-        log.info("Send ref list %s to %s from %s", [ref_repr(ref) for ref in ref_list], receiver, sender_identity)
+        log.info("Send ref list %s to %s from %s", ref_list, receiver, sender_identity)
         bundle = self._ref_collector(ref_list).bundle
         parcel = receiver.make_parcel(bundle, sender_identity)
         await self.send_parcel(parcel)
