fd52550e4985571b266ae03ecc7a82d1b47f931f
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-09-07 11:40:08 +0300

    test_sync_echo: move from interfaces to server paths

--- hyperapp/sync/rpc/test/echo_service.dyn.py
+++ hyperapp/sync/rpc/test/echo_service.dyn.py
@@ -25,32 +25,26 @@ class ThisModule(Module):
 
         master_service_bundle = packet_coders.decode('cdr', config['master_service_bundle_cdr'], bundle_t)
         services.unbundler.register_bundle(master_service_bundle)
-        master_service_ref = master_service_bundle.roots[0]
-        master_service = services.mosaic.resolve_ref(master_service_ref).value
+        master_peer_ref, *master_servant_path_refs = master_service_bundle.roots
+
+        master_peer = services.peer_registry.invite(master_peer_ref)
+        master_servant_path = services.servant_path_from_data(master_servant_path_refs)
 
         my_identity = services.generate_rsa_identity(fast=True)
         my_peer_ref = services.mosaic.put(my_identity.peer.piece)
 
-        echo_iface_ref = services.types.reverse_resolve(htypes.echo.echo_iface)
-
         rpc_endpoint = services.rpc_endpoint()
         services.endpoint_registry.register(my_identity, rpc_endpoint)
 
-        echo_object_id = 'echo'
+        echo_servant_name = 'echo'
+        echo_servant_path = services.servant_path().registry_name(echo_servant_name).get_attr('echo')
 
         servant = Echo()
-        rpc_endpoint.register_servant(echo_object_id, servant)
+        rpc_endpoint.register_servant(echo_servant_name, servant)
 
-        echo_service = htypes.rpc.endpoint(
-            peer_ref=my_peer_ref,
-            iface_ref=echo_iface_ref,
-            object_id=echo_object_id,
-            )
-        echo_service_ref = services.mosaic.put(echo_service)
+        rpc_call = services.rpc_call(rpc_endpoint, master_peer, master_servant_path, my_identity)
 
-        proxy = services.rpc_proxy(my_identity, rpc_endpoint, master_service)
-
-        self._thread = threading.Thread(target=self._run, args=[proxy, echo_service_ref])
+        self._thread = threading.Thread(target=self._run, args=[services.mosaic, rpc_call, my_peer_ref, echo_servant_path])
 
         services.on_start.append(self.start)
         services.on_stop.append(self.stop)
@@ -64,10 +58,10 @@ class ThisModule(Module):
         self._thread.join()
         log.info("echo_services thread is stopped")
 
-    def _run(self, proxy, echo_service_ref):
+    def _run(self, mosaic, rpc_call, my_peer_ref, echo_servant_path):
         log.info("echo_service thread is started")
         try:
-            proxy.run(echo_service_ref)
+            rpc_call(my_peer_ref, echo_servant_path.as_data(mosaic))
         except TimeoutWaitingForResponse as x:
             log.info("Timed out waiting for 'run' response - this is expected, because master is already shutting down subprocess: %s", x)
         except Exception as x:
