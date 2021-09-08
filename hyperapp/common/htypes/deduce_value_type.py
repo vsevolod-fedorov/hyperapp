191c4bee422910e778588e8e18b71a9d76a90af7
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-09-08 11:18:15 +0300

    deduce_complex_value_type to deduce list of non-primitive types

--- hyperapp/common/htypes/deduce_value_type.py
+++ hyperapp/common/htypes/deduce_value_type.py
@@ -1,7 +1,16 @@
 import datetime
 
-from .htypes import tNone, tString, tBinary, tInt, tBool, tDateTime, TList
+from .htypes import (
+    tNone,
+    tString,
+    tBinary,
+    tInt,
+    tBool,
+    tDateTime,
+    TList,
+    )
 from .record import TRecord
+from .meta_type import list_mt
 from .builtins import primitive_list_types
 
 
@@ -28,5 +37,24 @@ def deduce_value_type(value):
         return t
     if isinstance(value, list):
         element_t = deduce_value_type(value[0])
-        return primitive_list_types[element_t]  # element_t is expected to be a primitive, KeyError othersize
+        return primitive_list_types[element_t]  # Use deduce_complex_value_type for non-primitive-element lists.
     raise DeduceTypeError(f"Unable to deduce type for {value!r} (t: {t!r}). Use explicit type parameter.")
+
+
+def _deduce_list_type(mosaic, types, value):
+    element_t = deduce_complex_value_type(mosaic, types, value[0])
+    t = TList(element_t)
+    try:
+        _ = types.reverse_resolve(t)
+        return t
+    except:
+        element_t_ref = types.reverse_resolve(element_t)
+        piece = list_mt(element_t_ref)
+        type_ref = mosaic.put(piece)
+        return types.resolve(type_ref)
+
+
+def deduce_complex_value_type(mosaic, types, value):
+    if isinstance(value, list):
+        return _deduce_list_type(mosaic, types, value)
+    return deduce_value_type(value)
