375f065242b4f295d088b825a360586466ef8508
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-09-10 13:42:13 +0300

    Module list: show available modules too

--- hyperapp/server/module_list.dyn.py
+++ hyperapp/server/module_list.dyn.py
@@ -10,17 +10,25 @@ log = logging.getLogger(__name__)
 
 class Servant:
 
-    def __init__(self, web, imported_code_modules):
+    def __init__(self, web, available_code_modules, imported_code_modules):
         self._web = web
-        self._imported_code_modules = imported_code_modules
         self._name_to_item = {}
-        for module_name, rec in imported_code_modules.items():
-            module = web.summon(rec.module_ref)
-            self._name_to_item[module_name] = htypes.module_list.item(module_name, rec.module_ref, module.file_path)
+        for module_name, module_ref in available_code_modules.items():
+            module = web.summon(module_ref)
+            if module_name in imported_code_modules:
+                status = 'imported'
+            else:
+                status = 'available'
+            self._name_to_item[module_name] = htypes.module_list.item(
+                module_name, module_ref, module.file_path, status)
 
-    def list(self, request):
-        log.info("Servant.list()")
-        return list(self._name_to_item.values())
+    def list(self, status_filter, request):
+        log.info("Servant.list(%s)", status_filter)
+        return list(
+            item for item
+            in self._name_to_item.values()
+            if item.status in status_filter
+            )
 
     def open(self, request, current_key):
         item = self._name_to_item[current_key]
@@ -44,18 +52,22 @@ class ThisModule(Module):
             servant_path=servant_path.get_attr('open').as_data(services.mosaic),
             name='open',
             )
-        service = htypes.service.list_service(
-            peer_ref=server_peer_ref,
-            servant_path=servant_path.get_attr('list').as_data(services.mosaic),
-            dir_list=[[mosaic.put(htypes.module_list.module_list_d())]],
-            command_ref_list=[
-                mosaic.put(open_command),
-                ],
-            key_column_id='module_name',
-            column_list=item_t_to_column_list(services.types, htypes.module_list.item),
-            )
 
-        servant = Servant(services.web, services.imported_code_modules)
+        def service(status_filter):
+            return htypes.service.list_service(
+                peer_ref=server_peer_ref,
+                servant_path=servant_path.get_attr('list').partial(status_filter).as_data(services.mosaic),
+                dir_list=[[mosaic.put(htypes.module_list.module_list_d())]],
+                command_ref_list=[
+                    mosaic.put(open_command),
+                    ],
+                key_column_id='module_name',
+                column_list=item_t_to_column_list(services.types, htypes.module_list.item),
+                )
+
+        servant = Servant(services.web, services.available_code_modules, services.imported_code_modules)
         services.server_rpc_endpoint.register_servant(servant_name, servant)
 
-        services.server_ref_list.add_ref('module_list', 'Module list', mosaic.put(service))
+        services.server_ref_list.add_ref('all_module_list', 'Module list', mosaic.put(service(['imported', 'available'])))
+        services.server_ref_list.add_ref('imported_module_list', 'Imported modules', mosaic.put(service(['imported'])))
+        services.server_ref_list.add_ref('available_module_list', 'Available modules', mosaic.put(service(['available'])))
