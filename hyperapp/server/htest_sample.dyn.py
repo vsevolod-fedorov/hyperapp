0a33213f7cedf32f56049748837a4ef7a24477bf
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-09-25 13:12:57 +0300

    Htest: store test list

--- hyperapp/server/htest_sample.dyn.py
+++ hyperapp/server/htest_sample.dyn.py
@@ -2,4 +2,17 @@ import logging
 
 log = logging.getLogger(__name__)
 
+
+def test_one():
+    pass
+
+
+def test_two():
+    pass
+
+
+def test_three():
+    pass
+
+
 log.info("HTest_sample module is loaded")
