7086ea253cd5e5be7b3a2a3d187993c868a3a997
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-09-25 10:14:13 +0300

    Subprocess: log name of received event

--- hyperapp/sync/subprocess.dyn.py
+++ hyperapp/sync/subprocess.dyn.py
@@ -113,7 +113,7 @@ class ThisModule(Module):
             log.exception("Subprocess is exited")
             self._subprocess_is_stopped_now(process, connection)
             return
-        log.info("Subprocess recv thread: received %r from %r: %s", event, process.name, payload)
+        log.info("Subprocess recv thread: received %r from %r: %s", ConnectionEvent(event), process.name, payload)
         if event != ConnectionEvent.PARCEL.value:
             self._process_stop_event(process, connection, event, payload)
             return
