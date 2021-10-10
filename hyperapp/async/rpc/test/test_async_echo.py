69eb36ba60576139c8468c4491aced85f086c4d1
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-10-10 11:02:13 +0300

    Rpc: Error handling - tests

--- hyperapp/async/rpc/test/test_async_echo.py
+++ hyperapp/async/rpc/test/test_async_echo.py
@@ -1,9 +1,11 @@
 import queue
 import logging
+from contextlib import contextmanager
 from pathlib import Path
 
 import pytest
 
+from hyperapp.common.htypes import HException
 from hyperapp.common.htypes.packet_coders import packet_coders
 from hyperapp.common import cdr_coders  # self-registering
 
@@ -29,23 +31,19 @@ def code_module_list():
 
 class Servant:
 
-    def __init__(self, echo_response_queue, peer_registry, servant_path_from_data, rpc_call_factory, rpc_endpoint, master_identity):
-        self._echo_response_queue = echo_response_queue
+    def __init__(self, echo_servant_queue, peer_registry, servant_path_from_data):
+        self._echo_servant_queue = echo_servant_queue
         self._peer_registry = peer_registry
         self._servant_path_from_data = servant_path_from_data
-        self._rpc_call_factory = rpc_call_factory
-        self._rpc_endpoint = rpc_endpoint
-        self._master_identity = master_identity
 
     def run(self, request, echo_peer_ref, echo_servant_path_refs):
         echo_peer = self._peer_registry.invite(echo_peer_ref)
         echo_servant_path = self._servant_path_from_data(echo_servant_path_refs)
-        rpc_call = self._rpc_call_factory(self._rpc_endpoint, echo_peer, echo_servant_path, self._master_identity)
-        result = rpc_call('Hello')
-        self._echo_response_queue.put(result)
+        self._echo_servant_queue.put((echo_peer, echo_servant_path))
 
 
-def test_async_echo(services, htypes):
+@pytest.fixture
+def echo_set_up(services, htypes):
     master_identity = services.generate_rsa_identity(fast=True)
     master_peer_ref = services.mosaic.put(master_identity.peer.piece)
 
@@ -55,22 +53,22 @@ def test_async_echo(services, htypes):
     rpc_endpoint = services.rpc_endpoint()
     services.endpoint_registry.register(master_identity, rpc_endpoint)
 
-    echo_response_queue = queue.Queue()
-    servant = Servant(
-        echo_response_queue, services.peer_registry, services.servant_path_from_data, services.rpc_call,
-        rpc_endpoint, master_identity)
+    echo_servant_queue = queue.Queue()
+    servant = Servant(echo_servant_queue, services.peer_registry, services.servant_path_from_data)
     rpc_endpoint.register_servant(servant_name, servant)
 
     server = services.tcp_server()
     log.info("Tcp route: %r", server.route)
     services.route_table.add_route(master_peer_ref, server.route)
 
+    rpc_call_factory = services.rpc_call
+
     master_service_bundle = services.ref_collector([master_peer_ref, *servant_path.as_data]).bundle
     master_service_bundle_cdr = packet_coders.encode('cdr', master_service_bundle)
 
     subprocess = services.subprocess(
         'subprocess',
-        additional_code_module_dirs=[Path(__file__).parent],
+        additional_module_dirs=[Path(__file__).parent],
         code_module_list=[
             'async.event_loop',
             'async.async_main',
@@ -82,9 +80,38 @@ def test_async_echo(services, htypes):
             'echo_service': {'master_service_bundle_cdr': master_service_bundle_cdr},
             },
         )
-    with subprocess:
-        log.info("Waiting for echo response.")
-        result = echo_response_queue.get(timeout=20)
-        log.info("Got echo result: %s.", result)
+
+    @contextmanager
+    def make_rpc_call(method_name):
+        with subprocess:
+            log.info("Waiting for echo response.")
+            echo_peer, echo_servant_path = echo_servant_queue.get(timeout=20)
+            log.info("Got echo servant: %s %s.", echo_peer, echo_servant_path)
+            rpc_call = rpc_call_factory(rpc_endpoint, echo_peer, echo_servant_path.get_attr(method_name), master_identity)
+            yield rpc_call
+        log.info("Subprocess is finished.")
+
+    return make_rpc_call
+
+
+def test_successful_call(echo_set_up):
+    with echo_set_up('echo') as rpc_call:
+        result = rpc_call('Hello')
+        log.info("Got echo result: %s", result)
         assert result == 'Hello to you too'
-    log.info("Subprocess is finished.")
+
+
+def test_unexpected_error(htypes, echo_set_up):
+    with echo_set_up('raise_unexpected_error') as rpc_call:
+        with pytest.raises(HException) as excinfo:
+            result = rpc_call()
+        log.info("Got exception: %s", excinfo)
+        assert str(excinfo.value) == "internal_error(message='Some unexpected error')"
+
+
+def test_test_error(htypes, echo_set_up):
+    with echo_set_up('raise_test_error') as rpc_call:
+        with pytest.raises(HException) as excinfo:
+            result = rpc_call()
+        log.info("Got exception: %s", excinfo)
+        assert str(excinfo.value) == "test_error(message='Some error')"
