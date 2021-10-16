ddd176ce00e52522ca695400d54fc0b14a72de47
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-10-16 11:07:33 +0300

    Rename module htest_list -> htest_module_list

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ code_module_list = [
     'server.sample_live_list',
     'server.sample_tree',
     'server.module_list',
-    'server.htest_list',
+    'server.htest_module_list',
     ]
 
 config = {
