b506df8c801a00a2899556d4e618aedc9a422b86
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-10-24 12:34:22 +0300

    HTest global list

--- hyperapp/server/htest_global_list.dyn.py
+++ hyperapp/server/htest_global_list.dyn.py
@@ -9,7 +9,7 @@ from .item_column_list import item_t_to_column_list
 log = logging.getLogger(__name__)
 
 
-class TestListServant:
+class GlobalListServant:
 
     def __init__(self, mosaic, htest_list, module_name):
         self._mosaic = mosaic
@@ -17,15 +17,12 @@ class TestListServant:
         self._module_name = module_name
 
     def list(self, request):
-        log.info("HTest_list.list")
+        log.info("HTest_global_list.list")
         module = self._htest_list.dict[self._module_name]
-        return [
-            htypes.htest.test(name) for name
-            in module.test_list
-            ]
+        return module.global_list
 
     def run(self, request, current_key):
-        log.info("Run test: %s/%s", self._module_name, current_key)
+        log.info("Run global: %s/%s", self._module_name, current_key)
 
 
 class ThisModule(Module):
@@ -34,20 +31,20 @@ class ThisModule(Module):
         super().__init__(module_name, services, config)
 
         self._server_peer_ref = services.mosaic.put(services.server_identity.peer.piece)
-        self._servant_name = 'htest_list'
+        self._servant_name = 'htest_global_list'
         services.server_rpc_endpoint.register_servant(self._servant_name, partial(
-            self._htest_list_servant,
+            self._htest_global_list_servant,
             services.mosaic,
             services.htest_list,
             ))
-        services.htest_list_service_factory = partial(
-            self.htest_list_service_factory,
+        services.htest_global_list_service_factory = partial(
+            self.htest_global_list_service_factory,
             services.mosaic,
             services.types,
             services.servant_path,
             )
 
-    def htest_list_service_factory(self, mosaic, types, servant_path_factory, module_name):
+    def htest_global_list_service_factory(self, mosaic, types, servant_path_factory, module_name):
         servant_path = servant_path_factory().registry_name(self._servant_name).parameterized(module_name)
         run_command = htypes.rpc_command.rpc_command(
             peer_ref=self._server_peer_ref,
@@ -58,21 +55,21 @@ class ThisModule(Module):
         return htypes.service.list_service(
             peer_ref=self._server_peer_ref,
             servant_path=servant_path.get_attr('list').as_data,
-            dir_list=[[mosaic.put(htypes.htest.htest_list_d())]],
+            dir_list=[[mosaic.put(htypes.htest.htest_global_list_d())]],
             command_ref_list=[
                 mosaic.put(run_command),
                 ],
             key_column_id='name',
-            column_list=item_t_to_column_list(types, htypes.htest.test),
+            column_list=item_t_to_column_list(types, htypes.htest.global_fn),
             )
 
-    def _htest_list_servant(
+    def _htest_global_list_servant(
             self,
             mosaic,
             htest_list,
             module_name,
             ):
-        return TestListServant(
+        return GlobalListServant(
             mosaic,
             htest_list,
             module_name,
