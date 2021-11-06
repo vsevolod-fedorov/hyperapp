948008e2d4386f74a4fb50d783904dae8a42774c
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-11-06 13:59:54 +0300

    Refactor code module import: Move services to new importer

--- hyperapp/common/module.py
+++ hyperapp/common/module.py
@@ -14,27 +14,3 @@ class Module(object):
 
     def __repr__(self):
         return '<Module %r>' % self.name
-
-
-class ModuleRegistry(object):
-
-    def __init__(self):
-        self._module_list = []  # import order should be preserved
-
-    def register(self, module):
-        assert isinstance(module, Module), repr(module)
-        self._module_list.append(module)
-
-    def init_phases(self, services):
-        for phase_num in range(1, MAX_INIT_PHASE_COUNT + 1):
-            for module in self._module_list:
-                method = getattr(module, 'init_phase_{}'.format(phase_num), None)
-                if method:
-                    log.info('Run init phase %d for %r', phase_num, module)
-                    method(services)
-
-    def enum_modules_method(self, method_name):
-        for module in self._module_list:
-            method = getattr(module, method_name, None)
-            if method:
-                yield (module, method)
--- hyperapp/common/services.py
+++ hyperapp/common/services.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from types import SimpleNamespace
 
 from .htypes import BuiltinTypeRegistry, register_builtin_types
+from .code_module import code_module_t
 from .ref import ref_repr
 from .mosaic import Mosaic
 from .web import Web
@@ -14,16 +15,15 @@ from .type_module_loader import TypeModuleLoader
 from .type_system import TypeSystem
 from .code_module import register_code_module_types
 from .code_module_loader import CodeModuleLoader
-from .code_module_importer import CodeModuleImporter
-from .module import ModuleRegistry
+from .code_registry import CodeRegistry
+from .python_importer import PythonImporter
+from .module_registry import CodeModule, ModuleRegistry
 
 log = logging.getLogger(__name__)
 
 
 TYPE_MODULE_EXT = '.types'
 HYPERAPP_DIR = Path(__file__).parent.joinpath('../..').resolve()
-
-ModuleRec = namedtuple('ModuleRec', 'module module_ref')
 
 
 class Services(object):
@@ -49,10 +49,11 @@ class Services(object):
         register_code_module_types(self.builtin_types, self.mosaic, self.types)
         self.type_module_loader = TypeModuleLoader(self.builtin_types, self.mosaic, self.types)
         self.code_module_loader = CodeModuleLoader(self.mosaic, self.type_module_loader.registry)
-        self.module_registry = ModuleRegistry()
-        self.code_module_importer = CodeModuleImporter(self.mosaic, self.types)
-        self.code_module_importer.register_meta_hook()
-        self.imported_code_modules = {}  # full code module name -> ModuleRec
+        self._python_importer = PythonImporter()
+        self._module_code_registry = CodeRegistry('module', self.web, self.types)
+        self._module_code_registry.register_actor(code_module_t, CodeModule.from_piece, self.types, self.web)
+        self.module_registry = ModuleRegistry(self.mosaic, self.web, self._python_importer, self._module_code_registry)
+        self._python_importer.register_meta_hook()
 
     def start(self):
         log.info("Start services.")
@@ -73,58 +74,20 @@ class Services(object):
         log.info("Init modules.")
         try:
             self.type_module_loader.load_type_modules(self.module_dir_list)
-            self._load_code_module_list(code_module_list, config)
-            self.module_registry.init_phases(self)
+            self._load_code_module_list(code_module_list, config or {})
         except:
-            self.code_module_importer.unregister_meta_hook()
+            self._python_importer.unregister_meta_hook()
             raise
 
     def unregister_import_meta_hook(self):
-        self.code_module_importer.unregister_meta_hook()
+        self._python_importer.unregister_meta_hook()
 
     def _load_code_module_list(self, module_name_list, config):
+        local_modules = self.code_module_loader.load_code_modules(self.module_dir_list)
+        self.local_modules = local_modules
 
-        registry = self.code_module_loader.load_code_modules(self.module_dir_list)
-        self.loaded_code_modules = registry
-
-        preferred_modules = {
-            registry.by_name[module_name]
-            for module_name in module_name_list
-            }
-
-        for module_name in module_name_list:
-            log.info("Require import module %r", module_name)
-            self.code_module_importer.import_code_module(
-                registry.by_requirement, registry.by_name[module_name], preferred_modules)
-
-        module_name_by_ref = {
-            module_ref: module_name
-            for module_name, module_ref in registry.by_name.items()
-            }
-
-        # Should init modules in the same order as they were imported.
-        for module_ref, module in self.code_module_importer.registry.items():
-            module_name = module_name_by_ref[module_ref]
-            if config:
-                module_config = config.get(module_name, {})
-            else:
-                module_config = {}
-            self._init_module(module_name, module, module_config)
-            self.imported_code_modules[module_name] = ModuleRec(module, module_ref)
-
-    def _init_module(self, module_name, module, config):
-        this_module_class = module.__dict__.get('ThisModule')
-        if this_module_class:
-            log.info("Init module %s (%s) with config: %s", module_name, this_module_class, config)
-            this_module = this_module_class(module_name, self, config)
-            module.__dict__['this_module'] = this_module
-            self.module_registry.register(this_module)
-
-    def import_module(self, module_name, config=None):
-        module_ref = self.loaded_code_modules.by_name[module_name]
-        log.info("Import module %s %r", module_ref, module_name)
-        module = self.code_module_importer.import_code_module(
-            self.loaded_code_modules.by_requirement, module_ref, preferred_modules=set())
-        self._init_module(module_name, module, config or {})
-        self.imported_code_modules[module_name] = ModuleRec(module, module_ref)
-        return module
+        module_list = [
+            self.web.summon(local_modules.by_name[name])
+            for name in module_name_list
+            ]
+        self.module_registry.import_module_list(self, module_list, local_modules.by_requirement, config)
