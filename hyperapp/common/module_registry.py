0cccf2f2fd75d6d674c36601cc05b21c9300d9a7
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-11-06 13:33:15 +0300

    Refactor code module import: enum method

--- hyperapp/common/module_registry.py
+++ hyperapp/common/module_registry.py
@@ -1,7 +1,7 @@
 import codecs
 import importlib
 import logging
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from functools import cached_property
 
 from .python_importer import ROOT_PACKAGE, Finder
@@ -102,18 +102,25 @@ class CodeModule:
             file_path=self._code_module.file_path,
             )
 
-    def get_sub_loader_dict(self, imported_module_dict, module_name):
+    def get_sub_loader_dict(self, module_to_python_module, module_name):
         return {
             **dict(self._get_type_sub_loader_it(module_name)),
-            **dict(self._get_code_sub_loader_dict(imported_module_dict)),
+            **dict(self._get_code_sub_loader_dict(module_to_python_module)),
             }
 
     def init_module(self, services, python_module, config):
-        this_module_class = python_module.__dict__.get('ThisModule')
+        this_module_class = getattr(python_module, 'ThisModule', None)
         if this_module_class:
             log.info("Init module %s (%s) with config: %s", self.name, this_module_class, config)
             this_module = this_module_class(self.name, services, config)
             python_module.__dict__['this_module'] = this_module
+
+    def get_module_method(self, python_module, name):
+        this_module = getattr(python_module, 'this_module', None)
+        if this_module:
+            return getattr(this_module, name, None)
+        else:
+            return None
 
     @cached_property
     def _code_import_list(self):
@@ -134,20 +141,22 @@ class CodeModule:
             sub_name = f'htypes.{name}'
             yield (sub_name, _DictLoader(globals))
 
-    def _get_code_sub_loader_dict(self, imported_module_dict):
+    def _get_code_sub_loader_dict(self, module_to_python_module):
         for import_name, module in self._code_import_list:
-            python_module = imported_module_dict[module]
+            python_module = module_to_python_module[module]
             yield (import_name.split('.')[-1], _DictLoader(python_module.__dict__))
 
 
 class ModuleRegistry:
+
+    _Rec = namedtuple('_Rec', 'name code_module python_module')
 
     def __init__(self, mosaic, web, python_importer, module_code_registry):
         self._mosaic = mosaic
         self._web = web
         self._python_importer = python_importer
         self._module_code_registry = module_code_registry
-        self._imported_module_dict = {}  # code_module_t -> python module
+        self._registry = {}  # code_module_t -> _Rec
 
     def import_module_list(self, services, module_list, module_by_requirement, config_dict):
         module_code_list = self._resolve_requirements(module_list, module_by_requirement)
@@ -155,8 +164,14 @@ class ModuleRegistry:
             config = config_dict.get(module_code.name, {})
             self._import_module(services, module_code, config)
 
+    def enum_method(self, method_name):
+        for rec in self._registry.values():
+            method = rec.code_module.get_module_method(rec.python_module, method_name)
+            if method:
+                yield (rec.name, method)
+
     def get_python_module(self, module):
-        return self._imported_module_dict[module]
+        return self._registry[module].python_module
 
     def _resolve_requirements(self, module_list, module_by_requirement):
         seen_set = set()
@@ -181,13 +196,18 @@ class ModuleRegistry:
             
     def _import_module(self, services, module_code, config):
         module_name = self._make_module_name(module_code.module)
+        module_to_python_module = {
+            module: rec.python_module
+            for module, rec
+            in self._registry.items()
+            }
         python_module = self._python_importer.import_module(
             module_name,
             module_code.root_loader,
-            module_code.get_sub_loader_dict(self._imported_module_dict, module_name),
+            module_code.get_sub_loader_dict(module_to_python_module, module_name),
         )
         module_code.init_module(services, python_module, config)
-        self._imported_module_dict[module_code.module] = python_module
+        self._registry[module_code.module] = self._Rec(module_code.name, module_code, python_module)
 
     def _make_module_name(self, module):
         module_ref = self._mosaic.put(module)
