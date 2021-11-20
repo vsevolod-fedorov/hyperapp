6d9359085ac2ffc6fab87c1fb8fb0eeca1deaa25
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-11-20 11:44:17 +0300

    Htest sample: add SampleServant

--- hyperapp/server/htest_sample.dyn.py
+++ hyperapp/server/htest_sample.dyn.py
@@ -11,8 +11,23 @@ def test_two():
     pass
 
 
-def test_three():
-    pass
+class SampleServant:
+
+    def __init__(self, mosaic, web, peer_registry, rpc_call_factory):
+        pass
+
+    @property
+    def _dict(self):
+        return None
+
+    def list(self, request):
+        pass
+
+    def list_live(self, request, peer_ref, servant_path_data):
+        pass
+
+    def open(self, request, current_key):
+        pass
 
 
 log.info("HTest_sample module is loaded")
