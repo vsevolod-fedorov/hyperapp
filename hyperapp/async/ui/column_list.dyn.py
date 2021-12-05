2ef74a8124f4537f96cd4e3ba25a679bb2dffc30
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-12-05 11:29:49 +0300

    Column list: fetch object columms

--- hyperapp/async/ui/column_list.dyn.py
+++ hyperapp/async/ui/column_list.dyn.py
@@ -1,14 +1,56 @@
+import asyncio
 from collections import namedtuple
 
 from hyperapp.common.module import Module
 
 from . import htypes
 from .object_command import Command
-from .list_object import ListObject
+from .list_object import ListFetcher, ListObject
 from .simple_list_object import SimpleListObject
 
 
+ITEM_FETCH_COUNT = 100
+
 Item = namedtuple('Item', 'name visible')
+
+
+class _Fetcher(ListFetcher):
+
+    def __init__(self, object, columns_future):
+        self._object = object
+        self._columns_future = columns_future
+        self._columns = []  # attr name list
+        self._processed_item_count = 0
+
+    def process_fetch_results(self, item_list, fetch_finished):
+        key_attr = self._object.key_attribute
+        self._update_columns(item_list)
+        self._processed_item_count += len(item_list)
+        if self._processed_item_count >= ITEM_FETCH_COUNT:
+            self._send_result()
+            return
+        if item_list:
+            from_key = getattr(item_list[-1], key_attr)
+        else:
+            from_key = None
+        if fetch_finished and not self._columns_future.done():
+            asyncio.ensure_future(self._object.fetch_items(from_key, self))
+
+    def process_eof(self):
+        self._send_result()
+
+    def _send_result(self):
+        if not self._columns_future.done():
+            self._columns_future.set_result(self._columns)
+
+    def _update_columns(self, new_item_list):
+        seen_attrs = set()
+        for item in new_item_list:
+            seen_attrs |= set(
+                name for name in dir(item)
+                if not name.startswith('_') and not callable(getattr(item, name))
+            )
+        self._columns += list(seen_attrs - set(self._columns))
 
 
 class ColumnList(SimpleListObject):
@@ -43,7 +85,14 @@ class ColumnList(SimpleListObject):
         return 'name'
 
     async def get_all_items(self):
-        return []
+        columns_future = asyncio.Future()
+        fetcher = _Fetcher(self._object, columns_future)
+        asyncio.ensure_future(self._object.fetch_items(None, fetcher))
+        column_attr_list = await columns_future
+        return [
+            Item(name, visible=True)
+            for name in column_attr_list
+            ]
 
 
 class ThisModule(Module):
