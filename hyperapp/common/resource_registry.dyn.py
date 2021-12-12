833711d05da258cac7784cb677616ff1c931da3b
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-12-12 11:28:59 +0300

    Factory resource: resolve object, legacy module

--- hyperapp/common/resource_registry.dyn.py
+++ hyperapp/common/resource_registry.dyn.py
@@ -1,11 +1,23 @@
+import logging
+
 from hyperapp.common.module import Module
 
+log = logging.getLogger(__name__)
 
 
 class ResourceRegistry:
 
-    def __init__(self):
-        pass
+    def __init__(self, resource_type_registry, builtin_resource_by_name):
+        self._resource_type_registry = resource_type_registry
+        self._builtin_resource_by_name = builtin_resource_by_name
+
+    def load_definitions(self, resources):
+        name_to_resource = {**self._builtin_resource_by_name}
+        for name, definition in resources.items():
+            from_dict = self._resource_type_registry[definition['type']]
+            resource = from_dict(definition, name_to_resource)
+            name_to_resource[name] = resource
+            log.info("Loaded resource %r: %s", name, resource)
 
 
 class ThisModule(Module):
@@ -13,5 +25,6 @@ class ThisModule(Module):
     def __init__(self, module_name, services, config):
         super().__init__(module_name, services, config)
 
-        services.resource_registry = ResourceRegistry()
         services.resource_type_registry = {}  # resource name -> from_dict constructor.
+        services.builtin_resource_by_name = {}
+        services.resource_registry = ResourceRegistry(services.resource_type_registry, services.builtin_resource_by_name)
