48711af36dc4ae40d5c48c7b7b494b5013295bbc
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-12-19 10:57:33 +0300

    Resources: use pieces instead of classes

--- hyperapp/common/resource/call.dyn.py
+++ hyperapp/common/resource/call.dyn.py
@@ -1,12 +1,9 @@
 from hyperapp.common.module import Module
 
+from . import htypes
+
 
 class CallResource:
-
-    @classmethod
-    def from_dict(cls, data, name_to_resource):
-        function = name_to_resource[data['function']]
-        return cls(function)
 
     def __init__(self, function):
         self._function = function
@@ -19,9 +16,15 @@ class CallResource:
         return fn()
 
 
+
+def from_dict(data, name_to_resource):
+    fn_object_ref = name_to_resource[data['function']]
+    return htypes.call.call(fn_object_ref)
+
+
 class ThisModule(Module):
 
     def __init__(self, module_name, services, config):
         super().__init__(module_name, services, config)
 
-        services.resource_type_registry['call'] = CallResource.from_dict
+        services.resource_type_registry['call'] = from_dict
