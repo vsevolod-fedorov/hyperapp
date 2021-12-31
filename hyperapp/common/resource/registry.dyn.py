b88f013582ec455cd71758203238bf8bbcfcd68f
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-12-31 13:30:31 +0300

    CachedCodeRegistry; use for python_object_creg

--- hyperapp/common/resource/registry.dyn.py
+++ hyperapp/common/resource/registry.dyn.py
@@ -1,5 +1,6 @@
-from hyperapp.common.code_registry import CodeRegistry
 from hyperapp.common.module import Module
+
+from .cached_code_registry import CachedCodeRegistry
 
 
 class ThisModule(Module):
@@ -8,4 +9,4 @@ class ThisModule(Module):
         super().__init__(module_name, services, config)
 
         services.resource_type_registry = {}  # resource name -> factory.
-        services.python_object_creg = CodeRegistry('python_object', services.web, services.types)
+        services.python_object_creg = CachedCodeRegistry('python_object', services.web, services.types)
