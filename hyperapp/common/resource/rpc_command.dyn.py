22b712e6919bb82632b60ae651480698981c6b6d
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2021-12-31 13:58:43 +0300

    Add rpc_command resource; update async.ui.rpc_command; restore server ref list open command

--- hyperapp/common/resource/rpc_command.dyn.py
+++ hyperapp/common/resource/rpc_command.dyn.py
@@ -10,18 +10,13 @@ def factory(mosaic, python_object_creg, data, resolve_name):
     identity = python_object_creg.invite(identity_ref)
     peer_ref = mosaic.put(identity.peer.piece)
     servant_fn_ref = resolve_name(data['servant'])
-    command_ref_list = [
-        resolve_name(command_name)
-        for command_name
-        in data.get('commands', [])
-        ]
-    key_attribute = data['key_attribute']
-    return htypes.service.list_service(
+    state_attr_list = data['state_attributes']
+    name = data['name']
+    return htypes.rpc_command.rpc_command(
         peer_ref=peer_ref,
         servant_fn_ref=servant_fn_ref,
-        dir_list=[],
-        command_ref_list=command_ref_list,
-        key_attribute=key_attribute,
+        state_attr_list=state_attr_list,
+        name=name,
         )
 
 
@@ -30,5 +25,5 @@ class ThisModule(Module):
     def __init__(self, module_name, services, config):
         super().__init__(module_name, services, config)
 
-        services.resource_type_registry['list_service'] = partial(
+        services.resource_type_registry['rpc_command'] = partial(
             factory, services.mosaic, services.python_object_creg)
