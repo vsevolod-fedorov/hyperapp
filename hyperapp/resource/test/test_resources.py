6145c87f525944233efbdf0f8727e96ee0c78ad5
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2022-01-16 11:27:00 +0300

    Resource type: map resource_t -> definition_t

--- hyperapp/resource/test/test_resources.py
+++ hyperapp/resource/test/test_resources.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import pytest
 import yaml
 
+from hyperapp.common.htypes import tString, TList, TRecord
 from hyperapp.common import cdr_coders  # self-registering
 
 log = logging.getLogger(__name__)
@@ -22,6 +23,7 @@ def additional_module_dirs():
 @pytest.fixture
 def code_module_list():
     return [
+        'resource.resource_type',
         'resource.registry',
         'resource.legacy_module',
         'resource.legacy_service',
@@ -41,3 +43,16 @@ def test_resources(services):
 
     list_service = module.make('sample_list_service')
     log.info("List service: %r", list_service)
+
+
+def test_resource_type(services, htypes, code):
+    resource_t = htypes.partial.partial
+    resource_type = code.resource_type.ResourceType(services.types, services.mosaic, services.web, resource_t)
+    log.info("definition_t: %r", resource_type.definition_t)
+    assert resource_type.definition_t == TRecord('partial', {
+        'fn_ref': tString,
+        'params': TList(TRecord('param', {
+            'name': tString,
+            'value_ref': tString,
+            })),
+        })
