53975620ff2593dce70bcda1ae4f84e27e73d7b6
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2022-02-23 13:09:25 +0300

    LCS: prevent mixing single and multi value records

--- hyperapp/common/layered_config_sheet.dyn.py
+++ hyperapp/common/layered_config_sheet.dyn.py
@@ -1,5 +1,4 @@
 import logging
-from collections import defaultdict
 from dataclasses import dataclass, field
 from typing import List
 from pathlib import Path
@@ -15,31 +14,56 @@ lcs_path = Path('~/.local/share/hyperapp/client/lcs.cdr').expanduser()
 
 @dataclass
 class Record:
-    persist = False
     value_list: List = field(default_factory=list)
+    is_multi_value: bool = False
+    persist: bool = False
+
+    @classmethod
+    def from_piece(cls, piece, web, persist):
+        dir = [
+            web.summon(ref)
+            for ref in piece.dir
+            ]
+        value_list = [
+            web.summon(ref)
+            for ref in piece.value_list
+            ]
+        record = cls(value_list, piece.is_multi_value, persist)
+        return (dir, record)
+
+    def as_piece(self, dir, mosaic):
+        dir_refs = [
+            mosaic.put(element)
+            for element in dir
+            ]
+        values_refs = [
+            mosaic.put(element)
+            for element in self.value_list
+            ]
+        return htypes.layered_config_sheet.lcs_association(dir_refs, self.is_multi_value, values_refs)
 
 
 class LCSlice:
 
-    def __init__(self, dir_to_record):
-        self._dir_to_record = dir_to_record
+    def __init__(self, dir_to_record=None):
+        self._dir_to_record = dir_to_record or {}
 
     def get(self, dir):
         try:
-            piece_list = self._dir_to_record[frozenset(dir)].value_list
+            record = self._dir_to_record[frozenset(dir)]
+        except KeyError:
+            return None
         except TypeError as x:
-            raise RuntimeError(f"Type error: {x}: {dir!r}")
-        if len(piece_list) > 1:
-            raise RuntimeError(f"More than one value is registered for {dir}")
-        if piece_list:
-            return piece_list[0]
-        return None
+            raise RuntimeError(f"LCS: Type error: {x}: {dir!r}")
+        if record.is_multi_value:
+            raise RuntimeError(f"LCS: Attempt to get single value from multi-value record: {set(dir)}")
+        return record.value_list[0]
 
 
 class LCSheet(LCSlice):
 
     def __init__(self, mosaic, web, bundle):
-        super().__init__(defaultdict(Record))
+        super().__init__()
         self._mosaic = mosaic
         self._web = web
         self._bundle = bundle
@@ -51,103 +75,110 @@ class LCSheet(LCSlice):
         except FileNotFoundError:
             pass
         else:
-            for rec in storage.record_list:
-                dir = [
-                    self._web.summon(ref)
-                    for ref in rec.dir
-                    ]
-                value = self._web.summon(rec.value_ref)
-                log.info("LCS: loaded %s -> %s", dir, value)
-                record = self._add(dir, value)
-                record.persist = True
+            for association in storage.record_list:
+                dir, record = Record.from_piece(association, self._web, persist=True)
+                log.info("LCS: loaded %s -> %s", set(dir), record)
+                self._set(dir, record)
 
     def _save(self):
-        rec_list = []
-        for dir, record in self._dir_to_record.items():
-            if not record.persist:
-                continue
-            dir_refs = [
-                self._mosaic.put(element)
-                for element in dir
-                ]
-            for value in record.value_list:
-                rec = htypes.layered_config_sheet.lcs_record(
-                    dir=dir_refs,
-                    value_ref=self._mosaic.put(value),
-                    )
-                rec_list.append(rec)
+        rec_list = [
+            record.as_piece(dir, self._mosaic)
+            for dir, record
+            in self._dir_to_record.items()
+            if record.persist
+            ]
         self._bundle.save_piece(htypes.layered_config_sheet.lcs_storage(rec_list))
 
-    def _add(self, dir, piece):
-        record = self._dir_to_record[frozenset(dir)]
-        record.value_list.append(piece)
-        return record
+    def _set(self, dir, record):
+        self._dir_to_record[frozenset(dir)] = record
 
     def add(self, dir, piece, persist=False):
-        log.info("LCS: add%s %s -> %s", '/persist' if persist else '', dir, piece)
-        record = self._add(dir, piece)
+        log.info("LCS: add%s %s -> %s", '/persist' if persist else '', set(dir), piece)
+        try:
+            record = self._dir_to_record[frozenset(dir)]
+        except KeyError:
+            record = Record([piece], is_multi_value=True, persist=persist)
+            self._dir_to_record[frozenset(dir)] = record
+        else:
+            if not record.is_multi_value:
+                raise RuntimeError(f"LCS: Attempt to add value to single-value record: {set(dir)} -> {piece}")
+            if record.persist != persist:
+                raise RuntimeError(f"LCS: Attempt to change persistentency for: {set(dir)} -> {persist}")
+            record.value_list.append(piece)
         if persist:
-            record.persist = True
             self._save()
 
     def set(self, dir, piece, persist=False):
-        log.info("LCS: set%s %s -> %s", '/persist' if persist else '', dir, piece)
-        record = self._dir_to_record[frozenset(dir)]
-        record.value_list = [piece]
+        log.info("LCS: set%s %s -> %s", '/persist' if persist else '', set(dir), piece)
+        try:
+            record = self._dir_to_record[frozenset(dir)]
+        except KeyError:
+            record = Record([piece], is_multi_value=False, persist=persist)
+            self._dir_to_record[frozenset(dir)] = record
+        else:
+            if record.is_multi_value:
+                raise RuntimeError(f"LCS: Attempt to set value to multi-value record: {set(dir)} -> {piece}")
+            if record.persist != persist:
+                raise RuntimeError(f"LCS: Attempt to change persistentency for: {set(dir)} -> {persist}")
+            record.value_list = [piece]
         if persist:
-            record.persist = True
             self._save()
 
     def remove(self, dir):
-        record = self._dir_to_record[frozenset(dir)]
-        log.info("LCS: remove%s %s -> %s", '/persist' if record.persist else '', dir, record.value_list)
-        del self._dir_to_record[frozenset(dir)]
+        record = self._dir_to_record.pop(frozenset(dir))
+        log.info("LCS: remove%s %s -> %s", '/persist' if record.persist else '', set(dir), record.value_list)
         if record.persist:
             self._save()
 
-    def iter(self, filter_dir=None):
+    def _iter(self, filter_dir=None):
         filter_dir_set = set(filter_dir or [])
         for dir, record in self._dir_to_record.items():
-            dir_set = set(dir)
-            if not filter_dir_set <= dir_set:
-                continue
+            if filter_dir_set <= dir:
+                yield set(dir), record
+
+    def iter(self, filter_dir=None):
+        for dir, record in self._iter(filter_dir):
             yield (dir, record.value_list, record.persist)
 
     def iter_dir_list_values(self, dir_list):
         for dir in dir_list:
-            yield from self._dir_to_record[frozenset(dir)].value_list
+            try:
+                record = self._dir_to_record[frozenset(dir)]
+            except KeyError:
+                pass
+            else:
+                if not record.is_multi_value:
+                    raise RuntimeError(f"LCS: Attempt to iter over values for single-value record: {set(dir)}")
+                yield from record.value_list
 
     def slice(self, prefix_dir):
         prefix_dir_set = set(prefix_dir)
-        dir_to_record = defaultdict(Record)
+        dir_to_record = {}
         for dir, record in self._dir_to_record.items():
-            dir_set = set(dir)
-            if prefix_dir_set <= dir_set:
-                slice_dir = frozenset(dir_set - prefix_dir_set)
+            if prefix_dir_set <= dir:
+                slice_dir = frozenset(dir - prefix_dir_set)
                 dir_to_record[slice_dir] = record
         return LCSlice(dir_to_record)
 
     def aux_bundler_hook(self, ref, t, value):
-        for dir, value_list, persist in self.iter({value}):
-            log.info("LCS bundle aux: %s -> %s", set(dir), value_list)
-            for value in value_list:
-                dir_refs = tuple(
-                    self._mosaic.put(element)
-                    for element in dir
-                    )
-                association = htypes.layered_config_sheet.lcs_association(dir_refs, self._mosaic.put(value))
-                yield self._mosaic.put(association)
+        for dir, record in self._iter({value}):
+            log.info("LCS bundle aux: %s -> %s", dir, record)
+            yield self._mosaic.put(record.as_piece(dir, self._mosaic))
 
     def aux_unbundler_hook(self, ref, t, value):
         if t is not htypes.layered_config_sheet.lcs_association:
             return
-        dir = [
-            self._web.summon(ref)
-            for ref in value.dir
-            ]
-        lcs_value = self._web.summon(value.value_ref)
-        log.info("LCS unbundle aux: %s -> %s", set(dir), lcs_value)
-        self._add(dir, lcs_value)
+        dir, record = Record.from_piece(value, self._web, persist=False)
+        try:
+            prev_record = self._dir_to_record[frozenset(dir)]
+        except KeyError:
+            self._dir_to_record[frozenset(dir)] = record
+            log.info("LCS unbundle aux: %s -> %s", set(dir), record)
+        else:
+            if record.is_multi_value != prev_record.is_multi_value:
+                raise RuntimeError(f"LCS unbundle aux: Attempt to change is_multi_value for: {set(dir)}")
+            log.debug("LCS unbundle aux: Warning: overriding %s: %s -> %s",  set(dir), prev_record, record)
+            self._dir_to_record[frozenset(dir)] = record
 
 
 class ThisModule(ClientModule):
