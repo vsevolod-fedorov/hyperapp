ac36f37dfd67d7547749905c974ac090b385308b
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2022-03-08 13:56:34 +0300

    Resource constructor: convert camel case to snake

--- hyperapp/server/htest.dyn.py
+++ hyperapp/server/htest.dyn.py
@@ -1,5 +1,6 @@
 import logging
 import queue
+import re
 from contextlib import contextmanager
 from functools import cached_property, partial
 from pathlib import Path
@@ -10,6 +11,12 @@ from hyperapp.common.module import Module
 from . import htypes
 
 log = logging.getLogger(__name__)
+
+
+# https://stackoverflow.com/a/1176023 Camel case to snake case.
+def camel_to_snake(name):
+    name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
 
 
 class HTestList:
@@ -181,12 +188,13 @@ class HTest:
         return resource_module
 
     def _process_fn(self, module_name, resource_module, process, collect_attributes_call, module_res_name, name_to_module, globl):
+        global_snake_name = camel_to_snake(globl.name)
         attr_res_t = self._resource_type_reg['attribute']
         attr_def = attr_res_t.definition_t(
             object=module_res_name,
             attr_name=globl.name,
             )
-        attr_res_name = f'{globl.name}_attribute'
+        attr_res_name = f'{global_snake_name}_attribute'
 
         param_to_resource = {}
         for param_name in globl.param_list:
@@ -205,34 +213,34 @@ class HTest:
                 in param_to_resource.items()
                 ],
             )
-        partial_res_name = f'{globl.name}_partial'
+        partial_res_name = f'{global_snake_name}_partial'
 
         call_res_t = self._resource_type_reg['call']
         call_def = call_res_t.definition_t(
             function=partial_res_name,
             )
-        call_res_name = globl.name
+        call_res_name = global_snake_name
 
         resource_module.set_definition(attr_res_name, attr_res_t, attr_def)
         resource_module.set_definition(partial_res_name, partial_res_t, partial_def)
         resource_module.set_definition(call_res_name, call_res_t, call_def)
 
-        object_res = resource_module[globl.name]
-        log.info("Function resource %s: %r", globl.name, object_res)
+        object_res = resource_module[call_res_name]
+        log.info("Function resource %s: %r", global_snake_name, object_res)
         attr_list = collect_attributes_call(self._mosaic.put(object_res))
-        log.info("Attributes for %s: %r", globl.name, attr_list)
+        log.info("Attributes for %s: %r", global_snake_name, attr_list)
 
         for attr in attr_list:
             if attr.param_list in {(), ('request',)}:
-                self._process_service(module_name, resource_module, process, globl, object_res, attr)
+                self._process_service(module_name, resource_module, process, global_snake_name, object_res, attr)
 
-    def _process_service(self, module_name, resource_module, process, globl, object_res, attr):
+    def _process_service(self, module_name, resource_module, process, global_snake_name, object_res, attr):
         attr_res_t = self._resource_type_reg['attribute']
         attr_def = attr_res_t.definition_t(
-            object=globl.name,
+            object=global_snake_name,
             attr_name=attr.name,
             )
-        attr_res_name = f'{globl.name}_{attr.name}_method'
+        attr_res_name = f'{global_snake_name}_{attr.name}_method'
         resource_module.set_definition(attr_res_name, attr_res_t, attr_def)
         attr_res = resource_module[attr_res_name]
         attr_res_ref = self._mosaic.put(attr_res)
@@ -243,21 +251,21 @@ class HTest:
         else:
             args = []
         result_t = get_result_call(attr_res_ref, *args)
-        log.info("Attribute %s.%s result type: %r", globl.name, attr.name, result_t)
+        log.info("Attribute %s.%s result type: %r", global_snake_name, attr.name, result_t)
 
         for key_attribute in ['id', 'key', 'name']:
             if key_attribute in result_t.attr_name_list:
                 break
         else:
-            raise RuntimeError(f"Unable to deduce key attribute for {globl.name}.{attr.name}: {result_t}")
+            raise RuntimeError(f"Unable to deduce key attribute for {global_snake_name}.{attr.name}: {result_t}")
 
         module_type_name = module_name.split('.')[-1]
-        dir_t_res_name = f'legacy_type.{module_type_name}.{globl.name}_d'
+        dir_t_res_name = f'legacy_type.{module_type_name}.{global_snake_name}_d'
         call_res_t = self._resource_type_reg['call']
         dir_def = call_res_t.definition_t(
             function=dir_t_res_name,
             )
-        dir_res_name = f'{globl.name}_d'
+        dir_res_name = f'{global_snake_name}_d'
         resource_module.add_import(dir_t_res_name)
         resource_module.set_definition(dir_res_name, call_res_t, dir_def)
 
@@ -270,7 +278,7 @@ class HTest:
             commands=[],
             key_attribute=key_attribute,
             )
-        service_res_name = f'{globl.name}_service'
+        service_res_name = f'{global_snake_name}_service'
         resource_module.add_import(identity_res_name)
         resource_module.set_definition(service_res_name, service_res_t, service_def)
 
