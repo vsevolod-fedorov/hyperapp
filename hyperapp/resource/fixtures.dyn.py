b75037710b70581e34dc6ac88c209a9e5e10dbc5
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2022-03-20 11:47:05 +0300

    Python module: load python module

--- hyperapp/resource/fixtures.dyn.py
+++ hyperapp/resource/fixtures.dyn.py
@@ -7,11 +7,6 @@ from . import htypes
 log = logging.getLogger(__name__)
 
 
-def python_object(piece, python_object_creg):
-    fn = python_object_creg.invite(piece.function)
-    return fn()
-
-
 def load_fixtures(resource_module, resource_type, module_dir_list):
     ext = '.fixture.py'
     for root_dir in module_dir_list:
@@ -23,8 +18,9 @@ def load_fixtures(resource_module, resource_type, module_dir_list):
             module = htypes.python_module.python_module(
                 module_name=module_name,
                 source=path.read_text(),
+                file_path=str(path),
                 )
-            log.info("Fixture: %s", module_name)
+            log.info("Fixture %s: %s", module_name, path)
             resource_module.set_definition(module_name, resource_type, module)
             
 
@@ -38,4 +34,3 @@ class ThisModule(Module):
         resource_type = services.resource_type_factory('python_module', htypes.python_module.python_module)
         load_fixtures(resource_module, resource_type, services.module_dir_list)
         services.resource_module_registry['fixtures'] = resource_module
-        # services.python_object_creg.register_actor(htypes.python_module.python_module, python_object, services.python_object_creg)
--- hyperapp/server/test/test_resources/construct_resources_sample.fixture.py
+++ hyperapp/server/test/test_resources/construct_resources_sample.fixture.py
@@ -1,3 +1,10 @@
+import logging
+
+log = logging.getLogger(__name__)
+
 
 def sample_servant_piece():
     return 'test value'
+
+
+log.info("construct_resources_sample fixture module is loaded")
