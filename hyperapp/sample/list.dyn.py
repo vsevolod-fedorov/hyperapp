c905d55d435b1c47e72b9d8537f9e06be17de25f
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2022-05-01 11:51:16 +0300

    Sample list source

---
+++ hyperapp/sample/list.dyn.py
@@ -0,0 +1,21 @@
+import logging
+
+from . import htypes
+
+log = logging.getLogger(__name__)
+
+
+class SampleList:
+
+    def __init__(self, piece, mosaic, web, peer_registry, rpc_call_factory):
+        log.info("sapmle/list: SampleList ctr: %s, %s, %s, %s, %s", piece, mosaic, web, peer_registry, rpc_call_factory)
+
+    def get(self):
+        return [
+            htypes.sample_list.item(1, 'First', 'First item'),
+            htypes.sample_list.item(2, 'Second', 'Second item'),
+            htypes.sample_list.item(3, 'Thirt', 'Third item'),
+            ]
+
+
+log.info("sample/list module is loaded")
