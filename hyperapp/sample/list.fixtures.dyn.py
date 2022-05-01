214caa6c95257753cb038451030ff014c3465374
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2022-05-01 11:51:16 +0300

    Sample list source

---
+++ hyperapp/sample/list.fixtures.dyn.py
@@ -0,0 +1,13 @@
+import logging
+
+from .htypes import sample_list
+
+log = logging.getLogger(__name__)
+
+
+def sample_list_piece():
+    log.info("Fixture sample_list_piece is called")
+    return sample_list.sample_list(id=123)
+
+
+log.info("sample/list fixture module is loaded; piece t: %s", sample_list.sample_list)
