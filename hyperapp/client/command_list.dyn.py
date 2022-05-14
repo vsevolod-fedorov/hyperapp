00d03787faf4cea1fa79b2e7bc785fa12015143a
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2022-05-14 13:51:51 +0300

    Command list: fix shortcut

--- hyperapp/client/command_list.dyn.py
+++ hyperapp/client/command_list.dyn.py
@@ -15,13 +15,13 @@ class GlobalCommandList:
             dir = self._python_object_creg.invite(command.dir)
             item = htypes.command_list.item(
                 name=dir._t.name.rstrip('_d'),  # todo: load title from lcs.
-                shortcut=self._command_shortcut(command) or '',
+                shortcut=self._command_shortcut(dir) or '',
                 )
             record_list.append(item)
         return record_list
 
-    def _command_shortcut(self, command):
-        return self._lcs.get([*command.dir, htypes.command.command_shortcut_d()])
+    def _command_shortcut(self, dir):
+        return self._lcs.get([dir, htypes.command.command_shortcut_d()])
 
 
 def open_global_command_list():
