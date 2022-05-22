12379dea6c26ec393badb6999a34de5f71f891c3
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2022-05-22 13:23:18 +0300

    Update open_local_server client command

--- hyperapp/client/local_server.dyn.py
+++ hyperapp/client/local_server.dyn.py
@@ -1,13 +1,3 @@
-from .command import command
-from .module import ClientModule
 
-
-class ThisModule(ClientModule):
-
-    def __init__(self, module_name, services, config):
-        super().__init__(module_name, services, config)
-        self._local_server_ref = services.local_server_ref
-
-    @command
-    async def open_local_server(self):
-        return self._local_server_ref.load_piece()
+def open_local_server(local_server_ref):
+    return local_server_ref.load_piece()
