c0973460ac78e79cd980da4a7b4676d1dd8f017e
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2022-05-29 10:41:29 +0300

    File bundle: log writes

--- hyperapp/common/file_bundle.dyn.py
+++ hyperapp/common/file_bundle.dyn.py
@@ -1,8 +1,11 @@
+import logging
 from functools import partial
 
 from hyperapp.common.htypes import bundle_t
 from hyperapp.common.htypes.packet_coders import packet_coders
 from hyperapp.common.module import Module
+
+log = logging.getLogger(__name__)
 
 
 DEFAULT_ENCODING = 'json'
@@ -22,6 +25,7 @@ class FileBundle:
         data = packet_coders.encode(self._encoding, bundle)
         self.path.parent.mkdir(parents=True, exist_ok=True)
         self.path.write_bytes(data)
+        log.info("Saved %s to %s (%d bytes)", ref, self.path, len(data))
 
     def load_ref(self):
         bundle = packet_coders.decode(self._encoding, self.path.read_bytes(), bundle_t)
@@ -32,6 +36,7 @@ class FileBundle:
         return bundle.roots[0]
 
     def save_piece(self, piece):
+        log.info("Save %s to %s", piece, self.path)
         ref = self._mosaic.put(piece)
         self.save_ref(ref)
 
