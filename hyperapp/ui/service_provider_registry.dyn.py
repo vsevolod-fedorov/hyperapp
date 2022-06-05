433d5ac3e221eb9974e7dedcabed7b5c666294ae
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2022-06-05 13:24:35 +0300

    Service provider: add servant

--- hyperapp/ui/service_provider_registry.dyn.py
+++ hyperapp/ui/service_provider_registry.dyn.py
@@ -12,8 +12,8 @@ def register_service_provider(piece, web, peer_registry, service_provider_reg):
     service = web.summon(piece.service)
     server_peer = peer_registry.invite(piece.provider)
     spec = web.summon(piece.spec)
-    service_provider_reg[service] = (server_peer, spec)
-    _log.info("Service %s is provided by: %s, spec %s", service, server_peer, spec)
+    service_provider_reg[service] = (server_peer, piece.servant, spec)
+    _log.info("Service %s is provided by: %s, servant %s, spec %s", service, server_peer, piece.servant, spec)
 
 
 def aux_unbundler_hook(web, peer_registry, service_provider_reg, ref, t, value):
@@ -26,7 +26,7 @@ class ThisModule(Module):
     def __init__(self, module_name, services, config):
         super().__init__(module_name, services, config)
 
-        services.service_provider_reg = {}  # service piece value -> provider peer ref
+        services.service_provider_reg = {}  # service piece value -> (provider peer, servant ref, spec)
         services.meta_registry.register_actor(
             htypes.impl.service_provider, register_service_provider, services.web, services.peer_registry, services.service_provider_reg)
         services.aux_unbundler_hooks.append(
