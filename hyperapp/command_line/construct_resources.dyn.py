0266a761a2ef9e09a35718115f3d62a45481e9fe
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2022-06-26 13:37:56 +0300

    Move from htest to separate construct_resources module

--- hyperapp/command_line/construct_resources.dyn.py
+++ hyperapp/command_line/construct_resources.dyn.py
@@ -2,12 +2,14 @@ import logging
 
 log = logging.getLogger(__name__)
 
-from .services import htest, hyperapp_dir
+from .services import (
+    hyperapp_dir,
+    )
 
 
 def construct_resources(source_path_list):
     for path in source_path_list:
-        rel_path = path.relative_to(hyperapp_dir)
+        rel_path = path.absolute().relative_to(hyperapp_dir)
         ext = '.dyn.py'
         if rel_path.name.endswith(ext):
             stem = rel_path.name[:-len(ext)]
@@ -16,7 +18,7 @@ def construct_resources(source_path_list):
             log.error(f"Source file name should end with %r: %s", ext, path)
             continue
         log.info("Construct resources for: %s @ %s", name, hyperapp_dir)
-        resource_module = htest.construct_resources(name, hyperapp_dir)
+        resource_module = services.construct_resources(name, rel_path, hyperapp_dir)
         res_path = path.with_name(stem + '.resources.yaml')
         resource_module.save_as(res_path)
         log.info("Written resource file: %s", res_path)
