c45d22e5b0be81dbc720f95583f7a5a61848bd13
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2022-06-30 11:02:43 +0300

    Resource legacy type: separate loader

--- hyperapp/resource/legacy_type.dyn.py
+++ hyperapp/resource/legacy_type.dyn.py
@@ -1,5 +1,6 @@
 import logging
 from collections import defaultdict
+from functools import partial
 
 from hyperapp.common.module import Module
 
@@ -34,13 +35,18 @@ class LegacyTypeResourceModule:
         return set()
 
 
-def make_legacy_type_resource_module(builtin_types, types, local_types):
+def legacy_builtin_type_resource_loader(types, builtin_types):
     name_to_module = defaultdict(LegacyTypeResourceModule)
     for t in builtin_types.values():
         type_ref = types.reverse_resolve(t)
         type_piece = htypes.legacy_type.type(type_ref)
         name_to_module[f'legacy_type.{t.module_name}'][t.name] = type_piece
         log.info("Legacy type resource %s.%s: %s", t.module_name, t.name, type_piece)
+    return name_to_module
+
+
+def legacy_type_resource_loader(types, local_types):
+    name_to_module = defaultdict(LegacyTypeResourceModule)
     for module_name, local_type_module in local_types.items():
         for name, type_ref in local_type_module.items():
             type_piece = htypes.legacy_type.type(type_ref)
@@ -58,6 +64,9 @@ class ThisModule(Module):
     def __init__(self, module_name, services, config):
         super().__init__(module_name, services, config)
 
+        services.legacy_type_resource_loader = partial(legacy_type_resource_loader, services.types)
         services.resource_module_registry.update(
-            make_legacy_type_resource_module(services.builtin_types, services.types, services.local_types))
+            legacy_builtin_type_resource_loader(services.types, services.builtin_types))
+        services.resource_module_registry.update(
+            services.legacy_type_resource_loader(services.local_types))
         services.python_object_creg.register_actor(htypes.legacy_type.type, python_object, services.types)
