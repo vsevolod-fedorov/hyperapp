e50b05dbe9421e94c93fb04fceb6665f03a51af6
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2022-07-05 12:53:35 +0300

    Object commands factory: extract enum_object_command_pieces method

--- hyperapp/async/ui/object_commands_factory.dyn.py
+++ hyperapp/async/ui/object_commands_factory.dyn.py
@@ -9,10 +9,13 @@ class ObjectCommandsFactory:
         self._lcs = lcs
         self._command_registry = command_registry
 
-    async def get_object_command_list(self, navigator, object_piece, adapter, view):
-        command_piece_it = self._lcs.iter_dir_list_values(
+    async def enum_object_command_pieces(self, adapter):
+        return self._lcs.iter_dir_list_values(
             [[*dir, htypes.command.object_commands_d()] for dir in adapter.dir_list]
             )
+
+    async def get_object_command_list(self, navigator, object_piece, adapter, view):
+        command_piece_it = await self.enum_object_command_pieces(adapter)
         return [
             await self.command_from_piece(command_piece, navigator, object_piece, adapter, view)
             for command_piece in command_piece_it
