e7c63b24ca7e89ca51e85aada3b95e7bde7112f9
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2022-07-10 11:19:46 +0300

    Marker: add 'module' fixture root; add function wrapper functionality

--- hyperapp/guesser/marker.dyn.py
+++ hyperapp/guesser/marker.dyn.py
@@ -25,10 +25,21 @@ class Marker:
             attr_name = full_name.replace('.', '_')
             _log.info("Mark: %s: %s/%s=%r", module.__name__, attr_name, full_name, value)
             module.__dict__[attr_name] = value
-            if RESOURCE_NAMES_ATTR not in module.__dict__:
-                module.__dict__[RESOURCE_NAMES_ATTR] = {}
-            module.__dict__[RESOURCE_NAMES_ATTR][attr_name] = full_name
+            self._set_res_name(module, attr_name, full_name)
+
+    def __call__(self, fn):
+        frame = inspect.stack()[1].frame
+        module = inspect.getmodule(frame)
+        self._set_res_name(module, fn.__name__, self._name)
+
+    def _set_res_name(self, module, attr_name, res_name):
+        try:
+            res_name_dict = module.__dict__[RESOURCE_NAMES_ATTR]
+        except KeyError:
+            res_name_dict = module.__dict__[RESOURCE_NAMES_ATTR] = {}
+        res_name_dict[attr_name] = res_name
 
 
 param = Marker('param')
 service = Marker('service')
+module = Marker('module')
