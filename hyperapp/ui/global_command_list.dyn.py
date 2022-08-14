88d73adad81aea02b0a6893eb208cff4dd7f52ff
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2022-08-14 11:27:11 +0300

    register_global_command: update command hubs

--- hyperapp/ui/global_command_list.dyn.py
+++ hyperapp/ui/global_command_list.dyn.py
@@ -1,11 +1,14 @@
+import asyncio
+
 from hyperapp.common.module import Module
 
 from . import htypes
 
 
-def register_global_command(piece, web, global_command_list):
+def register_global_command(piece, web, event_loop_holder, command_hub_list, global_command_list):
     command = web.summon(piece.command)
     global_command_list.append(command)
+    event_loop_holder.create_task_if_started(command_hub_list.update())
 
 
 class ThisModule(Module):
@@ -15,4 +18,10 @@ class ThisModule(Module):
 
         services.global_command_list = []
         services.meta_registry.register_actor(
-            htypes.global_command.global_command_association, register_global_command, services.web, services.global_command_list)
+            htypes.global_command.global_command_association,
+            register_global_command,
+            services.web,
+            services.event_loop_holder,
+            services.command_hub_list,
+            services.global_command_list,
+            )
