adde0880dca7385ce7a286890edf2a66f5120423
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2022-08-21 13:57:06 +0300

    Constructor creg

---
+++ hyperapp/resource/constructor_creg.dyn.py
@@ -0,0 +1,11 @@
+from hyperapp.common.code_registry import CodeRegistry
+
+from .services import (
+  web,
+  types,
+  )
+from .service_decorator import service
+
+
+
+service.constructor_creg = CodeRegistry('resource_ctr', web, types)
