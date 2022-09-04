ea656750b45f616e0b80e3f229617174258a71ab
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2022-09-04 12:54:22 +0300

    Constructor creg: Add constructor_creg_association

--- hyperapp/resource/constructor_creg.dyn.py
+++ hyperapp/resource/constructor_creg.dyn.py
@@ -3,8 +3,15 @@ from hyperapp.common.code_registry import CodeRegistry
 from .services import (
   web,
   types,
+  python_object_creg,
   )
 from .service_decorator import service
+
+
+def register_constructor(piece):
+  t = types.resolve(piece.t)
+  fn = python_object_creg.invite(piece.fn)
+  constructor_creg.register_actor(t, fn)
 
 
 service.constructor_creg = CodeRegistry('resource_ctr', web, types)
