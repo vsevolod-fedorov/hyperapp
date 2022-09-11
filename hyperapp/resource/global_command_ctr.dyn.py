a7e59e358407160c15c7f9458b6281b768d6d9c1
Vsevolod Fedorov <vfedorov@networkoptix.com>
2022-09-11 13:22:19 +0300

    Complete global command constructor

--- hyperapp/resource/global_command_ctr.dyn.py
+++ hyperapp/resource/global_command_ctr.dyn.py
@@ -1,8 +1,10 @@
 import inspect
+import re
 
 from . import htypes
 from .services import (
-  mosaic,
+    mosaic,
+    resource_type_producer,
   )
 from .constants import RESOURCE_CTR_ATTR
 
@@ -16,5 +18,42 @@ def global_command(fn):
     return fn
 
 
-def construct(piece):
-    assert 0
+# https://stackoverflow.com/a/1176023 Camel case to snake case.
+def camel_to_snake(name):
+    name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+
+
+def _construct_dir(resource_module, target_res_name, dir_t_res_name):
+    call_res_t = resource_type_producer(htypes.call.call)
+    call_def = call_res_t.definition_t(
+        function=dir_t_res_name,
+        )
+    resource_module.set_definition(target_res_name, call_res_t, call_def)
+    resource_module.add_import(dir_t_res_name)
+
+
+def _construct_module_dir(resource_module, type_module_name, target_res_name):
+    dir_t_res_name = f'legacy_type.{type_module_name}:{target_res_name}'
+    _construct_dir(resource_module, target_res_name, dir_t_res_name)
+
+
+def construct(piece, resource_module, module_name, attr):
+    attr_res_name = attr.resource_name or attr.name
+    dir_res_name = camel_to_snake(attr_res_name) + '_d'
+    _construct_module_dir(resource_module, type_module_name=module_name, target_res_name=dir_res_name)
+
+    command_res_t = resource_type_producer(htypes.impl.global_command_impl)
+    command_def = command_res_t.definition_t(
+        function=attr_res_name,
+        dir=dir_res_name,
+        )
+    command_res_name = f'{attr_res_name}.command'
+    resource_module.set_definition(command_res_name, command_res_t, command_def)
+
+    association_res_t = resource_type_producer(htypes.global_command.global_command_association)
+    association_def = association_res_t.definition_t(
+        command=command_res_name,
+        )
+    resource_module.add_association(association_res_t, association_def)
+
