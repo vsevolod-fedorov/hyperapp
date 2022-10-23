cde0b801871191044ffafe8de85de4e210f6e650
Vsevolod Fedorov <vfedorov@networkoptix.com>
2022-10-23 12:57:25 +0300

    Server global commands

---
+++ hyperapp/server/server_global_commands.fixtures.dyn.py
@@ -0,0 +1,32 @@
+from hyperapp.common.htypes import phony_ref
+
+from . import htypes
+from .services import (
+    mosaic,
+    resource_module_registry,
+    types,
+    )
+from .marker import param
+
+
+function_res = resource_module_registry['server.server_global_commands.fixtures.aux']['sample_function']
+
+
+def _phony_global_command():
+    dir_t = htypes.server_global_commands_fixtures.sample_global_command_d
+    dir_t_ref = types.reverse_resolve(dir_t)
+    dir_t_res = htypes.legacy_type.type(dir_t_ref)
+    dir = htypes.call.call(mosaic.put(dir_t_res))
+    return htypes.impl.global_command_impl(
+        function=mosaic.put(function_res),
+        dir=mosaic.put(dir),
+        )
+
+
+param.ServerGlobalCommands.piece = htypes.server_global_commands.server_global_commands()
+param.ServerGlobalCommands.run.current_item = htypes.server_global_commands.item(
+    name='sample-item',
+    function=mosaic.put(function_res),
+    )
+
+param.global_command_to_item.piece = _phony_global_command()
