123201fb71786d2c24897c20e181067ccb571699
Vsevolod Fedorov <vfedorov@networkoptix.com>
2022-11-06 13:07:10 +0300

    Update local_server module

--- hyperapp/client/local_server.dyn.py
+++ hyperapp/client/local_server.dyn.py
@@ -1,3 +1,9 @@
+from .services import (
+    local_server_ref,
+    )
+from .global_command_ctr import global_command
 
-def open_local_server(local_server_ref):
+
+@global_command
+def open_local_server():
     return local_server_ref.load_piece()
--- hyperapp/client/local_server.fixtures.dyn.py
+++ hyperapp/client/local_server.fixtures.dyn.py
@@ -1,8 +1,10 @@
+from .marker import service
+
+
 class _PhonyFileBundle:
 
     def load_piece(self):
         return None
 
 
-def open_local_server_local_server_ref():
-    return _PhonyFileBundle()
+service.local_server_ref = _PhonyFileBundle()
