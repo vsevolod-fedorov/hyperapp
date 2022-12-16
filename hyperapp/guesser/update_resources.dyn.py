166e91c259255e5c817af866d72fd18647dc2e11
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2022-12-16 11:48:37 +0300

    Update resources: group discovered imports

--- hyperapp/guesser/update_resources.dyn.py
+++ hyperapp/guesser/update_resources.dyn.py
@@ -24,7 +24,9 @@ _log = logging.getLogger(__name__)
 AUTO_GEN_LINE = '# Automatically generated file. Do not edit.'
 
 
-FileInfo = namedtuple('FileInfo', 'name source_path resources_path used_modules')
+Deps = namedtuple('Deps', 'want_services want_code tests_services tests_code')
+FileInfo = namedtuple('FileInfo', 'name source_path resources_path deps')
+
 
 process_code_module_list = [
     'common.lcs',
@@ -32,35 +34,6 @@ process_code_module_list = [
     'ui.impl_registry',
     'ui.global_command_list',
     ]
-
-
-def update_resources(root_dir, subdir_list):
-    additional_dir_list = [root_dir / d for d in subdir_list]
-    resource_registry = resource_registry_factory()
-    deps = defaultdict(set)
-    file_dict = {}
-    for subdir in subdir_list:
-        for path in root_dir.joinpath(subdir).rglob('*.dyn.py'):
-            if 'test' in path.parts:
-                continue
-            file = process_file(resource_registry, root_dir, path)
-            if not file:
-                continue
-            _log.debug("File: %s", file)
-            deps[file.name] |= file.used_modules
-            file_dict[file.name] = file
-    for name in file_dict:
-        base_name = '.'.join(name.split('.')[:-1])
-        if base_name in file_dict:
-            deps[base_name].add(name)
-    for name, deps in sorted(deps.items()):
-        _log.info("Dep: %s -> %s", name, ', '.join(deps))
-
-    type_res_list = legacy_type_resources(root_dir, subdir_list)
-
-    with subprocess(additional_dir_list) as process:
-        for file in file_dict.values():
-            load_file_deps(process, type_res_list, file)
 
 
 @contextmanager
@@ -78,30 +51,6 @@ def subprocess(additional_dir_list):
         yield process
 
 
-def process_file(resource_registry, root_dir, path):
-    stem = path.name[:-len('.dyn.py')]
-    yaml_path = path.with_name(stem + '.yaml')
-    if yaml_path.exists():
-        _log.debug("%s: legacy module", path)
-        return None
-    res_path = path.with_name(stem + '.resources.yaml')
-    if res_path.exists() and not res_path.read_text().startswith(AUTO_GEN_LINE):
-        _log.debug("%s: manually generated", path)
-        return None
-    if res_path.exists():
-        res_module = resource_module_factory(resource_registry, stem, res_path)
-        used_modules = {
-            name for name in res_module.used_modules
-            if name.split('.')[0] not in {'legacy_type', 'legacy_service', 'legacy_module'}
-            }
-        _log.info("%s: %s", path, ', '.join(used_modules))
-    else:
-        _log.info("%s: no resources", path)
-        used_modules = set()
-    full_name = str(path.relative_to(root_dir).with_name(stem)).replace('/', '.')
-    return FileInfo(full_name, path, res_path, used_modules)
-
-
 def legacy_type_resources(root_dir, subdir_list):
     custom_types = {**local_types}
     dir_list = [root_dir / d for d in subdir_list]
@@ -117,7 +66,7 @@ def legacy_type_resources(root_dir, subdir_list):
     return resource_list
 
 
-def load_file_deps(process, type_res_list, file):
+def load_file_deps(process, type_res_list, module_name, source_path):
     import_recorder_res = htypes.import_recorder.import_recorder(type_res_list)
     import_recorder_ref = mosaic.put(import_recorder_res)
 
@@ -125,9 +74,9 @@ def load_file_deps(process, type_res_list, file):
     import_discoverer_ref = mosaic.put(import_discoverer_res)
 
     module_res = htypes.python_module.python_module(
-        module_name=file.name,
-        source=file.source_path.read_text(),
-        file_path=str(file.source_path),
+        module_name=module_name,
+        source=source_path.read_text(),
+        file_path=str(source_path),
         import_list=[
             htypes.python_module.import_rec('htypes.*', import_recorder_ref),
             htypes.python_module.import_rec('*', import_discoverer_ref),
@@ -137,4 +86,101 @@ def load_file_deps(process, type_res_list, file):
     collect_attributes = process.rpc_call(collect_attributes_ref)
     object_attrs = collect_attributes(object_ref=mosaic.put(module_res))
     attr_list = [web.summon(ref) for ref in object_attrs.attr_list]
-    _log.info("Collected attrs for %r, module %s: %s", file.name, object_attrs.object_module, attr_list)
+    _log.info("Collected attrs for %r, module %s: %s", module_name, object_attrs.object_module, attr_list)
+
+    import_discoverer = process.proxy(import_discoverer_ref)
+    discovered_imports = import_discoverer.discovered_imports()
+    _log.info("Discovered import list: %s", discovered_imports)
+
+    want_services = set()
+    want_code = set()
+    tests_services = set()
+    tests_code = set()
+    for imp in discovered_imports:
+        if len(imp) < 2:
+            continue
+        kind, name, *_ = imp
+        if kind == 'services':
+            want_services.add(name)
+            continue
+        if kind == 'code':
+            want_code.add(name)
+            continue
+        if kind == 'tested':
+            _, kind, name, *_ = imp
+            if kind == 'services':
+                tests_services.add(name)
+                continue
+            if kind == 'code':
+                tests_code.add(name)
+                continue
+        _log.warning("Unknown import kind (old-style import?): %r at %s", kind, source_path)
+    _log.info("Discovered import deps: want_services: %s", want_services)
+    _log.info("Discovered import deps: want_code: %s", want_code)
+    _log.info("Discovered import deps: tests_services: %s", tests_services)
+    _log.info("Discovered import deps: tests_code: %s", tests_code)
+
+    return Deps(
+        want_services=want_services,
+        want_code=want_code,
+        tests_services=tests_services,
+        tests_code=tests_code,
+        )
+
+
+def load_deps_from_resource(resource_registry, module_name, res_path):
+    res_module = resource_module_factory(resource_registry, module_name, res_path)
+    return {
+        name for name in res_module.used_modules
+        if name.split('.')[0] not in {'legacy_type', 'legacy_service', 'legacy_module'}
+        }
+
+
+def process_file(process, type_res_list, resource_registry, root_dir, source_path):
+    stem = source_path.name[:-len('.dyn.py')]
+    module_name = str(source_path.relative_to(root_dir).with_name(stem)).replace('/', '.')
+    yaml_path = source_path.with_name(stem + '.yaml')
+    if yaml_path.exists():
+        _log.debug("%s: legacy module", source_path)
+        return None
+    res_path = source_path.with_name(stem + '.resources.yaml')
+    if res_path.exists() and not res_path.read_text().startswith(AUTO_GEN_LINE):
+        _log.debug("%s: manually generated", source_path)
+        return None
+    # if res_path.exists():
+    #     used_modules = load_deps_from_resource(resource_registry, module_name, res_path)
+    #     _log.info("%s: %s", path, ', '.join(used_modules))
+    # else:
+    #     _log.info("%s: no resources", path)
+    #     used_modules = set()
+    deps = load_file_deps(process, type_res_list, module_name, source_path)
+    return FileInfo(module_name, source_path, res_path, deps)
+
+
+def update_resources(root_dir, subdir_list):
+    additional_dir_list = [root_dir / d for d in subdir_list]
+    resource_registry = resource_registry_factory()
+    deps = defaultdict(set)
+    file_dict = {}
+
+    type_res_list = legacy_type_resources(root_dir, subdir_list)
+
+    with subprocess(additional_dir_list) as process:
+
+        for subdir in subdir_list:
+            for path in root_dir.joinpath(subdir).rglob('*.dyn.py'):
+                if 'test' in path.parts:
+                    continue
+                file = process_file(process, type_res_list, resource_registry, root_dir, path)
+                if not file:
+                    continue
+                _log.debug("File: %s", file)
+                deps[file.name] |= file.deps.want_code
+                file_dict[file.name] = file
+
+    for name in file_dict:
+        base_name = '.'.join(name.split('.')[:-1])
+        if base_name in file_dict:
+            deps[base_name].add(name)
+    for name, deps in sorted(deps.items()):
+        _log.info("Dep: %s -> %s", name, ', '.join(deps))
