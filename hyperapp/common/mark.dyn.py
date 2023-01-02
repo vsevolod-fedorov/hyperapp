ac187a0a9e1c9d1950dfdaa57d874b59ef04aef3
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-01-02 13:28:40 +0300

    Mark: Manual resources

--- hyperapp/common/mark.dyn.py
+++ hyperapp/common/mark.dyn.py
@@ -1,3 +1,12 @@
+import inspect
+from types import SimpleNamespace
+
+from . import htypes
+from .services import (
+    mosaic,
+    )
+from .code.constants import RESOURCE_CTR_ATTR
+
 
 class Marker:
 
@@ -10,8 +19,16 @@ class Marker:
         return Marker(f'{self._name}.{name}')
 
     def __call__(self, fn):
+        module = inspect.getmodule(fn)
+        ctr_dict = module.__dict__.setdefault(RESOURCE_CTR_ATTR, {})
+        ctr_list = ctr_dict.setdefault(fn.__name__, [])
+        ctr = htypes.attr_constructors.service(name=fn.__name__)
+        ctr_list.append(mosaic.put(ctr))
         return fn
 
 
-param = Marker('param')
-service = Marker('service')
+def mark():
+    return SimpleNamespace(
+        param=Marker('param'),
+        service=Marker('service'),
+        )
