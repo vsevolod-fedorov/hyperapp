4deace02de8bc69d51224a46d968caed3bd7fabd
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-01-07 11:03:05 +0300

    Parameter constructor

--- hyperapp/guesser/parameter_ctr.dyn.py
+++ hyperapp/guesser/parameter_ctr.dyn.py
@@ -1,3 +1,17 @@
+from . import htypes
+from .services import (
+    mark,
+    mosaic,
+    )
 
-def construct(piece, custom_types, resource_module, attr):
-    assert 0, f"todo: {attr!r}"
+
+def construct(piece, custom_types, resource_module, module_res, attr):
+    attribute = htypes.attribute.attribute(
+        object=mosaic.put(module_res),
+        attr_name=attr.name,
+        )
+    resource_module[attr.name] = attribute
+    parameter = htypes.call.call(
+        function=mosaic.put(attribute),
+        )
+    resource_module['.'.join(piece.path)] = parameter
