3792ffe9151e91102ddfaf4873021690a2db6b88
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-01-08 11:01:24 +0300

    Fixtures: use param as decorator

--- hyperapp/resource/constructor_creg.fixtures.dyn.py
+++ hyperapp/resource/constructor_creg.fixtures.dyn.py
@@ -1,13 +1,13 @@
 from . import htypes
 from .services import (
-  mosaic,
-  types,
-  resource_registry,
-  )
-from .marker import param
+    mark,
+    mosaic,
+    types,
+    resource_registry,
+    )
 
 
-@param.register_constructor
+@mark.param.register_constructor
 def piece():
     return htypes.constructor_creg.constructor_creg_association(
         t=types.reverse_resolve(htypes.constructor_creg_fixtures.sample_type),
