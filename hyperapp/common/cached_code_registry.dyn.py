533983207868015b97e4a1c01cb6d4ede80f0a63
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-01-08 14:00:01 +0300

    CachedCodeRegistry: add reverse_resolve method

--- hyperapp/common/cached_code_registry.dyn.py
+++ hyperapp/common/cached_code_registry.dyn.py
@@ -6,6 +6,7 @@ class CachedCodeRegistry(CodeRegistry):
     def __init__(self, produce_name, web, types):
         super().__init__(produce_name, web, types)
         self._cache = {}  # piece -> actor
+        self._reverse_cache = {}  # actor id -> piece
 
     def _animate(self, t, piece, args, kw):
         try:
@@ -14,4 +15,8 @@ class CachedCodeRegistry(CodeRegistry):
             pass
         actor = super()._animate(t, piece, args, kw)
         self._cache[piece] = actor
+        self._reverse_cache[id(actor)] = piece
         return actor
+
+    def reverse_resolve(self, actor):
+        return self._reverse_cache[id(actor)]
