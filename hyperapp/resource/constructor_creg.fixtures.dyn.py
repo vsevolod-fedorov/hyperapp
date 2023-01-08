0c132b4dae2332c340d00e1e4b96f2370e874abf
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-01-08 13:20:41 +0300

    Update constructor_creg.fixtures

--- hyperapp/resource/constructor_creg.fixtures.dyn.py
+++ hyperapp/resource/constructor_creg.fixtures.dyn.py
@@ -9,7 +9,12 @@ from .services import (
 
 @mark.param.register_constructor
 def piece():
+    module_res = resource_registry['resource.constructor_creg.fixtures.aux', 'constructor_creg.fixtures.aux.module']
+    fn_res = htypes.attribute.attribute(
+        object=mosaic.put(module_res),
+        attr_name='sample_function',
+        )
     return htypes.constructor_creg.constructor_creg_association(
         t=types.reverse_resolve(htypes.constructor_creg_fixtures.sample_type),
-        fn=mosaic.put(resource_registry['resource.constructor_creg.fixtures.aux', 'sample_function']),
+        fn=mosaic.put(fn_res),
         )
