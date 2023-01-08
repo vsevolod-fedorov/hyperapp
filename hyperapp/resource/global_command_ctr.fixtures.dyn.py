8fd3cef8c57af621a21ca35c3be1c58ae1bcf13e
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-01-08 14:05:39 +0300

    Update global_command_ctr.fixtures

--- hyperapp/resource/global_command_ctr.fixtures.dyn.py
+++ hyperapp/resource/global_command_ctr.fixtures.dyn.py
@@ -1,39 +1,56 @@
 from . import htypes
 from .services import (
     local_types,
+    mark,
+    mosaic,
     resource_module_factory,
     )
-from .marker import param
-from .resource_registry import ResourceRegistry
+from .code.resource_registry import ResourceRegistry
 
 
 def _dummy_fn():
     pass
 
 
-param.global_command.fn = _dummy_fn
-
-param.camel_to_snake.name = ''
-
-param.construct.piece = None
-param.construct.custom_types = local_types
+@mark.param.global_command
+def fn():
+    return _dummy_fn
 
 
-@param.construct
+@mark.param.camel_to_snake
+def  name():
+    return ''
+
+
+@mark.param.construct
+def piece():
+    return None
+
+
+@mark.param.construct
+def custom_types():
+    return local_types
+
+
+@mark.param.construct
 def resource_module():
     return resource_module_factory(
-        resource_registry=ResourceRegistry(),
+        resource_registry=ResourceRegistry(mosaic),
         name='sample_resource_module',
-        path='/non-existend-dir/sample_module.resources.yaml',
-        load_from_file=False,
         )
 
 
-param.construct.module_name = 'global_command_ctr_fixtures'
-param.construct.attr = htypes.inspect.fn_attr(
-    name='sample_fn',
-    module=None,
-    resource_name=None,
-    constructors=[],
-    param_list=[],
-    )
+@mark.param.construct
+def module_name():
+    return 'global_command_ctr_fixtures'
+
+
+@mark.param.construct
+def attr():
+    return htypes.inspect.fn_attr(
+        name='sample_fn',
+        module=None,
+        resource_name=None,
+        constructors=[],
+        param_list=[],
+        )
