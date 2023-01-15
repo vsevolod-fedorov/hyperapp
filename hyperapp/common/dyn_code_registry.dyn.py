30bf34329a02b9609755882c9aaf4f23ed148897
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-01-15 13:05:54 +0300

    Dyn code registry: rename types; return fn from decorator

--- hyperapp/common/dyn_code_registry.dyn.py
+++ hyperapp/common/dyn_code_registry.dyn.py
@@ -14,10 +14,11 @@ class DynCodeRegistry(CodeRegistry):
         def register(fn):
             service_res = python_object_creg.reverse_resolve(self)
             type_res = python_object_creg.reverse_resolve(t)
-            ctr = htypes.code_registry.code_registry_constructor(
+            ctr = htypes.code_registry.constructor(
                 service=mosaic.put(service_res),
                 type=mosaic.put(type_res),
                 )
             add_fn_module_constructor(fn, ctr)
+            return fn
 
         return register
