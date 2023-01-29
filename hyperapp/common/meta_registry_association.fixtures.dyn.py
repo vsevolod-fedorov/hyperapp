802e62d35efd6eabd04223e2d1c4c5627c74cbb2
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-01-29 12:38:12 +0300

    Improve meta_registry_association fixtures

--- hyperapp/common/meta_registry_association.fixtures.dyn.py
+++ hyperapp/common/meta_registry_association.fixtures.dyn.py
@@ -1,15 +1,33 @@
 from hyperapp.common.code_registry import CodeRegistry
 
+from . import htypes
 from .services import (
     mark,
+    mosaic,
+    python_object_creg,
+    resource_registry,
     types,
     web,
     )
 
 
+def _sample_fn():
+    pass
+
+
 @mark.param.register_meta
 def piece():
-    return None
+    t = htypes.meta_registry_association_fixtures.sample
+    t_res = python_object_creg.reverse_resolve(t)
+    this_module_res = resource_registry['common.meta_registry_association.fixtures', 'meta_registry_association.fixtures.module']
+    fn_res = htypes.attribute.attribute(
+        object=mosaic.put(this_module_res),
+        attr_name='_sample_fn',
+        )
+    return htypes.meta_registry.meta_association(
+        t=mosaic.put(t_res),
+        fn=mosaic.put(fn_res),
+        )
 
 
 @mark.service
