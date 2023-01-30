bc3b4d91b65b8ace762bb3feaae7fa325d72f8ff
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-01-30 11:56:55 +0300

    Global command constructor

---
+++ hyperapp/guesser/global_command_ctr.fixtures.dyn.py
@@ -0,0 +1,50 @@
+from . import htypes
+from .services import (
+    local_types,
+    mark,
+    resource_module_factory,
+    resource_registry_factory,
+    )
+
+
+@mark.param.camel_to_snake
+def name():
+    return ''
+
+
+@mark.param.construct
+def piece():
+    return None
+
+
+@mark.param.construct
+def custom_types():
+    return local_types
+
+
+@mark.param.construct
+def resource_module():
+    registry = resource_registry_factory()
+    module = resource_module_factory(registry, 'global_command_ctr_fixtures')
+    module['sample_module'] = module_res()
+    return module
+
+
+@mark.param.construct
+def module_res():
+    return htypes.python_module.python_module(
+        module_name='sample_module',
+        source='',
+        file_path='/sample_module.dyn.py',
+        import_list=(),
+        )
+
+
+@mark.param.construct
+def attr():
+    return htypes.inspect.attr(
+        name='sample_attr',
+        module=None,
+        resource_name=None,
+        constructors=(),
+        )
