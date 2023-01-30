9ffb284e33d2046b27ae3a97a2178f9089109cde
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-01-30 12:51:07 +0300

    Mark: fix parameter function copying

--- hyperapp/common/mark.dyn.py
+++ hyperapp/common/mark.dyn.py
@@ -8,11 +8,22 @@ from .services import (
 from .code.constants import RESOURCE_CTR_ATTR
 
 
-def add_fn_module_constructor(fn, ctr):
-    module = inspect.getmodule(fn)
+def _copy_constructors(module, source_name, target_name):
     ctr_dict = module.__dict__.setdefault(RESOURCE_CTR_ATTR, {})
-    ctr_list = ctr_dict.setdefault(fn.__name__, [])
+    source_ctr_list = ctr_dict.setdefault(source_name, [])
+    target_ctr_list = ctr_dict.setdefault(target_name, [])
+    target_ctr_list += source_ctr_list
+
+
+def _add_constructor(module, fn_name, ctr):
+    ctr_dict = module.__dict__.setdefault(RESOURCE_CTR_ATTR, {})
+    ctr_list = ctr_dict.setdefault(fn_name, [])
     ctr_list.append(mosaic.put(ctr))
+
+
+def add_fn_module_constructor(fn, ctr, name=None):
+    module = inspect.getmodule(fn)
+    _add_constructor(module, fn.__name__, ctr)
 
 
 class ServiceMarker:
@@ -36,12 +47,9 @@ class ParamMarker:
         return ParamMarker([*self._path, name])
 
     def __call__(self, fn):
-        ctr = htypes.attr_constructors.parameter(
-            path=[*self._path, fn.__name__],
-            )
-        add_fn_module_constructor(fn, ctr)
         module = inspect.getmodule(fn)
         name = fn.__name__
+        attr_name = name
         try:
             prev_fn = getattr(module, name)
         except AttributeError:
@@ -55,9 +63,16 @@ class ParamMarker:
                 idx += 1
             if idx == 1:
                 # Save previous one.
-                setattr(module, f'{name}_{idx}', prev_fn)
+                attr_name = f'{name}_{idx}'
+                setattr(module, attr_name, prev_fn)
+                _copy_constructors(module, name, attr_name)
                 idx += 1
-            setattr(module, f'{name}_{idx}', fn)
+            attr_name = f'{name}_{idx}'
+            setattr(module, attr_name, fn)
+        ctr = htypes.attr_constructors.parameter(
+            path=[*self._path, fn.__name__],
+            )
+        _add_constructor(module, attr_name, ctr)
         return fn
 
 
