ebe6e54ce6f2467d9edb8bff82ac67c27c319fbe
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-02-03 12:44:43 +0300

    Resource updater: use types collected during tests

--- hyperapp/server/tested_module.dyn.py
+++ hyperapp/server/tested_module.dyn.py
@@ -1,6 +1,13 @@
 import logging
 
+from . import htypes
+
 log = logging.getLogger(__name__)
+
+
+def sample_fn(some_arg):
+    log.info("sample_fn: %s", some_arg)
+    return htypes.tested_module.two()
 
 
 log.info("Tested module")
--- hyperapp/server/tested_module.tests.dyn.py
+++ hyperapp/server/tested_module.tests.dyn.py
@@ -1,5 +1,6 @@
 import logging
 
+from . import htypes
 from .tested.code import (
     tested_module,
     )
@@ -8,4 +9,6 @@ log = logging.getLogger(__name__)
 
 
 def test_one():
+    one = htypes.tested_module.one()
+    two = tested_module.sample_fn(12345)
     log.info("Test one: tested_module=%r", tested_module)
