0c6d15ac6f988179e59d930200987900eb92fdc1
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-02-05 10:49:23 +0300

    Update construct_resources_sample test resources

--- hyperapp/guesser/test/test_resources/construct_resources_sample.fixtures.dyn.py
+++ hyperapp/guesser/test/test_resources/construct_resources_sample.fixtures.dyn.py
@@ -1,14 +1,29 @@
 import logging
 
 from . import htypes
-from .marker import param
+from .services import mark
 
 log = logging.getLogger(__name__)
 
 
-param.SampleServant.piece = htypes.construct_resources_sample.sample(id=123)
-param.SampleServant.open.current_key = None
-param.sample_command.adapter = None
+@mark.param.SampleList
+def piece():
+    return htypes.construct_resources_sample.sample(id=123)
+
+
+@mark.param.SampleList.open
+def current_key():
+    return None
+
+
+@mark.param.sample_object_command
+def piece():
+    return None
+
+
+@mark.param.sample_object_command
+def view_state():
+    return None
 
 
 log.info("construct_resources_sample fixture module is loaded; sample_item: %s", htypes.construct_resources_sample.sample_item)
