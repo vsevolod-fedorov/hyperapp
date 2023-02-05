0afdb87d67fdb8f489be8a5170b0ad6f41ce5dc2
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-02-05 10:48:20 +0300

    Resource updater: add/fix support for additional root dirs

--- hyperapp/command_line/update_resources.dyn.py
+++ hyperapp/command_line/update_resources.dyn.py
@@ -10,9 +10,9 @@ from .services import (
 from . import meta_registry_association
 
 
-def update_resources(update_resources_res, root_dir, subdir_list, module_list):
+def update_resources(update_resources_res, subdir_list, root_dirs, module_list):
     generator_ref = mosaic.put(update_resources_res)
     meta_registry_association.init()
     register_associations(resource_registry)
-    log.info("Update resources at: %s, %s: %s", root_dir, subdir_list, module_list)
-    services.update_resources(generator_ref, root_dir, subdir_list, module_list)
+    log.info("Update resources at: %s, %s: %s", subdir_list, root_dirs, module_list)
+    services.update_resources(generator_ref, subdir_list, root_dirs, module_list)
