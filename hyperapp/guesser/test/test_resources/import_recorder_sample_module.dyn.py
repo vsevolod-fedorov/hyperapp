3b9cbd3f8b08860aa271fd9c70d5bf667baae345
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-02-05 12:51:40 +0300

    Fix test sample modules prints

--- hyperapp/guesser/test/test_resources/import_recorder_sample_module.dyn.py
+++ hyperapp/guesser/test/test_resources/import_recorder_sample_module.dyn.py
@@ -5,11 +5,11 @@ from .htypes.sample_types import sample_rec
 from .htypes.sample_types import another_rec
 
 
-print("sample type 1: %s", htypes.sample_types.sample_rec)
-print("sample type 2: %s", sample_types.sample_rec)
-print("sample type 3: %s", sample_rec)
+print(f"sample type 1: {htypes.sample_types.sample_rec}")
+print(f"sample type 2: {sample_types.sample_rec}")
+print(f"sample type 3: {sample_rec}")
 
-print("another type 3: %s", another_rec)
+print(f"another type 3: {another_rec}")
 
 assert htypes.sample_types.sample_rec is sample_types.sample_rec
 assert htypes.sample_types.sample_rec is sample_rec
