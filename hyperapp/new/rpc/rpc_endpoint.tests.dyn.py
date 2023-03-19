17248c3077dfa6e19c402638f1a7c3c23ef2016f
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-03-19 13:12:02 +0300

    rpc_endpoint: Add tests; minor improvement

---
+++ hyperapp/new/rpc/rpc_endpoint.tests.dyn.py
@@ -0,0 +1,54 @@
+from . import htypes
+from .services import (
+    generate_rsa_identity,
+    mosaic,
+    python_object_creg,
+    )
+from .code.endpoint import Request
+from .tested.services import rpc_endpoint_factory
+
+
+def _return_str_list(phony_param):
+    return [
+        mosaic.put("Sample result string"),
+        ]
+
+
+def _raise_happ_error(phony_param):
+    raise htypes.rpc_endpoint_tests.sample_error("sample error message")
+
+
+def _raise_non_happ_error(phony_param):
+    raise RuntimeError("Sample non-happ error")
+
+
+def _run_test_with_servant(servant_fn):
+    python_object_creg.set_phony_servant(servant_fn)
+    sender_identity = generate_rsa_identity(fast=True)
+    rpc_request = htypes.rpc.request(
+        request_id='phony request id',
+        servant_ref=mosaic.put('phony servant'),
+        params=[
+            htypes.rpc.param('phony_param', mosaic.put('phony param')),
+        ],
+        sender_peer_ref=mosaic.put(sender_identity.peer.piece),
+        )
+    request = Request(
+        receiver_identity=None,
+        sender=None,
+        ref_list=[mosaic.put(rpc_request)],
+        )
+    endpoint = rpc_endpoint_factory()
+    endpoint.process(request)
+
+
+def test_str_list_response():
+    _run_test_with_servant(_return_str_list)
+
+
+def test_happ_error():
+    _run_test_with_servant(_raise_happ_error)
+
+
+def test_non_happ_error():
+    _run_test_with_servant(_raise_non_happ_error)
