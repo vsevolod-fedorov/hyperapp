9c217eb67a0ced471fd6f496e6ea4d264af83791
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-04-02 12:32:55 +0300

    Implement partial_ref service

---
+++ hyperapp/resource/partial_ref.dyn.py
@@ -0,0 +1,24 @@
+from . import htypes
+from .services import (
+    fn_to_ref,
+    mark,
+    mosaic,
+    )
+
+
+@mark.service
+def partial_ref():
+    def _partial_ref(fn, **kw):
+        partial = htypes.partial.partial(
+            function=fn_to_ref(fn),
+            params=[
+                htypes.partial.param(
+                    name,
+                    mosaic.put(htypes.raw.raw(
+                        mosaic.put(value))))
+                for name, value in kw.items()
+                ],
+            )
+        return mosaic.put(partial)
+
+    return _partial_ref
