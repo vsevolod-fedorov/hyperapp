68aa1684946e5505e1f6dfc975f121e2f84b93e1
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-04-30 11:20:02 +0300

    Subprocess rpc server test: add callback

---
+++ hyperapp/new/rpc/rpc_proxy.dyn.py
@@ -0,0 +1,28 @@
+from . import htypes
+from .services import (
+    mosaic,
+    rpc_call_factory,
+    )
+
+
+class RpcProxy:
+
+    def __init__(self, rpc_endpoint, identity, peer, servant_ref, timeout_sec=10):
+        self._rpc_endpoint = rpc_endpoint
+        self._identity = identity
+        self._peer = peer
+        self._servant_ref = servant_ref
+        self._timeout_sec = timeout_sec
+
+    def __getattr__(self, name):
+        fn_res = htypes.attribute.attribute(
+            object=self._servant_ref,
+            attr_name=name,
+            )
+        fn_ref = mosaic.put(fn_res)
+        rpc_call = rpc_call_factory(self._rpc_endpoint, self._peer, fn_ref, self._identity, self._timeout_sec)
+
+        def method(*args, **kw):
+            return rpc_call(*args, **kw)
+
+        return method
--- hyperapp/new/subprocess/subprocess_rpc_server.dyn.py
+++ hyperapp/new/subprocess/subprocess_rpc_server.dyn.py
@@ -8,25 +8,44 @@ from .services import (
     fn_to_ref,
     mark,
     partial_ref,
+    peer_registry,
+    rpc_call_factory,
     subprocess_running_2,
     )
 from .code.subprocess_rpc_server_main import rpc_server_main
+from .code.rpc_proxy import RpcProxy
 
 log = logging.getLogger(__name__)
 
 
 class _RpcServerProcess:
 
-    def __init__(self, connection):
+    def __init__(self, id, name, connection, peer, rpc_endpoint, identity, timeout_sec):
+        self.id = id
+        self.name = name
         self.connection = connection
+        self.peer = peer
+        self._rpc_endpoint = rpc_endpoint
+        self._identity = identity
+        self._timeout_sec = timeout_sec
+
+    def rpc_call(self, servant_fn):
+        servant_fn_ref = fn_to_ref(servant_fn)
+        return rpc_call_factory(
+            self._rpc_endpoint, self.peer, servant_fn_ref, self._identity, self._timeout_sec)
+
+    def proxy(self, servant_ref):
+        return RpcProxy(self._rpc_endpoint, self._identity, self.peer, servant_ref, self._timeout_sec)
 
 
 _subprocess_id_counter = itertools.count()
 _callback_signals = {}  # subprocess_id -> event.
+_subprocess_peer = {}  # subprocess_id -> Peer.
 
 
-def _rpc_subprocess_callback(subprocess_id):
+def _rpc_subprocess_callback(subprocess_id, subprocess_peer):
     log.info("Rpc subprocess callback is called")
+    _subprocess_peer[subprocess_id] = peer_registry.animate(subprocess_peer)
     _callback_signals[subprocess_id].set()
 
 
@@ -34,7 +53,7 @@ def _rpc_subprocess_callback(subprocess_id):
 def subprocess_rpc_server_running():
 
     @contextmanager
-    def _subprocess_rpc_server(name, rpc_endpoint, identity):
+    def _subprocess_rpc_server(name, rpc_endpoint, identity, timeout_sec=3):
         subprocess_id = next(_subprocess_id_counter)
         _callback_signals[subprocess_id] = event = threading.Event()
         main_ref = partial_ref(
@@ -45,11 +64,12 @@ def subprocess_rpc_server_running():
             subprocess_id=subprocess_id,
         )
         with subprocess_running_2(name, main_ref) as process:
-            connection = add_subprocess_server_connection(name, process.connection)
+            connection_rec = add_subprocess_server_connection(name, process.connection)
             try:
                 if not event.wait(timeout=3):
                     raise RuntimeError(f"Timed out waiting for subprocess #{subprocess_id} {name!r} (3 sec)")
-                yield _RpcServerProcess(process.connection)
+                peer = _subprocess_peer[subprocess_id]
+                yield _RpcServerProcess(subprocess_id, name, process.connection, peer, rpc_endpoint, identity, timeout_sec)
             finally:
-                connection.close()
+                connection_rec.close()
     return _subprocess_rpc_server
