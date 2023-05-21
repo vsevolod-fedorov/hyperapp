44725a36c9a3dcccfca2a5bccd4c160946b9c467
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-05-21 11:18:20 +0300

    Unbundler: add logging

--- hyperapp/common/unbundler.dyn.py
+++ hyperapp/common/unbundler.dyn.py
@@ -19,9 +19,11 @@ class Unbundler(object):
             self._mosaic.register_capsule(capsule)
         for aux_ref in bundle.aux_roots:
             decoded_capsule = self._mosaic.resolve_ref(aux_ref)
+            log.debug("Unbundle aux: %s %s: %s", aux_ref, decoded_capsule.t, decoded_capsule.value)
             handled_by_hook = False
             for hook in self._aux_unbundler_hooks:
                 if hook(aux_ref, decoded_capsule.t, decoded_capsule.value):
+                    log.debug("Unbundle aux: handled by hook: %s", hook)
                     handled_by_hook = True
             if not handled_by_hook:
                 self._association_reg.register_association(decoded_capsule.value)
