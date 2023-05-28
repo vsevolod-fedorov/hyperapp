c980c9f439e67e4597b070d6b3dabec3f3639fd8
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-05-28 12:45:03 +0300

    Convert 'call' resource module to resource-style

--- server.py
+++ server.py
@@ -32,7 +32,6 @@ code_module_list = [
     'resource.legacy_type',
     'resource.attribute',
     'resource.partial',
-    'resource.call',
     'resource.python_module',
     'transport.rsa_identity',
     'ui.impl_registry',
