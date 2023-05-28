30be32e41ab4ffe6ca21f0079b9ccad83c7cbe58
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-05-28 12:33:05 +0300

    Convert 'raw' resource module to resource-style

--- hyperapp/resource/raw.dyn.py
+++ hyperapp/resource/raw.dyn.py
@@ -1,17 +1,7 @@
-from functools import partial
-
-from hyperapp.common.module import Module
-
-from . import htypes
+from .services import (
+    web,
+    )
 
 
-def python_object(piece, web):
+def python_object(piece):
     return web.summon(piece.resource)
-
-
-class ThisModule(Module):
-
-    def __init__(self, module_name, services, config):
-        super().__init__(module_name, services, config)
-
-        services.python_object_creg.register_actor(htypes.raw.raw, python_object, services.web)
