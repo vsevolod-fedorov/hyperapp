468b9f88645ef112b39f6b5d1e299ce2ab192152
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-06-12 12:19:47 +0300

    Return base for python object association so that it is bundled for it's type

--- hyperapp/resource/pyobj_meta.py
+++ hyperapp/resource/pyobj_meta.py
@@ -1,5 +1,7 @@
 import logging
 
+from ..common.association_registry import Association
+
 log = logging.getLogger(__name__)
 
 
@@ -8,3 +10,9 @@ def register_pyobj_meta(python_object_creg, piece):
     function = python_object_creg.invite(piece.function)
     log.info("Register python object: %s -> %s", t, function)
     python_object_creg.register_actor(t, function)
+    # Register manually because python_object_creg does not support association_reg lookup.
+    # But also return base so that it would be bundled with registered type.
+    return Association(
+        bases=[t],
+        key_to_value={},
+        )
