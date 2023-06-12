609693ceb2f6bacaa83e8806b205417c93497e33
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-06-12 11:58:29 +0300

    Return base for meta association so that it is bundled for it's type

--- hyperapp/common/meta_registry_association.py
+++ hyperapp/common/meta_registry_association.py
@@ -1,12 +1,19 @@
 from functools import partial
 
 from .htypes.meta_association import meta_association
+from .association_registry import Association
 
 
 def register_meta(meta_registry, python_object_creg, piece):
     t = python_object_creg.invite(piece.t)
     fn = python_object_creg.invite(piece.fn)
     meta_registry.register_actor(t, fn)
+    # Register manually because meta_registry does not support association_reg lookup.
+    # But also return base so that it would be bundled with registered type.
+    return Association(
+        bases=[t],
+        key_to_value={},
+        )
 
 
 def register_meta_association(meta_registry, python_object_creg):
