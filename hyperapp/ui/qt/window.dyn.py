61f58c1978fb51e5f47dfad495e5dda3b10b9b90
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-07-16 12:24:08 +0300

    Qt window: Convert to controller

--- hyperapp/ui/qt/window.dyn.py
+++ hyperapp/ui/qt/window.dyn.py
@@ -6,11 +6,15 @@ from . import htypes
 DUP_OFFSET = htypes.window.pos(150, 50)
 
 
-class Window(QtWidgets.QMainWindow):
+class WindowCtl:
 
     def __init__(self, piece):
         super().__init__()
-        self.setCentralWidget(QtWidgets.QLabel("Hello!"))
 
-    def duplicate(self):
+    def construct_widget(self, ctx):
+        w = QtWidgets.QMainWindow()
+        w.setCentralWidget(QtWidgets.QLabel("Hello!"))
+        return w
+
+    def duplicate(self, widget):
         pass
