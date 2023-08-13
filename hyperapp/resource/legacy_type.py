8dd044f31f87f9b1fb08bbf6f429939694769fcb
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-08-13 13:42:50 +0300

    Add builtin types to python_object_creg cache

--- hyperapp/resource/legacy_type.py
+++ hyperapp/resource/legacy_type.py
@@ -43,6 +43,13 @@ class LegacyTypeResourceModule:
 #     return name_to_module
 
 
+def add_builtin_types_to_pyobj_cache(types, builtin_types, python_object_creg):
+    for t in builtin_types.values():
+        type_ref = types.reverse_resolve(t)
+        type_piece = legacy_type_t(type_ref)
+        python_object_creg.add_to_cache(type_piece, t)
+
+
 def convert_builtin_types_to_dict(types, builtin_types):
     name_to_module = defaultdict(dict)
     for t in builtin_types.values():
