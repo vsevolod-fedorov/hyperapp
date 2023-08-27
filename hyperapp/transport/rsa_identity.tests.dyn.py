da4df37671ffb9a26726ceb5c78075936c5371eb
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-08-27 11:01:39 +0300

    bundle_t: Rename aux_roots -> associations

--- hyperapp/transport/rsa_identity.tests.dyn.py
+++ hyperapp/transport/rsa_identity.tests.dyn.py
@@ -31,7 +31,7 @@ def test_rsa_parcel():
 
     test_ref = mosaic.put(receiver_identity.peer.piece)
 
-    bundle_1 = bundle_t(roots=(test_ref,), aux_roots=(), capsule_list=())
+    bundle_1 = bundle_t(roots=(test_ref,), associations=(), capsule_list=())
     parcel_1 = receiver_identity.peer.make_parcel(bundle_1, sender_identity)
 
     parcel_2 = parcel_registry.animate(parcel_1.piece)
