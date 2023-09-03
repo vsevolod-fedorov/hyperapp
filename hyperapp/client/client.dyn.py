713b310ddbb81efb0ae61aa6c1f36b1b93567bb6
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-09-03 13:24:55 +0300

    Client: Use ui_ctl_creg

--- hyperapp/client/client.dyn.py
+++ hyperapp/client/client.dyn.py
@@ -1,18 +1,27 @@
 from PySide6 import QtWidgets
 
+from . import htypes
 from .services import (
+    mosaic,
     ui_ctl_creg,
     )
+
+
+def make_window_layout():
+    return htypes.window.layout(
+        menu_bar_ref=mosaic.put(htypes.menu_bar.menu_bar()),
+        command_pane_ref=mosaic.put(htypes.command_pane.command_pane()),
+        central_view_ref=mosaic.put('phony view'),
+        )
 
 
 def _main():
     app = QtWidgets.QApplication()
 
-    window = QtWidgets.QMainWindow()
-    window.resize(1000, 800)
+    window_layout = make_window_layout()
+    window_ctl = ui_ctl_creg.animate(window_layout)
+    window = window_ctl.construct_widget(None, None)
     window.move(500, 100)
     window.show()
-
-    window.setCentralWidget(QtWidgets.QLabel("Hello!"))
 
     return app.exec()
