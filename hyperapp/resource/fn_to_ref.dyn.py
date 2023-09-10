5f5a3eaf2bc7d23ab098276258137d1b213d09ad
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-09-10 12:22:37 +0300

    Add fn_to_res service

--- hyperapp/resource/fn_to_ref.dyn.py
+++ hyperapp/resource/fn_to_ref.dyn.py
@@ -8,16 +8,22 @@ from .services import (
     )
 
 
+def _fn_to_res(fn):
+    module = inspect.getmodule(fn)
+    module_res = pyobj_creg.reverse_resolve(module)
+    return htypes.builtin.attribute(
+        object=mosaic.put(module_res),
+        attr_name=fn.__name__,
+        )
+
+
+@mark.service
+def fn_to_res():
+    return _fn_to_res
+
+
 @mark.service
 def fn_to_ref():
-
     def _fn_to_ref(fn):
-        module = inspect.getmodule(fn)
-        module_res = pyobj_creg.reverse_resolve(module)
-        fn_res = htypes.builtin.attribute(
-            object=mosaic.put(module_res),
-            attr_name=fn.__name__,
-            )
-        return mosaic.put(fn_res)
-
+        return mosaic.put(_fn_to_res(fn))
     return _fn_to_ref
