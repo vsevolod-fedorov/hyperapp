6a380b87b454c5058c9ea0c11dab88b8498a3c3f
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-09-10 11:21:19 +0300

    Combine application, window and tabs

--- hyperapp/ui/qt/application.tests.dyn.py
+++ hyperapp/ui/qt/application.tests.dyn.py
@@ -8,28 +8,37 @@ from .services import (
     )
 
 
-def make_window_layout():
-    return htypes.window.layout(
+def make_layout():
+    tabs_layout = htypes.tabs.layout(
+        tab_list=[mosaic.put("Nothing is here")],
+        )
+    window_layout = htypes.window.layout(
         menu_bar_ref=mosaic.put(htypes.menu_bar.menu_bar()),
         command_pane_ref=mosaic.put(htypes.command_pane.command_pane()),
-        central_view_ref=mosaic.put('phony view'),
+        central_view_ref=mosaic.put(tabs_layout),
+        )
+    return htypes.application.layout(
+        window_list=[mosaic.put(window_layout)],
         )
 
 
-def make_window_state():
-    return htypes.window.state(
+def make_state():
+    tabs_state = htypes.tabs.state(
+        current_tab=0,
+        )
+    window_state = htypes.window.state(
         size=htypes.window.size(100, 100),
         pos=htypes.window.pos(10, 10),
+        central_view_state=mosaic.put(tabs_state),
+        )
+    return htypes.application.state(
+        window_list=[mosaic.put(window_state)],
         )
 
 
 def test_app():
-    layout = htypes.application.layout(
-        window_list=[mosaic.put(make_window_layout())],
-    )
-    state = htypes.application.state(
-        window_list=[mosaic.put(make_window_state())],
-        )
+    layout = make_layout()
+    state = make_state()
     app = QtWidgets.QApplication()
     try:
         ctl = AppCtl.from_piece(layout)
