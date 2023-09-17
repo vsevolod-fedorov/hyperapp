70fceb56047318847461c0070549ccc359f1c40e
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-09-17 12:27:37 +0300

    UI command ctr: Switch to data_to_res service

--- hyperapp/rc/ui_command_ctr.dyn.py
+++ hyperapp/rc/ui_command_ctr.dyn.py
@@ -3,6 +3,7 @@ from hyperapp.common.association_registry import Association
 from . import htypes
 from .services import (
     constructor_creg,
+    data_to_res,
     mosaic,
     pyobj_creg,
     types,
@@ -13,10 +14,7 @@ from .code.utils import camel_to_snake
 
 @constructor_creg.actor(htypes.attr_constructors.ui_command_ctr)
 def construct(piece, custom_types, name_to_res, module_res, attr):
-    dir_t_res = pyobj_creg.reverse_resolve(htypes.ui.ui_command_d)
-    dir_res = htypes.builtin.call(
-        function=mosaic.put(dir_t_res),
-        )
+    dir_res = data_to_res(htypes.ui.ui_command_d())
     t_res = web.summon(piece.t)
     attribute = htypes.builtin.attribute(
         object=mosaic.put(module_res),
