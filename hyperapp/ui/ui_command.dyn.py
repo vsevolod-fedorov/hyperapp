4dea3a89941fb2310e151af86725145a59a93e57
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-09-17 12:24:20 +0300

    UI command factory: Use multi-value support from association registry

--- hyperapp/ui/ui_command.dyn.py
+++ hyperapp/ui/ui_command.dyn.py
@@ -24,7 +24,9 @@ def ui_command_factory():
     def _ui_command_factory(layout):
         layout_t = deduce_value_type(layout)
         d_res = data_to_res(htypes.ui.ui_command_d())
-        fn_res = association_reg[d_res, layout_t]
-        fn = pyobj_creg.animate(fn_res)
-        return [UiCommand(layout_t.name, fn)]
+        command_list = []
+        for fn_res in association_reg.get_all((d_res, layout_t)):
+            fn = pyobj_creg.animate(fn_res)
+            command_list.append(UiCommand(layout_t.name, fn))
+        return command_list
     return _ui_command_factory
