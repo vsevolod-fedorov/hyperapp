da50a822574c03b0c0896a46e57b52f504e06d78
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-09-17 11:42:24 +0300

    Ui command factory: Use data_to_res

--- hyperapp/ui/ui_command.dyn.py
+++ hyperapp/ui/ui_command.dyn.py
@@ -3,6 +3,7 @@ from hyperapp.common.htypes.deduce_value_type import deduce_value_type
 from . import htypes
 from .services import (
     association_reg,
+    data_to_res,
     pyobj_creg,
     mark,
     )
@@ -22,7 +23,8 @@ class UiCommand:
 def ui_command_factory():
     def _ui_command_factory(layout):
         layout_t = deduce_value_type(layout)
-        fn_res = association_reg[htypes.ui.ui_command_d(), layout_t]
+        d_res = data_to_res(htypes.ui.ui_command_d())
+        fn_res = association_reg[d_res, layout_t]
         fn = pyobj_creg.animate(fn_res)
         return [UiCommand(layout_t.name, fn)]
     return _ui_command_factory
