a406a7e25948d0ea049acc580ee268e8f26e0c63
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-10-01 13:54:33 +0300

    Cleanup: Remove old resource compiler modules

--- hyperapp/rc/utils.dyn.py
+++
@@ -1,7 +0,0 @@
-import re
-
-
-# https://stackoverflow.com/a/1176023 Camel case to snake case.
-def camel_to_snake(name):
-    name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
