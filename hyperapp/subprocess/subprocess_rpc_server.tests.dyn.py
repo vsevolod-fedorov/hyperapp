93fe6565fa438c5d7951397ac2b07ab972866e70
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-10-01 11:15:13 +0300

    Implement rpc_submit_factory

--- hyperapp/subprocess/subprocess_rpc_server.tests.dyn.py
+++ hyperapp/subprocess/subprocess_rpc_server.tests.dyn.py
@@ -13,7 +13,7 @@ from .code.subprocess_rpc_server_tests_aux import _callback
 log = logging.getLogger(__name__)
 
 
-def test_subprocess_rpc_server():
+def test_subprocess_rpc_server_call():
     name = 'test-subprocess-rpc-server-main'
     identity = generate_rsa_identity(fast=True)
     rpc_endpoint = rpc_endpoint_factory()
@@ -26,3 +26,16 @@ def test_subprocess_rpc_server():
         log.info("Stopping: %r", process)
     log.info("Stopped: %r", process)
 
+
+def test_subprocess_rpc_server_submit():
+    name = 'test-subprocess-rpc-server-main'
+    identity = generate_rsa_identity(fast=True)
+    rpc_endpoint = rpc_endpoint_factory()
+    endpoint_registry.register(identity, rpc_endpoint)
+    with subprocess_rpc_server_running(name, rpc_endpoint, identity) as process:
+        log.info("Started: %r", process)
+        submit = process.rpc_submit(_callback)
+        future = submit()
+        assert future.result() == 'ok'
+        log.info("Stopping: %r", process)
+    log.info("Stopped: %r", process)
