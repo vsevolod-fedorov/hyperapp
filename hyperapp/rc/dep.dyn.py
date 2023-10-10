12ebf268d350e11835b1f22f7d00d9c13250dd3e
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-10-10 11:56:53 +0300

    Deps: Shorten __repr__

--- hyperapp/rc/dep.dyn.py
+++ hyperapp/rc/dep.dyn.py
@@ -5,6 +5,9 @@ from dataclasses import dataclass
 class ServiceDep:
     service_name: str
 
+    def __repr__(self):
+        return f"service:{self.service_name}"
+
     def __eq__(self, rhs):
         return type(rhs) is ServiceDep and rhs.service_name == self.service_name
 
@@ -15,6 +18,9 @@ class ServiceDep:
 @dataclass
 class CodeDep:
     code_name: str
+
+    def __repr__(self):
+        return f"code:{self.code_name}"
 
     def __eq__(self, rhs):
         return type(rhs) is CodeDep and rhs.code_name == self.code_name
