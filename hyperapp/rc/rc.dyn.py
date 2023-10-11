a00b9823bd84990ec09c1ff4c3ddd87131e49df4
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-10-11 12:08:00 +0300

    Dump graph on exit; Add ordering for Deps

--- hyperapp/rc/rc.dyn.py
+++ hyperapp/rc/rc.dyn.py
@@ -32,40 +32,46 @@ def compile_resources(generator_ref, subdir_list, root_dirs, module_list, proces
     graph = Graph()
     # make = Make()
     initial_task = CollectorTask(generator_ref, hyperapp_dir, dir_list)
-    # make.run(initial_task)
-    initial_task.run(graph)
-    with process_pool_running(process_count, rpc_timeout) as pool:
-        task_to_unit = {}
-        while True:
-            for unit in graph.name_to_unit.values():
-                if unit in task_to_unit.values():
-                    continue  # No finished tasks for this unit yet.
-                if unit.is_up_to_date(graph):
-                    continue
-                deps = graph.name_to_deps[unit.name]
-                unknown_deps = deps - set(graph.dep_to_provider)
-                if unknown_deps:
-                    log.debug("%s: unknown deps: %s", unit, unknown_deps)
-                    continue
-                outdated_providers = {
-                    graph.dep_to_provider[d] for d in deps
-                    if not graph.dep_to_provider[d].is_up_to_date(graph)
-                    }
-                if outdated_providers:
-                    log.debug("%s: outdated providers: %s", unit, outdated_providers)
-                    continue
-                for task in unit.make_tasks(graph):
-                    log.info("Submit: %s", task)
-                    pool.submit(task)
-                    task_to_unit[task] = unit
-            if not task_to_unit and pool.task_count == 0:
-                break
-            task, future = pool.next_completed(timeout=rpc_timeout)
-            del task_to_unit[task]
-            try:
-                result = future.result()
-                log.info("%s result: %r", task, result)
-                task.process_result(graph, result)
-            except HException as x:
-                log.info("%s error: %r", task, x)
-                task.process_error(graph, x)
+    try:
+        # make.run(initial_task)
+        initial_task.run(graph)
+        with process_pool_running(process_count, rpc_timeout) as pool:
+            task_to_unit = {}
+            while True:
+                for unit in graph.name_to_unit.values():
+                    if unit in task_to_unit.values():
+                        continue  # No finished tasks for this unit yet.
+                    if unit.is_up_to_date(graph):
+                        continue
+                    deps = graph.name_to_deps[unit.name]
+                    unknown_deps = deps - set(graph.dep_to_provider)
+                    if unknown_deps:
+                        log.debug("%s: unknown deps: %s", unit, unknown_deps)
+                        continue
+                    outdated_providers = {
+                        graph.dep_to_provider[d] for d in deps
+                        if not graph.dep_to_provider[d].is_up_to_date(graph)
+                        }
+                    if outdated_providers:
+                        log.debug("%s: outdated providers: %s", unit, outdated_providers)
+                        continue
+                    for task in unit.make_tasks(graph):
+                        log.info("Submit: %s", task)
+                        pool.submit(task)
+                        task_to_unit[task] = unit
+                if not task_to_unit and pool.task_count == 0:
+                    break
+                task, future = pool.next_completed(timeout=rpc_timeout)
+                del task_to_unit[task]
+                try:
+                    result = future.result()
+                    log.info("%s result: %r", task, result)
+                    task.process_result(graph, result)
+                except HException as x:
+                    log.info("%s error: %r", task, x)
+                    task.process_error(graph, x)
+    finally:
+        for name, deps in sorted(graph.name_to_deps.items()):
+            log.debug("Deps for %s: %s", name, deps or '{}')
+        for dep, provider in sorted(graph.dep_to_provider.items()):
+            log.debug("Provider for %s: %s", dep, provider)
