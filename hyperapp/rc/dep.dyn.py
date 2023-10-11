6ea91814f6e1a7f2f82f4925dc18fab60d33499f
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-10-11 11:50:53 +0300

    Refactor deps: Extract base class

--- hyperapp/rc/dep.dyn.py
+++ hyperapp/rc/dep.dyn.py
@@ -1,18 +1,30 @@
+from abc import ABCMeta, abstractproperty
 from dataclasses import dataclass
 
 
-@dataclass
-class ServiceDep:
+class Dep(metaclass=ABCMeta):
+
+    def __eq__(self, rhs):
+        return type(rhs) is type(self) and rhs.key == self.key
+
+    def __hash__(self):
+        return hash((type(self), self.key))
+
+    @abstractproperty
+    def key(self):
+        pass
+
+
+@dataclass(eq=False)
+class ServiceDep(Dep):
     service_name: str
 
     def __repr__(self):
         return f"service:{self.service_name}"
 
-    def __eq__(self, rhs):
-        return type(rhs) is ServiceDep and rhs.service_name == self.service_name
-
-    def __hash__(self):
-        return hash((type(self), self.service_name))
+    @property
+    def key(self):
+        return self.service_name
 
     @property
     def import_name(self):
@@ -23,18 +35,16 @@ class ServiceDep:
         return f'{self.service_name}.service'
 
 
-@dataclass
-class CodeDep:
+@dataclass(eq=False)
+class CodeDep(Dep):
     code_name: str
 
     def __repr__(self):
         return f"code:{self.code_name}"
 
-    def __eq__(self, rhs):
-        return type(rhs) is CodeDep and rhs.code_name == self.code_name
-
-    def __hash__(self):
-        return hash((type(self), self.code_name))
+    @property
+    def key(self):
+        return self.code_name
 
     @property
     def import_name(self):
