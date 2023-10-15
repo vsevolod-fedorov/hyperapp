d6c62043522fb37c2239b618078e0e22595adc0c
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-10-15 10:55:13 +0300

    Call driver: Record imports

--- hyperapp/rc/call_driver.dyn.py
+++ hyperapp/rc/call_driver.dyn.py
@@ -6,6 +6,7 @@ from .services import (
     mosaic,
     pyobj_creg,
     )
+from .code.driver_recorders import ImportRecorders
 from .code.tracer import Tracer, value_type
 
 log = logging.getLogger(__name__)
@@ -13,8 +14,10 @@ log = logging.getLogger(__name__)
 
 def call_function(import_recorders, call_result_ref, trace_modules):
     tracer = Tracer(trace_modules)
+    recorders = ImportRecorders(import_recorders)
     with tracer.tracing():
-        value = pyobj_creg.invite(call_result_ref)
+        with recorders.recording():
+            value = pyobj_creg.invite(call_result_ref)
         log.info("Resource value: %s", repr(value))
 
         if inspect.isgenerator(value):
@@ -23,10 +26,8 @@ def call_function(import_recorders, call_result_ref, trace_modules):
 
     t = value_type(value)
 
-    imports = set()
-
     return htypes.inspect.object_type_info(
-        imports=list(sorted(imports)),
+        imports=recorders.module_imports_list,
         t=mosaic.put(t),
         calls=tracer.calls,
         )
