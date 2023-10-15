99036f9d4b0d76fe6516d557c6fefd3e6ded36ca
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-10-15 12:55:20 +0300

    Restore parameter_ctr module

---
+++ hyperapp/rc/parameter_ctr.dyn.py
@@ -0,0 +1,20 @@
+from . import htypes
+from .services import (
+    constructor_creg,
+    mark,
+    mosaic,
+    )
+
+
+@constructor_creg.actor(htypes.attr_constructors.parameter)
+def construct(piece, custom_types, name_to_res, module_res, attr):
+    attribute = htypes.builtin.attribute(
+        object=mosaic.put(module_res),
+        attr_name=attr.name,
+        )
+    name_to_res[attr.name] = attribute
+    parameter = htypes.builtin.call(
+        function=mosaic.put(attribute),
+        )
+    path_str = '.'.join(piece.path)
+    name_to_res[f'{path_str}.parameter'] = parameter
