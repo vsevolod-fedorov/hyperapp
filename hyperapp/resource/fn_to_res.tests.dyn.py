e517dd3cf8532de7f8f6ce32b211acff0b30c45a
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-10-16 12:40:26 +0300

    fn_to_res: Move service override from fixture to test

--- hyperapp/resource/fn_to_res.tests.dyn.py
+++ hyperapp/resource/fn_to_res.tests.dyn.py
@@ -1,6 +1,19 @@
+from .services import (
+    mark,
+    )
 from .tested.services import (
     fn_to_ref,
     )
+
+
+class _PhonyPyObjCReg:
+    def reverse_resolve(self, obj):
+        return 'some object'
+
+
+@mark.service
+def pyobj_creg():
+    return _PhonyPyObjCReg()
 
 
 def _test_fn():
