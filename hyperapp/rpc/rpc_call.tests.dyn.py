f769002856984b7484f4e54281908fbfa7df47b2
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-11-06 13:04:25 +0300

    Improve rpc_call tests: Add HException case

--- hyperapp/rpc/rpc_call.tests.dyn.py
+++ hyperapp/rpc/rpc_call.tests.dyn.py
@@ -1,3 +1,5 @@
+from hyperapp.common.htypes import HException
+
 from .services import (
     generate_rsa_identity,
     mark,
@@ -5,6 +7,10 @@ from .services import (
     )
 from .tested.code import rpc_call
 from .tested.services import rpc_call_factory
+
+
+class TestException(HException):
+    pass
 
 
 class PhonyTransport:
@@ -20,13 +26,20 @@ def transport():
 
 class PhonyRpcEndpoint:
 
+    def __init__(self, result=None, exception=None):
+        self._result = result
+        self._exception = exception
+
     def assign_future_to_request_id(self, request_id, future):
-        future.set_result('phony rpc response')
+        if self._result is not None:
+            future.set_result(self._result)
+        if self._exception is not None:
+            future.set_exception(self._exception)
 
 
-def test_rpc_call_factory():
+def test_rpc_call_factory_success():
     sender_identity = generate_rsa_identity(fast=True)
-    endpoint = PhonyRpcEndpoint()
+    endpoint = PhonyRpcEndpoint(result="Phony rpc response")
     rpc_call = rpc_call_factory(
         rpc_endpoint=endpoint,
         receiver_peer='phony receiver peer',
@@ -34,3 +47,20 @@ def test_rpc_call_factory():
         sender_identity=sender_identity,
         )
     rpc_call(sample_param=12345)
+
+
+def test_rpc_call_factory_exception():
+    sender_identity = generate_rsa_identity(fast=True)
+    endpoint = PhonyRpcEndpoint(exception=TestException("Phony rpc error"))
+    rpc_call = rpc_call_factory(
+        rpc_endpoint=endpoint,
+        receiver_peer='phony receiver peer',
+        servant_ref=mosaic.put('phony servant'),
+        sender_identity=sender_identity,
+        )
+    try:
+        rpc_call(sample_param=12345)
+    except TestException:
+        pass
+    else:
+        assert False, "No test exception was raised"
