d691882be9dedbaf1227fad3a16fabf83f5fa02e
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2023-11-06 12:54:53 +0300

    Rpc endpoint tests: Pick pyobj_creg from tested code module

--- hyperapp/rpc/rpc_endpoint.tests.dyn.py
+++ hyperapp/rpc/rpc_endpoint.tests.dyn.py
@@ -53,7 +53,7 @@ def _raise_non_happ_error(phony_param):
 
 
 def _run_test_with_servant(servant_fn):
-    pyobj_creg.set_phony_servant(servant_fn)
+    rpc_endpoint.pyobj_creg.set_phony_servant(servant_fn)
     sender_identity = generate_rsa_identity(fast=True)
     rpc_request = htypes.rpc.request(
         request_id='phony request id',
