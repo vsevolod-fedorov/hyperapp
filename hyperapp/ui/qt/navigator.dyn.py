3f0d09aaa7535a59555da12e68b4dbd560a4745f
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-01-01 13:11:59 +0300

    Navigator: Add a model command

--- hyperapp/ui/qt/navigator.dyn.py
+++ hyperapp/ui/qt/navigator.dyn.py
@@ -1,9 +1,36 @@
+import logging
+from functools import partial
+
 from . import htypes
 from .services import (
     mosaic,
     ui_ctl_creg,
+    visualizer,
     web,
     )
+
+log = logging.getLogger(__name__)
+
+
+class ModelCommand:
+
+    def __init__(self, fn, wrapper):
+        self._fn = fn
+        self._wrapper = wrapper
+
+    @property
+    def name(self):
+        return self._fn.__name__
+
+    async def run(self):
+        log.info("Run: %s", self._fn)
+        result = await self._fn()
+        log.info("Run result: %s -> %r", self._fn, result)
+        return self._wrapper(result)
+
+
+async def open_some_text():
+    return "Some text"
 
 
 class NavigatorCtl:
@@ -25,7 +52,16 @@ class NavigatorCtl:
         return htypes.navigator.state(mosaic.put(current_state))
 
     def get_commands(self, layout, widget, wrapper):
-        return []
+        command = ModelCommand(open_some_text, partial(self._wrapper, wrapper))
+        return [command]
+
+    def _wrapper(self, wrapper, piece):
+        if piece is None:
+            return None
+        layout = visualizer(piece)
+        # Navigator does not have specific diffs. It's diff is just a new layout.
+        return wrapper((layout, None))
 
     def apply(self, ctx, widget, layout_diff, state_diff):
         log.info("Navigator: apply: %s / %s", layout_diff, state_diff)
+        return (layout_diff, state_diff)
