21ed8b3be396440f5134ab43696787db19128063
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-01-02 13:27:07 +0300

    Add list and list adapter

---
+++ hyperapp/ui/qt/list_adapter.tests.dyn.py
@@ -0,0 +1,25 @@
+from hyperapp.common.htypes.deduce_value_type import deduce_complex_value_type
+
+from . import htypes
+from .services import (
+    mosaic,
+    types,
+    )
+from .tested.code import list_adapter
+
+
+def test_adapter():
+    value = [
+        htypes.list_tests.item(1, "First"),
+        htypes.list_tests.item(2, "Second"),
+        htypes.list_tests.item(3, "Third"),
+        ]
+    t = deduce_complex_value_type(mosaic, types, value)
+    piece = htypes.list_adapter.static_list_adapter(mosaic.put(value, t))
+    adapter = list_adapter.StaticListAdapter.from_piece(piece)
+    assert adapter.column_count() == 2
+    assert adapter.row_count() == 3
+    assert adapter.column_title(0) == 'id'
+    assert adapter.column_title(1) == 'title'
+    assert adapter.cell_data(1, 0) == 2
+    assert adapter.cell_data(2, 1) == "Third"
--- hyperapp/ui/visualizer.dyn.py
+++ hyperapp/ui/visualizer.dyn.py
@@ -1,15 +1,22 @@
+from hyperapp.common.htypes.deduce_value_type import deduce_complex_value_type
+
 from . import htypes
 from .services import (
     mark,
     mosaic,
+    types,
     )
 
 
 @mark.service
 def visualizer():
     def fn(value):
-        if type(value) is not str:
-            raise NotImplementedError(type(value))
-        adapter_layout = htypes.str_adapter.static_str_adapter(value)
-        return htypes.text.edit_layout(mosaic.put(adapter_layout))
+        if type(value) is str:
+            adapter_layout = htypes.str_adapter.static_str_adapter(value)
+            return htypes.text.edit_layout(mosaic.put(adapter_layout))
+        if type(value) is list:
+            t = deduce_complex_value_type(mosaic, types, value)
+            adapter_layout = htypes.list_adapter.static_list_adapter(mosaic.put(value, t))
+            return htypes.list.layout(mosaic.put(adapter_layout))
+        raise NotImplementedError(type(value))
     return fn
