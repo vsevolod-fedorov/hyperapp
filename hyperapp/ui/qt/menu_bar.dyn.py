233d7e734e79786e60a72b8c87a76b9edf3e94fc
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-01-03 11:08:20 +0300

    Menu bar: Add hard-coded shortcuts

--- hyperapp/ui/qt/menu_bar.dyn.py
+++ hyperapp/ui/qt/menu_bar.dyn.py
@@ -7,6 +7,17 @@ from PySide6 import QtGui, QtWidgets
 log = logging.getLogger(__name__)
 
 
+_hardcoded_shortcuts = {
+    'back': ['Escape'],
+    'forward': ['Alt+Right'],
+    'duplicate': ['Shift+f4', 'Ctrl+f4'],
+    'static_text_1': ['f1'],
+    'static_text_2': ['f2'],
+    'static_list': ['f3'],
+    'fn_list': ['f4'],
+    }
+
+
 class MenuBarCtl:
 
     @classmethod
@@ -15,6 +26,7 @@ class MenuBarCtl:
 
     def __init__(self):
         self._menu_command_to_action = {}
+        self._used_shortcuts = set()
 
     def construct_widget(self, state, ctx):
         w =  QtWidgets.QMenuBar()
@@ -23,6 +35,7 @@ class MenuBarCtl:
 
     def set_commands(self, w, commands):
         w.clear()
+        self._used_shortcuts.clear()
         self._command_to_action = {}
         menu = QtWidgets.QMenu('&All')
         for command in commands:
@@ -33,15 +46,23 @@ class MenuBarCtl:
         [menu_action] = w.actions()
         menu = menu_action.menu()
         for command in removed_commands:
-            menu.removeAction(self._menu_command_to_action[menu, command])
+            action = self._menu_command_to_action[menu, command]
+            menu.removeAction(action)
+            self._used_shortcuts.remove(action.shortcut())
         for command in added_commands:
             self._add_action(menu, command)
 
     def _add_action(self, menu, command):
-        idx = len(menu.actions()) + 1
         action = QtGui.QAction(command.name, menu)
         action.triggered.connect(partial(self._run_command, command))
-        action.setShortcut(f'f{idx}')
+        for name, shortcut_list in _hardcoded_shortcuts.items():
+            if name not in command.name:
+                continue
+            for shortcut in shortcut_list:
+                if shortcut not in self._used_shortcuts:
+                    action.setShortcut(shortcut)
+                    self._used_shortcuts.add(shortcut)
+                    break
         menu.addAction(action)
         self._menu_command_to_action[menu, command] = action
 
