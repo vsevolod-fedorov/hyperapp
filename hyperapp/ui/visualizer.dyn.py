02f39856674cc72e8e3b2dc7ad4dd4eb1cee6644
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-01-03 11:38:40 +0300

    WIP: Add fn list adapter

--- hyperapp/ui/visualizer.dyn.py
+++ hyperapp/ui/visualizer.dyn.py
@@ -1,3 +1,4 @@
+from hyperapp.common.htypes import tString, TList
 from hyperapp.common.htypes.deduce_value_type import deduce_complex_value_type
 
 from . import htypes
@@ -11,12 +12,15 @@ from .services import (
 @mark.service
 def visualizer():
     def fn(value):
-        if type(value) is str:
+        t = deduce_complex_value_type(mosaic, types, value)
+        if t is tString:
             adapter_layout = htypes.str_adapter.static_str_adapter(value)
             return htypes.text.edit_layout(mosaic.put(adapter_layout))
-        if type(value) is list:
-            t = deduce_complex_value_type(mosaic, types, value)
+        if isinstance(t, TList):
             adapter_layout = htypes.list_adapter.static_list_adapter(mosaic.put(value, t))
+            return htypes.list.layout(mosaic.put(adapter_layout))
+        if t is htypes.sample_list.sample_list:
+            adapter_layout = htypes.list_adapter.fn_list_adapter(mosaic.put(value))
             return htypes.list.layout(mosaic.put(adapter_layout))
         raise NotImplementedError(type(value))
     return fn
