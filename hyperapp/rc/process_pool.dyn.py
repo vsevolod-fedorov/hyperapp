f8e0c601e90d124e4aa9a10800a5109a638cb9d8
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-01-03 12:51:59 +0300

    Increase deadlock timeout 3->6 seconds

--- hyperapp/rc/process_pool.dyn.py
+++ hyperapp/rc/process_pool.dyn.py
@@ -61,7 +61,7 @@ class ProcessPool:
                     log.debug("Deadlock check: setup")
                     while True:
                         await self._process_available.wait()
-                        when = asyncio.get_running_loop().time() + 3
+                        when = asyncio.get_running_loop().time() + 6
                         log.debug("Deadlock check: reschedule to %s", when)
                         timeout.reschedule(when)
         except asyncio.CancelledError:
