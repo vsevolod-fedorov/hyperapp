1b62977dbb0163067684f3d309b8c17a692c1b33
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-01-05 12:05:56 +0300

    List adapter, visualizer: Use model association

--- hyperapp/ui/visualizer.dyn.py
+++ hyperapp/ui/visualizer.dyn.py
@@ -3,9 +3,13 @@ from hyperapp.common.htypes.deduce_value_type import deduce_complex_value_type
 
 from . import htypes
 from .services import (
+    association_reg,
+    data_to_res,
     mark,
     mosaic,
+    pyobj_creg,
     types,
+    web,
     )
 
 
@@ -13,14 +17,28 @@ from .services import (
 def visualizer():
     def fn(value):
         t = deduce_complex_value_type(mosaic, types, value)
+
         if t is tString:
             adapter_layout = htypes.str_adapter.static_str_adapter(value)
             return htypes.text.edit_layout(mosaic.put(adapter_layout))
         if isinstance(t, TList):
             adapter_layout = htypes.list_adapter.static_list_adapter(mosaic.put(value, t))
             return htypes.list.layout(mosaic.put(adapter_layout))
-        if t is htypes.sample_list.sample_list:
-            adapter_layout = htypes.list_adapter.fn_list_adapter(mosaic.put(value))
+
+        model_d_res = data_to_res(htypes.ui.model_d())
+        t_res = pyobj_creg.reverse_resolve(t)
+        try:
+            model = association_reg[model_d_res, t_res]
+        except KeyError:
+            raise NotImplementedError(t)
+        ui_t = web.summon(model.ui_t)
+        impl = web.summon(model.impl)
+        if isinstance(ui_t, htypes.ui.list_ui_t) and isinstance(impl, htypes.ui.fn_impl):
+            adapter_layout = htypes.list_adapter.fn_list_adapter(
+                model_piece=mosaic.put(value),
+                element_t=ui_t.element_t,
+                function=impl.function,
+                )
             return htypes.list.layout(mosaic.put(adapter_layout))
-        raise NotImplementedError(type(value))
+        raise NotImplementedError(f"Not supported model: {ui_t} / {impl}")
     return fn
