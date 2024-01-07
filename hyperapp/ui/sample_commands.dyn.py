daf321cbe76148784d4362a7f3f2f2628e4932eb
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-01-07 13:40:16 +0300

    Move sample commands into separate module; restore them

---
+++ hyperapp/ui/sample_commands.dyn.py
@@ -0,0 +1,18 @@
+from . import htypes
+
+
+async def open_sample_static_text_1():
+    return "Sample text 1"
+
+
+async def open_sample_static_text_2():
+    return "Sample text 2"
+
+
+async def open_sample_static_list():
+    items = [
+        htypes.sample_list.item(1, "First"),
+        htypes.sample_list.item(2, "Second"),
+        htypes.sample_list.item(3, "Third"),
+        ]
+    return items
