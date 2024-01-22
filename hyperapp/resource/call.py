b014028364fed1e589efa9cfec147e362de7f63d
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-01-22 11:07:55 +0300

    Rename modules dict_*coders -> dict_*coder

--- hyperapp/resource/call.py
+++ hyperapp/resource/call.py
@@ -1,6 +1,6 @@
 from ..common.htypes.call import call_t, call_def_t
-from ..common.dict_decoders import NamedPairsDictDecoder
-from ..common.dict_encoders import NamedPairsDictEncoder
+from ..common.dict_decoder import NamedPairsDictDecoder
+from ..common.dict_encoder import NamedPairsDictEncoder
 
 
 class CallResourceType:
