570fbc6e7416f3385f60e70ce91058b6bf6da77a
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-01-22 11:09:05 +0300

    Update server main; generate and save or load server identity

---
+++ hyperapp/server/server.dyn.py
@@ -0,0 +1,43 @@
+import logging
+from pathlib import Path
+
+from hyperapp.common import dict_coders
+
+from .services import (
+    endpoint_registry,
+    file_bundle,
+    generate_rsa_identity,
+    mosaic,
+    identity_registry,
+    peer_registry,
+    route_table,
+    rpc_call_factory,
+    rpc_endpoint_factory,
+    stop_signal,
+    )
+
+log = logging.getLogger(__name__)
+
+
+def _main():
+
+    identity_bundle = file_bundle(Path.home() / '.local/share/hyperapp/server/identity.json')
+    try:
+        server_identity = identity_registry.animate(identity_bundle.load_piece())
+        log.info("Server identity: loaded from: %s", identity_bundle.path)
+    except FileNotFoundError:
+        server_identity = generate_rsa_identity()
+        identity_bundle.save_piece(server_identity.piece)
+        log.info("Server identity: generated and saved to: %s", identity_bundle.path)
+
+    server_peer_ref = mosaic.put(server_identity.peer.piece)
+    rpc_endpoint = rpc_endpoint_factory()
+    endpoint_registry.register(server_identity, rpc_endpoint)
+
+    log.info("Server: Started")
+    try:
+        stop_signal.wait()
+    except KeyboardInterrupt:
+        print()  # Leave '^C' on separate line.
+        log.info("Server: Stopping")
+        return 0
