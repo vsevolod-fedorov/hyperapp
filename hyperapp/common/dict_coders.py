271558333e9a142d1a61f127ca494cfac5a6a1e5
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-01-22 11:08:16 +0300

    Add module dict_coders

---
+++ hyperapp/common/dict_coders.py
@@ -0,0 +1,6 @@
+from .htypes.packet_coders import packet_coders
+from .dict_encoder import JsonEncoder
+from .dict_decoder import JsonDecoder
+
+
+packet_coders.register('json', JsonEncoder(), JsonDecoder())
