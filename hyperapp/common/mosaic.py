5f342c46112911e15d55aded5cd4ed9c0c241805
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-04-07 09:51:24 +0300

    Mosaic: Cache pieces and decoded capsules

--- hyperapp/common/mosaic.py
+++ hyperapp/common/mosaic.py
@@ -1,6 +1,7 @@
 # reference registry: map refs to capsules
 
 import logging
+from collections import namedtuple
 
 from .htypes import capsule_t
 from .htypes.deduce_value_type import deduce_value_type
@@ -12,22 +13,33 @@ log = logging.getLogger(__name__)
 
 class Mosaic:
 
+    _Rec = namedtuple('_Rec', 'type_ref t value')
+
     def __init__(self, types):
         self._types = types
-        self._registry = {}  # ref -> capsule
+        self._ref_to_capsule = {}  # ref -> capsule
+        self._ref_to_rec = {}  # ref -> _Rec
+        self._piece_to_ref = {}
 
     def register_capsule(self, capsule):
         assert isinstance(capsule, capsule_t), repr(capsule)
         ref = make_ref(capsule)
         log.debug('Registering ref %s for capsule of type %s', ref, capsule.type_ref)
-        existing_capsule = self._registry.get(ref)
+        existing_capsule = self._ref_to_capsule.get(ref)
         if existing_capsule:
             log.debug('  (already exists)')
             assert capsule == existing_capsule, repr((existing_capsule, capsule))  # new capsule does not match existing one
-        self._registry[ref] = capsule
+        dc = decode_capsule(self._types, capsule)
+        self._ref_to_capsule[ref] = capsule
+        self._ref_to_rec[ref] = self._Rec(dc.type_ref, dc.t, dc.value)
+        self._piece_to_ref[dc.value] = ref
         return ref
 
     def put(self, piece, t=None):
+        try:
+            return self._piece_to_ref[piece]
+        except KeyError:
+            pass
         t = t or deduce_value_type(piece)
         log.debug('Registering piece %r: %s', t.name, piece)
         capsule = make_capsule(self._types, piece, t)
@@ -41,13 +53,17 @@ class Mosaic:
         return self.put(piece, t)
 
     def get(self, ref):
-        return self._registry.get(ref)
+        return self._ref_to_capsule.get(ref)
 
     # Alias for web source.
     def pull(self, ref):
         return self.get(ref)
 
     def resolve_ref(self, ref, expected_type=None) -> DecodedCapsule:
+        try:
+            return self._ref_to_rec[ref]
+        except KeyError:
+            pass
         capsule = self.get(ref)
         assert capsule is not None, f"Unknown ref: {ref}"
         return decode_capsule(self._types, capsule, expected_type)
