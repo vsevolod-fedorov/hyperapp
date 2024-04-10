2c22697db631dea35a3e0c8c23df6ab2a0c3b119
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-04-10 11:14:54 +0300

    Ui command factory: Return universal commands too

--- hyperapp/ui/qt/ui_command.dyn.py
+++ hyperapp/ui/qt/ui_command.dyn.py
@@ -108,8 +108,13 @@ def ui_command_factory():
         piece_t = deduce_value_type(view.piece)
         piece_t_res = pyobj_creg.reverse_resolve(piece_t)
         d_res = data_to_res(htypes.ui.ui_command_d())
+        universal_d_res = data_to_res(htypes.ui.universal_ui_command_d())
+        fn_res_list = [
+            *association_reg.get_all((d_res, piece_t_res)),
+            *association_reg.get_all(universal_d_res),
+            ]
         command_list = []
-        for fn_res in association_reg.get_all((d_res, piece_t_res)):
+        for fn_res in fn_res_list:
             fn = pyobj_creg.animate(fn_res)
             command_list.append(UiCommand(fn.__name__, fn, view, widget, wrappers))
         return command_list
