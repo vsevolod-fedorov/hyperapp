d0066ae950e18d189b368897dd07dc49e91f07ef
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-04-10 12:05:00 +0300

    Switch to UI command record: Create type and update RC

--- hyperapp/rc/ui_command_ctr.dyn.py
+++ hyperapp/rc/ui_command_ctr.dyn.py
@@ -12,6 +12,16 @@ from .services import (
 from .code.utils import camel_to_snake
 
 
+def _make_command_d_res(custom_types, module_res, attr):
+    d_attr = attr.name + '_d'
+    try:
+        command_d_ref = custom_types[module_res.module_name][d_attr]
+    except KeyError:
+        raise RuntimeError(f"Create directory type: {module_res.module_name}.{d_attr}")
+    command_d_t = types.resolve(command_d_ref)
+    return data_to_res(command_d_t())
+
+
 @constructor_creg.actor(htypes.attr_constructors.ui_command_ctr)
 def construct_ui_command(piece, custom_types, name_to_res, module_res, attr):
     dir_res = data_to_res(htypes.ui.ui_command_d())
@@ -20,13 +30,20 @@ def construct_ui_command(piece, custom_types, name_to_res, module_res, attr):
         object=mosaic.put(module_res),
         attr_name=attr.name,
         )
+    command_d_res = _make_command_d_res(custom_types, module_res, attr)
+    command = htypes.ui.ui_command(
+        d=mosaic.put(command_d_res),
+        function=mosaic.put(attribute),
+        )
     association = Association(
         bases=[dir_res, t_res],
         key=[dir_res, t_res],
-        value=attribute,
+        value=command,
         )
     name_to_res['ui_command_d'] = dir_res
     name_to_res[attr.name] = attribute
+    name_to_res[f'{attr.name}.d'] = command_d_res
+    name_to_res[f'{attr.name}.ui_command'] = command
     return [association]
 
 
@@ -37,12 +54,19 @@ def construct_universal_ui_command(piece, custom_types, name_to_res, module_res,
         object=mosaic.put(module_res),
         attr_name=attr.name,
         )
+    command_d_res = _make_command_d_res(custom_types, module_res, attr)
+    command = htypes.ui.ui_command(
+        d=mosaic.put(command_d_res),
+        function=mosaic.put(attribute),
+        )
     association = Association(
         bases=[dir_res],
         key=[dir_res],
-        value=attribute,
+        value=command,
         )
     name_to_res['universal_ui_command_d'] = dir_res
     name_to_res[attr.name] = attribute
+    name_to_res[f'{attr.name}.d'] = command_d_res
+    name_to_res[f'{attr.name}.ui_command'] = command
     return [association]
 
