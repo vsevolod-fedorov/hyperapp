e2340d1eeaeb6778494c48bf9546f9e889b8c3da
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-04-10 12:05:00 +0300

    Switch to UI command record: Create type and update RC

--- hyperapp/rc/ui_command_ctr.tests.dyn.py
+++ hyperapp/rc/ui_command_ctr.tests.dyn.py
@@ -10,9 +10,9 @@ from .tested.code import ui_command_ctr
 
 def test_construct_ui_command():
     module_res = htypes.builtin.python_module(
-        module_name='sample_module',
+        module_name='ui_command_ctr_tests',
         source='',
-        file_path='/sample_module.dyn.py',
+        file_path='/ui_command_ctr_tests.dyn.py',
         import_list=(),
         )
     string_res = pyobj_creg.reverse_resolve(htypes.builtin.string)
@@ -25,20 +25,20 @@ def test_construct_ui_command():
         name_to_res=name_to_res,
         module_res=module_res,
         attr=htypes.inspect.attr(
-            name='sample_attr',
+            name='sample_command',
             module=None,
             resource_name=None,
             constructors=(),
             ),
         )
-    assert 'sample_attr' in name_to_res
+    assert 'sample_command.ui_command' in name_to_res
 
 
 def test_construct_univeral_ui_command():
     module_res = htypes.builtin.python_module(
-        module_name='sample_module',
+        module_name='ui_command_ctr_tests',
         source='',
-        file_path='/sample_module.dyn.py',
+        file_path='/ui_command_ctr_tests.py',
         import_list=(),
         )
     name_to_res = {}
@@ -48,10 +48,10 @@ def test_construct_univeral_ui_command():
         name_to_res=name_to_res,
         module_res=module_res,
         attr=htypes.inspect.attr(
-            name='sample_attr',
+            name='sample_command',
             module=None,
             resource_name=None,
             constructors=(),
             ),
         )
-    assert 'sample_attr' in name_to_res
+    assert 'sample_command.ui_command' in name_to_res
