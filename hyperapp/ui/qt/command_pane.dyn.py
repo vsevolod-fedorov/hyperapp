a404e9567fd51766c4a2e714c82339491e8aabf6
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-04-12 10:37:07 +0300

    Command pane: Show only model commands

--- hyperapp/ui/qt/command_pane.dyn.py
+++ hyperapp/ui/qt/command_pane.dyn.py
@@ -9,26 +9,6 @@ from .code.view import Diff, Item, View
 log = logging.getLogger(__name__)
 
 
-_skip_commands = {
-    'duplicate_window',
-    'duplicate_tab',
-    'close_tab',
-    'go_back',
-    'go_forward',
-    'open_sample_static_text_1',
-    'open_sample_static_text_2',
-    'open_sample_static_list',
-    'open_sample_fn_list',
-    'open_feed_sample_fn_list',
-    'open_sample_fn_tree',
-    'open_feed_sample_fn_tree',
-    'open_layout_tree',
-    'make_piece',
-    'make_state',
-    'sample_list',
-    }
-
-
 class CommandPane(QtWidgets.QWidget):
 
     def __init__(self):
@@ -61,14 +41,17 @@ class CommandPaneView(View):
 
     def set_commands(self, widget, commands):
         layout = widget.layout()
-        removed_commands = set(widget._command_to_button) - set(commands)
-        new_commands = set(commands) - set(widget._command_to_button)
+        filter_d = htypes.ui.model_command_kind_d()
+        wanted_commands = {
+            cmd for cmd in commands
+            if filter_d in cmd.d
+            }
+        removed_commands = set(widget._command_to_button) - wanted_commands
+        new_commands = wanted_commands - set(widget._command_to_button)
         for cmd in removed_commands:
             button = widget._command_to_button.pop(cmd)
             button.deleteLater()
         for cmd in new_commands:
-            if cmd.name in _skip_commands:
-                continue
             button = cmd.make_button()
             layout.addWidget(button)
             widget._command_to_button[cmd] = button
