4f7b09fd2518a75d31c7516929ad0781587933f8
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-04-15 13:16:09 +0300

    Create sample_feed module

---
+++ hyperapp/sample/sample_feed.dyn.py
@@ -0,0 +1,24 @@
+import asyncio
+import logging
+from functools import partial
+
+from . import htypes
+from .services import (
+    feed_creg,
+    )
+from .code.list_diff import ListDiff
+
+log = logging.getLogger(__name__)
+
+
+
+def _send_diff(feed):
+    log.info("Sending list diff")
+    item = htypes.sample_list.item(44, "Sample item #4")
+    feed.send(ListDiff.Append(item))
+
+
+async def schedule_sample_feed(piece):
+    feed = feed_creg.animate(piece)
+    loop = asyncio.get_running_loop()
+    loop.call_soon(partial(_send_diff, feed))
