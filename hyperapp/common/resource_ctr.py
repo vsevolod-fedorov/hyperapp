f8c836c2a2ca93e38c18051a1e698829ddaa9a69
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-04-16 11:20:46 +0300

    RC: Rename constructor constant and function

--- hyperapp/common/resource_ctr.py
+++ hyperapp/common/resource_ctr.py
@@ -1,15 +1,15 @@
 import inspect
 
 
-RESOURCE_CTR_ATTR = '__resource_constructors__'
+RESOURCE_ATTR_CTR_NAME = '__resource_constructors__'
 
 
-def add_constructor(module, fn_name, ctr_ref):
-    ctr_dict = module.__dict__.setdefault(RESOURCE_CTR_ATTR, {})
+def add_attr_constructor(module, fn_name, ctr_ref):
+    ctr_dict = module.__dict__.setdefault(RESOURCE_ATTR_CTR_NAME, {})
     ctr_list = ctr_dict.setdefault(fn_name, [])
     ctr_list.append(ctr_ref)
 
 
 def add_fn_module_constructor(fn, ctr_ref, name=None):
     module = inspect.getmodule(fn)
-    add_constructor(module, fn.__name__, ctr_ref)
+    add_attr_constructor(module, fn.__name__, ctr_ref)
