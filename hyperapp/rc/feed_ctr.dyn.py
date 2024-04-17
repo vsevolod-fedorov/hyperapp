2ca424338166619463bc082893c2430d57dc1c7d
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-04-17 11:35:39 +0300

    Fix list feed constructor

--- hyperapp/rc/feed_ctr.dyn.py
+++ hyperapp/rc/feed_ctr.dyn.py
@@ -1,4 +1,3 @@
-from hyperapp.common.htypes.deduce_value_type import deduce_value_type
 from hyperapp.common.association_registry import Association
 
 from . import htypes
@@ -6,15 +5,14 @@ from .services import (
     constructor_creg,
     data_to_res,
     pyobj_creg,
+    web,
   )
 
 
 @constructor_creg.actor(htypes.rc_constructors.list_feed_ctr)
 def construct_list_feed(piece, custom_types, name_to_res, module_res):
     dir_res = data_to_res(htypes.ui.feed_d())
-    piece_t = deduce_value_type(piece)
-    t_res = pyobj_creg.reverse_resolve(piece_t)
-    element_t = pyobj_creg.invite(piece.element_t)
+    t_res = web.summon(piece.t)
     feed = htypes.ui.list_feed(
         element_t=piece.element_t,
         )
@@ -23,6 +21,7 @@ def construct_list_feed(piece, custom_types, name_to_res, module_res):
         key=[dir_res, t_res],
         value=feed,
         )
+    piece_t = pyobj_creg.invite(piece.t)
     name_to_res['feed_d'] = dir_res
-    name_to_res[f'{element_t.module_name}_{element_t.name}.list_feed'] = feed
+    name_to_res[f'{piece_t.module_name}_{piece_t.name}.list_feed'] = feed
     return [association]
--- hyperapp/rc/feed_ctr.tests.dyn.py
+++ hyperapp/rc/feed_ctr.tests.dyn.py
@@ -16,12 +16,17 @@ def test_construct_feed():
         )
     string_res = pyobj_creg.reverse_resolve(htypes.builtin.string)
     name_to_res = {}
+    feed_piece_t = htypes.feed_ctr_tests.sample_feed
+    feed_piece_t_res = pyobj_creg.reverse_resolve(feed_piece_t)
+    element_t = htypes.feed_ctr_tests.sample_item
+    element_t_res = pyobj_creg.reverse_resolve(element_t)
     feed_ctr.construct_list_feed(
         piece=htypes.rc_constructors.list_feed_ctr(
-            element_t=mosaic.put(string_res),
+            t=mosaic.put(feed_piece_t_res),
+            element_t=mosaic.put(element_t_res),
             ),
         custom_types=local_types,
         name_to_res=name_to_res,
         module_res=module_res,
         )
-    assert 'builtin_string.list_feed' in name_to_res
+    assert 'feed_ctr_tests_sample_feed.list_feed' in name_to_res
