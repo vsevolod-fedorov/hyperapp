2fc2802297ab16aa2d6be00a38e77065920b2c0c
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-04-22 11:33:13 +0300

    Feed: Add support for index tree diffs

--- hyperapp/rc/feed_ctr.tests.dyn.py
+++ hyperapp/rc/feed_ctr.tests.dyn.py
@@ -7,7 +7,7 @@ from .services import (
 from .tested.code import feed_ctr
 
 
-def test_construct_feed():
+def test_construct_list_feed():
     module_res = htypes.builtin.python_module(
         module_name='feed_ctr_tests',
         source='',
@@ -30,3 +30,28 @@ def test_construct_feed():
         module_res=module_res,
         )
     assert 'feed_ctr_tests_sample_feed.list_feed' in name_to_res
+
+
+def test_construct_index_tree_feed():
+    module_res = htypes.builtin.python_module(
+        module_name='feed_ctr_tests',
+        source='',
+        file_path='/feed_ctr_tests.dyn.py',
+        import_list=(),
+        )
+    string_res = pyobj_creg.reverse_resolve(htypes.builtin.string)
+    name_to_res = {}
+    feed_piece_t = htypes.feed_ctr_tests.sample_feed
+    feed_piece_t_res = pyobj_creg.reverse_resolve(feed_piece_t)
+    element_t = htypes.feed_ctr_tests.sample_item
+    element_t_res = pyobj_creg.reverse_resolve(element_t)
+    feed_ctr.construct_index_tree_feed(
+        piece=htypes.rc_constructors.index_tree_feed_ctr(
+            t=mosaic.put(feed_piece_t_res),
+            element_t=mosaic.put(element_t_res),
+            ),
+        custom_types=local_types,
+        name_to_res=name_to_res,
+        module_res=module_res,
+        )
+    assert 'feed_ctr_tests_sample_feed.index_tree_feed' in name_to_res
--- hyperapp/sample/sample_feed.dyn.py
+++ hyperapp/sample/sample_feed.dyn.py
@@ -6,16 +6,28 @@ from .services import (
     feed_factory,
     )
 from .code.list_diff import ListDiff
+from .code.tree_diff import TreeDiff
 
 log = logging.getLogger(__name__)
 
 
-async def _send_diff(feed):
+async def _send_list_diff(feed):
     log.info("Sending list diff")
     item = htypes.sample_list.item(44, "Sample item #4")
     await feed.send(ListDiff.Append(item))
 
 
-async def schedule_sample_feed(piece):
+async def schedule_sample_list_feed(piece):
     feed = feed_factory(piece)
-    asyncio.create_task(_send_diff(feed))
+    asyncio.create_task(_send_list_diff(feed))
+
+
+async def _send_tree_diff(feed):
+    log.info("Sending tree diff")
+    item = htypes.sample_tree.item(44, "Sample item #4")
+    await feed.send(TreeDiff.Append([], item))
+
+
+async def schedule_sample_tree_feed(piece):
+    feed = feed_factory(piece)
+    asyncio.create_task(_send_tree_diff(feed))
