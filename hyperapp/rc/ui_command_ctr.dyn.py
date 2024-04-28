d50b72d8d7008203d7bda0173a9ee055c5579e33
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-04-28 13:29:42 +0300

    RC command constructor: Add model commands

--- hyperapp/rc/ui_command_ctr.dyn.py
+++ hyperapp/rc/ui_command_ctr.dyn.py
@@ -21,21 +21,25 @@ def _make_command_d_res(custom_types, module_res, attr):
     return data_to_res(command_d_t())
 
 
-@constructor_creg.actor(htypes.rc_constructors.ui_command_ctr)
-def construct_ui_command(piece, custom_types, name_to_res, module_res, attr):
-    dir_res = data_to_res(htypes.ui.ui_command_d())
-    t_res = web.summon(piece.t)
+def _make_command(piece, custom_types, module_res, attr, command_t):
     attribute = htypes.builtin.attribute(
         object=mosaic.put(module_res),
         attr_name=attr.name,
         )
     command_d_res = _make_command_d_res(custom_types, module_res, attr)
-    command = htypes.ui.ui_command(
+    command = command_t(
         d=mosaic.put(command_d_res),
         name=piece.name,
         function=mosaic.put(attribute),
         params=piece.params,
         )
+    return (attribute, command_d_res, command)
+
+
+def _construct_ui_command(piece, custom_types, name_to_res, module_res, attr, command_t):
+    dir_res = data_to_res(htypes.ui.ui_command_d())
+    t_res = web.summon(piece.t)
+    attribute, command_d_res, command = _make_command(piece, custom_types, module_res, attr, command_t)
     association = Association(
         bases=[dir_res, t_res],
         key=[dir_res, t_res],
@@ -48,20 +52,19 @@ def construct_ui_command(piece, custom_types, name_to_res, module_res, attr):
     return [association]
 
 
-@constructor_creg.actor(htypes.rc_constructors.universal_ui_command_ctr)
-def construct_universal_ui_command(piece, custom_types, name_to_res, module_res, attr):
+@constructor_creg.actor(htypes.rc_constructors.ui_command_ctr)
+def construct_ui_command(piece, custom_types, name_to_res, module_res, attr):
+    return _construct_ui_command(piece, custom_types, name_to_res, module_res, attr, htypes.ui.ui_command)
+
+
+@constructor_creg.actor(htypes.rc_constructors.ui_model_command_ctr)
+def construct_ui_model_command(piece, custom_types, name_to_res, module_res, attr):
+    return _construct_ui_command(piece, custom_types, name_to_res, module_res, attr, htypes.ui.ui_model_command)
+
+
+def _construct_universal_ui_command(piece, custom_types, name_to_res, module_res, attr, command_t):
     dir_res = data_to_res(htypes.ui.universal_ui_command_d())
-    attribute = htypes.builtin.attribute(
-        object=mosaic.put(module_res),
-        attr_name=attr.name,
-        )
-    command_d_res = _make_command_d_res(custom_types, module_res, attr)
-    command = htypes.ui.ui_command(
-        d=mosaic.put(command_d_res),
-        name=piece.name,
-        function=mosaic.put(attribute),
-        params=piece.params,
-        )
+    attribute, command_d_res, command = _make_command(piece, custom_types, module_res, attr, command_t)
     association = Association(
         bases=[dir_res],
         key=[dir_res],
@@ -70,6 +73,15 @@ def construct_universal_ui_command(piece, custom_types, name_to_res, module_res,
     name_to_res['universal_ui_command_d'] = dir_res
     name_to_res[attr.name] = attribute
     name_to_res[f'{attr.name}.d'] = command_d_res
-    name_to_res[f'{attr.name}.ui_command'] = command
+    name_to_res[f'{attr.name}.universal_ui_command'] = command
     return [association]
 
+
+@constructor_creg.actor(htypes.rc_constructors.universal_ui_command_ctr)
+def construct_universal_ui_command(piece, custom_types, name_to_res, module_res, attr):
+    return _construct_universal_ui_command(piece, custom_types, name_to_res, module_res, attr, htypes.ui.ui_command)
+
+
+@constructor_creg.actor(htypes.rc_constructors.universal_ui_model_command_ctr)
+def construct_universal_ui_model_command(piece, custom_types, name_to_res, module_res, attr):
+    return _construct_universal_ui_command(piece, custom_types, name_to_res, module_res, attr, htypes.ui.ui_model_command)
--- hyperapp/rc/ui_command_ctr.tests.dyn.py
+++ hyperapp/rc/ui_command_ctr.tests.dyn.py
@@ -7,17 +7,29 @@ from .services import (
 from .tested.code import ui_command_ctr
 
 
-def test_construct_ui_command():
-    module_res = htypes.builtin.python_module(
+def make_module_res():
+    return htypes.builtin.python_module(
         module_name='ui_command_ctr_tests',
         source='',
         file_path='/ui_command_ctr_tests.dyn.py',
         import_list=(),
         )
+
+
+def make_inspect_attr():
+    return htypes.inspect.attr(
+        name='sample_command',
+        module=None,
+        constructors=(),
+        )
+
+
+def _test_construct_ui_command(command_ctr_t, fn):
+    module_res = make_module_res()
     string_res = pyobj_creg.reverse_resolve(htypes.builtin.string)
     name_to_res = {}
-    ui_command_ctr.construct_ui_command(
-        piece=htypes.rc_constructors.ui_command_ctr(
+    fn(
+        piece=command_ctr_t(
             t=mosaic.put(string_res),
             name='sample_command',
             params=('piece', 'state'),
@@ -25,35 +37,46 @@ def test_construct_ui_command():
         custom_types=local_types,
         name_to_res=name_to_res,
         module_res=module_res,
-        attr=htypes.inspect.attr(
-            name='sample_command',
-            module=None,
-            constructors=(),
-            ),
+        attr=make_inspect_attr(),
         )
     assert 'sample_command.ui_command' in name_to_res
 
 
-def test_construct_univeral_ui_command():
-    module_res = htypes.builtin.python_module(
-        module_name='ui_command_ctr_tests',
-        source='',
-        file_path='/ui_command_ctr_tests.py',
-        import_list=(),
-        )
+def test_construct_ui_command():
+    _test_construct_ui_command(
+        htypes.rc_constructors.ui_command_ctr, ui_command_ctr.construct_ui_command)
+
+
+def test_construct_ui_model_command():
+    _test_construct_ui_command(
+        htypes.rc_constructors.ui_model_command_ctr, ui_command_ctr.construct_ui_model_command)
+
+
+def _test_construct_univeral_ui_command(command_ctr_t, fn):
+    module_res = make_module_res()
     name_to_res = {}
-    ui_command_ctr.construct_universal_ui_command(
-        piece=htypes.rc_constructors.universal_ui_command_ctr(
+    fn(
+        piece=command_ctr_t(
             name='sample_command',
             params=('piece', 'state'),
             ),
         custom_types=local_types,
         name_to_res=name_to_res,
         module_res=module_res,
-        attr=htypes.inspect.attr(
-            name='sample_command',
-            module=None,
-            constructors=(),
-            ),
+        attr=make_inspect_attr(),
         )
-    assert 'sample_command.ui_command' in name_to_res
+    assert 'sample_command.universal_ui_command' in name_to_res
+
+
+def test_construct_univeral_ui_command():
+    _test_construct_univeral_ui_command(
+        htypes.rc_constructors.universal_ui_command_ctr,
+        ui_command_ctr.construct_universal_ui_command,
+    )
+
+
+def test_construct_univeral_ui_model_command():
+    _test_construct_univeral_ui_command(
+        htypes.rc_constructors.universal_ui_model_command_ctr,
+        ui_command_ctr.construct_universal_ui_model_command,
+    )
