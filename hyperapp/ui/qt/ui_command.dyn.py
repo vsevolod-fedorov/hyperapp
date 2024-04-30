02ea15a27d74a3dfc91e5a8f22250b1ab8054036
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-04-30 13:14:19 +0300

    Assign hotkey for open_tab_list command

--- hyperapp/ui/qt/ui_command.dyn.py
+++ hyperapp/ui/qt/ui_command.dyn.py
@@ -42,7 +42,8 @@ _hardcoded_shortcuts = {
     'add_view_command': 'Insert',
     'unwrap_master_details': 'Ctrl+U',
     'wrap_master_details': 'Ctrl+W',
-    'move_tab_to_new_group': 'Alt+T',
+    'move_tab_to_new_group': 'Shift+Alt+T',
+    'open_tab_list': 'Alt+T',
     }
 
 
