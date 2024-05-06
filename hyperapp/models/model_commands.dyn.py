99279b0eb1398830deca561fc3e620ff948fadd4
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-05-06 13:07:37 +0300

    model_commands module: Implement list_model_commands

--- hyperapp/models/model_commands.dyn.py
+++ hyperapp/models/model_commands.dyn.py
@@ -2,8 +2,23 @@
 
 from . import htypes
 from .services import (
+    model_command_factory,
     mosaic,
+    web,
     )
+
+
+def list_model_commands(piece, ctx):
+    model = web.summon(piece.model)
+    command_list = model_command_factory(model)
+    return [
+        htypes.model_commands.item(
+            name=command.name,
+            d=str(command.d),
+            params=", ".join(command.params),
+            )
+        for command in command_list
+        ]
 
 
 def open_model_commands(piece):
--- hyperapp/models/model_commands.tests.dyn.py
+++ hyperapp/models/model_commands.tests.dyn.py
@@ -1,4 +1,10 @@
 from . import htypes
+from .services import (
+    fn_to_res,
+    mark,
+    mosaic,
+    )
+from .code.context import Context
 from .tested.code import model_commands
 
 
@@ -9,3 +15,34 @@ def test_browse_current_model():
     model_2 = htypes.model_commands_tests.sample_model_2()
     piece_2 = model_commands.open_model_commands(model_2)
     assert piece_2
+
+
+def _phony_fn(piece, ctx):
+    pass
+
+
+@mark.service
+def model_command_factory():
+    def _model_command_factory(model):
+        return [
+            htypes.ui.model_command(
+                name="Sample model command",
+                d=(),
+                function=mosaic.put(fn_to_res(_phony_fn)),
+                params=('piece', 'ctx'),
+                )
+            ]
+    return _model_command_factory
+
+
+def test_list_model_commands():
+    model = htypes.model_commands_tests.sample_model_1()
+    piece = htypes.model_commands.model_commands(
+        model=mosaic.put(model),
+        )
+    ctx = Context()
+    result = model_commands.list_model_commands(piece, ctx)
+    assert result
+    assert len(result) == 1
+    assert result[0].name == "Sample model command"
+    assert result[0].params == "piece, ctx"
