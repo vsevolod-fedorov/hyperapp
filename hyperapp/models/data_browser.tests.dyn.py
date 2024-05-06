819360c40ae43b5ac5c5bd77e4d2101c7c8dab87
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-05-06 12:32:21 +0300

    RC: Add support for 'piece' parameters for global commands

--- hyperapp/models/data_browser.tests.dyn.py
+++ hyperapp/models/data_browser.tests.dyn.py
@@ -5,5 +5,5 @@ from .tested.code import data_browser
 def test_browse_current_model():
     piece_1 = htypes.data_browser_tests.sample_model_1()
     data_browser.browse_current_model(piece_1)
-    piece_2 = htypes.data_browser_tests.sample_model_1()
+    piece_2 = htypes.data_browser_tests.sample_model_2()
     data_browser.browse_current_model(piece_2)
