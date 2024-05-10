13f4b83b7ab1dce56da818d095d4cc2b0a11001d
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-05-10 12:10:18 +0300

    Browser: Implement 'open' command; Add record types support for it

--- hyperapp/models/data_browser.dyn.py
+++ hyperapp/models/data_browser.dyn.py
@@ -28,6 +28,19 @@ def browse(piece):
     raise RuntimeError(f"Browser: Unsupported type: {data_t}: {data}")
 
 
+def open(piece, current_item):
+    data = web.summon(piece.data)
+    data_t = deduce_t(data)
+    if isinstance(data_t, TRecord):
+        name = current_item.name
+        t = data_t.fields[name]
+        value = getattr(data, name)
+        return htypes.data_browser.data_browser(
+            data=mosaic.put(value, t),
+            )
+    raise RuntimeError(f"Browser open: Unsupported type: {data_t}: {data}")
+
+
 def browse_current_model(piece):
     return htypes.data_browser.data_browser(
         data=mosaic.put(piece),
--- hyperapp/models/data_browser.tests.dyn.py
+++ hyperapp/models/data_browser.tests.dyn.py
@@ -17,9 +17,36 @@ def test_browse_current_model():
 def test_browser():
     data = htypes.data_browser_tests.sample_data(
         name="Sample name",
+        sample_list=(11, 22, 33),
+        inner=htypes.data_browser_tests.inner_data("Sample inner text"),
         value=mosaic.put("Sample value"),
         )
     piece = htypes.data_browser.data_browser(
         data=mosaic.put(data),
         )
     result = data_browser.browse(piece)
+    assert len(result) == 4
+    assert isinstance(result[0], htypes.data_browser.item)
+    assert result[0].name == 'name'
+    assert result[0].type == 'str'
+    assert result[0].value == data.name
+    assert result[1].name == 'sample_list'
+
+
+def test_open():
+    data = htypes.data_browser_tests.sample_data(
+        name="Sample name",
+        sample_list=(11, 22, 33),
+        inner=htypes.data_browser_tests.inner_data("Sample inner text"),
+        value=mosaic.put("Sample value"),
+        )
+    piece = htypes.data_browser.data_browser(
+        data=mosaic.put(data),
+        )
+    current_item = htypes.data_browser.item(
+        name="inner",
+        type="",
+        value="",
+        )
+    result = data_browser.open(piece, current_item)
+    assert isinstance(result, htypes.data_browser.data_browser)
