6caf774e4d79961b3fdd269e896cd5af2911b3d7
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-05-10 11:54:54 +0300

    Browser: Implement browser; Add record types support

--- hyperapp/models/data_browser.dyn.py
+++ hyperapp/models/data_browser.dyn.py
@@ -1,7 +1,31 @@
+from hyperapp.common.htypes import TRecord
+from hyperapp.common.htypes.deduce_value_type import deduce_complex_value_type
+
 from . import htypes
 from .services import (
     mosaic,
+    types,
+    web,
     )
+
+
+def deduce_t(data):
+    return deduce_complex_value_type(mosaic, types, data)
+
+
+def browse(piece):
+    data = web.summon(piece.data)
+    data_t = deduce_t(data)
+    if isinstance(data_t, TRecord):
+        return [
+            htypes.data_browser.item(
+                name=name,
+                type=str(t),
+                value=str(getattr(data, name)),
+                )
+            for name, t in data_t.fields.items()
+            ]
+    raise RuntimeError(f"Browser: Unsupported type: {data_t}: {data}")
 
 
 def browse_current_model(piece):
