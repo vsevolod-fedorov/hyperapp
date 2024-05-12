e7a01ce935d737045227d07b5be0a2877b32452d
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-05-12 12:46:26 +0300

    File bundles: Implement 'open' command; load file bundle contents

--- hyperapp/models/file_bundles.dyn.py
+++ hyperapp/models/file_bundles.dyn.py
@@ -1,12 +1,34 @@
+from pathlib import Path
+
+from hyperapp.common import dict_coders  # register codec
+
 from . import htypes
-
-
-def file_bundle_list(piece):
-    return [
-        htypes.file_bundles.bundle("lcs", "~/.local/share/hyperapp/client/lcs.cdr"),
-        htypes.file_bundles.bundle("client state", "~/.local/share/hyperapp/client/state.json"),
-        ]
+from .services import (
+    file_bundle,
+    )
 
 
 def open_file_bundle_list():
     return htypes.file_bundles.bundle_list()
+
+
+def file_bundle_list(piece):
+    return [
+        htypes.file_bundles.bundle_item("lcs", "~/.local/share/hyperapp/client/lcs.cdr"),
+        htypes.file_bundles.bundle_item("client state", "~/.local/share/hyperapp/client/state.json"),
+        ]
+
+
+def open(piece, current_item):
+    path = Path(current_item.path).expanduser()
+    if path.suffix == '.json':
+        encoding = 'json'
+    elif path.suffix == '.cdr':
+        encoding = 'cdr'
+    else:
+        raise RuntimeError(f"Unknown file bundle encoding suffix: {path.suffix!r}")
+    bundle = file_bundle(path, encoding)
+    piece_ref = bundle.load_ref()
+    return htypes.data_browser.data_browser(
+        data=piece_ref,
+        )
--- hyperapp/models/file_bundles.tests.dyn.py
+++ hyperapp/models/file_bundles.tests.dyn.py
@@ -1,8 +1,13 @@
+from pathlib import Path
+
 from . import htypes
+from .services import (
+    file_bundle,
+    )
 from .tested.code import file_bundles
 
 
-def test_open():
+def test_open_list():
     piece = file_bundles.open_file_bundle_list()
     assert isinstance(piece, htypes.file_bundles.bundle_list)
 
@@ -11,4 +16,19 @@ def test_bundle_list():
     piece = htypes.file_bundles.bundle_list()
     result = file_bundles.file_bundle_list(piece)
     assert type(result) is list
-    assert isinstance(result[0], htypes.file_bundles.bundle)
+    assert isinstance(result[0], htypes.file_bundles.bundle_item)
+
+
+def test_open_bundle():
+    path = "~/.cache/hyperapp/test/file_bundles.cdr"
+    root = htypes.file_bundles_tests.sample_root()
+    bundle = file_bundle(Path(path).expanduser(), 'cdr')
+    bundle.save_piece(root)
+
+    piece = htypes.file_bundles.bundle_list()
+    current_item = htypes.file_bundles.bundle_item(
+        name="sample test bundle",
+        path=path,
+        )
+    result = file_bundles.open(piece, current_item)
+    assert result
