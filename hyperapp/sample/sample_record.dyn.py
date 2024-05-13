a29c3e926131a053c5d2d162c5aacbebae541858
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-05-13 11:35:59 +0300

    Add open_sample_fn_record command

--- hyperapp/sample/sample_record.dyn.py
+++ hyperapp/sample/sample_record.dyn.py
@@ -7,3 +7,7 @@ from .services import (
 @mark.model
 def sample_record(piece):
     return htypes.sample_record.item(123, "Sample title")
+
+
+def open_sample_fn_record():
+    return htypes.sample_record.sample_record()
