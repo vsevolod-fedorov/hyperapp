eed733c78fa17a2def35dd0ba35c33c7abfdffa5
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-05-23 11:10:25 +0300

    Add command groups

---
+++ hyperapp/ui/command_groups.dyn.py
@@ -0,0 +1,19 @@
+from . import htypes
+
+
+def default_command_groups(properties):
+    global_d = htypes.command_groups.global_d()
+    view_d = htypes.command_groups.view_d()
+    model_d = htypes.command_groups.model_d()
+    context_d = htypes.command_groups.context_d()
+    remotable_d = htypes.command_groups.remotable_d()
+
+    groups = set()
+    if properties.is_global:
+        groups.add(global_d)
+    if properties.uses_state:
+        groups.add(context_d)
+    if properties.remotable:
+        groups.add(remotable_d)
+
+    return groups
