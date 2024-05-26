4ad7ba97a3c5811eab92baf1578f7e35d682816f
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-05-26 11:27:05 +0300

    Command pane: Add spacing to separate context and model commands

--- hyperapp/ui/qt/command_pane.dyn.py
+++ hyperapp/ui/qt/command_pane.dyn.py
@@ -14,6 +14,7 @@ class CommandPane(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self._command_to_button = {}
+        self._spacing_idx = None
 
 
 class CommandPaneView(View):
@@ -34,19 +35,24 @@ class CommandPaneView(View):
         layout = QtWidgets.QVBoxLayout(w, spacing=1)
         layout.setAlignment(QtCore.Qt.AlignTop)
         layout.setContentsMargins(2, 2, 2, 2)
+        layout.addSpacing(10)
+        w._spacing_idx = 0
         return w
 
     def widget_state(self, widget):
         return htypes.command_pane.state()
 
     def set_commands(self, widget, commands):
+        pane_1_d = htypes.command_groups.pane_1_d()
+        pane_2_d = htypes.command_groups.pane_2_d()
         layout = widget.layout()
-        wanted_commands = {
+        new_commands = {
             cmd for cmd in commands
-            if self._show_command(cmd.groups)
+            if {pane_1_d, pane_2_d} & cmd.groups
             }
-        removed_commands = set(widget._command_to_button) - wanted_commands
-        new_commands = wanted_commands - set(widget._command_to_button)
+        removed_commands = set(widget._command_to_button) - new_commands
+        widget._spacing_idx -= sum(1 for cmd in removed_commands if pane_1_d in cmd.groups)
+        new_commands = new_commands - set(widget._command_to_button)
         for cmd in removed_commands:
             button = widget._command_to_button.pop(cmd)
             button.deleteLater()
@@ -54,15 +60,12 @@ class CommandPaneView(View):
             # All except context commands are present at menu bar;
             # avoid setting shortcut to prevent ambigous ones.
             button = self._make_button(cmd, add_shortcut=True)
-            layout.addWidget(button)
+            if pane_1_d in cmd.groups:
+                layout.insertWidget(widget._spacing_idx, button)
+            else:
+                layout.addWidget(button)
             widget._command_to_button[cmd] = button
-
-    def _show_command(self, groups):
-        pane_1_d = htypes.command_groups.pane_1_d()
-        pane_2_d = htypes.command_groups.pane_2_d()
-        if {pane_1_d, pane_2_d} & groups:
-            return True
-        return False
+        widget._spacing_idx += sum(1 for cmd in new_commands if pane_1_d in cmd.groups)
 
     @staticmethod
     def _make_button(cmd, add_shortcut):
