c619e0a06dd5c14c440932ee0f915d0bb31a9934
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-05-31 11:54:58 +0300

    Tcp tests callback: Add logging

--- hyperapp/transport/tcp_tests_callback.dyn.py
+++ hyperapp/transport/tcp_tests_callback.dyn.py
@@ -1,3 +1,5 @@
+import logging
+
 from .services import (
     endpoint_registry,
     generate_rsa_identity,
@@ -6,11 +8,16 @@ from .services import (
     rpc_endpoint_factory,
     )
 
+log = logging.getLogger(__name__)
+
 
 def tcp_callback(tcp_master_peer_piece, master_fn_ref):
+    log.info("tcp_callback: entered")
     tcp_master_peer = peer_registry.animate(tcp_master_peer_piece)
     rpc_endpoint = rpc_endpoint_factory()
     my_identity = generate_rsa_identity(fast=True)
     endpoint_registry.register(my_identity, rpc_endpoint)
     rpc_call = rpc_call_factory(rpc_endpoint, tcp_master_peer, master_fn_ref, my_identity)
+    log.info("tcp_callback: Calling master:")
     rpc_call(message='hello')
+    log.info("tcp_callback: Calling master: done")
