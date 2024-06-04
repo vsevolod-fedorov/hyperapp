c476e521cc75e61e2097b7935a473992a4753eb0
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-06-04 12:35:24 +0300

    Ref picker tests: Remove types service and adding phohy ref to cache

--- hyperapp/common/ref_picker.tests.dyn.py
+++ hyperapp/common/ref_picker.tests.dyn.py
@@ -6,12 +6,10 @@ from hyperapp.common.htypes import (
     tString,
     ref_t,
     )
-from hyperapp.common.htypes.phony_ref import phony_ref
 from hyperapp.common.htypes.deduce_value_type import deduce_value_type
 
 from .services import (
     mosaic,
-    types,
     )
 from .tested.code import ref_picker
 from .tested.services import pick_refs
@@ -45,8 +43,6 @@ def test_record():
         'some_string': tString,
         'some_ref': ref_t,
         })
-    # Test tracer resolves traced types to refs.
-    # types.add_to_cache(phony_ref('ref_picker_tests-test_record'), t)
     picker = ref_picker._t_to_picker(t)
     ref = mosaic.put("Sample text")
     record = t("Some string", ref)
@@ -57,8 +53,6 @@ def test_one_field_record():
     t = TRecord('ref_picker_tests', 'test_one_field_record', {
         'some_ref': ref_t,
         })
-    # Test tracer resolves traced types to refs.
-    # types.add_to_cache(phony_ref('ref_picker_tests-test_one_field_record'), t)
     picker = ref_picker._t_to_picker(t)
     ref = mosaic.put("Sample text")
     record = t(ref)
@@ -70,8 +64,6 @@ def test_exception():
         'some_string': tString,
         'some_ref': ref_t,
         })
-    # Test tracer resolves traced types to refs.
-    # types.add_to_cache(phony_ref('ref_picker_tests-test_exception-exception'), t)
     picker = ref_picker._t_to_picker(t)
     ref = mosaic.put("Sample text")
     record = t("Some string", ref)
@@ -85,8 +77,6 @@ def test_combined():
         'some_opt': opt_t,
         'some_list': list_t,
         })
-    # Test tracer resolves traced types to refs.
-    # types.add_to_cache(phony_ref('ref_picker_tests-test_composite-record'), t)
     picker = ref_picker._t_to_picker(t)
     ref_1 = mosaic.put("Sample text 1")
     ref_2 = mosaic.put("Sample text 2")
@@ -102,8 +92,6 @@ def test_service():
         'some_opt': opt_t,
         'some_list': list_t,
         })
-    # Test tracer resolves traced types to refs.
-    # types.add_to_cache(phony_ref('ref_picker_tests-test_service-record'), t)
     ref_1 = mosaic.put("Sample text 1")
     ref_2 = mosaic.put("Sample text 2")
     ref_3 = mosaic.put("Sample text 3")
