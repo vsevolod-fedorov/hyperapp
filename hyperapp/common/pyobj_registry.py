dec3f3701eac15e9fe1d79d26fdb3dfce6e08148
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-06-06 14:10:31 +0300

    Log reconstructed actors

--- hyperapp/common/pyobj_registry.py
+++ hyperapp/common/pyobj_registry.py
@@ -1,5 +1,9 @@
+import logging
+
 from .htypes import register_builtin_meta_types, register_meta_types
 from .cached_code_registry import CachedCodeRegistry
+
+log = logging.getLogger(__name__)
 
 
 class PyObjRegistry(CachedCodeRegistry):
@@ -22,6 +26,7 @@ class PyObjRegistry(CachedCodeRegistry):
             for rctr in self._reconstructors:
                 piece = rctr(actor)
                 if piece is not None:
+                    log.debug("Reconstructed actor %r to %r using %r", actor, piece, rctr)
                     self.add_to_cache(piece, actor)
                     return piece
             raise
