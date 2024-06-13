b1de7bb3da3fcda785f0f1ae48f85e5ee69178fd
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-06-13 13:34:03 +0300

    LCS resource storage: Save, load and check mapping

--- hyperapp/common/lcs_resource_storage.dyn.py
+++ hyperapp/common/lcs_resource_storage.dyn.py
@@ -4,29 +4,79 @@ import yaml
 
 from hyperapp.common.htypes import TRecord
 
+from . import htypes
 from .services import (
     deduce_t,
+    mosaic,
     resource_module_factory,
     resource_registry,
+    web,
     )
 
 
 class LcsResourceStorage:
 
-    def __init__(self, name, path=None):
+    _mapping_name = 'lcs_storage_mapping'
+
+    def __init__(self, name, path):
         self._path = path
-        self._res_module = resource_module_factory(resource_registry, name, path, load_from_file=False)
+        self._res_module = resource_module_factory(
+            resource_registry, name, path, load_from_file=path.exists())
+        self._mapping_cache = None
 
     def set(self, dir, piece):
         frozen_dir = frozenset(dir)
-        name = self._make_name(piece)
-        self._res_module[name] = piece
+        for elt in dir:
+            self._store(elt)
+        self._store(piece)
+        self._mapping[frozen_dir] = piece
         self._save()
 
     def get(self, dir):
         frozen_dir = frozenset(dir)
+        return self._mapping.get(frozen_dir)
+
+    def _store(self, piece):
+        name = self._make_name(piece)
+        self._res_module[name] = piece
+
+    @property
+    def _mapping(self):
+        if self._mapping_cache is not None:
+            return self._mapping_cache
+        self._mapping_cache = {}
+        try:
+            mapping = self._res_module[self._mapping_name]
+        except KeyError:
+            return self._mapping_cache
+        for ref in mapping:
+            elt = web.summon(ref)
+            key = frozenset(web.summon(d) for d in elt.key)
+            value = web.summon(elt.value)
+            self._mapping_cache[key] = value
+        return self._mapping_cache
 
     def _save(self):
+        element_list = []
+        for item, idx in itertools.zip_longest(
+                self._mapping.items(), itertools.count()):
+            name = f'mapping_element_{idx}'
+            if item:
+                key, value = item
+                element = htypes.lcs_resource_storage.element(
+                    key=tuple(mosaic.put(d) for d in key),
+                    value=mosaic.put(value),
+                    )
+                self._res_module[name] = element
+                element_list.append(element)
+            else:
+                if name not in self._res_module:
+                    break
+                del self._res_module[name]
+        mapping = htypes.lcs_resource_storage.mapping(
+            elements=tuple(mosaic.put(elt) for elt in element_list),
+            )
+        self._res_module[self._mapping_name] = mapping
         text = yaml.dump(self._res_module.as_dict, sort_keys=False)
         self._path.write_text(text)
 
--- hyperapp/common/lcs_resource_storage.tests.dyn.py
+++ hyperapp/common/lcs_resource_storage.tests.dyn.py
@@ -1,16 +1,33 @@
 from pathlib import Path
 
 from . import htypes
+from .services import (
+    legacy_type_resource_loader,
+    local_types,
+    resource_registry,
+    )
 from .tested.code import lcs_resource_storage
 
 
 def test_set():
+    # Subprocess does not add local types to resource registry.
+    resource_registry.update_modules(legacy_type_resource_loader(local_types))
+
     path = Path('/tmp/lcs-storage-test.yaml')
-    storage = lcs_resource_storage.LcsResourceStorage('test.lcs_storage', path)
+    try:
+        path.unlink()
+    except FileNotFoundError:
+        pass
+
     d_1 = htypes.lcs_resource_storage_tests.sample_1_d()
     d_2 = htypes.lcs_resource_storage_tests.sample_2_d('some-path')
     piece = htypes.lcs_resource_storage_tests.sample_piece(
         direction='sample-direction',
         stretch=12345,
         )
-    storage.set({d_1, d_2}, piece)
+
+    storage_1 = lcs_resource_storage.LcsResourceStorage('test.lcs_storage', path)
+    storage_1.set({d_1, d_2}, piece)
+
+    storage_2 = lcs_resource_storage.LcsResourceStorage('test.lcs_storage', path)
+    assert storage_2.get({d_1, d_2}) == piece
