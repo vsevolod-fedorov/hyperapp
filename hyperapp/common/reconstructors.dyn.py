dad3907c465433b7a05f61a2fac166d7ce41e4fb
Vsevolod Fedorov <vfedorov@networkoptix.com>
2024-06-17 10:21:21 +0300

    Revert "Add data reconstructor"
    This reverts commit 5007ee36b3cb9e9e705644f51de6a6617164706d.
    
    Reconstructed data should not be added to pyobj_creg cache.

--- hyperapp/common/reconstructors.dyn.py
+++ hyperapp/common/reconstructors.dyn.py
@@ -2,11 +2,9 @@ from .services import (
     reconstructors,
     )
 from .code.type_reconstructor import type_to_piece
-from .code.data_reconstructor import data_to_piece
 
 
 def register_reconstructors():
     reconstructors.extend([
         type_to_piece,
-        data_to_piece,
         ])
--- hyperapp/resource/data_to_res.dyn.py
+++ hyperapp/resource/data_to_res.dyn.py
@@ -1,4 +1,9 @@
+from hyperapp.common.htypes import TRecord
+from hyperapp.common.htypes.deduce_value_type import deduce_value_type
+
+from . import htypes
 from .services import (
+    deduce_t,
     mark,
     pyobj_creg,
     )
@@ -7,5 +12,11 @@ from .services import (
 @mark.service
 def data_to_res():
     def _data_to_res(piece):
-        return pyobj_creg.actor_to_piece(piece)
+        t = deduce_t(piece)
+        assert isinstance(t, TRecord)  # TODO: Add support for other types.
+        assert not t.fields  # TODO: Add support for non-empty records.
+        t_ref = pyobj_creg.actor_to_ref(t)
+        return htypes.builtin.call(
+            function=t_ref,
+            )
     return _data_to_res
