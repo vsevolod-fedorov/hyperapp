ca28e5d5eea03e81a4da562dbf8b5de6d6ca2373
Vsevolod Fedorov <vfedorov@networkoptix.com>
2024-06-17 10:26:43 +0300

    Data to res: Improve test; Remove unused import

--- hyperapp/resource/data_to_res.tests.dyn.py
+++ hyperapp/resource/data_to_res.tests.dyn.py
@@ -1,4 +1,7 @@
 from . import htypes
+from .services import (
+    pyobj_creg,
+    )
 from .tested.code import data_to_res
 from .tested.services import data_to_res
 
@@ -6,4 +9,5 @@ from .tested.services import data_to_res
 def test_data_to_res():
     piece = htypes.data_to_res_tests.sample_empty_record()
     res = data_to_res(piece)
-    assert res
+    piece_2 = pyobj_creg.animate(res)
+    assert piece_2 == piece
