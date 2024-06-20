4b762720773095a2ab8aecae63b6e1fc6557f682
Vsevolod Fedorov <vfedorov@networkoptix.com>
2024-06-20 11:00:52 +0300

    Improve lcs storage test - add type resource d

--- hyperapp/common/lcs_resource_storage.tests.dyn.py
+++ hyperapp/common/lcs_resource_storage.tests.dyn.py
@@ -2,9 +2,11 @@ from pathlib import Path
 
 from . import htypes
 from .services import (
+    builtin_types_as_dict,
     legacy_type_resource_loader,
     local_types,
     mosaic,
+    pyobj_creg,
     resource_registry,
     )
 from .tested.code import lcs_resource_storage
@@ -12,7 +14,11 @@ from .tested.code import lcs_resource_storage
 
 def test_set():
     # Subprocess does not add local types to resource registry.
-    resource_registry.update_modules(legacy_type_resource_loader(local_types))
+    resource_registry.update_modules(legacy_type_resource_loader({
+        **builtin_types_as_dict(), **local_types}))
+    # Test driver does not load types from resources.
+    # Manually add used type to cache.
+    _ = resource_registry['legacy_type.lcs_resource_storage_tests', 'sample_1_d']
 
     path = Path('/tmp/lcs-storage-test.yaml')
     try:
@@ -20,7 +26,8 @@ def test_set():
     except FileNotFoundError:
         pass
 
-    d_1 = htypes.lcs_resource_storage_tests.sample_1_d()
+    d_1_t = htypes.lcs_resource_storage_tests.sample_1_d
+    d_1 = pyobj_creg.actor_to_piece(d_1_t)
     d_2 = htypes.lcs_resource_storage_tests.sample_2_d('some-path')
     inner = htypes.lcs_resource_storage_tests.inner_piece(
         value=(11, 22, 33),
