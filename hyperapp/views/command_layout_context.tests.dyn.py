24fbba49ad59fb2b45c48e74a22e5365222f1634
Vsevolod Fedorov <vfedorov@networkoptix.com>
2024-06-21 13:39:26 +0300

    WIP: Command layout context

---
+++ hyperapp/views/command_layout_context.tests.dyn.py
@@ -0,0 +1,61 @@
+from unittest.mock import Mock
+
+from PySide6 import QtWidgets
+
+from . import htypes
+from .services import (
+    data_to_res,
+    fn_to_ref,
+    mark,
+    mosaic,
+    )
+from .code.context import Context
+from .tested.code import command_layout_context
+
+
+_view_creg_mock = Mock()
+
+
+@mark.service
+def view_creg():
+    return _view_creg_mock
+
+
+def _sample_command_fn(piece, ctx):
+    return "Sample result"
+
+
+def _make_sample_command():
+    d_res = data_to_res(htypes.command_layout_context_tests.sample_d())
+    model_impl = htypes.ui.model_command_impl(
+        function=fn_to_ref(_sample_command_fn),
+        params=('piece', 'ctx'),
+        )
+    return htypes.ui.model_command(
+        d=mosaic.put(d_res),
+        impl=mosaic.put(model_impl),
+        )
+
+
+def test_open_command_layout_context():
+    sample_command = _make_sample_command()
+    ctx = Context()
+    model = htypes.command_layout_context_tests.sample_model()
+    model_state = htypes.command_layout_context_tests.sample_model_state()
+    piece = htypes.model_commands.model_commands(
+        model=mosaic.put(model),
+        model_state=mosaic.put(model_state)
+        )
+    current_item = htypes.model_commands.item(
+        command=mosaic.put(sample_command),
+        name="<unused>",
+        impl="<unused>",
+        )
+    view = Mock(
+        piece=htypes.label.view("Sample view"),
+        )
+    state = htypes.label.state()
+    hook = Mock()
+    command_layout_context.open_command_layout_context(piece, current_item, view, state, hook, ctx)
+    _view_creg_mock.animate.assert_called_once()
+    hook.replace_view.assert_called_once()
