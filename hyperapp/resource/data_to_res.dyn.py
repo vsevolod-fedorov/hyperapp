6a1b9013442154f178103b8c1b86dd853963acd6
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-06-25 12:35:09 +0300

    data_to_ref service

--- hyperapp/resource/data_to_res.dyn.py
+++ hyperapp/resource/data_to_res.dyn.py
@@ -9,32 +9,41 @@ from .services import (
     )
 
 
+def _data_to_res(piece, t=None):
+    if t is None:
+        t = deduce_t(piece)
+    assert isinstance(t, TRecord)  # TODO: Add support for other types.
+    t_ref = pyobj_creg.actor_to_ref(t)
+    if t.fields:
+        params = tuple(
+            htypes.partial.param(
+                name=name,
+                value=mosaic.put(
+                    htypes.raw.raw(
+                        mosaic.put(
+                            getattr(piece, name)))),
+                )
+            for name in t.fields
+            )
+        partial = htypes.partial.partial(
+            function=t_ref,
+            params=params,
+            )
+        fn_ref = mosaic.put(partial)
+    else:
+        fn_ref = t_ref
+    return htypes.builtin.call(
+        function=fn_ref,
+        )
+
+
 @mark.service
 def data_to_res():
-    def _data_to_res(piece, t=None):
-        if t is None:
-            t = deduce_t(piece)
-        assert isinstance(t, TRecord)  # TODO: Add support for other types.
-        t_ref = pyobj_creg.actor_to_ref(t)
-        if t.fields:
-            params = tuple(
-                htypes.partial.param(
-                    name=name,
-                    value=mosaic.put(
-                        htypes.raw.raw(
-                            mosaic.put(
-                                getattr(piece, name)))),
-                    )
-                for name in t.fields
-                )
-            partial = htypes.partial.partial(
-                function=t_ref,
-                params=params,
-                )
-            fn_ref = mosaic.put(partial)
-        else:
-            fn_ref = t_ref
-        return htypes.builtin.call(
-            function=fn_ref,
-            )
     return _data_to_res
+
+
+@mark.service
+def data_to_ref():
+    def _data_to_ref(piece, t=None):
+        return mosaic.put(_data_to_res(piece, t))
+    return _data_to_ref
