5792360e114f14ee4d321ec74fcde51f01fb4af3
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-06-26 13:04:48 +0300

    add_identity_command command placeholder and test

---
+++ hyperapp/ui/identity_command.tests.dyn.py
@@ -0,0 +1,36 @@
+from . import htypes
+from .services import (
+    data_to_res,
+    fn_to_ref,
+    mosaic,
+    )
+from .code.context import Context
+from .tested.code import identity_command
+
+
+def _sample_command_fn(piece, ctx):
+    return "Sample result"
+
+
+def _make_sample_command():
+    d_res = data_to_res(htypes.identity_command_tests.sample_d())
+    model_impl = htypes.ui.model_command_impl(
+        function=fn_to_ref(_sample_command_fn),
+        params=('piece', 'ctx'),
+        )
+    return htypes.ui.model_command(
+        d=mosaic.put(d_res),
+        impl=mosaic.put(model_impl),
+        )
+
+
+def test_add_identity_command():
+    sample_command = _make_sample_command()
+    ctx = Context()
+    model = htypes.identity_command_tests.sample_model()
+    model_state = htypes.identity_command_tests.sample_model_state()
+    piece = htypes.model_commands.model_commands(
+        model=mosaic.put(model),
+        model_state=mosaic.put(model_state)
+        )
+    identity_command.add_identity_command(piece)
