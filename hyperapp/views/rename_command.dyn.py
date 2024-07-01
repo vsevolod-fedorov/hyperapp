d0f6dbf6b83f251dc3d5308fe354a0579684de1a
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-07-01 13:06:26 +0300

    Rename command: Use text editor as base view; move to model view

--- hyperapp/views/rename_command.dyn.py
+++ hyperapp/views/rename_command.dyn.py
@@ -2,17 +2,18 @@ from . import htypes
 from .services import (
     mosaic,
     pyobj_creg,
-    view_creg,
+    model_view_creg,
     web,
     )
 from .code.context_view import ContextView
+from .code.command import d_res_ref_to_name
 
 
 class RenameCommandContextView(ContextView):
 
     @classmethod
-    def from_piece(cls, piece, ctx):
-        base_view = view_creg.invite(piece.base, ctx)
+    def from_piece(cls, piece, model, ctx):
+        base_view = model_view_creg.invite(piece.base, model, ctx)
         command = web.summon(piece.ui_command)
         impl = web.summon(command.impl)
         command_d = pyobj_creg.invite(command.d)
@@ -43,12 +44,19 @@ class RenameCommandContextView(ContextView):
 def rename_command(piece, current_item, navigator, ctx):
     if current_item is None:
         return None  # Empty command list - no item is selected.
+    adapter = htypes.str_adapter.static_str_adapter()
+    text_view = htypes.text.edit_view(
+        adapter=mosaic.put(adapter),
+        )
     new_view_piece = htypes.rename_command.view(
-        base=mosaic.put(navigator.view.piece),
+        base=mosaic.put(text_view),
         ui_command=current_item.command,
         )
+    text_state = htypes.text.state()
     new_state = htypes.rename_command.state(
-        base=mosaic.put(navigator.state),
+        base=mosaic.put(text_state),
         )
-    new_view = view_creg.animate(new_view_piece, ctx)
-    navigator.hook.replace_view(new_view, new_state)
+    command = web.summon(current_item.command)
+    name = d_res_ref_to_name(command.d)
+    new_view = model_view_creg.animate(new_view_piece, name, ctx)
+    navigator.view.open(ctx, name, new_view, navigator.widget_wr())
