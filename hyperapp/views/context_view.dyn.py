24a02558f8108647bb69ef07a7e6052d32a6bfa7
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-07-01 12:21:28 +0300

    Refactoring: Extract context view

---
+++ hyperapp/views/context_view.dyn.py
@@ -0,0 +1,49 @@
+from PySide6 import QtWidgets
+
+from .services import (
+    web,
+    )
+from .code.view import Item, View
+
+
+class ContextView(View):
+
+    def __init__(self, base_view, label):
+        super().__init__()
+        self._base_view = base_view
+        self._label = label
+
+    def construct_widget(self, state, ctx):
+        if state is not None:
+            base_state = web.summon(state.base)
+        else:
+            base_state = None
+        base_widget = self._base_view.construct_widget(base_state, ctx)
+        widget = QtWidgets.QWidget()
+        layout = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.Direction.TopToBottom, widget)
+        layout.addWidget(QtWidgets.QLabel(text=self._label))
+        layout.addWidget(base_widget)
+        return widget
+
+    def get_current(self, widget):
+        return 0
+
+    def replace_child_widget(self, widget, idx, new_child_widget):
+        if idx != 0:
+            return super().replace_child_widget(widget, idx, new_child_widget)
+        layout = widget.layout()
+        old_w = layout.itemAt(1).widget()
+        layout.replaceWidget(old_w, new_child_widget)
+        old_w.deleteLater()
+
+    def items(self):
+        return [Item('base', self._base_view)]
+
+    def item_widget(self, widget, idx):
+        if idx == 0:
+            return self._base_widget(widget)
+        return super().item_widget(widget, idx)
+
+    def _base_widget(self, widget):
+        layout = widget.layout()
+        return layout.itemAt(1).widget()
--- hyperapp/views/local_server_context.dyn.py
+++ hyperapp/views/local_server_context.dyn.py
@@ -1,19 +1,16 @@
 from functools import cached_property
 
-from PySide6 import QtWidgets
-
 from . import htypes
 from .services import (
     mark,
     mosaic,
     view_creg,
-    web,
     )
-from .code.view import Item, View
+from .code.context_view import ContextView
 from .code.local_server import local_server_peer
 
 
-class LocalServerContextView(View):
+class LocalServerContextView(ContextView):
 
     @classmethod
     def from_piece(cls, piece, ctx):
@@ -21,26 +18,13 @@ class LocalServerContextView(View):
         return cls(base_view)
 
     def __init__(self, base_view):
-        super().__init__()
-        self._base_view = base_view
+        super().__init__(base_view, label="Local server")
 
     @property
     def piece(self):
         return htypes.local_server_context.view(
             base=mosaic.put(self._base_view.piece),
             )
-
-    def construct_widget(self, state, ctx):
-        if state is not None:
-            base_state = web.summon(state.base)
-        else:
-            base_state = None
-        base_widget = self._base_view.construct_widget(base_state, ctx)
-        widget = QtWidgets.QWidget()
-        layout = QtWidgets.QBoxLayout(QtWidgets.QBoxLayout.Direction.TopToBottom, widget)
-        layout.addWidget(QtWidgets.QLabel(text="Local server"))
-        layout.addWidget(base_widget)
-        return widget
 
     def children_context(self, ctx):
         return ctx.clone_with(
@@ -51,35 +35,12 @@ class LocalServerContextView(View):
     def _local_server_peer(self):
         return local_server_peer()
 
-    def get_current(self, widget):
-        return 0
-
     def widget_state(self, widget):
         base_widget = self._base_widget(widget)
         base_state = self._base_view.widget_state(base_widget)
         return htypes.local_server_context.state(
             base=mosaic.put(base_state),
             )
-
-    def replace_child_widget(self, widget, idx, new_child_widget):
-        if idx != 0:
-            return super().replace_child_widget(widget, idx, new_child_widget)
-        layout = widget.layout()
-        old_w = layout.itemAt(1).widget()
-        layout.replaceWidget(old_w, new_child_widget)
-        old_w.deleteLater()
-
-    def items(self):
-        return [Item('base', self._base_view)]
-
-    def item_widget(self, widget, idx):
-        if idx == 0:
-            return self._base_widget(widget)
-        return super().item_widget(widget, idx)
-
-    def _base_widget(self, widget):
-        layout = widget.layout()
-        return layout.itemAt(1).widget()
 
 
 @mark.ui_command(htypes.navigator.view)
