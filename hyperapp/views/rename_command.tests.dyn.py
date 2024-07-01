a86540f935c302ecf91b22dde2e2777276389887
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-07-01 12:34:17 +0300

    Rename command: Create, open context view

---
+++ hyperapp/views/rename_command.tests.dyn.py
@@ -0,0 +1,92 @@
+from unittest.mock import Mock
+
+from PySide6 import QtWidgets
+
+from . import htypes
+from .services import (
+    data_to_res,
+    fn_to_ref,
+    mark,
+    mosaic,
+    web,
+    )
+from .services import view_creg as real_view_creg
+from .code.context import Context
+from .tested.code import rename_command
+
+
+_view_creg_mock = Mock()
+
+
+@mark.service
+def view_creg():
+    return _view_creg_mock
+
+
+def _sample_command_fn(piece, ctx):
+    return "Sample result"
+
+
+def _make_sample_ui_command():
+    d_res = data_to_res(htypes.command_layout_context_tests.sample_d())
+    model_impl = htypes.ui.model_command_impl(
+        function=fn_to_ref(_sample_command_fn),
+        params=('piece', 'ctx'),
+        )
+    ui_impl = htypes.ui.ui_model_command_impl(
+        model_command_impl=mosaic.put(model_impl),
+        layout=None,
+        )
+    return htypes.ui.command(
+        d=mosaic.put(d_res),
+        impl=mosaic.put(ui_impl),
+        )
+
+
+def test_view():
+    _view_creg_mock.invite = real_view_creg.invite  # Used to resolve base view.
+    sample_ui_command = _make_sample_ui_command()
+    ctx = Context(
+        lcs=Mock(),
+        )
+    model = htypes.rename_command_tests.sample_model()
+    base_piece = htypes.label.view("Sample label")
+    piece = htypes.rename_command.view(
+        base=mosaic.put(base_piece),
+        ui_command=mosaic.put(sample_ui_command),
+        )
+    base_state = htypes.label.state()
+    state = htypes.rename_command.state(
+        base=mosaic.put(base_state),
+        )
+    app = QtWidgets.QApplication()
+    try:
+        view = rename_command.RenameCommandContextView.from_piece(piece, ctx)
+        widget = view.construct_widget(state, ctx)
+        assert view.piece
+        state = view.widget_state(widget)
+        assert state
+    finally:
+        app.shutdown()
+
+
+def test_rename_command():
+    sample_ui_command = _make_sample_ui_command()
+    ctx = Context()
+    model = htypes.rename_command_tests.sample_model()
+    model_state = htypes.rename_command_tests.sample_model_state()
+    piece = htypes.model_commands.model_commands(
+        model=mosaic.put(model),
+        model_state=mosaic.put(model_state)
+        )
+    current_item = htypes.model_commands.item(
+        command=mosaic.put(sample_ui_command),
+        name="<unused>",
+        impl="<unused>",
+        )
+    navigator = Mock()
+    navigator.view.piece = htypes.label.view("Sample view")
+    navigator.state = htypes.label.state()
+    rename_command.rename_command(piece, current_item, navigator, ctx)
+    _view_creg_mock.animate.assert_called_once()
+    navigator.hook.replace_view.assert_called_once()
