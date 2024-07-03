66b5e0ef3201eef742e81398c1719451149eb135
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-07-03 12:03:02 +0300

    Import job: Remove actor wrapper

--- hyperapp/rc/import_job.dyn.py
+++ hyperapp/rc/import_job.dyn.py
@@ -1,12 +1,8 @@
 from . import htypes
-from .services import (
-    rc_job_creg,
-    )
 
 
 class ImportJob:
 
-    @rc_job_creg.actor(htypes.import_job.job)
     @classmethod
     def from_piece(cls, piece):
         return cls()
