aa7fabecac30ab476bb25245a16cd75434f59613
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-07-03 12:01:15 +0300

    Add rc_job_creg

--- hyperapp/rc/rc_job_driver.dyn.py
+++ hyperapp/rc/rc_job_driver.dyn.py
@@ -1,7 +1,12 @@
 import logging
 
+from .services import (
+    rc_job_creg,
+    )
+
 log = logging.getLogger(__name__)
 
 
 def run_rc_job(job_piece):
-    log.info("Run job: %r", job_piece)
+    job = rc_job_creg.animate(job_piece)
+    log.info("Run job: %r", job)
