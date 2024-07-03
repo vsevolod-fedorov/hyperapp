de36fbcc758c14eed81aa0cd500147cd06da0707
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-07-03 12:01:15 +0300

    Add rc_job_creg

--- hyperapp/rc/import_job.dyn.py
+++ hyperapp/rc/import_job.dyn.py
@@ -1,7 +1,15 @@
 from . import htypes
+from .services import (
+    rc_job_creg,
+    )
 
 
 class ImportJob:
+
+    @rc_job_creg.actor(htypes.import_job.job)
+    @classmethod
+    def from_piece(cls, piece):
+        return cls()
 
     def __init__(self):
         pass
