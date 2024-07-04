92352a0d94b5574dd0b2c5e34b738be64eace942
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-07-04 12:54:26 +0300

    Pass deps to import job

---
+++ hyperapp/rc/import_dep.dyn.py
@@ -0,0 +1,28 @@
+from . import htypes
+from .services import (
+    mosaic,
+    web,
+    )
+
+
+class ImportDep:
+
+    @classmethod
+    def from_type_src(cls, type_src):
+        import_name = ['htypes', type_src.module_name, type_src.name]
+        return cls(import_name, type_src.type_piece)
+
+    @classmethod
+    def from_piece(cls, piece):
+        return cls(piece.import_name, web.summon(piece.resource))
+
+    def __init__(self, import_name, resource):
+        self._import_name = import_name
+        self._resource = resource
+
+    @property
+    def piece(self):
+        return htypes.import_dep.import_dep(
+            import_name=tuple(self._import_name),
+            resource=mosaic.put(self._resource),
+            )
