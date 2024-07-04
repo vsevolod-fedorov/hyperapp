493c5904dc0097ca2b31bcd5a429f87d9065a232
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-07-04 12:57:16 +0300

    Add, call job.run method

--- hyperapp/rc/rc_job_driver.dyn.py
+++ hyperapp/rc/rc_job_driver.dyn.py
@@ -10,3 +10,5 @@ log = logging.getLogger(__name__)
 def run_rc_job(job_piece):
     job = rc_job_creg.animate(job_piece)
     log.info("Run job: %r", job)
+    job.run()
+    log.info("Job completed: %r", job)
