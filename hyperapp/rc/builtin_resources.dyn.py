481f0fb70d00232748795ff555739645c21a408a
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-07-08 11:25:30 +0300

    Add mark service to builtin services

--- hyperapp/rc/builtin_resources.dyn.py
+++ hyperapp/rc/builtin_resources.dyn.py
@@ -1,6 +1,8 @@
 from . import htypes
 from .services import (
     builtin_services,
+    mark,
+    pyobj_creg,
     )
 from .code.import_resource import ImportResource
 
@@ -9,3 +11,5 @@ def enum_builtin_resources():
     for service_name in builtin_services:
         piece = htypes.builtin.builtin_service(service_name)
         yield ImportResource(['services', service_name], piece)
+    mark_service_piece = pyobj_creg.actor_to_piece(mark)
+    yield ImportResource(['services', 'mark'], mark_service_piece)
