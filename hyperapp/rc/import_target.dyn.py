58bfa641b9773e714a9bf468d0e64c95083d6890
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-07-15 11:29:44 +0300

    Resource: Add used types to resources; Import alias: add deps

--- hyperapp/rc/import_target.dyn.py
+++ hyperapp/rc/import_target.dyn.py
@@ -46,12 +46,13 @@ class ImportTargetAlias:
     def name_for_src(python_module_src):
         return f'import/{python_module_src.name}'
 
-    def __init__(self, python_module_src, type_src_list, custom_resource_registry):
+    def __init__(self, python_module_src, custom_resource_registry, type_src_list):
         self._python_module_src = python_module_src
         self._type_src_list = type_src_list
         self._custom_resource_registry = custom_resource_registry
         self._completed = False
-        self._deps = set()
+        self._got_requirements = False
+        self._req_to_target = {}
         self._resources = []
 
     def __repr__(self):
@@ -71,32 +72,38 @@ class ImportTargetAlias:
 
     @property
     def deps(self):
-        return self._deps
+        return set(self._req_to_target.values())
 
     def update_status(self):
-        pass
+        if self._got_requirements:
+            self._completed = all(target.completed for target in self.deps)
 
-    def set_completed(self, req_to_target):
-        self._deps = {*req_to_target.values()}
-        for req, target in req_to_target.items():
-            resource = req.make_resource(target)
-            if resource is None:
-                continue  # TODO: Remove when all make_resource methods are implemented.
-            self._resources.append(resource)
-        self._completed = True
+    def set_requirements(self, req_to_target):
+        self._req_to_target = req_to_target
+        self._got_requirements = True
+        self.update_status()
 
     def create_resource_target(self, all_imports_known_tgt):
-        return CompiledPythonModuleResourceTarget(self._python_module_src, self._custom_resource_registry, all_imports_known_tgt, self)
+        return CompiledPythonModuleResourceTarget(
+            self._python_module_src, self._custom_resource_registry, self._type_src_list, all_imports_known_tgt, self)
 
     def recorded_python_module(self):
+        assert self._completed
         type_resources = [
             ImportResource.from_type_src(src)
             for src in self._type_src_list
             ]
-        all_resources = [*enum_builtin_resources(), *type_resources, *self._resources]
+        all_resources = [*enum_builtin_resources(), *type_resources, *self._enum_resources()]
         import_list = flatten(d.import_records for d in all_resources)
         recorder_piece, module_piece = self._python_module_src.recorded_python_module(import_list)
         return (self._python_module_src.name, recorder_piece, module_piece)
+
+    def _enum_resources(self):
+        for req, target in self._req_to_target.items():
+            resource = req.make_resource(target)
+            if resource is None:
+                continue  # TODO: Remove when all make_resource methods are implemented.
+            yield resource
 
 
 class ImportTarget:
@@ -143,8 +150,8 @@ class ImportTarget:
         self._completed = True
         result.update_targets(self, target_set)
 
-    def set_alias_completed(self, req_to_target):
-        self._alias.set_completed(req_to_target)
+    def set_alias_requirements(self, req_to_target):
+        self._alias.set_requirements(req_to_target)
 
     def create_next_target(self, req_to_target):
         return ImportTarget(self._python_module_src, self._type_src_list, self._alias, self._idx + 1, req_to_target)
@@ -163,7 +170,7 @@ def create_import_targets(root_dir, target_set, python_module_src_list, type_src
     all_imports_known_tgt = AllImportsKnownTarget()
     for src in python_module_src_list:
         if root_dir.joinpath(src.resource_path).read_text().startswith(AUTO_GEN_LINE):
-            alias_tgt = ImportTargetAlias(src, type_src_list, custom_resource_registry)
+            alias_tgt = ImportTargetAlias(src, custom_resource_registry, type_src_list)
             import_tgt = ImportTarget(src, type_src_list, alias_tgt)
             all_imports_known_tgt.add_import_target(import_tgt)
             target_set.add(import_tgt)
--- hyperapp/rc/python_module_resource_target.dyn.py
+++ hyperapp/rc/python_module_resource_target.dyn.py
@@ -1,6 +1,7 @@
 import logging
 import subprocess
 from dataclasses import dataclass
+from functools import cached_property
 
 from hyperapp.common.util import flatten
 
@@ -10,6 +11,7 @@ from .services import (
     resource_module_factory,
     )
 from .code.rc_requirement import Requirement
+from .code.import_resource import ImportResource
 
 rc_log = logging.getLogger('rc')
 
@@ -31,7 +33,7 @@ class PythonModuleReq(Requirement):
         return target_factory.python_module_resource_by_code_name(self.code_name)
 
     def make_resource(self, target):
-        return None
+        return ImportResource(['code', self.code_name], target.python_module_piece)
 
 
 class PythonModuleResourceTarget:
@@ -76,12 +78,18 @@ class ManualPythonModuleResourceTarget(PythonModuleResourceTarget):
 
 class CompiledPythonModuleResourceTarget(PythonModuleResourceTarget):
 
-    def __init__(self, python_module_src, custom_resource_registry, all_imports_known_tgt, import_alias_tgt):
+    def __init__(self, python_module_src, custom_resource_registry, type_src_list, all_imports_known_tgt, import_alias_tgt):
         super().__init__(python_module_src, custom_resource_registry)
+        self._type_src_list = type_src_list
+        self._name_to_src = {
+            (rec.module_name, rec.name): rec
+            for rec in type_src_list
+            }
         self._all_imports_known_tgt = all_imports_known_tgt
         self._import_alias_tgt = import_alias_tgt
         self._completed = False
         self._req_to_target = {}
+        self._type_resources = []
         self._tests = set()
 
     def __repr__(self):
@@ -93,8 +101,7 @@ class CompiledPythonModuleResourceTarget(PythonModuleResourceTarget):
 
     @property
     def completed(self):
-        return False
-        # return self._completed
+        return self._completed
 
     @property
     def deps(self):
@@ -104,8 +111,8 @@ class CompiledPythonModuleResourceTarget(PythonModuleResourceTarget):
         if self._completed:
             return
         if all(target.completed for target in self.deps):
-            self._construct_res_module()
             self._completed = True
+            self._construct_res_module()
 
     def add_import_requirements(self, req_to_target):
         self._req_to_target = req_to_target
@@ -114,17 +121,30 @@ class CompiledPythonModuleResourceTarget(PythonModuleResourceTarget):
         self._tests.add(test_target)
 
     def add_used_imports(self, import_list):
-        assert not import_list, import_list
+        for name in import_list:
+            if len(name) < 3:
+                continue
+            if name[0] != 'htypes':
+                continue
+            type_src = self._name_to_src[name[1:]]
+            resource = ImportResource.from_type_src(type_src)
+            self._type_resources.append(resource)
+
+    @cached_property
+    def python_module_piece(self):
+        assert self._completed
+        resources = list(filter(None, self._enum_resources()))  # TODO: Remove filter when all make_resource methods are implemented.
+        import_list = flatten(d.import_records for d in resources)
+        return self._src.python_module(import_list)
 
     def _enum_resources(self):
+        yield from self._type_resources
         for req, target in self._req_to_target.items():
             yield req.make_resource(target)
 
     def _construct_res_module(self):
         rc_log.info("Construct: %s", self.name)
-        resources = list(filter(None, self._enum_resources()))  # TODO: Remove filter when all make_resource methods are implemented.
-        import_list = flatten(d.import_records for d in resources)
-        python_module = self._src.python_module(import_list)
+        python_module = self.python_module_piece
         resource_module = resource_module_factory(self._custom_resource_registry, self._src.name)
         resource_module[f'{self._src.stem}.module'] = python_module
         text = resource_module.as_text
