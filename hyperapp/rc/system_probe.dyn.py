445fcbb2059aeb29c0204d5c7292e03eca2730b8
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-07-24 11:19:15 +0300

    System probe: resolve and run templates

--- hyperapp/rc/system_probe.dyn.py
+++ hyperapp/rc/system_probe.dyn.py
@@ -2,16 +2,35 @@ class ServiceProbe:
 
     def __init__(self, fn, params):
         self._fn = fn
-        self._params = params
+        self.params = params
 
     def __repr__(self):
-        return f"<ServiceProbe {self._fn} {self._params}>"
+        return f"<ServiceProbe {self._fn} {self.params}>"
+
+    def run(self, params):
+        return self._fn(*params)
 
 
 class SystemProbe:
 
     def __init__(self, service_templates):
         self._name_to_template = service_templates
+        self._name_to_service = {}
 
     def run(self, root_name):
-        assert 0, (root_name, self._name_to_template)
+        template = self._name_to_template[root_name]
+        self._run(template)
+
+    def _run(self, template):
+        params = [self._resolve_service(name) for name in template.params]
+        return template.run(params)
+
+    def _resolve_service(self, name):
+        try:
+            return self._name_to_service[name]
+        except KeyError:
+            pass
+        template = self._name_to_template[name]
+        service = self._run(template)
+        self._name_to_service[name] = service
+        return service
--- hyperapp/rc/test/test_system.dyn.py
+++ hyperapp/rc/test/test_system.dyn.py
@@ -1,13 +1,18 @@
+import logging
+
 from .services import (
     mark,
     )
 from .tested.code import sample_service as sample_service_module
 
+log = logging.getLogger(__name__)
+
 
 @mark.fixture
 def fixture_1():
-    pass
+    log.info("Fixture 1")
+    return "fixture 1 value"
 
 
 def test_sample_service(fixture_1):
-    pass
+    log.info("test_sample_service: fixture_1=%r", fixture_1)
