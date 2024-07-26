68476f1bf63f11a4de394e06ed94956c098445cb
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-07-26 11:44:58 +0300

    Add config-item-ready target

--- hyperapp/rc/config_item_target.dyn.py
+++ hyperapp/rc/config_item_target.dyn.py
@@ -1,16 +1,16 @@
 from .code.rc_target import Target
 
 
-class ConfigItemCompleteTarget(Target):
+class ConfigItemReadyTarget(Target):
 
     @staticmethod
     def target_name(service_name, key):
-        return f'item-complete/{service_name}/{key}'
+        return f'item-ready/{service_name}/{key}'
 
     def __init__(self, service_name, key):
         self._service_name = service_name
         self._key = key
-        self._completed = True
+        self._completed = False
 
     @property
     def name(self):
@@ -19,3 +19,28 @@ class ConfigItemCompleteTarget(Target):
     @property
     def completed(self):
         return self._completed
+
+
+class ConfigItemCompleteTarget(Target):
+
+    @staticmethod
+    def target_name(service_name, key):
+        return f'item-complete/{service_name}/{key}'
+
+    def __init__(self, service_name, key, ready_tgt):
+        self._service_name = service_name
+        self._key = key
+        self._ready_tgt = ready_tgt
+        self._completed = False
+
+    @property
+    def name(self):
+        return self.target_name(self._service_name, self._key)
+
+    @property
+    def completed(self):
+        return self._completed
+
+    @property
+    def deps(self):
+        return {self._ready_tgt}
