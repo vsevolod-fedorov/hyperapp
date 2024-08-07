e90b25538793170de5ba33e8de3916ac3946ebe4
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-08-07 12:16:38 +0300

    Update subprocess transport module

--- hyperapp/subprocess/subprocess_transport.dyn.py
+++ hyperapp/subprocess/subprocess_transport.dyn.py
@@ -10,12 +10,8 @@ from hyperapp.common.htypes.packet_coders import packet_coders
 from .services import (
     bundler,
     failed,
-    mark,
     mosaic,
     on_stop,
-    parcel_registry,
-    route_table,
-    transport,
     stop_signal,
     unbundler,
     )
@@ -35,7 +31,8 @@ def encode_packet(data):
 
 class ConnectionRec:
 
-    def __init__(self, connection, name, seen_refs, on_eof=None, on_reset=None):
+    def __init__(self, subprocess_transport, connection, name, seen_refs, on_eof=None, on_reset=None):
+        self._subprocess_transport = subprocess_transport
         self._connection = connection
         self.name = name
         self.seen_refs = seen_refs
@@ -48,9 +45,8 @@ class ConnectionRec:
 
     def close(self):
         log.info("Subprocess transport: close connection %r", self.name)
-        del _server_connections[self._connection]
         self._connection.close()
-        _signal_connection_in.send(None)  # Wake up server main.
+        self._subprocess_transport.server_connection_closed(self._connection)
 
     def read(self):
         self._buffer += self._connection.recv()
@@ -97,90 +93,90 @@ class SubprocessRoute:
         log.debug("Subprocess %s: parcel is sent: %s", self._name, parcel_ref)
 
 
-@mark.service
-def add_subprocess_server_connection():
+class SubprocessTransport:
 
-    def _add_subprocess_server_connection(name, connection, seen_refs, on_eof=None, on_reset=None):
-        rec = ConnectionRec(connection, name, seen_refs, on_eof, on_reset)
-        _server_connections[connection] = rec
-        _signal_connection_in.send(None)  # Wake up server main.
+    def __init__(self, parcel_registry, transport, route_table):
+        self._parcel_registry = parcel_registry
+        self._transport = transport
+        self._route_table = route_table
+        self._server_connections = {}  # connection -> ConnectionRec
+        out_c, in_c = multiprocessing.Pipe()
+        self._signal_connection_in = in_c
+        self._signal_connection_out = out_c
+        self._server_thread = threading.Thread(target=self._server_thread_main, name='SubpServer')
+        _server_thread.start()
+        on_stop.append(self._stop)
+
+    def add_server_connection(self, name, connection, seen_refs, on_eof=None, on_reset=None):
+        rec = ConnectionRec(self, connection, name, seen_refs, on_eof, on_reset)
+        self._server_connections[connection] = rec
+        self._signal_connection_in.send(None)  # Wake up server main.
         return rec
 
-    return _add_subprocess_server_connection
+    def server_connection_closed(self, connection):
+        del self._server_connections[connection]
+        self._signal_connection_in.send(None)  # Wake up server main.
 
+    def _stop(self):
+        my_name = "Subprocess transport server thread"
+        log.info("Stop %s", my_name)
+        self._signal_connection_in.send(None)  # Wake up server main.
+        self._server_thread.join()
+        log.info("%s is stopped", my_name)
 
-def _process_parcel(connection, connection_rec, parcel):
-    sender_ref = mosaic.put(parcel.sender.piece)
-    route = SubprocessRoute(connection_rec.name, connection_rec.seen_refs, connection)
-    route_table.add_route(sender_ref, route)
-    transport.send_parcel(parcel)
-
-
-def _process_bundle(connection, connection_rec, data):
-    log.debug("Subprocess transport: received bundle from %r. Bundle size: %.2f KB", connection_rec.name, len(data)/1024)
-    parcel_bundle = packet_coders.decode('cdr', data, bundle_t)
-    ref_set = unbundler.register_bundle(parcel_bundle)
-    connection_rec.seen_refs |= ref_set
-    parcel_piece_ref = parcel_bundle.roots[0]
-    parcel = parcel_registry.invite(parcel_piece_ref)
-    _process_parcel(connection, connection_rec, parcel)
-
-
-def _process_ready_connection(connection):
-    rec = _server_connections[connection]
-    try:
-        data = rec.read()
-    except EOFError as x:
-        log.info("Subprocess connection %s was closed by the other side: %s", rec.name, x)
-        rec.on_eof()
-    except (OSError, ConnectionResetError) as x:
-        log.warning("Subprocess connection %s was reset by the other side: %s", rec.name, x)
-        rec.on_reset()
-    else:
-        if data is None:
-            return  # Partial packet is received.
+    def _server_thread_main(self):
+        my_name = "Subprocess transport server thread"
+        log.info("%s is started", my_name)
         try:
-            _process_bundle(connection, rec, data)
-            return  # Keep connection.
+            while not stop_signal.is_set():
+                all_connections = [self._signal_connection_out, *self._server_connections]
+                ready_connections = multiprocessing.connection.wait(all_connections)
+                for connection in ready_connections:
+                    if connection is not self._signal_connection_out:
+                        # If is just removed, both signal connection and removed connection may be ready.
+                        if connection in self._server_connections:
+                            self._process_ready_connection(connection)
+                    else:
+                        connection.recv()  # Clear signal connection
         except Exception as x:
-            my_name = f"Processing bundle from {rec.name}"
             log.exception("%s is failed:", my_name)
             failed(f"{my_name} is failed: {x}", x)
-    del _server_connections[connection]
-    _signal_connection_in.send(None)
+        log.info("%s is finished", my_name)
 
+    def _process_ready_connection(self, connection):
+        rec = self._server_connections[connection]
+        try:
+            data = rec.read()
+        except EOFError as x:
+            log.info("Subprocess connection %s was closed by the other side: %s", rec.name, x)
+            rec.on_eof()
+        except (OSError, ConnectionResetError) as x:
+            log.warning("Subprocess connection %s was reset by the other side: %s", rec.name, x)
+            rec.on_reset()
+        else:
+            if data is None:
+                return  # Partial packet is received.
+            try:
+                self._process_bundle(connection, rec, data)
+                return  # Keep connection.
+            except Exception as x:
+                my_name = f"Processing bundle from {rec.name}"
+                log.exception("%s is failed:", my_name)
+                failed(f"{my_name} is failed: {x}", x)
+        del self._server_connections[connection]
+        self._signal_connection_in.send(None)
 
-def _server_thread_main():
-    my_name = "Subprocess transport server thread"
-    log.info("%s is started", my_name)
-    try:
-        while not stop_signal.is_set():
-            all_connections = [_signal_connection_out, *_server_connections]
-            ready_connections = multiprocessing.connection.wait(all_connections)
-            for connection in ready_connections:
-                if connection is not _signal_connection_out:
-                    # If is just removed, both signal connection and removed connection may be ready.
-                    if connection in _server_connections:
-                        _process_ready_connection(connection)
-                else:
-                    connection.recv()  # Clear signal connection
-    except Exception as x:
-        log.exception("%s is failed:", my_name)
-        failed(f"{my_name} is failed: {x}", x)
-    log.info("%s is finished", my_name)
+    def _process_bundle(self, connection, connection_rec, data):
+        log.debug("Subprocess transport: received bundle from %r. Bundle size: %.2f KB", connection_rec.name, len(data)/1024)
+        parcel_bundle = packet_coders.decode('cdr', data, bundle_t)
+        ref_set = unbundler.register_bundle(parcel_bundle)
+        connection_rec.seen_refs |= ref_set
+        parcel_piece_ref = parcel_bundle.roots[0]
+        parcel = self._parcel_registry.invite(parcel_piece_ref)
+        self._process_parcel(connection, connection_rec, parcel)
 
-
-def _stop():
-    my_name = "Subprocess transport server thread"
-    log.info("Stop %s", my_name)
-    _signal_connection_in.send(None)  # Wake up server main.
-    _server_thread.join()
-    log.info("%s is stopped", my_name)
-
-
-_server_connections = {}  # connection -> ConnectionRec
-_signal_connection_out, _signal_connection_in = multiprocessing.Pipe()
-_server_thread = threading.Thread(target=_server_thread_main, name='SubpServer')
-
-_server_thread.start()
-on_stop.append(_stop)
+    def _process_parcel(self, connection, connection_rec, parcel):
+        sender_ref = mosaic.put(parcel.sender.piece)
+        route = SubprocessRoute(connection_rec.name, connection_rec.seen_refs, connection)
+        self._route_table.add_route(sender_ref, route)
+        self._transport.send_parcel(parcel)
