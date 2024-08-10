0254f0d64cffc0b62a03bc1dc39443140c1b23bd
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-08-10 11:06:15 +0300

    System: Add finalizer-by-generator service template

--- hyperapp/rc/system.dyn.py
+++ hyperapp/rc/system.dyn.py
@@ -1,3 +1,4 @@
+import inspect
 from collections import defaultdict
 from functools import partial
 
@@ -15,7 +16,27 @@ class UnknownServiceError(Exception):
         self.service_name = service_name
 
 
-class ServiceTemplate:
+class ServiceTemplateBase:
+
+    def __init__(self, name, fn, service_params, want_config):
+        self.service_name = name
+        self._fn = fn
+        self._service_params = service_params
+        self._want_config = want_config
+
+    def _resolve_service_args(self, system):
+        if self._want_config:
+            config_args = [system.resolve_config(self.service_name)]
+        else:
+            config_args = []
+        service_args = [
+            system.resolve_service(name)
+            for name in self._service_params
+            ]
+        return [*config_args, *service_args]
+
+
+class ServiceTemplate(ServiceTemplateBase):
 
     @classmethod
     def from_piece(cls, piece):
@@ -28,11 +49,8 @@ class ServiceTemplate:
             )
 
     def __init__(self, name, fn, free_params, service_params, want_config):
-        self.service_name = name
-        self._fn = fn
+        super().__init__(name, fn, service_params, want_config)
         self._free_params = free_params
-        self._service_params = service_params
-        self._want_config = want_config
 
     def __repr__(self):
         return f"<ServiceTemplate {self.service_name}: {self._fn} {self._free_params} {self._service_params} {self._want_config}>"
@@ -48,18 +66,55 @@ class ServiceTemplate:
             )
 
     def resolve(self, system, service_name):
-        if self._want_config:
-            config_args = [system.resolve_config(self.service_name)]
-        else:
-            config_args = []
-        service_args = [
-            system.resolve_service(name)
-            for name in self._service_params
-            ]
+        service_args = self._resolve_service_args(system)
         if self._free_params:
-            return partial(self._fn, *config_args, *service_args)
+            return partial(self._fn, *service_args)
         else:
-            return self._fn(*config_args, *service_args)
+            return self._fn(*service_args)
+
+
+class FinalizerGenServiceTemplate(ServiceTemplateBase):
+
+    @classmethod
+    def from_piece(cls, piece):
+        return cls(
+            name=piece.name,
+            fn=pyobj_creg.invite(piece.function),
+            service_params=piece.service_params,
+            want_config=piece.want_config,
+            )
+
+    def __init__(self, name, fn, service_params, want_config):
+        super().__init__(name, fn, service_params, want_config)
+        if not inspect.isgeneratorfunction(fn):
+            raise RuntimeError(f"Function {fn!r} expected to be a generator function")
+
+    def __repr__(self):
+        return f"<FinalizerGenServiceTemplate {self.service_name}: {self._fn} {self._service_params} {self._want_config}>"
+
+    @property
+    def piece(self):
+        return htypes.system.finalizer_gen_service_template(
+            name=self.service_name,
+            function=pyobj_creg.actor_to_ref(self._fn),
+            service_params=tuple(self._service_params),
+            want_config=self._want_config,
+            )
+
+    def resolve(self, system, service_name):
+        service_args = self._resolve_service_args(system)
+        gen = self._fn(*service_args)
+        service = next(gen)
+        system.add_finalizer(partial(self._finalize, gen))
+        return service
+
+    def _finalize(self, gen):
+        try:
+            next(gen)
+        except StopIteration:
+            pass
+        else:
+            raise RuntimeError(f"Generator function {self._fn!r} should have only one 'yield' statement")
 
 
 class ActorTemplate:
@@ -99,6 +154,18 @@ class ServiceTemplateCfg:
         self.value = template
 
 
+class FinalizerGenServiceTemplateCfg:
+
+    @classmethod
+    def from_piece(cls, piece, service_name):
+        template = FinalizerGenServiceTemplate.from_piece(piece)
+        return cls(template)
+
+    def __init__(self, template):
+        self.key = template.service_name
+        self.value = template
+
+
 class ActorTemplateCfg:
 
     @classmethod
@@ -117,6 +184,7 @@ class System:
         self._configs = defaultdict(dict)
         self._name_to_template = self._configs['system']
         self._name_to_service = {}
+        self._finalizers = []
 
     def add_core_service(self, name, service):
         self._name_to_service[name] = service
@@ -126,7 +194,9 @@ class System:
 
     def run(self, root_name, *args, **kw):
         service = self.resolve_service(root_name)
-        return service(*args, **kw)
+        result = service(*args, **kw)
+        assert not inspect.iscoroutine(result), f"todo: {root_name}/{result}"
+        self._run_finalizers()
 
     def resolve_config(self, service_name):
         config = {}
@@ -147,6 +217,13 @@ class System:
         self._name_to_service[name] = service
         return service
 
+    def add_finalizer(self, finalizer):
+        self._finalizers.append(finalizer)
+
+    def _run_finalizers(self):
+        for fn in reversed(self._finalizers):
+            fn()
+
     def _raise_missing_service(self, service_name):
         raise UnknownServiceError(service_name)
 
@@ -154,6 +231,7 @@ class System:
 def load_config(system, config_piece):
     cfg_item_creg_config = {
         htypes.system.service_template: ServiceTemplateCfg.from_piece,
+        htypes.system.finalizer_gen_service_template: FinalizerGenServiceTemplateCfg.from_piece,
         htypes.system.actor_template: ActorTemplateCfg.from_piece,
         }
     cfg_item_creg = code_registry_ctr2('cfg-item', cfg_item_creg_config)
