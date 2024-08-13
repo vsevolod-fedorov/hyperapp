5c8c938ca41e7125514e3467dde4d56a2ae30d86
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-08-13 11:17:22 +0300

    Service template resource: Add support for different template types

--- hyperapp/rc/service_resource.dyn.py
+++ hyperapp/rc/service_resource.dyn.py
@@ -1,5 +1,3 @@
-from dataclasses import dataclass
-
 from . import htypes
 from .services import (
     mosaic,
@@ -7,51 +5,58 @@ from .services import (
     )
 from .code.rc_requirement import Requirement
 from .code.rc_resource import Resource
-from .code.system import ServiceTemplate
 
 
-@dataclass(frozen=True, unsafe_hash=True)
 class ServiceReq(Requirement):
 
-    service_name: str
-
     @classmethod
-    def from_piece(cls, piece):
-        return cls(piece.service_name)
+    def from_piece(cls, piece, cfg_item_creg):
+        return cls(piece.service_name, cfg_item_creg)
+
+    def __init__(self, service_name, cfg_item_creg):
+        self._service_name = service_name
+        self._cfg_item_creg = cfg_item_creg
+
+    def __eq__(self, rhs):
+        return type(rhs) == ServiceReq and rhs._service_name == self._service_name
+
+    def __hash__(self):
+        return hash(('service_req', self._service_name))
 
     @property
     def piece(self):
-        return htypes.service_resource.service_req(self.service_name)
+        return htypes.service_resource.service_req(self._service_name)
 
     def get_target(self, target_factory):
-        return target_factory.config_item_complete('system', self.service_name)
+        return target_factory.config_item_complete('system', self._service_name)
 
     def make_resource(self, target):
         resource_tgt = target.provider_resource_tgt
         template_piece = resource_tgt.get_resource(target.constructor)
+        cfg_item = self._cfg_item_creg.animate(template_piece, self._service_name)
         return ServiceTemplateResource(
-            service_name=self.service_name,
-            template=ServiceTemplate.from_piece(template_piece),
+            service_name=self._service_name,
+            cfg_item=cfg_item,
             )
 
 
 class ServiceTemplateResource(Resource):
 
     @classmethod
-    def from_piece(cls, piece):
-        template = ServiceTemplate.from_piece(piece.template)
-        return cls(piece.service_name, template)
+    def from_piece(cls, piece, cfg_item_creg):
+        cfg_item = cfg_item_creg.invite(piece.template, piece.service_name)
+        return cls(piece.service_name, cfg_item)
 
-    def __init__(self, service_name, template):
+    def __init__(self, service_name, cfg_item):
         self._service_name = service_name
-        self._template = template  # ServiceTemplate
+        self._cfg_item = cfg_item
 
     @property
     def piece(self):
         return htypes.service_resource.service_template_resource(
             service_name=self._service_name,
-            template=self._template.piece,
+            template=mosaic.put(self._cfg_item.piece),
             )
 
     def configure_system(self, system):
-        system.update_config('system', {self._service_name: self._template})
+        system.update_config('system', {self._cfg_item.key: self._cfg_item.value})
