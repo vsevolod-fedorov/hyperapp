fda1281c2550f9cad78a567e530e32db63177ea9
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-08-14 13:51:57 +0300

    Actor requirement: Implement make_resource method

--- hyperapp/rc/actor_resource.dyn.py
+++ hyperapp/rc/actor_resource.dyn.py
@@ -1,43 +1,55 @@
-from dataclasses import dataclass
-
 from hyperapp.common.htypes import Type
 
 from . import htypes
 from .services import (
-    mosaic,
     pyobj_creg,
-    web,
     )
 from .code.rc_requirement import Requirement
-# from .code.rc_resource import Resource
+from .code.config_item_resource import ConfigItemResource
 
 
-@dataclass(frozen=True, unsafe_hash=True)
 class ActorReq(Requirement):
 
-    service_name: str
-    t: Type
-
     @classmethod
-    def from_piece(cls, piece):
+    def from_piece(cls, piece, cfg_item_creg):
         return cls(
+            cfg_item_creg=cfg_item_creg,
             service_name=piece.service_name,
             t=pyobj_creg.invite(piece.t),
             )
 
+    def __init__(self, cfg_item_creg, service_name, t):
+        self._cfg_item_creg = cfg_item_creg
+        self._service_name = service_name
+        self._t = t
+
+    def __eq__(self, rhs):
+        return (type(rhs) == ActorReq
+                and rhs._service_name == self._service_name
+                and rhs._t == self._t)
+
+    def __hash__(self):
+        return hash(('action_req', self._service_name, self._t))
+
     @property
     def piece(self):
         return htypes.actor_resource.actor_req(
-            service_name=self.service_name,
-            t=pyobj_creg.actor_to_ref(self.t),
+            service_name=self._service_name,
+            t=pyobj_creg.actor_to_ref(self._t),
             )
 
     def get_target(self, target_factory):
-        return target_factory.config_item_complete(self.service_name, self._type_name)
+        return target_factory.config_item_complete(self._service_name, self._type_name)
 
     def make_resource(self, target):
-        assert 0, f"todo: {self}: {target}"
+        resource_tgt = target.provider_resource_tgt
+        template_piece = resource_tgt.get_resource(target.constructor)
+        cfg_item = self._cfg_item_creg.animate(template_piece, self._service_name)
+        return ConfigItemResource(
+            service_name=self._service_name,
+            cfg_item=cfg_item,
+            )
 
     @property
     def _type_name(self):
-        return f'{self.t.module_name}_{self.t.name}'
+        return f'{self._t.module_name}_{self._t.name}'
