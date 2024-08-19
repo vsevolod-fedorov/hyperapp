5989d841ebcb4d2a7ed989decb7fdb5d06cd68cc
Vsevolod Fedorov <vfedorov@networkoptix.com>
2024-08-19 13:30:49 +0300

    Update partial_ref test

--- hyperapp/resource/partial_ref.tests.dyn.py
+++ hyperapp/resource/partial_ref.tests.dyn.py
@@ -1,14 +1,14 @@
 from .services import (
     pyobj_creg,
     )
-from .tested.services import partial_ref
+from .tested.code import partial_ref as partial_ref_module
 
 
 def _sample_fn(sample_param):
     return f'passed: {sample_param}'
 
 
-def test_partial_ref():
+def test_partial_ref(partial_ref):
     fn_ref = partial_ref(_sample_fn, sample_param='sample param')
     fn = pyobj_creg.invite(fn_ref)
     assert fn() == 'passed: sample param'
