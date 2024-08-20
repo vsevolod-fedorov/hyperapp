9ccb100a1d336f8c110f9426ed0ad97497a6a675
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-08-20 12:35:38 +0300

    Update feed constructor

--- hyperapp/rc/rc_constructor.dyn.py
+++ hyperapp/rc/rc_constructor.dyn.py
@@ -5,13 +5,13 @@ class Constructor:
     def is_fixture(self):
         return False
 
-    def update_resource_targets(self, resource_tgt, target_set):
-        pass
-
     def update_fixtures_targets(self, import_alias_tgt, target_set):
         pass
 
     def update_targets(self, target_set):
+        pass
+
+    def update_resource_targets(self, resource_tgt, target_set):
         pass
 
     def make_component(self, python_module, name_to_res=None):
@@ -22,3 +22,16 @@ class Constructor:
 
     def make_resource(self, module_name, python_module):
         pass
+
+
+class ModuleCtr(Constructor):
+
+    def __init__(self, module_name):
+        self._module_name = module_name
+
+    def update_targets(self, target_set):
+        resource_tgt = target_set.factory.python_module_resource_by_module_name(self._module_name)
+        self.update_resource_targets(resource_tgt, target_set)
+
+    def update_resource_targets(self, resource_tgt, target_set):
+        raise NotImplementedError()
