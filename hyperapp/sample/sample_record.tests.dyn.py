1e72adc986a27b20fd0792e550456c91504c3273
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-08-25 11:15:06 +0300

    Add test for sample record open command

--- hyperapp/sample/sample_record.tests.dyn.py
+++ hyperapp/sample/sample_record.tests.dyn.py
@@ -6,3 +6,8 @@ def test_sample_record():
     piece = htypes.sample_record.sample_record()
     value = sample_record.sample_record(piece)
     assert value
+
+
+def test_open():
+    piece = sample_record.open_sample_fn_record()
+    assert piece
