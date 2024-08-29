bac0a0323e1d73f67d1a56672bbf924f57f28796
Vsevolod Fedorov <vfedorov@networkoptix.com>
2024-08-29 10:44:56 +0300

    Update str adapter

--- hyperapp/ui/str_adapter.dyn.py
+++ hyperapp/ui/str_adapter.dyn.py
@@ -1,8 +1,10 @@
+from .code.mark import mark
 
 
 class StaticStrAdapter:
 
     @classmethod
+    @mark.actor.ui_adapter_creg
     def from_piece(cls, piece, model, ctx):
         return cls(model)
 
