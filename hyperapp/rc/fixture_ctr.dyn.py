920683599bf6c36eb5a4b9b30c421852e5410392
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-09-01 12:01:51 +0300

    RC: Split fixture and service modules

---
+++ hyperapp/rc/fixture_ctr.dyn.py
@@ -0,0 +1,87 @@
+from . import htypes
+from .services import (
+    mosaic,
+    )
+from .code.rc_constructor import ModuleCtr
+from .code.fixture_probe_resource import ConfigItemFixtureResource, FixtureProbeResource
+
+
+class FixtureCtr(ModuleCtr):
+
+    @classmethod
+    def from_piece(cls, piece):
+        return cls(piece.module_name, piece.attr_name, piece.name, piece.params)
+
+    def __init__(self, module_name, attr_name, name, params):
+        super().__init__(module_name)
+        self._attr_name = attr_name
+        self._name = name
+        self._params = params
+
+    @property
+    def piece(self):
+        return htypes.fixture_resource.fixture_ctr(
+            module_name=self._module_name,
+            attr_name=self._attr_name,
+            name=self._name,
+            params=self._params,
+            )
+
+    @property
+    def is_fixture(self):
+        return True
+
+    def update_fixtures_targets(self, import_alias_tgt, target_set):
+        assert import_alias_tgt.module_name == self._module_name
+        import_alias_tgt.add_component(self)
+
+    def make_component(self, python_module, name_to_res=None):
+        return htypes.builtin.attribute(
+            object=mosaic.put(python_module),
+            attr_name=self._attr_name,
+            )
+
+    def make_resource(self, module_name, python_module):
+        return FixtureProbeResource(self._name, self.make_component(python_module), self._params)
+
+
+class ConfigItemFixtureCtr(ModuleCtr):
+
+    @classmethod
+    def from_piece(cls, piece):
+        return cls(piece.module_name, piece.attr_qual_name, piece.service_name, piece.service_params)
+
+    def __init__(self, module_name, attr_qual_name, service_name, service_params):
+        super().__init__(module_name)
+        self._attr_qual_name = attr_qual_name
+        self._service_name = service_name
+        self._service_params = service_params
+
+    @property
+    def piece(self):
+        return htypes.fixture_resource.config_item_fixture_ctr(
+            module_name=self._module_name,
+            attr_qual_name=tuple(self._attr_qual_name),
+            service_name=self._service_name,
+            service_params=self._service_params,
+            )
+
+    @property
+    def is_fixture(self):
+        return True
+
+    def update_fixtures_targets(self, import_alias_tgt, target_set):
+        assert import_alias_tgt.module_name == self._module_name
+        import_alias_tgt.add_component(self)
+
+    def make_component(self, python_module, name_to_res=None):
+        object = python_module
+        for name in self._attr_qual_name:
+            object = htypes.builtin.attribute(
+                object=mosaic.put(object),
+                attr_name=name,
+                )
+        return object
+
+    def make_resource(self, module_name, python_module):
+        return ConfigItemFixtureResource(self._service_name, self.make_component(python_module), self._service_params)
