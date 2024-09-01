e31fbfa15a95991a540f5fcdda09d9366c7143b4
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-09-01 13:43:43 +0300

    RC: Add empty ui_model_command marker

--- hyperapp/rc/ui_command_marker.dyn.py
+++ hyperapp/rc/ui_command_marker.dyn.py
@@ -3,3 +3,9 @@ def ui_command_marker(t, module_name):
     def _ui_command_wrapper(fn):
         return fn
     return _ui_command_wrapper
+
+
+def ui_model_command_marker(t, module_name):
+    def _ui_command_wrapper(fn):
+        return fn
+    return _ui_command_wrapper
