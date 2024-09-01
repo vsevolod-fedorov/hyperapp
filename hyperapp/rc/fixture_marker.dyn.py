9c6a2a0613dfa676fb2a79ad225a8c164da33a5f
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-09-01 11:43:38 +0300

    Split service and fixture marker modules

--- hyperapp/rc/fixture_marker.dyn.py
+++ hyperapp/rc/fixture_marker.dyn.py
@@ -1,12 +1,12 @@
 import inspect
 
-from .code.service_ctr import ServiceProbeCtr
+from .code.service_ctr import FixtureCtr
 
 
-def service_marker(fn, module_name, ctr_collector):
+def fixture_marker(fn, module_name, ctr_collector):
     if '.' in fn.__qualname__:
-        raise RuntimeError(f"Only free functions are suitable for services: {fn!r}")
-    ctr = ServiceProbeCtr(
+        raise RuntimeError(f"Only free functions are suitable for fixtures: {fn!r}")
+    ctr = FixtureCtr(
         module_name=module_name, 
         attr_name=fn.__name__,
         name=fn.__name__,
