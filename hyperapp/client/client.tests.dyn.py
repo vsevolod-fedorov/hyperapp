8d1599def22f541d315ab92f7b79e0d71ea35315
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-09-01 12:45:09 +0300

    Update client module

--- hyperapp/client/client.tests.dyn.py
+++ hyperapp/client/client.tests.dyn.py
@@ -1,9 +1,10 @@
-from unittest.mock import Mock
-
 from .tested.code import client
 
 
-def test_make_default_layout():
-    lcs = Mock()
-    layout = client.make_default_layout(lcs)
-    assert layout
+def test_client(client_main):
+    sys_argv = [
+        '--clean=true',
+        '--lcs-storage-path=/tmp/client-test-lcs-storage-path.yaml',
+        '--layout-path=/tmp/client-test-layout-path.jaon',
+        ]
+    client_main(sys_argv)
