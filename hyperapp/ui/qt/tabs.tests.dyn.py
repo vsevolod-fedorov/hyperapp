a08dda5fdb8f4fe73c8864e611f69f3e53a486a2
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-09-01 13:44:11 +0300

    Update tabs view, without commands

--- hyperapp/ui/qt/tabs.tests.dyn.py
+++ hyperapp/ui/qt/tabs.tests.dyn.py
@@ -1,7 +1,5 @@
 from unittest.mock import Mock
 
-from PySide6 import QtWidgets
-
 from . import htypes
 from .services import (
     mosaic,
@@ -9,6 +7,7 @@ from .services import (
     )
 from .code.list_diff import ListDiff
 from .code.context import Context
+from .fixtures import qapp_fixtures
 from .tested.code import tabs
 
 
@@ -44,35 +43,29 @@ def make_outer_state(inner_tab_state):
         )
 
 
-def test_tabs():
+def test_tabs(qapp):
     ctx = Context()
     piece = make_inner_piece()
     state = make_inner_state()
-    app = QtWidgets.QApplication()
-    try:
-        view = tabs.TabsView.from_piece(piece, ctx)
-        view.set_controller_hook(Mock())
-        widget = view.construct_widget(state, ctx)
-        assert view.piece
-        state = view.widget_state(widget)
-        assert state
-    finally:
-        app.shutdown()
+
+    view = tabs.TabsView.from_piece(piece, ctx)
+    view.set_controller_hook(Mock())
+    widget = view.construct_widget(state, ctx)
+    assert view.piece
+    state = view.widget_state(widget)
+    assert state
 
 
-def test_tab_list():
+def test_tab_list(qapp):
     ctx = Context()
     piece = make_inner_piece()
     state = make_inner_state()
-    app = QtWidgets.QApplication()
-    try:
-        view = tabs.TabsView.from_piece(piece, ctx)
-        view.set_controller_hook(Mock())
-        widget = view.construct_widget(state, ctx)
-        assert view.piece
-        state = view.widget_state(widget)
-        assert state
-        tab_list = tabs.open_tab_list(view)
-        assert tab_list
-    finally:
-        app.shutdown()
+
+    view = tabs.TabsView.from_piece(piece, ctx)
+    view.set_controller_hook(Mock())
+    widget = view.construct_widget(state, ctx)
+    assert view.piece
+    state = view.widget_state(widget)
+    assert state
+    tab_list = tabs.open_tab_list(view)
+    assert tab_list
