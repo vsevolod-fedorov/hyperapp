26f1d21ce4f586eb918fbcb479eaca8a6e0d4596
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-09-01 13:33:34 +0300

    Update label view

--- hyperapp/ui/qt/label.tests.dyn.py
+++ hyperapp/ui/qt/label.tests.dyn.py
@@ -1,23 +1,19 @@
-from PySide6 import QtWidgets
-
 from . import htypes
 from .services import (
     mosaic,
     )
 from .code.context import Context
+from .fixtures import qapp_fixtures
 from .tested.code import label
 
 
-def test_label():
+def test_label(qapp):
     ctx = Context()
     piece = htypes.label.view("Sample label")
     state = htypes.label.state()
-    app = QtWidgets.QApplication()
-    try:
-        view = label.LabelView.from_piece(piece, ctx)
-        widget = view.construct_widget(state, ctx)
-        assert view.piece
-        state = view.widget_state(widget)
-        assert state
-    finally:
-        app.shutdown()
+
+    view = label.LabelView.from_piece(piece, ctx)
+    widget = view.construct_widget(state, ctx)
+    assert view.piece
+    state = view.widget_state(widget)
+    assert state
