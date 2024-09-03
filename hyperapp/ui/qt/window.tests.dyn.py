52988946836a2d2a41ea3142ecf6f205ee9cc4cb
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-09-03 13:12:58 +0300

    Update window view and tests

--- hyperapp/ui/qt/window.tests.dyn.py
+++ hyperapp/ui/qt/window.tests.dyn.py
@@ -1,53 +1,44 @@
-from PySide6 import QtWidgets
-
 from . import htypes
 from .services import (
     mosaic,
-    web,
     )
+from .code.mark import mark
 from .code.context import Context
-from .code.list_diff import ListDiff
-from .code import menu_bar  # Used implicitly
+from .fixtures import qapp_fixtures
 from .tested.code import window
 
 
-def make_window_piece():
+@mark.fixture
+def piece():
     label = htypes.label.view("Sample label")
     tabs_piece = htypes.tabs.view(
         tabs=(htypes.tabs.tab("One", mosaic.put(label)),),
         )
-    piece = htypes.window.view(
+    return htypes.window.view(
         menu_bar_ref=mosaic.put(htypes.menu_bar.view()),
         central_view_ref=mosaic.put(tabs_piece),
         )
-    return (tabs_piece, piece)
 
 
-def make_window_state():
+@mark.fixture
+def state():
     label_state = htypes.label.state()
     tabs_state = htypes.tabs.state(
         current_tab=0,
         tabs=(mosaic.put(label_state),),
         )
-    state = htypes.window.state(
+    return htypes.window.state(
         menu_bar_state=mosaic.put(htypes.menu_bar.state()),
         central_view_state=mosaic.put(tabs_state),
         size=htypes.window.size(100, 100),
         pos=htypes.window.pos(10, 10),
         )
-    return (tabs_state, state)
 
 
-def test_construct_widget():
+def test_construct_widget(qapp, piece, state):
     ctx = Context()
-    tabs_piece, piece = make_window_piece()
-    tabs_state, state = make_window_state()
-    app = QtWidgets.QApplication()
-    try:
-        view = window.WindowView.from_piece(piece, ctx)
-        assert view.piece
-        widget = view.construct_widget(state, ctx)
-        state = view.widget_state(widget)
-        assert state
-    finally:
-        app.shutdown()
+    view = window.WindowView.from_piece(piece, ctx)
+    assert view.piece
+    widget = view.construct_widget(state, ctx)
+    state = view.widget_state(widget)
+    assert state
