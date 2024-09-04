055fd71df4525f7acb04d32f7f4d59900e6152dd
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-09-04 11:20:14 +0300

    Update window view; Extract commands to separate module

--- hyperapp/ui/qt/window.dyn.py
+++ hyperapp/ui/qt/window.dyn.py
@@ -13,8 +13,6 @@ from .code.view import Item, View
 
 log = logging.getLogger(__name__)
 
-DUP_OFFSET = htypes.window.pos(150, 50)
-
 
 class WindowView(View):
 
@@ -74,23 +72,3 @@ class WindowView(View):
         if idx == 1:
             return widget.centralWidget()
         return super().item_widget(widget, idx)
-
-
-@mark.ui_command(htypes.window.view)
-async def duplicate_window(root, view, state):
-    log.info("Duplicate window: %s / %s", view, state)
-    new_state = htypes.window.state(
-        menu_bar_state=state.menu_bar_state,
-        central_view_state=state.central_view_state,
-        size=state.size,
-        pos=htypes.window.pos(
-            x=state.pos.x + DUP_OFFSET.x,
-            y=state.pos.y + DUP_OFFSET.y,
-            ),
-        )
-    await root.create_window(view.piece, new_state)
-
-
-@mark.ui_command(htypes.window.view)
-def quit():
-    QtWidgets.QApplication.quit()
