04e67792a7464ad0e4aa549b8873c18dba6f3233
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-09-05 11:01:16 +0300

    Refactoring: Extract marker_utils module

--- hyperapp/rc/ui_command_marker.dyn.py
+++ hyperapp/rc/ui_command_marker.dyn.py
@@ -1,37 +1,12 @@
 import inspect
 
 from .code.command_ctr import CommandTemplateCtr
-
-
-def _check_is_function(fn):
-    if not inspect.isfunction(fn):
-        raise RuntimeError(
-            f"Unknown object attempted to be marked as an actor: {fn!r};"
-            " Expected function, classmethod or staticmethod"
-            )
-
-
-def _check_not_classmethod(fn):
-    if type(fn) is classmethod:
-        raise RuntimeError(
-            f"Wrap this method first with marker and then with classmethod (classmethod should be first): {fn!r}"
-            )
-
-    
-def _is_cls_arg(fn, arg):
-    # Check arg is a class (MyClass).
-    if not isinstance(arg, type):
-        return False
-    # Resolve MyClass.my_function attribute.
-    attr = getattr(arg, fn.__name__, None)
-    if attr is None:
-        return False
-    # Check if class MyClass.my_function is bound to is arg.
-    return getattr(attr, '__self__', None) is arg
-
-
-def _fn_params(fn):
-    return list(inspect.signature(fn).parameters)
+from .code.marker_utils import (
+    check_is_function,
+    check_not_classmethod,
+    is_cls_arg,
+    fn_params,
+    )
 
 
 class CommandProbe:
@@ -50,9 +25,9 @@ class CommandProbe:
         self._ctr_collector = system_probe.resolve_service('ctr_collector')
 
     def __call__(self, *args, **kw):
-        params = _fn_params(self._fn)
+        params = fn_params(self._fn)
         param_ofs = 0
-        if args and _is_cls_arg(self._fn, args[0]):
+        if args and is_cls_arg(self._fn, args[0]):
             # self._fn is a classmethod and args[0] is a 'cls' argument.
             param_ofs = 1
         creg_param_count = len(args) - param_ofs + len(kw)
@@ -88,8 +63,8 @@ class CommandWrapper:
 
     def __call__(self, fn):
         qual_name = fn.__qualname__.split('.')
-        _check_not_classmethod(fn)
-        _check_is_function(fn)
+        check_not_classmethod(fn)
+        check_is_function(fn)
         return CommandProbe(self._system, self._ctr_collector, self._module_name, self._service_name, fn, self._t)
 
 
