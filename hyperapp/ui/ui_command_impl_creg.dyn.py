6a97cde8fa7f0beea751ade59751097c9f4d0406
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-09-06 13:02:51 +0300

    ui_command_impl_creg: Move to new code registry

--- hyperapp/ui/ui_command_impl_creg.dyn.py
+++ hyperapp/ui/ui_command_impl_creg.dyn.py
@@ -1,9 +1,7 @@
-from .services import (
-    code_registry_ctr,
-    )
+from .services import code_registry_ctr2
 from .code.mark import mark
 
 
 @mark.service2
-def ui_command_impl_creg():
-    return code_registry_ctr('ui_command_impl')
+def ui_command_impl_creg(config):
+    return code_registry_ctr2('ui_command_impl', config)
