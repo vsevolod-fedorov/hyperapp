21481b84445e589b8c7e4a309ecdd9c657b36fea
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-09-08 11:59:31 +0300

    WIP: UI command: Move to system

--- hyperapp/ui/ui_command.dyn.py
+++ hyperapp/ui/ui_command.dyn.py
@@ -3,14 +3,10 @@ from functools import cached_property
 
 from . import htypes
 from .services import (
-    association_reg,
-    data_to_res,
     deduce_t,
-    mark,
-    mosaic,
     pyobj_creg,
-    ui_command_impl_creg,
     )
+from .code.mark import mark
 from .code.command import Command, CommandKind, FnCommandImpl
 from .code.command_groups import default_command_groups
 
@@ -19,60 +15,50 @@ log = logging.getLogger(__name__)
 
 class UiCommand(Command):
 
-    def __init__(self, d, impl):
+    def __init__(self, d, impl, groups):
         super().__init__(d, impl)
+        self._groups = groups
 
     def __repr__(self):
         return f"<UiCommand: {self.name}: {self._impl}>"
 
     @property
     def groups(self):
-        return self._impl.groups
+        return self._groups
 
 
 class UiCommandImpl(FnCommandImpl):
-
-    def __init__(self, ctx, fn, params, properties):
-        super().__init__(ctx, fn, params)
-        self._properties = properties
-
-    @property
-    def properties(self):
-        return self._properties
-
-    @property
-    def groups(self):
-        return default_command_groups(self.properties, CommandKind.VIEW)
+    pass
 
 
-@ui_command_impl_creg.actor(htypes.ui.ui_command_impl)
+@mark.actor.ui_command_impl_creg(htypes.ui.ui_command_impl)
 def ui_command_impl_from_piece(piece, ctx):
-    command_properties_d_res = data_to_res(htypes.ui.command_properties_d())
     fn = pyobj_creg.invite(piece.function)
-    properties = association_reg[command_properties_d_res, piece]
-    return UiCommandImpl(ctx, fn, piece.params, properties)
+    return UiCommandImpl(ctx, fn, piece.ctx_params)
 
 
-@mark.service
-def list_view_commands():
-    def _list_view_commands(view):
-        piece_t = deduce_t(view.piece)
-        piece_t_res = pyobj_creg.actor_to_piece(piece_t)
-        d_res = data_to_res(htypes.ui.ui_command_d())
-        universal_d_res = data_to_res(htypes.ui.universal_ui_command_d())
-        command_list = [
-            *association_reg.get_all((d_res, piece_t_res)),
-            *association_reg.get_all(universal_d_res),
-            ]
-        return command_list
-    return _list_view_commands
+@mark.service2
+def view_ui_command_reg(config, view_t):
+    return config[view_t]
 
 
-@mark.service
-def ui_command_factory():
-    def _ui_command_factory(piece, ctx):
-        command_d = pyobj_creg.invite(piece.d)
-        impl = ui_command_impl_creg.invite(piece.impl, ctx)
-        return UiCommand(command_d, impl)
+@mark.service2
+def universal_ui_command_reg(config):
+    return config
 
-    return _ui_command_factory
+
+@mark.service2
+def get_view_commands(view_ui_command_reg, universal_ui_command_reg, view):
+    view_t = deduce_t(view.piece)
+    return [
+        *view_ui_command_reg(view_t),
+        *universal_ui_command_reg,
+        ]
+
+
+@mark.service2
+def ui_command_factory(piece, ctx):
+    command_d = pyobj_creg.invite(piece.d)
+    impl = ui_command_impl_creg.invite(piece.impl, ctx)
+    groups = default_command_groups(piece.properties, CommandKind.VIEW)
+    return UiCommand(command_d, impl, groups)
--- hyperapp/ui/ui_command.tests.dyn.py
+++ hyperapp/ui/ui_command.tests.dyn.py
@@ -2,45 +2,12 @@ import weakref
 
 from . import htypes
 from .services import (
-    data_to_res,
-    fn_to_ref,
-    mark,
     mosaic,
+    pyobj_creg,
     )
+from .code.mark import mark
 from .code.context import Context
 from .tested.code import ui_command
-from .tested.services import ui_command_factory, list_view_commands
-
-
-def _sample_fn(view, state):
-    return view.piece + state
-
-
-class PhonyAssociationRegistry:
-
-    def __getitem__(self, key):
-        return htypes.ui.command_properties(
-            is_global=False,
-            uses_state=False,
-            remotable=False,
-            )
-
-    def get_all(self, key):
-        command_d_res = data_to_res(htypes.ui_command_tests.sample_command_d())
-        impl = htypes.ui.ui_command_impl(
-            function=fn_to_ref(_sample_fn),
-            params=('view', 'state'),
-            )
-        command = htypes.ui.ui_command(
-            d=mosaic.put(command_d_res),
-            impl=mosaic.put(impl),
-            )
-        return [command]
-
-
-@mark.service
-def association_reg():
-    return PhonyAssociationRegistry()
 
 
 class PhonyWidget:
@@ -51,32 +18,62 @@ class PhonyView:
 
     @property
     def piece(self):
-        return 23
+        return 'a-piece'
 
     def widget_state(self, widget):
-        return 100
+        return 'a-state'
 
 
-async def test_list_view_commands():
+def _sample_fn(view, state, sample_service):
+    return 'sample-fn: {view.piece}, {state}, {sample_service}'
+
+
+@mark.fixture
+def sample_service():
+    return 'a-service'
+
+
+@mark.config_item_fixture('view_ui_command_reg')
+def view_ui_command_reg_config(data_to_res):
+    properties = htypes.ui.command_properties(
+        is_global=False,
+        uses_state=False,
+        remotable=False,
+        )
+    command_d_res = data_to_res(htypes.ui_command_tests.sample_command_d())
+    impl = htypes.ui.ui_command_impl(
+        function=pyobj_creg.actor_to_ref(_sample_fn),
+        ctx_params=('view', 'state'),
+        service_params=('sample_service',),
+        )
+    command = htypes.ui.ui_command(
+        d=mosaic.put(command_d_res),
+        properties=properties,
+        impl=mosaic.put(impl),
+        )
+    return [command]
+
+
+async def test_view_commands(get_view_commands, ui_command_factory):
     view = PhonyView()
     widget = PhonyWidget()  # Should hold ref to it.
     ctx = Context(
         view=view,
         widget=weakref.ref(widget),
         )
-    command_piece_list = list_view_commands(view)
+    command_piece_list = get_view_commands(view)
     assert command_piece_list
     command = ui_command_factory(command_piece_list[0], ctx)
     result = await command.run()
-    assert result == 123, repr(result)
+    assert result == 'sample-fn: a-piece, a-state, a-service', repr(result)
 
 
 def test_command_impl_from_piece():
     ctx = Context()
     piece = htypes.ui.ui_command_impl(
-        function=fn_to_ref(_sample_fn),
-        params=('view', 'state'),
+        function=pyobj_creg.actor_to_ref(_sample_fn),
+        ctx_params=('view', 'state'),
+        service_params=('sample_service',),
         )
     impl = ui_command.ui_command_impl_from_piece(piece, ctx)
     assert impl
-    assert impl.properties
