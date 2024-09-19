0072ffb2ee94f5b10a7b74a8e13b1107ecc09ffa
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-09-19 12:41:44 +0300

    Add file bundle profiling scripts

---
+++ scripts/file-bundle-profile-load.py
@@ -0,0 +1,20 @@
+import cProfile
+import pstats
+from pathlib import Path
+
+path = Path.home() / '.local/share/hyperapp/rc-job-cache.cdr'
+b = file_bundle(path, 'cdr')
+print('bundle:', b)
+
+print("Load - profiling")
+with cProfile.Profile() as pr:
+    pr.runcall(b.load_piece)
+    stats = pstats.Stats(pr)
+
+stats.dump_stats('/tmp/file-bundle-first-load.profile')
+
+stats.sort_stats('cumulative')
+stats.print_stats()
+
+stats.sort_stats('time')
+stats.print_stats()
