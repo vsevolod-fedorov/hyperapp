f4b74c81c5a28708893e74ca9151ad484858641d
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-09-19 12:41:44 +0300

    Add file bundle profiling scripts

---
+++ scripts/file-bundle-profile-save.py
@@ -0,0 +1,28 @@
+import cProfile
+import pstats
+from pathlib import Path
+
+path = Path.home() / '.local/share/hyperapp/rc-job-cache.cdr'
+b = file_bundle(path, 'cdr')
+print('bundle:', b)
+p = b.load_piece()
+
+out_b = file_bundle(Path('/tmp/test.cdr'), 'cdr2')
+def test_save():
+    out_b.save_piece(p)
+
+print("First save")
+test_save()  # Populate ref picker cache.
+
+print("Second save - profiling")
+with cProfile.Profile() as pr:
+    pr.runcall(test_save)
+    stats = pstats.Stats(pr)
+
+stats.dump_stats('/tmp/file-bundle.profile')
+
+stats.sort_stats('cumulative')
+stats.print_stats()
+
+stats.sort_stats('time')
+stats.print_stats()
---
+++ scripts/file-bundle-time.py
@@ -0,0 +1,24 @@
+import time
+from timeit import timeit, repeat
+from pathlib import Path
+
+path = Path.home() / '.local/share/hyperapp/rc-job-cache.cdr'
+b = file_bundle(path, 'cdr')
+print('bundle:', b)
+
+# timer = time.process_time
+timer = time.perf_counter  # default
+
+print("Load:")
+for t in repeat(b.load_piece, number=1, repeat=5, timer=timer):
+    print(t)
+
+p = b.load_piece()
+
+print("Save:")
+out_b = file_bundle(Path('/tmp/test.cdr'), 'cdr')
+def test_save():
+    out_b.save_piece(p)
+
+for t in repeat(test_save, number=1, repeat=5, timer=timer):
+    print(t)
