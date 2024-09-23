b0ad94d22203bceb9df41a2e07703690dfe49bb3
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-09-23 13:08:59 +0300

    Implement model and global model command constructors; Update ui command types

--- hyperapp/rc/command_ctr.dyn.py
+++ hyperapp/rc/command_ctr.dyn.py
@@ -12,37 +12,13 @@ LOCAL_PARAMS = {'controller', 'ctx', 'lcs', 'rpc_endpoint', 'identity', 'remote_
 
 class CommandTemplateCtr(Constructor):
 
-    @classmethod
-    def from_piece(cls, piece, data_to_res):
-        return cls(
-            data_to_res=data_to_res,
-            module_name=piece.module_name,
-            attr_qual_name=piece.attr_qual_name,
-            service_name=piece.service_name,
-            t=pyobj_creg.invite(piece.t),
-            ctx_params=piece.ctx_params,
-            service_params=piece.service_params,
-            )
-
-    def __init__(self, data_to_res, module_name, attr_qual_name, service_name, t, ctx_params, service_params):
+    def __init__(self, data_to_res, module_name, attr_qual_name, service_name, ctx_params, service_params):
         self._data_to_res = data_to_res
         self._module_name = module_name
         self._attr_qual_name = attr_qual_name
         self._service_name = service_name
-        self._t = t
         self._ctx_params = ctx_params
         self._service_params = service_params
-
-    @property
-    def piece(self):
-        return htypes.command_resource.command_template_ctr(
-            module_name=self._module_name,
-            attr_qual_name=tuple(self._attr_qual_name),
-            service_name=self._service_name,
-            t=pyobj_creg.actor_to_ref(self._t),
-            ctx_params=tuple(self._ctx_params),
-            service_params=tuple(self._service_params),
-            )
 
     def update_targets(self, target_set):
         resource_tgt = target_set.factory.python_module_resource_by_module_name(self._module_name)
@@ -61,7 +37,7 @@ class CommandTemplateCtr(Constructor):
         target_set.update_deps_for(resource_tgt)
 
     def get_component(self, name_to_res):
-        return name_to_res[f'{self._resource_name}.ui-command']
+        return name_to_res[f'{self._resource_name}.{self._resource_suffix}']
 
     def make_component(self, types, python_module, name_to_res=None):
         object = python_module
@@ -80,29 +56,107 @@ class CommandTemplateCtr(Constructor):
         assert d_t_piece, (type_module, d_name)  # TODO: Make type if missing.
         d_t = pyobj_creg.animate(d_t_piece)
         d_piece = self._data_to_res(d_t())
-        impl = htypes.ui.ui_command_impl(
+        properties = htypes.ui.command_properties(
+            is_global=self._is_global,
+            uses_state=bool(set(self._ctx_params) & STATE_PARAMS),
+            remotable=not set(self._ctx_params) & LOCAL_PARAMS,
+            )
+        command = self._ui_command_t(
+            d=mosaic.put(d_piece),
+            properties=properties,
             function=mosaic.put(object),
             ctx_params=tuple(self._ctx_params),
             service_params=tuple(self._service_params),
             )
-        properties = htypes.ui.command_properties(
-            is_global=False,
-            uses_state=bool(set(self._ctx_params) & STATE_PARAMS),
-            remotable=not set(self._ctx_params) & LOCAL_PARAMS,
-            )
-        command = htypes.ui.ui_command(
-            d=mosaic.put(d_piece),
-            properties=properties,
-            impl=mosaic.put(impl),
-            )
         if name_to_res is not None:
             name_to_res[f'{self._resource_name}.d'] = d_piece
-            name_to_res[f'{self._resource_name}.ui-command-impl'] = impl
-            name_to_res[f'{self._resource_name}.command-properties'] = properties
-            name_to_res[f'{self._resource_name}.ui-command'] = command
+            name_to_res[f'{self._resource_name}.{self._resource_suffix}'] = command
         return command
+
+
+class UntypedCommandTemplateCtr(CommandTemplateCtr):
+
+    @classmethod
+    def from_piece(cls, piece, data_to_res):
+        return cls(
+            data_to_res=data_to_res,
+            module_name=piece.module_name,
+            attr_qual_name=piece.attr_qual_name,
+            service_name=piece.service_name,
+            ctx_params=piece.ctx_params,
+            service_params=piece.service_params,
+            )
+
+    @property
+    def piece(self):
+        return self._template_ctr_t(
+            module_name=self._module_name,
+            attr_qual_name=tuple(self._attr_qual_name),
+            service_name=self._service_name,
+            ctx_params=tuple(self._ctx_params),
+            service_params=tuple(self._service_params),
+            )
+
+    @property
+    def _resource_name(self):
+        attr_name = '_'.join(self._attr_qual_name)
+        return attr_name
+
+
+class TypedCommandTemplateCtr(CommandTemplateCtr):
+
+    @classmethod
+    def from_piece(cls, piece, data_to_res):
+        return cls(
+            data_to_res=data_to_res,
+            module_name=piece.module_name,
+            attr_qual_name=piece.attr_qual_name,
+            service_name=piece.service_name,
+            t=pyobj_creg.invite(piece.t),
+            ctx_params=piece.ctx_params,
+            service_params=piece.service_params,
+            )
+
+    def __init__(self, data_to_res, module_name, attr_qual_name, service_name, t, ctx_params, service_params):
+        super().__init__(data_to_res, module_name, attr_qual_name, service_name, ctx_params, service_params)
+        self._t = t
+
+    @property
+    def piece(self):
+        return self._template_ctr_t(
+            module_name=self._module_name,
+            attr_qual_name=tuple(self._attr_qual_name),
+            service_name=self._service_name,
+            t=pyobj_creg.actor_to_ref(self._t),
+            ctx_params=tuple(self._ctx_params),
+            service_params=tuple(self._service_params),
+            )
 
     @property
     def _resource_name(self):
         attr_name = '_'.join(self._attr_qual_name)
         return f'{self._t.module_name}_{self._t.name}_{attr_name}'
+
+
+class UiCommandTemplateCtr(TypedCommandTemplateCtr):
+
+    _ui_command_t = htypes.ui.ui_command
+    _template_ctr_t = htypes.command_resource.ui_command_template_ctr
+    _is_global = False
+    _resource_suffix = 'ui-command'
+
+
+class ModelCommandTemplateCtr(TypedCommandTemplateCtr):
+
+    _ui_command_t = htypes.ui.model_command
+    _template_ctr_t = htypes.command_resource.model_command_template_ctr
+    _is_global = False
+    _resource_suffix = 'model-command'
+
+
+class GlobalModelCommandTemplateCtr(UntypedCommandTemplateCtr):
+
+    _ui_command_t = htypes.ui.model_command
+    _template_ctr_t = htypes.command_resource.global_model_command_template_ctr
+    _is_global = True
+    _resource_suffix = 'global-model-command'
