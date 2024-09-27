1687023a9fab444100cfe22a3820e414f1fef946
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-09-27 12:24:45 +0300

    Update feed_factory after moving to system with feed type template

--- hyperapp/ui/feed.tests.dyn.py
+++ hyperapp/ui/feed.tests.dyn.py
@@ -9,14 +9,9 @@ from .tested.code import feed as feed_module
 
 @mark.config_fixture('feed_factory')
 def feed_factory_config():
-    element_t = pyobj_creg.actor_to_piece(htypes.feed_tests.sample_item)
     return {
-        htypes.feed_tests.sample_list_feed: htypes.feed.list_feed_type(
-            element_t=mosaic.put(element_t),
-            ),
-        htypes.feed_tests.sample_index_tree_feed: htypes.feed.index_tree_feed_type(
-            element_t=mosaic.put(element_t),
-            ),
+        htypes.feed_tests.sample_list_feed: feed_module.ListFeed(),
+        htypes.feed_tests.sample_index_tree_feed: feed_module.IndexTreeFeed(),
         }
 
 
@@ -29,4 +24,20 @@ def test_list_feed_factory(feed_factory):
 def test_index_tree_feed_factory(feed_factory):
     piece = htypes.feed_tests.sample_index_tree_feed()
     feed = feed_factory(piece)
+    assert isinstance(feed, feed_module.IndexTreeFeed), repr(feed)
+
+@mark.fixture
+def element_t():
+    return pyobj_creg.actor_to_piece(htypes.feed_tests.sample_item)
+
+
+def test_list_feed_actor(feed_creg, element_t):
+    piece = htypes.feed.list_feed_type(mosaic.put(element_t))
+    feed = feed_creg.animate(piece)
+    assert isinstance(feed, feed_module.ListFeed), repr(feed)
+
+
+def test_index_tree_feed_actor(feed_creg, element_t):
+    piece = htypes.feed.index_tree_feed_type(mosaic.put(element_t))
+    feed = feed_creg.animate(piece)
     assert isinstance(feed, feed_module.IndexTreeFeed), repr(feed)
