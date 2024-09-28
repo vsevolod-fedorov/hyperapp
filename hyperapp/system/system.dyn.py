e430749a7682b55a538b713e1da4414739054530
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-09-28 12:25:29 +0300

    Fix system: cfg_item_creg and other system elements should be added to config

--- hyperapp/system/system.dyn.py
+++ hyperapp/system/system.dyn.py
@@ -298,30 +298,29 @@ class System:
             rec.service: web.summon(rec.config)
             for rec in config_piece.services
             }
+        self._load_config_piece('cfg_item_creg', service_to_config.get('cfg_item_creg'))
+        self._load_config_piece('config_ctl_creg', service_to_config.get('config_ctl_creg'))
         self._load_config_piece('system', service_to_config['system'])
         for service_name, service_template in self._configs['system'].items():
             self._config_ctl[service_name] = self._config_ctl_creg.invite(service_template.ctl_ref)
-        self._load_cfg_item_creg(service_to_config['cfg_item_creg'])
-        self._load_pyobj_creg(service_to_config['pyobj_creg'])
+        self._load_pyobj_creg(service_to_config.get('pyobj_creg'))
         for service_name, config_piece in service_to_config.items():
-            if service_name in {'system', 'cfg_item_creg', 'config_ctl', 'pyobj_creg'}:
+            if service_name in {'system', 'cfg_item_creg', 'config_ctl_creg', 'pyobj_creg'}:
                 continue
             self._load_config_piece(service_name, config_piece)
         self.add_core_service('system_config_template', self._configs)
 
     def _load_config_piece(self, service_name, config_piece):
+        if not config_piece:
+            return
         ctl = self._config_ctl[service_name]
         config = ctl.from_data(config_piece)
         self.update_config(service_name, config)
 
-    def _load_cfg_item_creg(self, config_piece):
-        ctl = self._config_ctl['cfg_item_creg']
-        config_template = ctl.from_data(config_piece)
-        config = ctl.resolve(self, 'cfg_item_creg', config_template)
-        self._cfg_item_creg.update_config(config)
-
     # TODO: Think how to load pyobj_creg config not by system. It is a global registry.
     def _load_pyobj_creg(self, config_piece):
+        if not config_piece:
+            return
         ctl = self._config_ctl['pyobj_creg']
         config_template = ctl.from_data(config_piece)
         config = ctl.resolve(self, 'pyobj_creg', config_template)
