b94af9597bb203f375c699f71cc2abcb51a0168f
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-09-29 12:50:50 +0300

    Add command_cfg_item module

---
+++ hyperapp/ui/command_cfg_item.dyn.py
@@ -0,0 +1,33 @@
+from . import htypes
+from .services import pyobj_creg
+from .code.mark import mark
+
+
+class CommandCfgItem:
+
+    @classmethod
+    @mark.actor.cfg_item_creg
+    def from_piece(cls, piece):
+        return cls(
+            t=pyobj_creg.invite(piece.t),
+            command_ref=piece.command,
+            )
+
+    def __init__(self, t, command_ref):
+        self._t = t
+        self._command_ref = command_ref
+
+    @property
+    def piece(self):
+        return htypes.command.cfg_item(
+            t=pyobj_creg.actor_to_ref(self._t),
+            command=self._command_ref,
+            )
+
+    @property
+    def key(self):
+        return self._t
+
+    def resolve(self, system, service_name):
+        command_creg = system.resolve_service('command_creg')
+        return command_creg.invite(self._command_ref)
