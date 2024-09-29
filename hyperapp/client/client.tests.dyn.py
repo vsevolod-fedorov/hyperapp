a31b3b1b3904a2a128b7fe0e7469ea1caa6d1dda
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-09-29 13:40:44 +0300

    Client test: Fix shutting down event loop

--- hyperapp/client/client.tests.dyn.py
+++ hyperapp/client/client.tests.dyn.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from PySide6 import QtWidgets
 
 from .tested.code import client
@@ -15,4 +17,6 @@ def test_client(client_main):
     finally:
         app = QtWidgets.QApplication.instance()
         if app:
+            loop = asyncio.get_event_loop()
+            loop.close()
             app.shutdown()
