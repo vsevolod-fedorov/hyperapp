67a73979284814239083a505cadd4d8f3b3d587b
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-09-29 11:58:16 +0300

    Add command_creg, actor for ui_command

--- hyperapp/ui/ui_command.tests.dyn.py
+++ hyperapp/ui/ui_command.tests.dyn.py
@@ -66,3 +66,16 @@ async def test_view_commands(get_view_commands, view, widget):
     bound_command = unbound_command.bind(ctx)
     result = await bound_command.run()
     assert result == 'sample-fn: a-state, a-service', repr(result)
+
+
+def test_ui_command_from_piece(data_to_ref):
+    d = htypes.ui_command_tests.sample_command_d()
+    piece = htypes.command.ui_command(
+        d=data_to_ref(d),
+        properties=htypes.command.properties(False, False, False),
+        function=pyobj_creg.actor_to_ref(_sample_fn),
+        ctx_params=('view', 'state'),
+        service_params=('sample_service',),
+        )
+    command = ui_command.ui_command_from_piece(piece)
+    assert isinstance(command, ui_command.UnboundUiCommand)
