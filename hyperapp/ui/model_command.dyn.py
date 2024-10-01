f26a30762dfcf73a2b0e3f709a321501d257d4d3
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-10-01 11:23:11 +0300

    Update model commands: global_model_command_reg

--- hyperapp/ui/model_command.dyn.py
+++ hyperapp/ui/model_command.dyn.py
@@ -1,108 +1,110 @@
 import logging
-import weakref
+from functools import partial
 
-from hyperapp.common.htypes.deduce_value_type import DeduceTypeError, deduce_value_type
-
-from . import htypes
 from .services import (
-    association_reg,
-    data_to_res,
-    mark,
-    model_command_impl_creg,
     pyobj_creg,
     )
-from .code.command import Command, FnCommandImpl
+from .code.mark import mark
+from .code.command import UnboundCommand, BoundCommand
 
 log = logging.getLogger(__name__)
 
 
 
-class ModelCommand(Command):
+class UnboundModelCommand(UnboundCommand):
+
+    def __init__(self, d, fn, ctx_params, properties):
+        super().__init__(d, fn, ctx_params)
+        self._properties = properties
 
     def __repr__(self):
-        return f"<ModelCommand: {self.name}: {self._impl}>"
+        return f"<UnboundModelCommand: {self.name}>"
+
+    def bind(self, ctx):
+        return BoundModelCommand(self._d, self._fn, self._ctx_params, ctx, self._properties)
 
 
-class ModelCommandImpl(FnCommandImpl):
+class BoundModelCommand(BoundCommand):
 
-    def __init__(self, ctx, fn, params, properties):
-        super().__init__(ctx, fn, params)
+    def __init__(self, d, fn, ctx_params, ctx, properties):
+        super().__init__(d, fn, ctx_params, ctx)
         self._properties = properties
+
+    def __repr__(self):
+        return f"<BoundModelCommand: {self.name}>"
 
     @property
     def properties(self):
         return self._properties
 
 
-@model_command_impl_creg.actor(htypes.ui.model_command_impl)
-def model_command_impl_from_piece(piece, ctx):
+@mark.actor.command_creg
+def model_command_from_piece(piece, system):
+    kw = {
+        name: system.resolve_service(name)
+        for name in piece.service_params
+        }
     fn = pyobj_creg.invite(piece.function)
-    props_d_res = data_to_res(htypes.ui.command_properties_d())
-    try:
-        properties = association_reg[props_d_res, piece]
-    except KeyError:
-        raise RuntimeError(f"Properties are missing for model command: {piece}")
-    return ModelCommandImpl(ctx, fn, piece.params, properties)
+    return UnboundModelCommand(
+        d=pyobj_creg.invite(piece.d),
+        fn=partial(fn, **kw),
+        ctx_params=piece.ctx_params,
+        properties=piece.properties,
+        )
 
 
-@mark.service
-def global_commands():
-
-    def _global_commands():
-        d_res = data_to_res(htypes.ui.global_model_command_d())
-        command_list = association_reg.get_all(d_res)
-        return command_list
-
-    return _global_commands
+@mark.service2
+def global_model_command_reg(config):
+    return config
 
 
-@mark.service
-def model_commands():
+# @mark.service
+# def model_commands():
 
-    def _model_commands(piece):
-        try:
-            t = deduce_value_type(piece)
-        except DeduceTypeError:
-            return []
-        t_res = pyobj_creg.actor_to_piece(t)
-        d_res = data_to_res(htypes.ui.model_command_d())
-        command_list = association_reg.get_all((d_res, t_res))
-        return command_list
+#     def _model_commands(piece):
+#         try:
+#             t = deduce_value_type(piece)
+#         except DeduceTypeError:
+#             return []
+#         t_res = pyobj_creg.actor_to_piece(t)
+#         d_res = data_to_res(htypes.ui.model_command_d())
+#         command_list = association_reg.get_all((d_res, t_res))
+#         return command_list
 
-    return _model_commands
-
-
-@mark.service
-def enum_model_commands():
-
-    def _enum_model_commands(piece, ctx):
-        try:
-            t = deduce_value_type(piece)
-        except DeduceTypeError:
-            return []
-        t_res = pyobj_creg.actor_to_piece(t)
-        d_res = data_to_res(htypes.ui.model_command_enumerator_d())
-        enumerator_list = association_reg.get_all((d_res, t_res))
-        for enumerator in enumerator_list:
-            fn = pyobj_creg.invite(enumerator.function)
-            params = set(enumerator.params)
-            kw = {}
-            if 'piece' in params:
-                kw['piece'] = piece
-                params.remove('piece')
-            kw.update({
-                name: getattr(ctx, name)
-                for name in params
-                })
-            yield from fn(**kw)
-
-    return _enum_model_commands
+#     return _model_commands
 
 
-@mark.service
-def model_command_factory():
-    def _model_command_factory(piece, ctx):
-        command_d = pyobj_creg.invite(piece.d)
-        impl = model_command_impl_creg.invite(piece.impl, ctx)
-        return ModelCommand(command_d, impl)
-    return _model_command_factory
+# @mark.service
+# def enum_model_commands():
+
+#     def _enum_model_commands(piece, ctx):
+#         try:
+#             t = deduce_value_type(piece)
+#         except DeduceTypeError:
+#             return []
+#         t_res = pyobj_creg.actor_to_piece(t)
+#         d_res = data_to_res(htypes.ui.model_command_enumerator_d())
+#         enumerator_list = association_reg.get_all((d_res, t_res))
+#         for enumerator in enumerator_list:
+#             fn = pyobj_creg.invite(enumerator.function)
+#             params = set(enumerator.params)
+#             kw = {}
+#             if 'piece' in params:
+#                 kw['piece'] = piece
+#                 params.remove('piece')
+#             kw.update({
+#                 name: getattr(ctx, name)
+#                 for name in params
+#                 })
+#             yield from fn(**kw)
+
+#     return _enum_model_commands
+
+
+# @mark.service
+# def model_command_factory():
+#     def _model_command_factory(piece, ctx):
+#         command_d = pyobj_creg.invite(piece.d)
+#         impl = model_command_impl_creg.invite(piece.impl, ctx)
+#         return ModelCommand(command_d, impl)
+#     return _model_command_factory
--- hyperapp/ui/model_command.tests.dyn.py
+++ hyperapp/ui/model_command.tests.dyn.py
@@ -1,64 +1,78 @@
-from unittest.mock import Mock
-
 from . import htypes
 from .services import (
-    fn_to_ref,
-    mark,
     mosaic,
+    pyobj_creg,
     )
-from .services import association_reg as real_association_reg
+from .code.mark import mark
 from .code.context import Context
 from .tested.code import model_command
-from .tested.services import (
-    enum_model_commands,
-    global_commands,
-    model_commands,
-    )
 
 
-def test_global_commands():
-    commands = global_commands()
+def _sample_fn(model, state, sample_service):
+    return f'sample-fn: {state}, {sample_service}'
+
+
+@mark.fixture
+def sample_service():
+    return 'a-service'
+
+
+def test_model_command_from_piece(data_to_ref):
+    d = htypes.model_command_tests.sample_command_d()
+    piece = htypes.command.model_command(
+        d=data_to_ref(d),
+        properties=htypes.command.properties(False, False, False),
+        function=pyobj_creg.actor_to_ref(_sample_fn),
+        ctx_params=('model', 'state'),
+        service_params=('sample_service',),
+        )
+    command = model_command.model_command_from_piece(piece)
+    assert isinstance(command, model_command.UnboundModelCommand)
+
+
+def test_global_commands(global_model_command_reg):
+    commands = global_model_command_reg()
     # assert commands
 
 
-def test_model_commands():
-    piece = htypes.model_command_tests.sample_model()
-    commands = model_commands(piece)
+# def test_model_commands():
+#     piece = htypes.model_command_tests.sample_model()
+#     commands = model_commands(piece)
 
 
-def test_enum_model_commands():
-    ctx = Context()
-    piece = htypes.model_command_tests.sample_model()
-    commands = list(enum_model_commands(piece, ctx))
+# def test_enum_model_commands():
+#     ctx = Context()
+#     piece = htypes.model_command_tests.sample_model()
+#     commands = list(enum_model_commands(piece, ctx))
 
 
-def _sample_fn():
-    return 123
+# def _sample_fn():
+#     return 123
 
 
-class PhonyAssociationRegistry:
+# class PhonyAssociationRegistry:
 
-    def get_all(self, key):
-        return real_association_reg.get_all(key)
+#     def get_all(self, key):
+#         return real_association_reg.get_all(key)
 
-    def __getitem__(self, key):
-        return htypes.ui.command_properties(
-            is_global=False,
-            uses_state=False,
-            remotable=False,
-            )
-
-
-@mark.service
-def association_reg():
-    return PhonyAssociationRegistry()
+#     def __getitem__(self, key):
+#         return htypes.ui.command_properties(
+#             is_global=False,
+#             uses_state=False,
+#             remotable=False,
+#             )
 
 
-def test_model_command_ctr():
-    piece = htypes.ui.model_command_impl(
-        function=fn_to_ref(_sample_fn),
-        params=(),
-        )
-    ctx = Context()
-    command = model_command.model_command_impl_from_piece(piece, ctx)
-    assert isinstance(command, model_command.ModelCommandImpl)
+# @mark.service
+# def association_reg():
+#     return PhonyAssociationRegistry()
+
+
+# def test_model_command_ctr():
+#     piece = htypes.ui.model_command_impl(
+#         function=fn_to_ref(_sample_fn),
+#         params=(),
+#         )
+#     ctx = Context()
+#     command = model_command.model_command_impl_from_piece(piece, ctx)
+#     assert isinstance(command, model_command.ModelCommandImpl)
