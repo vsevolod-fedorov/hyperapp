fb1d78a7ad62718fbc0f3655875f40003df72e8c
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-10-04 12:00:15 +0300

    RC: Model marker: Create model ctr

--- hyperapp/rc/model_marker.dyn.py
+++ hyperapp/rc/model_marker.dyn.py
@@ -1,10 +1,18 @@
-from .services import deduce_t
+from hyperapp.common.htypes import TList
+from hyperapp.common.htypes.deduce_value_type import DeduceTypeError
+
+from . import htypes
+from .services import (
+    deduce_t,
+    pyobj_creg,
+    )
 from .code.marker_utils import (
     check_is_function,
     check_not_classmethod,
     is_cls_arg,
     fn_params,
     )
+from .code.model_ctr import ModelCtr
 
 
 class ModelProbe:
@@ -48,17 +56,49 @@ class ModelProbe:
             try:
                 model = param_values['piece']
             except KeyError:
-                raise RuntimeError(f"'model' or 'piece' argument is expected for model function: {list(param_values)}")
+                self._raise_error(f"'model' or 'piece' argument is expected for model function: {list(param_values)}")
         model_t = deduce_t(model)
-        self._add_constructor(model_t, ctx_params, service_params)
         service_kw = {
             name: self._system.resolve_service(name)
             for name in service_params
             }
-        return self._fn(*args, **kw, **service_kw)
+        result = self._fn(*args, **kw, **service_kw)
+        self._add_constructor(ctx_params, service_params, param_values, model_t, result)
+        return result
 
-    def _add_constructor(self, model_t, ctx_params, service_params):
-        assert 0, (model_t, ctx_params, service_params)
+    def _add_constructor(self, ctx_params, service_params, param_values, model_t, result):
+        result_t = self._deduce_t(result, f"{self._fn}: Returned not a deducible data type: {result!r}")
+        try:
+            parent = param_values['parent']
+        except KeyError:
+            assert 0, (result_t, model_t, ctx_params, service_params)  # not a tree model, TODO.
+        if not isinstance(result_t, TList):
+            self._raise_error(f"Tree model should return an item list: {result!r}")
+        if parent is not None:
+            parent_t = self._deduce_t(parent, f"{self._fn}: 'parent' parameter is not a deducible data type: {parent!r}")
+            if parent_t is not result_t.element_t:
+                self._raise_error(f"Parent type should match result list element type: parent: {parent_t}, result element: {result_t.element_t}")
+        ui_t = htypes.model.tree_ui_t(
+            element_t=pyobj_creg.actor_to_ref(result_t.element_t),
+            )
+        ctr = ModelCtr(
+            module_name=self._module_name,
+            attr_qual_name=self._fn.__qualname__.split('.'),
+            model_t=model_t,
+            ui_t=ui_t,
+            ctx_params=ctx_params,
+            service_params=service_params,
+            )
+        self._ctr_collector.add_constructor(ctr)
+
+    def _deduce_t(self, value, error_msg):
+        try:
+            return deduce_t(value)
+        except DeduceTypeError:
+            self._raise_error(error_msg)
+
+    def _raise_error(self, error_msg):
+        raise RuntimeError(f"{self._fn}: {error_msg}")
 
 
 def model_marker(fn, module_name, system, ctr_collector):
