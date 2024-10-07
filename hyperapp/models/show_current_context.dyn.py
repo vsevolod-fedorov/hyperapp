7ad91dace844559254a06c5025e39834583749ea
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-10-07 10:56:34 +0300

    Update show_current_context module

--- hyperapp/models/show_current_context.dyn.py
+++ hyperapp/models/show_current_context.dyn.py
@@ -1,6 +1,8 @@
 from . import htypes
+from .code.mark import mark
 
 
+@mark.model
 def current_context_list(piece, ctx):
     item_list = []
     for name, value in ctx.as_dict().items():
@@ -12,5 +14,6 @@ def current_context_list(piece, ctx):
     return item_list
 
 
+@mark.global_command
 def show_current_context(piece):
     return htypes.show_current_context.view()
