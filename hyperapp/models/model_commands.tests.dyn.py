2901202137c11fc05cf5572b4cbe4319dfc70d2d
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-10-12 11:16:42 +0300

    Model commands: Do not show global commands

--- hyperapp/models/model_commands.tests.dyn.py
+++ hyperapp/models/model_commands.tests.dyn.py
@@ -23,47 +23,44 @@ def test_open_model_commands():
     assert piece_2
 
 
-def _sample_fn_1(model, state, sample_service):
-    return f'sample-fn-1: {state}, {sample_service}'
+def _sample_fn_1(model, sample_service):
+    return f'sample-fn-1: {sample_service}'
 
 
 def _sample_fn_2(model, sample_service):
     return f'sample-fn-2: {sample_service}'
 
 
-@mark.config_fixture('global_model_command_reg')
-def global_model_command_reg_config(partial_ref):
-    system_fn = ContextFn(
+@mark.config_fixture('model_command_reg')
+def model_command_reg_config(partial_ref):
+    system_fn_1 = ContextFn(
         partial_ref=partial_ref, 
-        ctx_params=('view', 'state'),
+        ctx_params=('model',),
         service_params=('sample_service',),
         unbound_fn=_sample_fn_1,
         bound_fn=partial(_sample_fn_1, sample_service='a-service'),
         )
-    command = UnboundModelCommand(
+    command_1 = UnboundModelCommand(
         d=htypes.ui_model_command_tests.sample_command_1_d(),
-        ctx_fn=system_fn,
+        ctx_fn=system_fn_1,
         properties=htypes.command.properties(False, False, False),
         )
-    return [command]
-
-
-@mark.config_fixture('model_command_reg')
-def model_command_reg_config(partial_ref):
-    system_fn = ContextFn(
+    system_fn_2 = ContextFn(
         partial_ref=partial_ref, 
         ctx_params=('model',),
         service_params=('sample_service',),
         unbound_fn=_sample_fn_2,
         bound_fn=partial(_sample_fn_2, sample_service='a-service'),
         )
-    command = UnboundModelCommand(
+    command_2 = UnboundModelCommand(
         d=htypes.ui_model_command_tests.sample_command_2_d(),
-        ctx_fn=system_fn,
+        ctx_fn=system_fn_2,
         properties=htypes.command.properties(False, False, False),
         )
-    model_t = htypes.model_commands_tests.sample_model_1
-    return {model_t: [command]}
+    return {
+        htypes.model_commands_tests.sample_model_1: [command_1],
+        htypes.model_commands_tests.sample_model_2: [command_2],
+        }
 
 
 @mark.fixture
@@ -75,7 +72,7 @@ def lcs():
 
 @mark.fixture
 def piece():
-    model = htypes.model_commands_tests.sample_model_1()
+    model = htypes.model_commands_tests.sample_model_2()
     model_state = htypes.model_commands_tests.sample_model_state()
     return htypes.model_commands.model_commands(
         model=mosaic.put(model),
@@ -86,8 +83,7 @@ def piece():
 def test_list_model_commands(lcs, piece):
     ctx = Context()
     item_list = model_commands.list_model_commands(piece, ctx, lcs)
-    assert len(item_list) == 2
-    assert sorted(item.name for item in item_list) == ['sample_command_1', 'sample_command_2']
+    assert sorted(item.name for item in item_list) == ['sample_command_2']
 
 
 async def test_run_command(data_to_ref, lcs, piece):
@@ -95,6 +91,7 @@ async def test_run_command(data_to_ref, lcs, piece):
     current_item = htypes.model_commands.item(
         command_d=data_to_ref(htypes.ui_model_command_tests.sample_command_2_d()),
         name="<unused>",
+        groups="<unused>",
         repr="<unused>",
         )
     ctx = Context(
