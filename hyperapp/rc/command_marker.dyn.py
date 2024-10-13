ec3e0058a46cac67f88eabdfcf1f07f1f57929ea
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-10-13 12:37:18 +0300

    RC: Add support for multiple markers for commands

--- hyperapp/rc/command_marker.dyn.py
+++ hyperapp/rc/command_marker.dyn.py
@@ -14,6 +14,14 @@ from .code.marker_utils import (
     )
 
 
+def real_fn(fn):
+    if isinstance(fn, CommandProbe):
+        # Multiple wrappers.
+        return fn.real_fn
+    else:
+        return fn
+
+
 class CommandProbe:
 
     def __init__(self, system_probe, ctr_collector, data_to_res, module_name, service_name, fn, t=None):
@@ -31,14 +39,23 @@ class CommandProbe:
         self._ctr_collector = system_probe.resolve_service('ctr_collector')
         self._data_to_res = system_probe.resolve_service('data_to_res')
 
+    @property
+    def real_fn(self):
+        return real_fn(self._fn)
+
     def __call__(self, *args, **kw):
-        params = split_params(self._fn, args, kw)
-        self._add_constructor(params)
+        params = split_params(self.real_fn, args, kw)
+        self.add_constructor(params)
         service_kw = {
             name: self._system.resolve_service(name)
             for name in params.service_names
             }
-        return self._fn(*args, **kw, **service_kw)
+        return self.real_fn(*args, **kw, **service_kw)
+
+    def add_constructor(self, params):
+        self._add_constructor(params)
+        if isinstance(self._fn, CommandProbe):  # Multiple wrappers?
+            self._fn.add_constructor(params)
 
     def _deduce_piece_t(self, params, name_list):
         for name in name_list:
@@ -56,25 +73,22 @@ class CommandProbe:
         return dict(
             data_to_res=self._data_to_res,
             module_name=self._module_name,
-            attr_qual_name=self._fn.__qualname__.split('.'),
+            attr_qual_name=self.real_fn.__qualname__.split('.'),
             service_name=self._service_name,
             ctx_params=params.ctx_names,
             service_params=params.service_names,
             )
 
     def _raise_error(self, error_msg):
-        raise RuntimeError(f"{self._fn}: {error_msg}")
+        raise RuntimeError(f"{self.real_fn}: {error_msg}")
 
 
 class UiCommandProbe(CommandProbe):
 
     def _add_constructor(self, params):
-        if self._t:
-            t = self._t
-        else:
-            t = self._deduce_piece_t(params, ['piece'])
+        # Only type specialized markers are allowed.
         ctr = UiCommandTemplateCtr(
-            t=t,
+            t=self._t,
             **self._common_ctr_kw(params),
             )
         self._ctr_collector.add_constructor(ctr)
@@ -115,7 +129,7 @@ class GlobalModelCommandProbe(CommandProbe):
         ctr = GlobalModelCommandTemplateCtr(
             self._data_to_res,
             module_name=self._module_name,
-            attr_qual_name=self._fn.__qualname__.split('.'),
+            attr_qual_name=self.real_fn.__qualname__.split('.'),
             service_name=self._service_name,
             ctx_params=params.ctx_names,
             service_params=params.service_names,
@@ -135,7 +149,7 @@ class CommandWrapper:
 
     def __call__(self, fn):
         check_not_classmethod(fn)
-        check_is_function(fn)
+        check_is_function(real_fn(fn))
         return self._probe_class(self._system, self._ctr_collector, self._data_to_res, self._module_name, self._service_name, fn, self._t)
 
 
@@ -172,8 +186,7 @@ def model_command_marker(fn_or_t, module_name, system, ctr_collector, data_to_re
         return ModelCommandWrapper(system, ctr_collector, data_to_res, module_name, service_name, t=fn_or_t)
     else:
         # Not type-specialized variant  (@mark.command).
-        check_not_classmethod(fn_or_t)
-        check_is_function(fn_or_t)
+        check_is_function(real_fn(fn_or_t))
         return ModelCommandProbe(system, ctr_collector, data_to_res, module_name, service_name, fn=fn_or_t)
 
 
@@ -185,7 +198,7 @@ def model_command_enumerator_marker(fn_or_t, module_name, system, ctr_collector,
     else:
         # Not type-specialized variant  (@mark.command_enum).
         check_not_classmethod(fn_or_t)
-        check_is_function(fn_or_t)
+        check_is_function(real_fn(fn_or_t))
         return ModelCommandEnumeratorProbe(system, ctr_collector, data_to_res, module_name, service_name, fn=fn_or_t)
 
 
@@ -196,5 +209,5 @@ def global_model_command_marker(fn_or_t, module_name, system, ctr_collector, dat
     else:
         # Not type-specialized variant  (@mark.global_command).
         check_not_classmethod(fn_or_t)
-        check_is_function(fn_or_t)
+        check_is_function(real_fn(fn_or_t))
         return GlobalModelCommandProbe(system, ctr_collector, data_to_res, module_name, service_name, fn=fn_or_t)
