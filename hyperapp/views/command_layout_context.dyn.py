3655f38f951d8495df3530004513f8f2c52ad918
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-10-14 12:02:14 +0300

    Command layout context: Implement set_layout

--- hyperapp/views/command_layout_context.dyn.py
+++ hyperapp/views/command_layout_context.dyn.py
@@ -2,12 +2,12 @@ from functools import partial
 
 from . import htypes
 from .services import (
+    deduce_t,
     mosaic,
     pyobj_creg,
     web,
     )
 from .code.mark import mark
-from .code.ui_model_command import change_command
 from .code.context_view import ContextView
 
 
@@ -15,43 +15,41 @@ class CommandLayoutContextView(ContextView):
 
     @classmethod
     @mark.actor.view_creg
-    def from_piece(cls, piece, ctx, data_to_ref, view_creg):
+    def from_piece(cls, piece, ctx, data_to_ref, view_creg, set_ui_model_command):
         base_view = view_creg.invite(piece.base, ctx)
-        model = web.summon_opt(piece.model)
-        command_d = pyobj_creg.invite(piece.ui_command_d)
-        return cls(data_to_ref, base_view, ctx.lcs, model, command_d)
+        if piece.model_t is not None:
+            model_t = pyobj_creg.invite(piece.model_t)
+        else:
+            model_t = None
+        ui_command_d = pyobj_creg.invite(piece.ui_command_d)
+        model_command_d = pyobj_creg.invite(piece.model_command_d)
+        return cls(data_to_ref, set_ui_model_command, base_view, ctx.lcs, model_t, ui_command_d, model_command_d)
 
-    def __init__(self, data_to_ref, base_view, lcs, model, command_d):
+    def __init__(self, data_to_ref, set_ui_model_command, base_view, lcs, model_t, ui_command_d, model_command_d):
         super().__init__(base_view, label="Command layout")
         self._data_to_ref = data_to_ref
+        self._set_ui_model_command = set_ui_model_command
         self._lcs = lcs
-        self._model = model
-        self._command_d = command_d
+        self._model_t = model_t
+        self._ui_command_d = ui_command_d
+        self._model_command_d = model_command_d
 
     @property
     def piece(self):
         return htypes.command_layout_context.view(
             base=mosaic.put(self._base_view.piece),
-            model=mosaic.put_opt(self._model),
-            ui_command_d=self._data_to_ref(self._command_d),
+            model_t=pyobj_creg.actor_to_ref(self._model_t) if self._model_t is not None else None,
+            ui_command_d=self._data_to_ref(self._ui_command_d),
+            model_command_d=self._data_to_ref(self._model_command_d),
             )
 
     def _set_layout(self, layout):
-        if isinstance(self._command_impl_piece, htypes.ui.external_ui_model_command_impl):
-            set_ui_model_command_layout(self._lcs, self._command_d, layout)
-        else:
-            change_command(self._lcs, self._model, self._command_piece.d,
-                           partial(self._command_with_new_layout, layout))
-
-    def _command_with_new_layout(self, layout, command):
-        new_impl = htypes.ui.ui_model_command_impl(
-            model_command_impl=self._command_impl_piece.model_command_impl,
+        command = htypes.command.ui_model_command(
+            ui_command_d=self._data_to_ref(self._ui_command_d),
+            model_command_d=self._data_to_ref(self._model_command_d),
             layout=mosaic.put(layout),
             )
-        return htypes.ui.ui_command(
-            d=self._command_piece.d,
-            impl=mosaic.put(new_impl),
-            )
+        self._set_ui_model_command(self._lcs, self._model_t, command)
 
     def children_context(self, ctx):
         return ctx.clone_with(
@@ -69,14 +67,16 @@ class CommandLayoutContextView(ContextView):
 @mark.command
 def open_command_layout_context(piece, current_item, navigator, ctx, view_creg):
     if isinstance(piece, htypes.model_commands.view):
-        model = piece.model
+        model_t = deduce_t(web.summon(piece.model))
+        model_t_ref = pyobj_creg.actor_to_ref(model_t)
     else:
         assert isinstance(piece, htypes.global_commands.view)
-        model = None
+        model_t_ref = None
     new_view_piece = htypes.command_layout_context.view(
         base=mosaic.put(navigator.view.piece),
-        model=model,
-        ui_command_d=current_item.command_d,
+        model_t=model_t_ref,
+        ui_command_d=current_item.ui_command_d,
+        model_command_d=current_item.model_command_d,
         )
     new_state = htypes.command_layout_context.state(
         base=mosaic.put(navigator.state),
