84a4cee3318c37d8156fc5991e4986e0689ff85a
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-10-16 13:20:24 +0300

    WIP: Update master-details view

--- hyperapp/ui/qt/master_details.tests.dyn.py
+++ hyperapp/ui/qt/master_details.tests.dyn.py
@@ -1,93 +1,113 @@
+from functools import partial
 from unittest.mock import Mock
-
-from PySide6 import QtWidgets
 
 from . import htypes
 from .services import (
-    data_to_res,
-    fn_to_ref,
-    mark,
     mosaic,
     )
+from .code.mark import mark
 from .code.context import Context
+from .code.system_fn import ContextFn
+from .code.model_command import UnboundModelCommand
+from .fixtures import qapp_fixtures
 from .tested.code import master_details
 
 
-def _details_command_impl(piece):
-    return f"Details for: {piece}"
+def _sample_fn(model, sample_service):
+    return f'sample-fn: {sample_service}'
 
 
-def _details_command():
-    d_res = data_to_res(htypes.master_details_tests.sample_details_command_d())
-    impl = htypes.ui.model_command_impl(
-        function=fn_to_ref(_details_command_impl),
-        params=('piece',),
+# @mark.fixture
+# def model_t():
+#     return htypes.master_details_tests.sample_model
+
+
+@mark.fixture
+def command_d():
+    return htypes.master_details_tests.sample_command_d()
+
+
+@mark.config_fixture('global_model_command_reg')
+def global_model_command_reg_config(partial_ref, command_d):
+    system_fn = ContextFn(
+        partial_ref=partial_ref, 
+        ctx_params=('model',),
+        service_params=('sample_service',),
+        unbound_fn=_sample_fn,
+        bound_fn=partial(_sample_fn, sample_service='a-service'),
         )
-    return htypes.ui.model_command(
-        d=mosaic.put(d_res),
-        impl=mosaic.put(impl),
+    command = UnboundModelCommand(
+        d=command_d,
+        ctx_fn=system_fn,
+        properties=htypes.command.properties(False, False, False),
         )
+    return [command]
 
 
-def make_piece():
+@mark.fixture
+def master_piece():
     master_adapter = htypes.str_adapter.static_str_adapter()
-    master = htypes.text.readonly_view(mosaic.put(master_adapter))
-    command = _details_command()
+    return htypes.text.readonly_view(mosaic.put(master_adapter))
+
+
+@mark.fixture
+def piece(data_to_ref, master_piece, command_d):
     return htypes.master_details.view(
-        master_view=mosaic.put(master),
-        details_command=mosaic.put(command),
+        master_view=mosaic.put(master_piece),
+        details_command_d=data_to_ref(command_d),
         direction='LeftToRight',
         master_stretch=1,
         details_stretch=1,
         )
 
 
-def make_state():
-    text_state = htypes.text.state()
+@mark.fixture
+def state():
+    master_state = htypes.text.state()
     return htypes.master_details.state(
-        master_state=mosaic.put(text_state),
+        master_state=mosaic.put(master_state),
         details_state=mosaic.put(htypes.label.state()),
         )
 
 
-def test_master_details():
-    ctx = Context()
-    piece = make_piece()
-    state = make_state()
-    model = "Sample master text"
-    app = QtWidgets.QApplication()
-    try:
-        view = master_details.MasterDetailsView.from_piece(piece, model, ctx)
-        widget = view.construct_widget(state, ctx)
-        assert view.piece
-        state = view.widget_state(widget)
-        assert state
-    finally:
-        app.shutdown()
+@mark.fixture
+def ctx():
+    return Context()
 
 
-@mark.service
-def model_commands():
-    def _factory(model):
-        return [_details_command()]
-    return _factory
+@mark.fixture.obj
+def model():
+    return "Sample string"
 
 
-async def test_wrap_master_details():
-    ctx = Context()
-    piece = make_piece()
-    state = make_state()
-    model = "Sample master text"
-    app = QtWidgets.QApplication()
-    try:
-        view = master_details.MasterDetailsView.from_piece(piece, model, ctx)
-        widget = view.construct_widget(state, ctx)
-        assert view.piece
-        state = view.widget_state(widget)
-        assert state
-        model = "Sample model"
-        hook = Mock()
-        await master_details.wrap_master_details(model, view, hook, ctx)
-        hook.replace_view.assert_called_once()
-    finally:
-        app.shutdown()
+@mark.fixture
+def model_state():
+    return htypes.master_details_tests.sample_model_state()
+
+
+def test_view(qapp, piece, state, model, ctx):
+    view = master_details.MasterDetailsView.from_piece(piece, model, ctx)
+    assert isinstance(view.piece, htypes.master_details.view)
+    widget = view.construct_widget(state, ctx)
+    assert widget
+    state = view.widget_state(widget)
+    assert isinstance(state, htypes.master_details.state)
+
+
+def test_wrap_master_details(qapp, model_view_creg, master_piece, model, model_state, ctx):
+    model_view_creg.update_config({
+        htypes.master_details.view: master_details.MasterDetailsView.from_piece,
+        })
+    lcs = Mock()
+    hook = Mock()
+    view = Mock()
+    view.piece = master_piece
+    master_details.wrap_master_details(model, model_state, view, hook, lcs, ctx)
+    hook.replace_view.assert_called_once()
+
+
+def test_unwrap_master_details(qapp, piece, state, model, ctx):
+    view = master_details.MasterDetailsView.from_piece(piece, model, ctx)
+    hook = Mock()
+    master_details.unwrap_master_details(model, view, state, hook, ctx)
+    hook.replace_view.assert_called_once()
