b4dc81266af5e2bf3b36e2b7de89b808751f894d
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-10-16 13:24:03 +0300

    universal_ui_command_reg: Set proper ctl; Add cfg item

--- hyperapp/ui/command_cfg_item.tests.dyn.py
+++ hyperapp/ui/command_cfg_item.tests.dyn.py
@@ -5,7 +5,7 @@ from .services import (
     )
 from .code.mark import mark
 from .code import model_command as model_command_module
-from .code import ui_command
+from .code import ui_command as ui_command_module
 from .tested.code import command_cfg_item
 
 
@@ -39,6 +39,21 @@ def cfg_item_piece(data_to_ref):
 
 
 @mark.fixture.obj
+def ui_command(data_to_ref):
+    d = htypes.command_cfg_item_tests.sample_command_d()
+    system_fn = htypes.system_fn.ctx_fn(
+        function=pyobj_creg.actor_to_ref(_sample_fn),
+        ctx_params=('view', 'state'),
+        service_params=('sample_service',),
+        )
+    return htypes.command.ui_command(
+        d=data_to_ref(d),
+        properties=htypes.command.properties(False, False, False),
+        system_fn=mosaic.put(system_fn),
+        )
+
+
+@mark.fixture.obj
 def model_command(data_to_ref):
     d = htypes.command_cfg_item_tests.sample_command_d()
     system_fn = htypes.system_fn.ctx_fn(
@@ -58,18 +73,29 @@ def test_construct_typed(cfg_item_piece):
     assert cfg_item.piece == cfg_item_piece
 
 
-def test_construct_untyped(model_command):
+def test_construct_untyped_model_command(model_command):
     cfg_item = command_cfg_item.UntypedCommandCfgItem.from_piece(model_command)
     assert cfg_item.piece == model_command
+
+
+def test_construct_untyped_ui_command(ui_command):
+    cfg_item = command_cfg_item.UntypedCommandCfgItem.from_piece(ui_command)
+    assert cfg_item.piece == ui_command
 
 
 def test_resolve_typed(system, cfg_item_piece):
     cfg_item = command_cfg_item.TypedCommandCfgItem.from_piece(cfg_item_piece)
     command = cfg_item.resolve(system, 'cfg_item_creg')
-    assert isinstance(command, ui_command.UnboundUiCommand)
+    assert isinstance(command, ui_command_module.UnboundUiCommand)
 
 
-def test_resolve_untyped(system, model_command):
+def test_resolve_untyped_model_command(system, model_command):
     cfg_item = command_cfg_item.UntypedCommandCfgItem.from_piece(model_command)
     command = cfg_item.resolve(system, 'cfg_item_creg')
     assert isinstance(command, model_command_module.UnboundModelCommand)
+
+
+def test_resolve_untyped_ui_command(system, ui_command):
+    cfg_item = command_cfg_item.UntypedCommandCfgItem.from_piece(ui_command)
+    command = cfg_item.resolve(system, 'cfg_item_creg')
+    assert isinstance(command, ui_command_module.UnboundUiCommand)
