dc458437ab6c5eb3495bc7f81e75b7b72f2dbb1f
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-10-21 13:22:41 +0300

    RC: Convert actor probe into config item

--- hyperapp/rc/actor_marker.dyn.py
+++ hyperapp/rc/actor_marker.dyn.py
@@ -1,63 +1,13 @@
 from hyperapp.common.htypes import Type
 
-from .services import deduce_t
-from .code.actor_ctr import ActorProbeCtr, ActorTemplateCtr
+from .code.actor_probe import ActorProbe
+from .code.actor_ctr import ActorProbeCtr
 from .code.marker_utils import (
     check_is_function,
     check_not_classmethod,
     is_cls_arg,
     fn_params,
     )
-
-
-class ServiceActorProbe:
-
-    def __init__(self, system_probe, ctr_collector, module_name, service_name, fn, t=None):
-        self._system = system_probe
-        self._ctr_collector = ctr_collector
-        self._module_name = module_name
-        self._service_name = service_name
-        self._fn = fn
-        self._t = t
-        system_probe.add_global(self)
-
-    def migrate_to(self, system_probe):
-        self._system = system_probe
-        self._ctr_collector = system_probe.resolve_service('ctr_collector')
-
-    def __call__(self, *args, **kw):
-        params = fn_params(self._fn)
-        piece_param_ofs = 0
-        if args and is_cls_arg(self._fn, args[0]):
-            # self._fn is a classmethod and args[0] is a 'cls' argument.
-            piece_param_ofs = 1
-        if len(args) < piece_param_ofs + 1:
-            raise RuntimeError(f"First parameter expected to be a piece: {self._fn!r}")
-        piece = args[piece_param_ofs]
-        creg_param_count = len(args) - piece_param_ofs - 1 + len(kw)
-        creg_params = params[piece_param_ofs + 1:creg_param_count + piece_param_ofs + 1]
-        service_params = params[creg_param_count + piece_param_ofs + 1:]
-        if self._t is None:
-            t = deduce_t(piece)
-        else:
-            t = self._t
-        self._add_constructor(t, creg_params, service_params)
-        service_kw = {
-            name: self._system.resolve_service(name)
-            for name in service_params
-            }
-        return self._fn(*args, **kw, **service_kw)
-
-    def _add_constructor(self, t, creg_params, service_params):
-        ctr = ActorTemplateCtr(
-            module_name=self._module_name,
-            attr_qual_name=self._fn.__qualname__.split('.'),
-            service_name=self._service_name,
-            t=t,
-            creg_params=creg_params,
-            service_params=service_params,
-            )
-        self._ctr_collector.add_constructor(ctr)
 
 
 class ServiceActorDecorator:
@@ -73,15 +23,13 @@ class ServiceActorDecorator:
         qual_name = fn.__qualname__.split('.')
         check_not_classmethod(fn)
         check_is_function(fn)
-        params = fn_params(fn)
         ctr = ActorProbeCtr(
             attr_qual_name=qual_name,
             service_name=self._service_name,
             t=self._t,
-            params=params,
             )
         self._ctr_collector.add_constructor(ctr)
-        return ServiceActorProbe(self._system, self._ctr_collector, self._module_name, self._service_name, fn, self._t)
+        return ActorProbe(self._system, self._ctr_collector, self._module_name, self._service_name, fn, self._t)
 
 
 class ServiceActorMarker:
@@ -103,7 +51,7 @@ class ServiceActorMarker:
         check_not_classmethod(fn_or_t)
         check_is_function(fn_or_t)
         # Not type-specialized variant  (@mark.actor.my_registry).
-        return ServiceActorProbe(self._system, self._ctr_collector, self._module_name, self._service_name, fn_or_t)
+        return ActorProbe(self._system, self._ctr_collector, self._module_name, self._service_name, fn_or_t)
 
 
 class ActorMarker:
---
+++ hyperapp/rc/actor_probe.dyn.py
@@ -0,0 +1,89 @@
+from .services import (
+    deduce_t,
+    pyobj_creg,
+    web,
+    )
+from .code.actor_ctr import ActorTemplateCtr
+from .code.marker_utils import (
+    is_cls_arg,
+    fn_params,
+    )
+
+
+class ActorProbe:
+
+    def __init__(self, system_probe, ctr_collector, module_name, service_name, fn, t=None):
+        self._system = system_probe
+        self._ctr_collector = ctr_collector
+        self._module_name = module_name
+        self._service_name = service_name
+        self._fn = fn
+        self._t = t
+        system_probe.add_global(self)
+
+    def migrate_to(self, system_probe):
+        self._system = system_probe
+        self._ctr_collector = system_probe.resolve_service('ctr_collector')
+
+    def __call__(self, *args, **kw):
+        params = fn_params(self._fn)
+        piece_param_ofs = 0
+        if args and is_cls_arg(self._fn, args[0]):
+            # self._fn is a classmethod and args[0] is a 'cls' argument.
+            piece_param_ofs = 1
+        if len(args) < piece_param_ofs + 1:
+            raise RuntimeError(f"First parameter expected to be a piece: {self._fn!r}")
+        piece = args[piece_param_ofs]
+        creg_param_count = len(args) - piece_param_ofs - 1 + len(kw)
+        creg_params = params[piece_param_ofs + 1:creg_param_count + piece_param_ofs + 1]
+        service_params = params[creg_param_count + piece_param_ofs + 1:]
+        if self._t is None:
+            t = deduce_t(piece)
+        else:
+            t = self._t
+        self._add_constructor(t, creg_params, service_params)
+        service_kw = {
+            name: self._system.resolve_service(name)
+            for name in service_params
+            }
+        return self._fn(*args, **kw, **service_kw)
+
+    def _add_constructor(self, t, creg_params, service_params):
+        ctr = ActorTemplateCtr(
+            module_name=self._module_name,
+            attr_qual_name=self._fn.__qualname__.split('.'),
+            service_name=self._service_name,
+            t=t,
+            creg_params=creg_params,
+            service_params=service_params,
+            )
+        self._ctr_collector.add_constructor(ctr)
+
+
+class ActorProbeTemplate:
+
+    @classmethod
+    def from_piece(cls, piece):
+        return cls(
+            t=pyobj_creg.invite(piece.t),
+            fn_piece=web.summon(piece.function),
+            )
+
+    def __init__(self, t, fn_piece):
+        self._t = t
+        self._fn = fn_piece
+
+    def __repr__(self):
+        return f"<ActorProbeTemplate {self._t}: {self._fn}>"
+
+    @property
+    def key(self):
+        return self._t
+
+    def resolve(self, system, service_name):
+        fn = pyobj_creg.animate(self._fn)
+        assert (
+            isinstance(fn, ActorProbe)
+            or hasattr(fn, '__self__') and isinstance(fn.__func__, ActorProbe)
+            ) , repr(fn)
+        return fn
