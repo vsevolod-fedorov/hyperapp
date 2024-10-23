9aa4d41aff22d175d3fdbb420547c0fae28191f0
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-10-23 11:23:02 +0300

    Improve error message for ConfigItemMissingError

--- hyperapp/common/config_item_missing.py
+++ hyperapp/common/config_item_missing.py
@@ -1,6 +1,6 @@
 class ConfigItemMissingError(Exception):
 
-    def __init__(self, service_name, key):
-        super().__init__(f"Configuration item is missing for {service_name}: {key}")
+    def __init__(self, service_name, key, message=None):
+        super().__init__(message or f"Configuration item is missing for {service_name}: {key}")
         self.service_name = service_name
         self.key = key
