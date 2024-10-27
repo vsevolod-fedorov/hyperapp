b1822e1ca792f533c432df10a1da834c4ab57230
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-10-27 11:42:44 +0300

    RC: Remove ServiceCtr class; Remove rc_constructors type module

--- hyperapp/rc/service_ctr.dyn.py
+++ hyperapp/rc/service_ctr.dyn.py
@@ -7,43 +7,6 @@ from .services import (
 from .code.rc_constructor import Constructor, ModuleCtr
 
 
-class ServiceCtr(Constructor):
-
-    @classmethod
-    def from_piece(cls, piece):
-        return cls(piece.attr_name, piece.name)
-
-    def __init__(self, attr_name, name):
-        self._attr_name = attr_name
-        self._name = name
-
-    def update_resource_targets(self, resource_tgt, target_set):
-        service_found_tgt = target_set.factory.service_found(self._name)
-        service_found_tgt.set_provider(resource_tgt, self, target_set)
-        resolved_tgt = target_set.factory.service_resolved(self._name)
-        resource_tgt.add_cfg_item_target(resolved_tgt)
-        resolved_tgt.resolve(self)
-        target_set.update_deps_for(service_found_tgt)
-        target_set.update_deps_for(resolved_tgt)
-        target_set.update_deps_for(resource_tgt)
-
-    def make_component(self, types, python_module, name_to_res=None):
-        attribute = htypes.builtin.attribute(
-            object=mosaic.put(python_module),
-            attr_name=self._attr_name,
-            )
-        service = htypes.builtin.call(
-            function=mosaic.put(attribute),
-            )
-        if name_to_res is not None:
-            name_to_res[self._attr_name] = attribute
-            name_to_res[f'{self._name}.service'] = service
-        return service
-
-    def get_component(self, name_to_res):
-        return name_to_res[f'{self._name}.service']
-
-
 class ServiceTemplateCtrBase(Constructor):
 
     def __init__(self, name):
