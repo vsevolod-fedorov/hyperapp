5caced3c2633869093da9f1193611e5a30ffc27f
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-10-27 12:30:08 +0300

    Update server

--- hyperapp/server/server.dyn.py
+++ hyperapp/server/server.dyn.py
@@ -1,30 +1,44 @@
+import argparse
 import logging
 from pathlib import Path
 
 from hyperapp.common import dict_coders
 
 from .services import (
-    endpoint_registry,
-    file_bundle,
-    generate_rsa_identity,
     mosaic,
-    identity_registry,
-    peer_registry,
-    route_table,
-    rpc_call_factory,
-    rpc_endpoint_factory,
-    tcp_server_factory,
     )
+from .code.mark import mark
 from .code.reconstructors import register_reconstructors
 
 log = logging.getLogger(__name__)
 
 
-def _main():
+DEFAULT_IDENTITY_PATH = Path.home() / '.local/share/hyperapp/server/identity.json'
+
+
+def _parse_args(sys_argv):
+    parser = argparse.ArgumentParser(description='Hyperapp server')
+    parser.add_argument('--identity-path', type=Path, default=DEFAULT_IDENTITY_PATH, help="Path to server identity")
+    return parser.parse_args(sys_argv)
+
+
+@mark.service2
+def server_main(
+        stop_signal,
+        route_table,
+        identity_registry,
+        generate_rsa_identity,
+        endpoint_registry,
+        rpc_endpoint,
+        file_bundle,
+        tcp_server_factory,
+        sys_argv,
+        ):
+    args = _parse_args(sys_argv)
 
     register_reconstructors()
 
-    identity_bundle = file_bundle(Path.home() / '.local/share/hyperapp/server/identity.json')
+    identity_bundle = file_bundle(args.identity_path)
     try:
         server_identity = identity_registry.animate(identity_bundle.load_piece())
         log.info("Server identity: loaded from: %s", identity_bundle.path)
@@ -34,7 +48,6 @@ def _main():
         log.info("Server identity: generated and saved to: %s", identity_bundle.path)
 
     server_peer_ref = mosaic.put(server_identity.peer.piece)
-    rpc_endpoint = rpc_endpoint_factory()
     endpoint_registry.register(server_identity, rpc_endpoint)
 
     server = tcp_server_factory()
@@ -51,4 +64,3 @@ def _main():
     except KeyboardInterrupt:
         print()  # Leave '^C' on separate line.
         log.info("Server: Stopping")
-        return 0
--- run_server.sh
+++ run_server.sh
@@ -1,9 +1,11 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
 venv=${VENV:-$HOME/venv/hyperapp}
+
+export LOG_CFG="${LOG_CFG:-server}"
 
 source $venv/bin/activate
 
 cd "$( dirname "$0" )"
 
-$HOME/bin/autoreload.py -f '*.types' -f '*.yaml' -f '*.py'  . ./server.py
+$HOME/bin/autoreload.py -f '*.types' -f '*.yaml' -f '*.py'  . ./boot.py config server_main
