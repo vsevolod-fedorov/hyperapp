045de57a98af5b020cce3685fd4b1dc67927c3d6
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-10-27 13:03:56 +0300

    Update local_server module

--- hyperapp/ui/local_server.dyn.py
+++ hyperapp/ui/local_server.dyn.py
@@ -3,16 +3,19 @@ from pathlib import Path
 
 from hyperapp.common import dict_coders
 
-from .services import (
-    file_bundle,
-    peer_registry,
-    )    
+from .code.mark import mark
 
 log = logging.getLogger(__name__)
 
 
-def local_server_peer():
-    peer_bundle = file_bundle(Path.home() / '.local/share/hyperapp/server/peer.json')
+@mark.service2
+def local_server_peer_path():
+    return Path.home() / '.local/share/hyperapp/server/peer.json'
+
+
+@mark.service2
+def local_server_peer(peer_registry, file_bundle, local_server_peer_path):
+    peer_bundle = file_bundle(local_server_peer_path)
     try:
         server_peer = peer_registry.animate(peer_bundle.load_piece())
         log.info("Server peer: loaded from: %s", peer_bundle.path)
