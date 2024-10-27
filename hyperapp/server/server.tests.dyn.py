4053d2f66ca0b5d9614b81332c6554bc1140b085
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-10-27 12:30:08 +0300

    Update server

---
+++ hyperapp/server/server.tests.dyn.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from unittest.mock import Mock
+
+from .code.mark import mark
+from .tested.code import server
+
+
+@mark.fixture
+def stop_signal():
+    return Mock()
+
+
+def test_server(server_main):
+    identity_path = Path('/tmp/server-test-identity.json')
+    try:
+        identity_path.unlink()
+    except FileNotFoundError:
+        pass
+    sys_argv = [
+        f'--identity-path={identity_path}',
+        ]
+    server_main(sys_argv)
