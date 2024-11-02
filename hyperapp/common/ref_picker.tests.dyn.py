3c5dabf207888f2e6df35f0e3f4737dbddf24597
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-11-02 13:25:36 +0300

    Update ref_picker tests

--- hyperapp/common/ref_picker.tests.dyn.py
+++ hyperapp/common/ref_picker.tests.dyn.py
@@ -6,24 +6,23 @@ from hyperapp.common.htypes import (
     tString,
     ref_t,
     )
-from hyperapp.common.htypes.deduce_value_type import deduce_value_type
 
 from .services import (
+    deduce_t,
     mosaic,
     )
 from .tested.code import ref_picker
-from .tested.services import pick_refs
 
 
 def test_ref():
     ref = mosaic.put("Sample text")
-    picker = ref_picker._t_to_picker(deduce_value_type(ref))
+    picker = ref_picker._type_to_picker(deduce_t(ref))
     assert set(picker.pick_refs(ref)) == {ref}
 
 
 def test_opt():
     t = TOptional(ref_t)
-    picker = ref_picker._t_to_picker(t)
+    picker = ref_picker._type_to_picker(t)
     ref = mosaic.put("Sample text")
     assert set(picker.pick_refs(ref)) == {ref}
     assert set(picker.pick_refs(None)) == set()
@@ -31,7 +30,7 @@ def test_opt():
 
 def test_list():
     t = TList(ref_t)
-    picker = ref_picker._t_to_picker(t)
+    picker = ref_picker._type_to_picker(t)
     ref_1 = mosaic.put("Sample text 1")
     ref_2 = mosaic.put("Sample text 2")
     assert set(picker.pick_refs((ref_1, ref_2))) == {ref_1, ref_2}
@@ -43,7 +42,7 @@ def test_record():
         'some_string': tString,
         'some_ref': ref_t,
         })
-    picker = ref_picker._t_to_picker(t)
+    picker = ref_picker._type_to_picker(t)
     ref = mosaic.put("Sample text")
     record = t("Some string", ref)
     assert set(picker.pick_refs(record)) == {ref}
@@ -53,7 +52,7 @@ def test_one_field_record():
     t = TRecord('ref_picker_tests', 'test_one_field_record', {
         'some_ref': ref_t,
         })
-    picker = ref_picker._t_to_picker(t)
+    picker = ref_picker._type_to_picker(t)
     ref = mosaic.put("Sample text")
     record = t(ref)
     assert set(picker.pick_refs(record)) == {ref}
@@ -64,7 +63,7 @@ def test_exception():
         'some_string': tString,
         'some_ref': ref_t,
         })
-    picker = ref_picker._t_to_picker(t)
+    picker = ref_picker._type_to_picker(t)
     ref = mosaic.put("Sample text")
     record = t("Some string", ref)
     assert set(picker.pick_refs(record)) == {ref}
@@ -77,7 +76,7 @@ def test_combined():
         'some_opt': opt_t,
         'some_list': list_t,
         })
-    picker = ref_picker._t_to_picker(t)
+    picker = ref_picker._type_to_picker(t)
     ref_1 = mosaic.put("Sample text 1")
     ref_2 = mosaic.put("Sample text 2")
     ref_3 = mosaic.put("Sample text 3")
@@ -85,7 +84,7 @@ def test_combined():
     assert set(picker.pick_refs(t(ref_1, (ref_2, ref_3)))) == {ref_1, ref_2, ref_3}
 
 
-def test_service():
+def test_service(pick_refs):
     opt_t = TOptional(ref_t)
     list_t = TList(ref_t)
     t = TRecord('ref_picker_tests', 'test_service_record', {
