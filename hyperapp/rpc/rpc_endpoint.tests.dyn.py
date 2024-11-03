2f5efe11d14abcbe8dabd476165136fb3ff09187
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-11-03 10:45:43 +0300

    Update rpc endpoint tests

--- hyperapp/rpc/rpc_endpoint.tests.dyn.py
+++ hyperapp/rpc/rpc_endpoint.tests.dyn.py
@@ -1,83 +1,67 @@
+import logging
+from unittest.mock import Mock
+
 from . import htypes
 from .services import (
-    generate_rsa_identity,
-    mark,
     mosaic,
     pyobj_creg,
     )
+from .code.mark import mark
 from .code.endpoint import Request
-# from .tested.code import rpc_endpoint
-from .tested.services import rpc_endpoint_factory
+from .tested.code import rpc_endpoint
+
+log = logging.getLogger(__name__)
 
 
-class PhonyTransport:
-
-    def send(self, receiver, sender_identity, ref_list):
-        pass
-
-
-@mark.service
+@mark.fixture
 def transport():
-    return PhonyTransport()
+    return Mock()
 
 
-class PhonyPythonObjectCReg:
-
-    def __init__(self):
-        self._phony_servant = None
-
-    def set_phony_servant(self, fn):
-        self._phony_servant = fn
-
-    def invite(self, servant_ref):
-        return self._phony_servant
-
-
-@mark.service
-def pyobj_creg():
-    return PhonyPythonObjectCReg()
-
-
-def _return_str_list(phony_param):
-    return [
-        mosaic.put("Sample result string"),
-        ]
-
-
-def _raise_happ_error(phony_param):
-    raise htypes.rpc_endpoint_tests.sample_error("sample error message")
-
-
-def _raise_non_happ_error(phony_param):
-    raise RuntimeError("Sample non-happ error")
-
-
-def _run_test_with_servant(servant_fn):
-    # rpc_endpoint.pyobj_creg.set_phony_servant(servant_fn)
+@mark.fixture
+def _test_with(transport, rpc_endpoint, generate_rsa_identity, servant_fn):
     sender_identity = generate_rsa_identity(fast=True)
+    target = htypes.rpc.function_target(
+        servant_ref=pyobj_creg.actor_to_ref(servant_fn),
+        params=(htypes.rpc.param('sample_param', mosaic.put("Sample param value")),),
+        )
     rpc_request = htypes.rpc.request(
-        request_id='phony request id',
-        servant_ref=mosaic.put('phony servant'),
-        params=(
-            htypes.rpc.param('phony_param', mosaic.put('phony param')),
-            ),
+        request_id='Phony request id',
+        target=mosaic.put(target),
         )
     request = Request(
         receiver_identity=None,
         sender=None,
         ref_list=[mosaic.put(rpc_request)],
         )
-    endpoint = rpc_endpoint_factory()
-    endpoint.process(request)
+    rpc_endpoint.process(request)
+    transport.send.assert_called_once()
 
 
-def test_str_list_response():
-    _run_test_with_servant(_return_str_list)
+def _return_str_list(sample_param):
+    log.info("_return_str_list: %s", sample_param)
+    return [
+        mosaic.put("Sample result string"),
+        ]
 
 
-def test_happ_error():
-    _run_test_with_servant(_raise_happ_error)
+def test_str_list_response(_test_with):
+    _test_with(_return_str_list)
 
 
-def test_non_happ_error():
-    _run_test_with_servant(_raise_non_happ_error)
+def _raise_happ_error(sample_param):
+    log.info("_raise_happ_error: %s", sample_param)
+    raise htypes.rpc_endpoint_tests.sample_error("sample error message")
+
+
+def test_happ_error(_test_with):
+    _test_with(_raise_happ_error)
+
+
+def _raise_non_happ_error(sample_param):
+    log.info("_raise_non_happ_error: %s", sample_param)
+    raise RuntimeError("Sample non-happ error")
+
+
+def test_non_happ_error(_test_with):
+    _test_with(_raise_non_happ_error)
