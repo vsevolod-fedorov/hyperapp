943e2f53b0bee610f4472ea20e6d3b8ab20f5279
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-11-09 12:31:36 +0300

    Subprocess test: Move main fn from aux to test module

--- hyperapp/subprocess/subprocess.tests.dyn.py
+++ hyperapp/subprocess/subprocess.tests.dyn.py
@@ -1,12 +1,23 @@
 import logging
 
-from .code.subprocess_tests_aux import process_main
 from .tested.code import subprocess
 
 log = logging.getLogger(__name__)
 
 
+def _process_main(connection, received_refs, name):
+    my_name = "Subprocess test main"
+    log.info("%s: Started", my_name)
+    try:
+        value = connection.recv()
+        log.info("%s: Received: %s", my_name, value)
+    except EOFError as x:
+        log.info("%s: Connection EOF: %s", my_name, x)
+    except ConnectionResetError as x:
+        log.info("%s: Connection reset: %s", my_name, x)
+
+
 def test_subprocess(partial_ref, subprocess_running):
-    main_ref = partial_ref(process_main, name='test-subprocess-main')
+    main_ref = partial_ref(_process_main, name='test-subprocess-main')
     with subprocess_running('test-subprocess', main_ref) as process:
         log.info("Started %r", process)
