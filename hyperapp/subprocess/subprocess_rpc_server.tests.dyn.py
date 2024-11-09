9827e18b048f65f77a50843533c8d0555a45a29f
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-11-09 12:43:38 +0300

    Subprocess_rpc_server tests: Add test for service call

--- hyperapp/subprocess/subprocess_rpc_server.tests.dyn.py
+++ hyperapp/subprocess/subprocess_rpc_server.tests.dyn.py
@@ -1,6 +1,7 @@
 import logging
 import time
 
+from .code.mark import mark
 from .tested.code import subprocess_rpc_server
 
 log = logging.getLogger(__name__)
@@ -8,6 +9,12 @@ log = logging.getLogger(__name__)
 
 def _callback():
     log.info("Test rpc subprocess callback is called")
+    return 'ok'
+
+
+@mark.fixture
+def _sample_subprocess_rpc_server_tests_service():
+    log.info("Test rpc subprocess sample service is called")
     return 'ok'
 
 
@@ -35,5 +42,19 @@ def test_subprocess_rpc_server_submit(
         submit = process.rpc_submit(_callback)
         future = submit()
         assert future.result() == 'ok'
+        log.info("Stopping: %r", process)
+    log.info("Stopped: %r", process)
+
+
+def test_subprocess_rpc_server_service(
+        generate_rsa_identity, endpoint_registry, rpc_endpoint, subprocess_rpc_server_running):
+    name = 'test-subprocess-rpc-server-main'
+    identity = generate_rsa_identity(fast=True)
+    endpoint_registry.register(identity, rpc_endpoint)
+    with subprocess_rpc_server_running(name, identity) as process:
+        log.info("Started: %r", process)
+        call = process.service_call('_sample_subprocess_rpc_server_tests_service')
+        result = call()
+        assert result == 'ok'
         log.info("Stopping: %r", process)
     log.info("Stopped: %r", process)
