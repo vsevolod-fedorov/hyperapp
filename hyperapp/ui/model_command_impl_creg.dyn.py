8888449624a8c9249e1f5996bc68bfc6adefa14e
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-11-09 13:04:31 +0300

    Cleanup: Remove model_command_impl_creg

--- hyperapp/ui/model_command_impl_creg.dyn.py
+++
@@ -1,9 +0,0 @@
-from .services import (
-    code_registry_ctr,
-    )
-from .code.mark import mark
-
-
-@mark.service2
-def model_command_impl_creg():
-    return code_registry_ctr('model_command_impl')
