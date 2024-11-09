dafeb39ed9ff03a9baef65233fc8b2118a5d33ba
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-11-09 12:39:43 +0300

    Update subprocess_rpc_server tests

--- hyperapp/subprocess/subprocess_rpc_server.tests.dyn.py
+++ hyperapp/subprocess/subprocess_rpc_server.tests.dyn.py
@@ -2,17 +2,21 @@ import logging
 import time
 
 from .tested.code import subprocess_rpc_server
-from .code.subprocess_rpc_server_tests_aux import _callback
 
 log = logging.getLogger(__name__)
 
 
-def _test_subprocess_rpc_server_call():
+def _callback():
+    log.info("Test rpc subprocess callback is called")
+    return 'ok'
+
+
+def test_subprocess_rpc_server_call(
+        generate_rsa_identity, endpoint_registry, rpc_endpoint, subprocess_rpc_server_running):
     name = 'test-subprocess-rpc-server-main'
     identity = generate_rsa_identity(fast=True)
-    rpc_endpoint = rpc_endpoint_factory()
     endpoint_registry.register(identity, rpc_endpoint)
-    with subprocess_rpc_server_running(name, rpc_endpoint, identity) as process:
+    with subprocess_rpc_server_running(name, identity) as process:
         log.info("Started: %r", process)
         call = process.rpc_call(_callback)
         result = call()
@@ -21,12 +25,12 @@ def _test_subprocess_rpc_server_call():
     log.info("Stopped: %r", process)
 
 
-def _test_subprocess_rpc_server_submit():
+def test_subprocess_rpc_server_submit(
+        generate_rsa_identity, endpoint_registry, rpc_endpoint, subprocess_rpc_server_running):
     name = 'test-subprocess-rpc-server-main'
     identity = generate_rsa_identity(fast=True)
-    rpc_endpoint = rpc_endpoint_factory()
     endpoint_registry.register(identity, rpc_endpoint)
-    with subprocess_rpc_server_running(name, rpc_endpoint, identity) as process:
+    with subprocess_rpc_server_running(name, identity) as process:
         log.info("Started: %r", process)
         submit = process.rpc_submit(_callback)
         future = submit()
--- hyperapp/subprocess/subprocess_rpc_server_tests_aux.dyn.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-log = logging.getLogger(__name__)
-
-
-def _callback():
-    log.info("Test rpc subprocess callback is called")
-    return 'ok'
