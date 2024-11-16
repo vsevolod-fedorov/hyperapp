b0aabf797d1f19e3a1cb727584cca0bcea179c59
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-11-16 11:54:37 +0300

    RC: Add TypeReq

---
+++ hyperapp/rc/type_req.dyn.py
@@ -0,0 +1,42 @@
+from . import htypes
+from .code.rc_requirement import Requirement
+
+
+class TypeReq(Requirement):
+
+    @classmethod
+    def from_type_src(cls, type_src):
+        return cls(type_src.module_name, type_src.name)
+
+    @classmethod
+    def from_piece(cls, piece):
+        return cls(
+            module_name=piece.module_name,
+            name=piece.name,
+            )
+
+    def __init__(self, module_name, name):
+        self._module_name = module_name
+        self._name = name
+
+    def __str__(self):
+        return f"TypeReq({self._module_name}.{self._name})"
+
+    def __eq__(self, rhs):
+        return (type(rhs) == TypeReq
+                and rhs._module_name == self._module_name
+                and rhs._name == self._name)
+
+    def __hash__(self):
+        return hash(('type_req', self._module_name, self._name))
+
+    @property
+    def piece(self):
+        return htypes.type_resource.type_req(
+            module_name=self._module_name,
+            name=self._name,
+            )
+
+    @property
+    def desc(self):
+        return f"{self._module_name}.{self._name} type"
