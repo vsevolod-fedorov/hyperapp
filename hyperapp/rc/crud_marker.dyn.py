47dfb371ae88aa9378c48a79fc85043e3ddd8e18
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-11-19 12:52:28 +0300

    RC: Crud marker: Add probe

--- hyperapp/rc/crud_marker.dyn.py
+++ hyperapp/rc/crud_marker.dyn.py
@@ -5,12 +5,41 @@ from .code.marker_utils import (
     )
 
 
+class CrudProbe:
+
+    def __init__(self, system_probe, ctr_collector, module_name, action, fn):
+        self._system = system_probe
+        self._ctr_collector = ctr_collector
+        self._module_name = module_name
+        self._action = action
+        self._fn = fn
+        system_probe.add_global(self)
+
+    def migrate_to(self, system_probe):
+        self._system = system_probe
+        self._ctr_collector = system_probe.resolve_service('ctr_collector')
+
+    def __call__(self, *args, **kw):
+        params = split_params(self._fn, args, kw)
+        service_kw = {
+            name: self._system.resolve_service(name)
+            for name in params.service_names
+            }
+        return self._fn(*args, **kw, **service_kw)
+
+
 class CrudDecorator:
+
+    def __init__(self, system_probe, ctr_collector, module_name, action):
+        self._system = system_probe
+        self._ctr_collector = ctr_collector
+        self._module_name = module_name
+        self._action = action
 
     def __call__(self, fn):
         check_not_classmethod(fn)
         check_is_function(fn)
-        return fn
+        return CrudProbe(self._system, self._ctr_collector, self._module_name, self._action, fn)
 
 
 class CrudMarker:
@@ -21,4 +50,4 @@ class CrudMarker:
         self._ctr_collector = ctr_collector
 
     def __getattr__(self, action):
-        return CrudDecorator()
+        return CrudDecorator(self._system, self._ctr_collector, self._module_name, action)
