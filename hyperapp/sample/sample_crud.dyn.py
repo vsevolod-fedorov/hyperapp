1c7b911492b26317d74b80be69b2cef90d08cd53
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-11-19 12:37:39 +0300

    Sample CRUD: Add crud functions

--- hyperapp/sample/sample_crud.dyn.py
+++ hyperapp/sample/sample_crud.dyn.py
@@ -1,5 +1,9 @@
+import logging
+
 from . import htypes
 from .code.mark import mark
+
+log = logging.getLogger(__name__)
 
 
 @mark.global_command
@@ -13,3 +17,17 @@ def list_crud_sample(piece):
         htypes.sample_crud.item(idx, f"item#{idx}", "Crud sample item #{idx}")
         for idx in range(10)
         ]
+
+
+@mark.crud.get
+def get_crud_sample(piece, current_item):
+    return htypes.sample_crud.edit_item(
+        id=current_item.id,
+        name=current_item.name,
+        desc=current_item.desc,
+        )
+
+
+@mark.crud.update
+def update_crud_sample(piece, value):
+    log.info("Sample CRUD: Update %s: %s", piece, value)
