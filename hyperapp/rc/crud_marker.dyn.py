c2233c2c73126ff299a27a7d6854ddcf6b1f9226
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-11-20 13:19:26 +0300

    Crud: Pick model ui_t

--- hyperapp/rc/crud_marker.dyn.py
+++ hyperapp/rc/crud_marker.dyn.py
@@ -1,8 +1,16 @@
+from hyperapp.common.htypes import TRecord
+
+from . import htypes
+from .services import (
+    deduce_t,
+    pyobj_creg,
+    )
 from .code.marker_utils import (
     check_is_function,
     check_not_classmethod,
     split_params,
     )
+from .code.crud_ctr import CrudTemplateCtr
 
 
 class CrudProbe:
@@ -25,7 +33,69 @@ class CrudProbe:
             name: self._system.resolve_service(name)
             for name in params.service_names
             }
-        return self._fn(*args, **kw, **service_kw)
+        result = self._fn(*args, **kw, **service_kw)
+        self._add_constructor(params)
+        return result
+
+    def _deduce_piece_t(self, params, name_list):
+        for name in name_list:
+            try:
+                piece = params.values[name]
+                break
+            except KeyError:
+                pass
+        else:
+            names_str = " or ".join(name_list)
+            self._raise_error(f"{names_str} argument is expected for CRUD function: {list(params.values)}")
+        return (name, deduce_t(piece))
+
+    def _pick_model_ui_t(self, model_t):
+        visualizer_reg = self._system['visualizer_reg']
+        ui_t, fn_ref = visualizer_reg(model_t)
+        return ui_t
+
+    @staticmethod
+    def _get_item_t(ui_t):
+        if (isinstance(ui_t, htypes.model.list_ui_t)
+            or isinstance(ui_t, htypes.model.tree_ui_t)):
+            item_t = pyobj_creg.invite(ui_t.item_t)
+        else:
+            raise RuntimeError(f"Not supported model UI type: {ui_t}. Only list and tree are supported")
+        if not isinstance(item_t, TRecord):
+            raise RuntimeError(f"Model item type is expected to be a record: {item_t}")
+        return item_t
+
+    def _pick_key_field(self, item_t, params):
+        fields = []
+        expected_fields = ", ".join(item_t.fields)
+        for name in params.other_names:
+            if name in {'piece', 'model', 'value'}:
+                continue
+            if name not in item_t.fields:
+                raise RuntimeError(f"Unexpected parameter: {name!r}. Expected key field, one of {expected_fields}")
+            fields.append(name)
+        if not fields:
+            raise RuntimeError(f"Expected one key parameter. One of {expected_fields}")
+        if len(fields) > 1:
+            fields_str = ", ".join(fields)
+            raise RuntimeError(f"Only one key parameter is expected, but got: {fields_str}")
+        return fields[0]
+
+    def _add_constructor(self, params):
+        model_field, model_t = self._deduce_piece_t(params, ['piece', 'model'])
+        ui_t = self._pick_model_ui_t(model_t)
+        item_t = self._get_item_t(ui_t)
+        key_field = self._pick_key_field(item_t, params)
+        ctr = CrudTemplateCtr(
+            module_name=self._module_name,
+            attr_qual_name=self._fn.__qualname__.split('.'),
+            model_t=model_t,
+            action=self._action,
+            key_field=key_field,
+            crud_params=params.other_names,
+            service_params=params.service_names,
+            )
+        self._ctr_collector.add_constructor(ctr)
 
 
 class CrudDecorator:
---
+++ hyperapp/sample/sample_list_crud.dyn.py
@@ -0,0 +1,25 @@
+import logging
+
+from . import htypes
+from .code.mark import mark
+from .code.sample_list import sample_list
+
+log = logging.getLogger(__name__)
+
+
+@mark.crud.get
+def sample_list_get(piece, id):
+    item_to_id = {
+        item.id: item
+        for item in sample_list(piece)
+        }
+    item = item_to_id[id]
+    return htypes.sample_list_crud.form_item(
+        id=item.id,
+        title=item.title,
+        )
+
+
+@mark.crud.update
+def sample_list_update(piece, id, value):
+    log.info("Sample CRUD: Update %s #%d: %s", piece, id, value)
