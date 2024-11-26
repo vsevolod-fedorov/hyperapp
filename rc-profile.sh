54afa13542832981acbf1b30bf0bf985f3e784de
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-11-26 13:10:45 +0300

    Update rc-profile.sh; Add quites

--- rc-profile.sh
+++ rc-profile.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 
-venv=${VENV:-$HOME/venv/hyperapp}
+venv="${VENV:-$HOME/venv/hyperapp}"
 
 export LOG_CFG="${LOG_CFG:-rc}"
 
-source $venv/bin/activate
+source "$venv/bin/activate"
 
 cd "$( dirname "$0" )"
 
 set -x
 
-time python -m profile -o "$OUTPUT_FILE" ./boot.py rc_main --timeout=7 "$@"
+time python -m profile -o "$OUTPUT_FILE" ./boot.py rc.config rc_main --timeout=7 "$@"
