7ad02768401ce01cab4a51bdba33e465f7e62ba6
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-11-27 11:34:00 +0300

    CRUD: Add init system fn

--- hyperapp/ui/crud.tests.dyn.py
+++ hyperapp/ui/crud.tests.dyn.py
@@ -22,27 +22,55 @@ def _sample_get_fn():
         )
 
 
-def test_open(_sample_get_fn):
-    piece = htypes.crud.crud_open_command_fn(
+def test_open_command_fn(_sample_get_fn):
+    record_t = htypes.crud_tests.sample_record
+    piece = htypes.crud.open_command_fn(
         name='edit',
+        record_t=pyobj_creg.actor_to_ref(record_t),
         key_field='id',
         init_action_fn=mosaic.put(_sample_get_fn),
         commit_action='update',
         )
+    fn = crud.CrudOpenFn.from_piece(piece)
+
+    assert fn.missing_params(Context()) == {'model', 'current_item'}
     ctx = Context(
         model=htypes.crud_tests.sample_model(),
         current_item=htypes.crud_tests.sample_item(id=123),
         )
-    fn = crud.CrudOpenFn.from_piece(piece)
     assert not fn.missing_params(ctx)
-    assert fn.missing_params(Context()) == {'model', 'current_item'}
     crud_model = fn.call(ctx)
     assert isinstance(crud_model, htypes.crud.model)
 
 
+def test_init_fn(_sample_get_fn):
+    piece = htypes.crud.init_fn()
+    fn = crud.CrudInitFn.from_piece(piece)
+
+    record_t = htypes.crud_tests.sample_record
+    model = htypes.crud_tests.sample_model()
+    crud_model = htypes.crud.model(
+        record_t=pyobj_creg.actor_to_ref(record_t),
+        model=mosaic.put(model),
+        key=mosaic.put(123),
+        key_field='id',
+        init_action_fn=mosaic.put(_sample_get_fn),
+        commit_action='update',
+        )
+    assert fn.missing_params(Context()) == {'model'}
+    ctx = Context(
+        model=crud_model,
+        )
+    assert not fn.missing_params(ctx)
+    result = fn.call(ctx)
+    assert result == htypes.crud_tests.sample_record(123, 'item#123')
+
+
 def test_model_layout(_sample_get_fn):
     model = htypes.crud_tests.sample_model()
+    record_t = htypes.crud_tests.sample_record
     piece = htypes.crud.model(
+        record_t=pyobj_creg.actor_to_ref(record_t),
         model=mosaic.put(model),
         key=mosaic.put(123),
         key_field='id',
@@ -50,5 +78,4 @@ def test_model_layout(_sample_get_fn):
         commit_action='update',
         )
     result = crud.crud_model_layout(piece)
-    assert isinstance(result, htypes.crud_tests.sample_record)
-    assert result.id == 123
+    assert isinstance(result, htypes.form.view)
