1b3784277422aff19744dd00bb440295ec19e210
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-12-10 12:49:04 +0300

    RC: enum_builtin_resources: Add imports for any module

--- hyperapp/rc/builtin_resources.dyn.py
+++ hyperapp/rc/builtin_resources.dyn.py
@@ -8,4 +8,4 @@ from .code.import_resource import ImportResource
 def enum_builtin_resources(module_name):
     for service_name in builtin_services:
         piece = htypes.builtin.builtin_service(service_name)
-        yield ImportResource(module_name, ['services', service_name], piece)
+        yield ImportResource('', ['services', service_name], piece)
