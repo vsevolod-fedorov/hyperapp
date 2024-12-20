4d3179a2f55f9ceb29a0d4cca9b334c214bd4b00
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-12-20 12:00:19 +0300

    Column list: Add toggle_visibility command

--- hyperapp/models/column_list.tests.dyn.py
+++ hyperapp/models/column_list.tests.dyn.py
@@ -15,13 +15,28 @@ def lcs():
     return lcs
 
 
-def test_column_list(lcs):
-    piece = htypes.column_list.view(
+@mark.fixture
+def piece():
+    return htypes.column_list.view(
         model_t=pyobj_creg.actor_to_ref(htypes.column_list_tests.sample_model),
         item_t=pyobj_creg.actor_to_ref(htypes.column_list_tests.sample_item),
         )
+
+
+def test_column_list(lcs, piece):
     items = column_list.column_list(piece, lcs)
     assert len(items) == 3
+
+
+def test_toggle_visibility(lcs, piece):
+    lcs.get = lambda key, default: default
+    current_item = htypes.column_list.item(
+        name='id',
+        show=False,  # Should not be used.
+        )
+    column_list.toggle_visibility(piece, current_item, lcs)
+    lcs.set.assert_called_once()
+    assert lcs.set.call_args.args[1] == False
 
 
 def test_open():
