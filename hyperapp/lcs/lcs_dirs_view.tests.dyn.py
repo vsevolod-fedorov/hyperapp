7b38d5ba8b22844d7543af016e1d6ab2926450e6
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-12-22 13:34:23 +0300

    Add lcs dirs view

---
+++ hyperapp/lcs/lcs_dirs_view.tests.dyn.py
@@ -0,0 +1,52 @@
+from unittest.mock import MagicMock
+
+from . import htypes
+from .services import (
+    pyobj_creg,
+    )
+from .code.mark import mark
+from .tested.code import lcs_dirs_view
+
+
+@mark.fixture
+def layer_d():
+    return htypes.lcs_dirs_view_tests.layer_d()
+
+
+@mark.fixture
+def lcs(layer_d):
+    dir_1 = {
+        htypes.lcs_dirs_view_tests.sample_1_d('id'),
+        }
+    dir_2 = {
+        htypes.lcs_dirs_view_tests.sample_1_d('name'),
+        }
+    dir_3 = {
+        htypes.lcs_dirs_view_tests.sample_1_d('name'),
+        pyobj_creg.actor_to_piece(htypes.lcs_dirs_view_tests.sample_2_d),
+        }
+    items = [
+        (layer_d, dir_1, htypes.lcs_dirs_view_tests.sample_model_1()),
+        (layer_d, dir_2, htypes.lcs_dirs_view_tests.sample_model_2()),
+        (layer_d, dir_3, "some string"),
+        ]
+    mock = MagicMock()
+    mock.__iter__.return_value = items
+    return mock
+
+
+@mark.fixture
+def piece():
+    return htypes.lcs_dirs_view.view()
+
+
+def test_view(lcs, piece):
+    item_list = lcs_dirs_view.lcs_dirs_view(piece, lcs)
+    assert type(item_list) is list
+    assert len(item_list) == 3
+
+
+def test_open():
+    lcs_view = htypes.lcs_view.view()
+    piece = lcs_dirs_view.lcs_open_dirs(lcs_view)
+    assert piece
