a5e72ac74692b5300c10fe8a69985dff4e0af631
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-12-23 11:27:06 +0300

    Identity command: Do not use data_to_ref

--- hyperapp/ui/identity_command.dyn.py
+++ hyperapp/ui/identity_command.dyn.py
@@ -3,7 +3,6 @@ from functools import cached_property
 from . import htypes
 from .services import (
     mosaic,
-    pyobj_creg,
     web,
     )
 from .code.mark import mark
@@ -20,7 +19,7 @@ class UnboundIdentityModelCommand(UnboundCommandBase):
     @classmethod
     @mark.actor.command_creg
     def from_piece(cls, piece):
-        d = pyobj_creg.invite(piece.d)
+        d = web.summon(piece.d)
         return cls(d)
 
     def __repr__(self):
@@ -62,7 +61,7 @@ class BoundIdentityModelCommand(BoundCommandBase):
 
 
 @mark.command
-async def add_identity_command(piece, lcs, ctx, data_to_ref, feed_factory, ui_model_command_items):
+async def add_identity_command(piece, lcs, ctx, feed_factory, ui_model_command_items):
     feed = feed_factory(piece)
     model, model_t = web.summon_with_t(piece.model)
     model_state = web.summon(piece.model_state)
@@ -70,7 +69,7 @@ async def add_identity_command(piece, lcs, ctx, data_to_ref, feed_factory, ui_mo
     commands_item_list = ui_model_command_items(lcs, model_t, command_ctx)
     command_d = name_to_d('custom_command', 'identity')
     model_command_piece = htypes.identity_command.identity_command(
-        d=data_to_ref(command_d),
+        d=mosaic.put(command_d),
         )
     command_item = commands_item_list.add_custom_model_command(command_d, model_command_piece)
     new_item = command_item_to_view_item(lcs, command_item)
