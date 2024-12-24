dedc893211e1f59ab9cdc5ffe6e9740287975d02
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-12-24 12:42:56 +0300

    Test resource module registry: Add legacy types to module registry registry

--- hyperapp/rc/custom_resource_registry.dyn.py
+++ hyperapp/rc/custom_resource_registry.dyn.py
@@ -1,3 +1,5 @@
+from hyperapp.resource.legacy_type import add_legacy_types_to_cache
+
 from .services import (
     builtin_service_resource_loader,
     builtin_types_as_dict,
@@ -8,16 +10,10 @@ from .services import (
     )
 
 
-def _add_legacy_types_to_cache(res_reg, legacy_type_modules):
-    for module_name, module in legacy_type_modules.items():
-        for var_name in module:
-            res_reg.add_to_cache((module_name, var_name), module[var_name])
-
-
 def create_custom_resource_registry(build):
     res_reg = resource_registry_factory()
     legacy_type_modules = legacy_type_resource_loader(build.types.as_dict)
-    _add_legacy_types_to_cache(res_reg, legacy_type_modules)
+    add_legacy_types_to_cache(res_reg, legacy_type_modules)
     res_reg.update_modules(legacy_type_modules)
     res_reg.set_module('builtins', builtin_service_resource_loader(res_reg))
     return res_reg
