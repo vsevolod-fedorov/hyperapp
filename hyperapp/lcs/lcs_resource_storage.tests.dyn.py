7157dcec2b48af10829dc249b35baa61e3030c42
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-12-25 11:03:11 +0300

    Update lcs resource storage tests

--- hyperapp/lcs/lcs_resource_storage.tests.dyn.py
+++ hyperapp/lcs/lcs_resource_storage.tests.dyn.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+from hyperapp.resource.legacy_type import add_legacy_types_to_cache
+
 from . import htypes
 from .services import (
     builtin_types_as_dict,
@@ -45,7 +47,13 @@ def piece():
         )
 
 
-def test_persistence(lcs_resource_storage_factory, path, key, piece):
+# Subprocess job runner does not have builtin types in resource registry cache by default.
+@mark.fixture
+def prereq():
+    add_legacy_types_to_cache(resource_registry, legacy_type_resource_loader(builtin_types_as_dict()))
+
+
+def test_persistence(lcs_resource_storage_factory, prereq, path, key, piece):
     storage_1 = lcs_resource_storage_factory('test.lcs_storage', path)
     storage_1.set(key, piece)
     assert storage_1.get(key) == piece
@@ -53,20 +61,20 @@ def test_persistence(lcs_resource_storage_factory, path, key, piece):
     assert storage_2.get(key) == piece
 
 
-def test_primitive(lcs_resource_storage_factory, path, key):
+def test_primitive(lcs_resource_storage_factory, prereq, path, key):
     storage = lcs_resource_storage_factory('test.lcs_storage', path)
     storage.set(key, "Sample string")
     assert storage.get(key) == "Sample string"
 
 
-def test_replace(lcs_resource_storage_factory, path, key, piece):
+def test_replace(lcs_resource_storage_factory, prereq, path, key, piece):
     storage = lcs_resource_storage_factory('test.lcs_storage', path)
     storage.set(key, piece)
     storage.set(key, "Sample string")
     assert storage.get(key) == "Sample string"
 
 
-def test_remove(lcs_resource_storage_factory, path, key, piece):
+def test_remove(lcs_resource_storage_factory, prereq, path, key, piece):
     storage = lcs_resource_storage_factory('test.lcs_storage', path)
     storage.set(key, piece)
     storage.remove(key)
