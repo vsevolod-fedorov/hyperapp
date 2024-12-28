87ff3ed86f4dfe70b85f07af179c90f772b51f8a
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2024-12-28 11:23:39 +0300

    WIP: command list view crud: tests in command_list_view.tests

--- hyperapp/models/command_list_view.tests.dyn.py
+++ hyperapp/models/command_list_view.tests.dyn.py
@@ -94,3 +94,32 @@ async def test_model_set_shortcut(feed_factory, lcs, current_item):
     await command_list_view.set_shortcut(piece, 0, current_item, lcs)
     lcs.set.assert_called_once()
     await feed.wait_for_diffs(count=1)
+
+
+@mark.fixture
+def model_piece():
+    model = htypes.command_list_view_tests.sample_model()
+    model_state = htypes.command_list_view_tests.sample_model_state()
+    return htypes.model_commands.view(
+        model=mosaic.put(model),
+        model_state=mosaic.put(model_state)
+        )
+
+
+@mark.fixture
+def global_piece():
+    return htypes.global_commands.view()
+
+
+@mark.fixture
+def command_d():
+    return mosaic.put(htypes.command_list_view_tests.sample_command_d())
+
+
+def test_model_command_get(lcs, model_piece, command_d):
+    form = command_list_view.command_get(model_piece, command_d, lcs)
+
+
+def test_model_command_update(lcs, model_piece, command_d):
+    value = htypes.command_list_view.form("new text", "new tooltip")
+    command_list_view.command_update(model_piece, command_d, value, lcs)
