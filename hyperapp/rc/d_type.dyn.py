08fb4174de655ab958064d92db96614800aa02fa
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-01-01 12:22:15 +0300

    RC: Use d_type in crud ctr

--- hyperapp/rc/d_type.dyn.py
+++ hyperapp/rc/d_type.dyn.py
@@ -5,10 +5,9 @@ from .services import (
     )
 
 
-def d_type(types, module_name, name):
+def d_type(types, type_module_name, name):
     d_name = name + '_d'
-    type_module = module_name.split('.')[-1]
-    t_piece = types.get(type_module, d_name)
+    t_piece = types.get(type_module_name, d_name)
     if t_piece is not None:
         return pyobj_creg.animate(t_piece)
-    return TRecord(type_module, d_name)
+    return TRecord(type_module_name, d_name)
