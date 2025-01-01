e0007679c713cbae5356d9a5f330fc32567e1f5e
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-01-01 11:55:38 +0300

    RC: Create view factory marker

---
+++ hyperapp/rc/view_factory_ctr.dyn.py
@@ -0,0 +1,75 @@
+from . import htypes
+from .services import (
+    mosaic,
+    pyobj_creg,
+    )
+from .code.rc_constructor import Constructor
+
+
+class ViewFactoryTemplateCtr(Constructor):
+
+    _service_name = 'view_factory_reg'
+
+    @classmethod
+    def from_piece(cls, piece):
+        return cls(
+            module_name=piece.module_name,
+            attr_qual_name=piece.attr_qual_name,
+            ctx_params=piece.ctx_params,
+            service_params=piece.service_params,
+            )
+
+    def __init__(self, module_name, attr_qual_name, ctx_params, service_params):
+        self._module_name = module_name
+        self._attr_qual_name = attr_qual_name
+        self._ctx_params = ctx_params
+        self._service_params = service_params
+
+    @property
+    def piece(self):
+        return htypes.view_factory_ctr.ctr(
+            module_name=self._module_name,
+            attr_qual_name=tuple(self._attr_qual_name),
+            ctx_params=tuple(self._ctx_params),
+            service_params=tuple(self._service_params),
+            )
+
+    def update_targets(self, target_set):
+        resource_tgt = target_set.factory.python_module_resource_by_module_name(self._module_name)
+        ready_tgt, resolved_tgt, _ = target_set.factory.config_items(
+            self._service_name, self._fn_name,
+            provider=resource_tgt,
+            ctr=self,
+            )
+        resource_tgt.add_cfg_item_target(resolved_tgt)
+
+    def get_component(self, name_to_res):
+        return name_to_res[f'{self._fn_name}.view-factory-template']
+
+    def make_component(self, types, python_module, name_to_res=None):
+        object = python_module
+        prefix = []
+        for name in self._attr_qual_name:
+            object = htypes.builtin.attribute(
+                object=mosaic.put(object),
+                attr_name=name,
+                )
+            if name_to_res is not None:
+                name_to_res['.'.join([*prefix, name])] = object
+            prefix.append(name)
+        system_fn = htypes.system_fn.ctx_fn(
+            function=mosaic.put(object),
+            ctx_params=tuple(self._ctx_params),
+            service_params=tuple(self._service_params),
+            )
+        template = htypes.view_factory.template(
+            system_fn=mosaic.put(system_fn),
+            )
+        if name_to_res is not None:
+            name_to_res[f'{self._fn_name}.system-fn'] = system_fn
+            name_to_res[f'{self._fn_name}.view-factory-template'] = template
+        return template
+
+    @property
+    def _fn_name(self):
+        return '_'.join(self._attr_qual_name)
---
+++ hyperapp/rc/view_factory_marker.dyn.py
@@ -0,0 +1,45 @@
+from .code.marker_utils import (
+    check_is_function,
+    check_not_classmethod,
+    split_params,
+    )
+from .code.view_factory_ctr import ViewFactoryTemplateCtr
+
+
+class ViewFactoryProbe:
+
+
+    def __init__(self, system_probe, ctr_collector, module_name, fn):
+        self._system = system_probe
+        self._ctr_collector = ctr_collector
+        self._module_name = module_name
+        self._fn = fn
+        system_probe.add_global(self)
+
+    def migrate_to(self, system_probe):
+        self._system = system_probe
+        self._ctr_collector = system_probe.resolve_service('ctr_collector')
+
+    def __call__(self, *args, **kw):
+        params = split_params(self._fn, args, kw)
+        self._add_constructor(params)
+        service_kw = {
+            name: self._system.resolve_service(name)
+            for name in params.service_names
+            }
+        return self._fn(*args, **kw, **service_kw)
+
+    def _add_constructor(self, params):
+        ctr = ViewFactoryTemplateCtr(
+            module_name=self._module_name,
+            attr_qual_name=self._fn.__qualname__.split('.'),
+            ctx_params=params.ctx_names,
+            service_params=params.service_names,
+            )
+        self._ctr_collector.add_constructor(ctr)
+
+
+def view_factory_marker(fn, module_name, system, ctr_collector):
+    check_not_classmethod(fn)
+    check_is_function(fn)
+    return ViewFactoryProbe(system, ctr_collector, module_name, fn)
