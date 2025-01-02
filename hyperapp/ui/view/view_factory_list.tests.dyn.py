74c9b81967a698bc61e8fc82b572cf9109a58108
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-01-02 11:07:13 +0300

    Add view factory list view

---
+++ hyperapp/ui/view/view_factory_list.tests.dyn.py
@@ -0,0 +1,45 @@
+from . import htypes
+from .services import (
+    pyobj_creg,
+    mosaic,
+    )
+from .code.mark import mark
+from .code.system_fn import ContextFn
+from .tested.code import view_factory_list
+
+
+def _sample_fn():
+    return 'sample-fn'
+
+
+@mark.config_fixture('view_factory_reg')
+def view_factory_reg_config(partial_ref):
+    system_fn = ContextFn(
+        partial_ref=partial_ref, 
+        ctx_params=(),
+        service_params=(),
+        raw_fn=_sample_fn,
+        bound_fn=_sample_fn,
+        )
+    item = htypes.view_factory.item(
+        d=mosaic.put(htypes.view_factory_list_tests.sample_d()),
+        view_t=pyobj_creg.actor_to_ref(htypes.view_factory_list_tests.sample_view),
+        is_wrapper=False,
+        system_fn=mosaic.put(system_fn.piece),
+        )
+    return {item.d: item}
+
+
+@mark.fixture
+def piece():
+    return htypes.view_factory_list.view()
+
+
+def test_view_factory_list(piece):
+    items = view_factory_list.view_factory_list(piece)
+    assert items
+
+
+def test_open():
+    piece = view_factory_list.open_view_factory_list()
+    assert isinstance(piece, htypes.view_factory_list.view)
