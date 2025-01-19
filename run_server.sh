1aa0c0dc617031cebea993925f70b143ae130f95
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-01-19 10:54:02 +0300

    Boot: Filter projects to load

--- run_server.sh
+++ run_server.sh
@@ -8,4 +8,4 @@ source $venv/bin/activate
 
 cd "$( dirname "$0" )"
 
-$HOME/bin/autoreload.py -f '*.types' -f '*.yaml' -f '*.py'  . ./boot.py config server_main
+$HOME/bin/autoreload.py -f '*.types' -f '*.yaml' -f '*.py'  . ./boot.py base,common,ui,models,sample,server server_main
