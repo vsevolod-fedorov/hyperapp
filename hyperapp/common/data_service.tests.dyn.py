b29bd10bf7c4419a9bf6c1aea0ca8d7d100c9247
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-01-27 12:15:16 +0300

    Data service config ctl: Add piece property

--- hyperapp/common/data_service.tests.dyn.py
+++ hyperapp/common/data_service.tests.dyn.py
@@ -5,6 +5,7 @@ from .tested.code import data_service
 def test_config_ctl():
     ctl_piece = htypes.data_service.config_ctl()
     ctl = data_service.DataServiceConfigCtl.from_piece(ctl_piece)
+    assert ctl.piece == ctl_piece
     config = {
         htypes.data_service_tests.sample_key(123): htypes.data_service_tests.sample_value('sample-value'),
         }
