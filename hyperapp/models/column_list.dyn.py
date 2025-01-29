2f57bcefbe4a1403cf5f2ea1dbcae4c17310e237
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-01-29 13:42:29 +0300

    Column list model: Switch to column_visible_reg

--- hyperapp/models/column_list.dyn.py
+++ hyperapp/models/column_list.dyn.py
@@ -9,41 +9,39 @@ from .code.mark import mark
 from .code.list_diff import ListDiff
 
 
-def _column_d(model_t, name):
-    return {
-        htypes.column.list_d(),
-        pyobj_creg.actor_to_piece(model_t),
-        htypes.column.column_d(name),
-        }
+def _column_key(model_t, name):
+    return htypes.column.column_k(
+        model_t=pyobj_creg.actor_to_ref(model_t),
+        column_name=name,
+        )
 
 
-def _item(lcs, model_t, name):
-    d = _column_d(model_t, name)
-    show_d = d | {htypes.column.show_d()}
+def _item(column_visible_reg, model_t, name):
+    key = _column_key(model_t, name)
     return htypes.column_list.item(
         name=name,
-        show=lcs.get(show_d, True),
+        show=column_visible_reg.get(key, True),
         )
 
 
 @mark.model
-def column_list(piece, lcs):
+def column_list(piece, column_visible_reg):
     model_t = pyobj_creg.invite(piece.model_t)
     item_t = pyobj_creg.invite(piece.item_t)
     return [
-        _item(lcs, model_t, name)
+        _item(column_visible_reg, model_t, name)
         for name in item_t.fields
         ]
 
 
 @mark.command
-async def toggle_visibility(piece, current_idx, current_item, lcs, feed_factory):
+async def toggle_visibility(piece, current_idx, current_item, feed_factory, column_visible_reg):
     feed = feed_factory(piece)
     model_t = pyobj_creg.invite(piece.model_t)
-    key = _column_d(model_t, current_item.name) | {htypes.column.show_d()}
-    prev_value = lcs.get(key, True)
-    lcs.set(key, not prev_value)
-    item = _item(lcs, model_t, current_item.name)
+    key = _column_key(model_t, current_item.name)
+    prev_value = column_visible_reg.get(key, True)
+    column_visible_reg[key] = not prev_value
+    item = _item(column_visible_reg, model_t, current_item.name)
     await feed.send(ListDiff.Replace(current_idx, item))
 
 
