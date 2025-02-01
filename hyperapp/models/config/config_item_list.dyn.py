8716071d6cf59f50a0a4bb03148d5dc242b813bc
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-02-01 13:04:36 +0300

    Config item list: Add support for non-dict configs

--- hyperapp/models/config/config_item_list.dyn.py
+++ hyperapp/models/config/config_item_list.dyn.py
@@ -1,16 +1,30 @@
+from operator import itemgetter
+
 from . import htypes
 from .code.mark import mark
 
 
 @mark.model
 def config_item_list(piece, system):
+    def enum_items(config):
+        if type(config) in {list, set}:
+            for item in config:
+                yield ('', item)
+        elif type(config) is dict:
+            for key, value in config.items():
+                for _, item in enum_items(value):
+                    yield str(key), item
+        else:
+            yield ('', config)
+
+    config = system.get_config_template(piece.service_name)
     return [
         htypes.config_item_list.item(
-            key=str(key),
+            key=key_str,
             value=str(value),
             )
-        for key, value
-        in sorted(system.get_config_template(piece.service_name).items())
+        for key_str, value
+        in sorted(enum_items(config), key=itemgetter(0))
         ]
 
 
