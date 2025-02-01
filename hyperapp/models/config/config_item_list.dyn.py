0225edb94d53187b674474fe7421bd0a1fda9359
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-02-01 13:49:34 +0300

    Config item list: Add layers column

--- hyperapp/models/config/config_item_list.dyn.py
+++ hyperapp/models/config/config_item_list.dyn.py
@@ -1,4 +1,4 @@
-from operator import itemgetter
+from collections import defaultdict
 
 from . import htypes
 from .code.mark import mark
@@ -6,6 +6,7 @@ from .code.mark import mark
 
 @mark.model
 def config_item_list(piece, system):
+
     def enum_items(config):
         if type(config) in {list, set}:
             for item in config:
@@ -13,18 +14,35 @@ def config_item_list(piece, system):
         elif type(config) is dict:
             for key, value in config.items():
                 for _, item in enum_items(value):
-                    yield str(key), item
+                    yield key, item
         else:
             yield ('', config)
+
+    key_to_item_layer = defaultdict(list)
+    for layer_name, layer in system.name_to_layer.items():
+        try:
+            config = layer.config[piece.service_name]
+        except KeyError:
+            continue
+        for key, value in enum_items(config):
+            key_to_item_layer[key].append((value, layer_name))
+
+    def item_layers(key, value):
+        result = []
+        for v, layer_name in key_to_item_layer.get(key, []):
+            if v is value:
+                result.append(layer_name)
+        return result
 
     config = system.get_config_template(piece.service_name)
     return [
         htypes.config_item_list.item(
-            key=key_str,
+            key=str(key),
             value=str(value),
+            layers=", ".join(item_layers(key, value)),
             )
-        for key_str, value
-        in sorted(enum_items(config), key=itemgetter(0))
+        for key, value
+        in sorted(enum_items(config), key=lambda rec: str(rec[0]))
         ]
 
 
