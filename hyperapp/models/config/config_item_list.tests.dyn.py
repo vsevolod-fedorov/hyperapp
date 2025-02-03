d4f44321488a572bdc92931eddfd148a6933bd83
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-02-03 13:08:19 +0300

    Config item list: Fix moving items

--- hyperapp/models/config/config_item_list.tests.dyn.py
+++ hyperapp/models/config/config_item_list.tests.dyn.py
@@ -1,16 +1,16 @@
 from unittest.mock import Mock
 
 from . import htypes
+from .services import (
+    mosaic,
+    )
 from .code.mark import mark
+from .code.data_service import DataServiceConfigCtl
 from .tested.code import config_item_list
 
 
-@mark.fixture
-def piece():
-    return htypes.config_item_list.model(service_name='system')
-
-
-def test_item_list_model(piece):
+def test_item_list_model():
+    piece = htypes.config_item_list.model(service_name='system')
     item_list = config_item_list.config_item_list(piece)
     assert item_list
     assert isinstance(item_list[0], htypes.config_item_list.item)
@@ -19,11 +19,21 @@ def test_item_list_model(piece):
 def test_open():
     piece = htypes.config_service_list.model()
     current_item = htypes.config_service_list.item(
-        service_name='system',
+        service_name='sample_service',
         item_count=0,  # Unused.
         )
     model = config_item_list.open_config_item_list(piece, current_item)
     assert isinstance(model, htypes.config_item_list.model)
+
+
+@mark.fixture(ctl=DataServiceConfigCtl())
+def sample_service(config):
+    return config
+
+
+@mark.fixture
+def piece():
+    return htypes.config_item_list.model(service_name='sample_service')
 
 
 def test_get_layer(piece):
@@ -35,16 +45,23 @@ def test_get_layer(piece):
 
 
 def test_move(system, piece):
-    source_layer = Mock(config={})
+    key = 456
+
+    config = {
+        'sample_service': {
+            key: 'value-456',
+            }
+        }
+    source_layer = Mock(config=config)
     target_layer = Mock(config={})
     system.load_config_layer('source-layer', source_layer)
     system.load_config_layer('target-layer', target_layer)
 
-    key = 123
+    key_ref = mosaic.put(key)
     layers = ('source-layer',)
     value = htypes.config_layer_list.layer(
         name='target-layer',
         )
-    config_item_list.config_item_move_to_another_layer(piece, key, layers, value)
+    config_item_list.config_item_move_to_another_layer(piece, key_ref, layers, value)
     source_layer.remove.assert_called_once()
     target_layer.set.assert_called_once()
