f4ecc9ba5160bc776a0d0bc85d0f379a69f5d7c8
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-02-04 13:12:42 +0300

    RC: Add config item completed target to proper config resource

--- hyperapp/rc/target_set.dyn.py
+++ hyperapp/rc/target_set.dyn.py
@@ -328,10 +328,8 @@ class TargetFactory:
             # Configuration item requires it's service to be complete because it uses it's config_ctl.
             service_cfg_item_complete_tgt = self.config_item_complete('system', service_name, ServiceReq(service_name))
         resolved_tgt = self.config_item_resolved(service_name, key)
-        target = ConfigItemCompleteTarget(service_name, key, resolved_tgt, service_cfg_item_complete_tgt)
+        target = ConfigItemCompleteTarget(service_name, key, resolved_tgt, service_cfg_item_complete_tgt, req)
         self._target_set.globals.add(target)
-        config_tgt = self.config_resource()
-        config_tgt.add_item(service_name, target, req)
         return target
 
     def config_items(self, service_name, key, req=None, provider=None, ctr=None):
