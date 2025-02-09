06fb3d26687626c9bdae5a8454a0dbc609fa4320
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-02-09 13:52:57 +0300

    View factory: Add selector

--- hyperapp/ui/view/view_factory_list.dyn.py
+++ hyperapp/ui/view/view_factory_list.dyn.py
@@ -10,3 +10,15 @@ def view_factory_list(piece, view_factory_reg):
 @mark.global_command
 def open_view_factory_list():
     return htypes.view_factory_list.view()
+
+
+@mark.selector.get
+def view_factory_list_get(value):
+    return htypes.view_factory_list.view()
+
+
+@mark.selector.pick
+def view_factory_list_pick(piece, current_item):
+    return htypes.view_factory.factory(
+        d=current_item.d,
+        )
