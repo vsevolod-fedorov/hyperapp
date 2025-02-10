ded3385430606aed05076e3b6c3a9912b33cc85c
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-02-10 11:44:44 +0300

    Wrap view: animate view and call hook

--- hyperapp/ui/view/wrap_view.tests.dyn.py
+++ hyperapp/ui/view/wrap_view.tests.dyn.py
@@ -16,11 +16,18 @@ def view_factory_reg_config():
     return {d: factory}
 
 
+@mark.fixture
+def view_reg():
+    return Mock()
+
+
 def test_wrap_view():
     ctx = Context()
     d = htypes.wrap_view_tests.sample_d()
     view_factory = htypes.view_factory.factory(
         d=mosaic.put(d),
         )
+    hook = Mock()
     view = Mock(piece=None)
-    wrapper = wrap_view.wrap_view(view_factory, view, ctx)
+    wrapper = wrap_view.wrap_view(view_factory, view, hook, ctx)
+    hook.replace_view.assert_called_once()
