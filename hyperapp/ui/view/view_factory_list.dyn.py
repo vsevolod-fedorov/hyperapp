575a22a1ae218eefdb0d72d8070ee9078da77107
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-02-17 11:08:50 +0300

    Add model_t to view factory type; Add editor default; Rename list type: view to model

--- hyperapp/ui/view/view_factory_list.dyn.py
+++ hyperapp/ui/view/view_factory_list.dyn.py
@@ -1,4 +1,9 @@
 from . import htypes
+from .services import (
+    deduce_t,
+    mosaic,
+    pyobj_creg,
+    )
 from .code.mark import mark
 
 
@@ -9,16 +14,31 @@ def view_factory_list(piece, view_factory_reg):
 
 @mark.global_command
 def open_view_factory_list():
-    return htypes.view_factory_list.view()
+    return htypes.view_factory_list.model()
+
+
+@mark.editor.default
+def pick_view_factory_context(ctx):
+    try:
+        model = ctx.model
+    except KeyError:
+        model_t = None
+    else:
+        model_t = deduce_t(model)
+    return htypes.view_factory.factory(
+        model_t=pyobj_creg.actor_to_ref_opt(model_t),
+        k=None,
+        )
 
 
 @mark.selector.get
 def view_factory_list_get(value):
-    return htypes.view_factory_list.view()
+    return htypes.view_factory_list.model()
 
 
 @mark.selector.pick
 def view_factory_list_pick(piece, current_item):
     return htypes.view_factory.factory(
+        model_t=None,
         k=current_item.k,
         )
