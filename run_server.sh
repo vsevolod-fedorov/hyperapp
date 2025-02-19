f5beaec42486ca75a8a843dbc34d1b87651b43d8
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-02-19 11:13:25 +0300

    run_server.sh: Add -a option

--- run_server.sh
+++ run_server.sh
@@ -6,6 +6,18 @@ export LOG_CFG="${LOG_CFG:-server}"
 
 source $venv/bin/activate
 
+cmd=(
+  ./boot.py
+  base,rc,common,ui,models,sample,server
+  server_main
+  )
+
 cd "$( dirname "$0" )"
 
-$HOME/bin/autoreload.py -f '*.types' -f '*.yaml' -f '*.py'  . ./boot.py base,rc,common,ui,models,sample,server server_main
+if [[ "$1" == "-a" ]]; then
+  shift
+  $HOME/bin/autoreload.py -f '*.types' -f '*.yaml' -f '*.py'  . "${cmd[@]}"
+else
+  "${cmd[@]}"
+fi
+
