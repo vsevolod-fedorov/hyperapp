3ad89ab3518329aed415d318dfd70d8d44845b2c
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-02-22 13:34:17 +0300

    CRUD: Move open function to 'crud' service

--- hyperapp/ui/crud/crud.dyn.py
+++ hyperapp/ui/crud/crud.dyn.py
@@ -37,20 +37,20 @@ class CrudContextView(ContextView):
 
     @classmethod
     @mark.view
-    def from_piece(cls, piece, ctx, system_fn_creg, view_reg, crud_helpers):
+    def from_piece(cls, piece, ctx, system_fn_creg, view_reg, crud):
         base_view = view_reg.invite(piece.base_view, ctx)
         model = web.summon_opt(piece.model)
         command_d = web.summon(piece.commit_command_d)
         return cls(
-            system_fn_creg, crud_helpers, base_view, piece.label, model,
+            system_fn_creg, crud, base_view, piece.label, model,
             command_d, _args_tuple_to_dict(piece.args), piece.pick_fn, piece.commit_fn, piece.commit_value_field)
 
     def __init__(
-            self, system_fn_creg, helpers, base_view, label, model,
+            self, system_fn_creg, crud, base_view, label, model,
             command_d, args, pick_fn_ref, commit_fn_ref, commit_value_field):
         super().__init__(base_view, label)
         self._system_fn_creg = system_fn_creg
-        self._helpers = helpers
+        self._crud = crud
         self._model = model
         self._command_d = command_d
         self._args = args
@@ -76,7 +76,7 @@ class CrudContextView(ContextView):
         pick_fn = self._system_fn_creg.invite_opt(self._pick_fn_ref)
         commit_fn = self._system_fn_creg.invite(self._commit_fn_ref)
         return UnboundCrudCommitCommand(
-            self._helpers, self._command_d, self._model, self._args, pick_fn, commit_fn, self._commit_value_field)
+            self._crud, self._command_d, self._model, self._args, pick_fn, commit_fn, self._commit_value_field)
 
 
 class CrudOpenFn:
@@ -85,12 +85,9 @@ class CrudOpenFn:
 
     @classmethod
     @mark.actor.system_fn_creg
-    def from_piece(cls, piece, system_fn_creg, visualizer, view_reg, selector_reg, crud_helpers):
+    def from_piece(cls, piece, system_fn_creg, crud):
         return cls(
-            visualizer=visualizer,
-            view_reg=view_reg,
-            selector_reg=selector_reg,
-            helpers=crud_helpers,
+            crud=crud,
             name=piece.name,
             value_t=pyobj_creg.invite(piece.value_t),
             key_fields=piece.key_fields,
@@ -99,13 +96,8 @@ class CrudOpenFn:
             commit_action_fn_ref=piece.commit_action_fn,
             )
 
-    def __init__(
-            self, visualizer, view_reg, selector_reg, helpers,
-            name, value_t, key_fields, init_action_fn, commit_command_d_ref, commit_action_fn_ref):
-        self._visualizer = visualizer
-        self._view_reg = view_reg
-        self._selector_reg = selector_reg
-        self._helpers = helpers
+    def __init__(self, crud, name, value_t, key_fields, init_action_fn, commit_command_d_ref, commit_action_fn_ref):
+        self._crud = crud
         self._name = name
         self._value_t = value_t
         self._key_fields = key_fields
@@ -130,90 +122,32 @@ class CrudOpenFn:
             )
 
     def _open(self, navigator_rec, model, current_item, ctx):
-        try:
-            selector = self._selector_reg[self._value_t]
-        except KeyError:
-            get_fn = None
-            pick_fn = None
-        else:
-            get_fn = selector.get_fn
-            pick_fn = selector.pick_fn
         args = {
             name: getattr(current_item, name)
             for name in self._key_fields
             }
-        value = self._run_init(ctx, model, args)
-        if not get_fn:
-            # assert piece.init_action_fn  # Init action fn may be omitted only for selectors.
-            base_view_piece = self._editor_view(value)
-        else:
-            base_view_piece = self._selector_view(ctx, get_fn, value)
-        new_view_piece = htypes.crud.view(
-            base_view=mosaic.put(base_view_piece),
+        self._crud.open_view(
+            navigator_rec=navigator_rec,
+            ctx=ctx,
+            value_t=self._value_t,
             label=self._name,
-            model=mosaic.put(model),
-            commit_command_d=self._commit_command_d_ref,
-            args=_args_dict_to_tuple(args),
-            pick_fn=mosaic.put(pick_fn.piece) if pick_fn else None,
-            commit_fn=self._commit_action_fn_ref,
+            init_action_fn=self._init_action_fn,
+            commit_command_d_ref=self._commit_command_d_ref,
+            commit_action_fn_ref=self._commit_action_fn_ref,
             commit_value_field='value',
+            model=model,
+            args=args,
             )
-        new_ctx = ctx.clone_with(
-            model=value,
-            )
-        new_view = self._view_reg.animate(new_view_piece, new_ctx)
-        navigator_widget = navigator_rec.widget_wr()
-        if navigator_widget is None:
-            raise RuntimeError("Navigator widget is gone")
-        navigator_rec.view.open(ctx, value, new_view, navigator_widget)
-
-    def _editor_view(self, value):
-        if isinstance(self._value_t, TPrimitive):
-            return self._primitive_view(value)
-        else:
-            return self._form_view()
-
-    def _form_view(self):
-        adapter = htypes.record_adapter.static_record_adapter(
-            record_t=pyobj_creg.actor_to_ref(self._value_t),
-            )
-        return htypes.form.view(mosaic.put(adapter))
-
-    def _primitive_view(self, value):
-        if type(value) is str:
-            adapter = htypes.str_adapter.static_str_adapter()
-            return htypes.text.edit_view(mosaic.put(adapter))
-        raise NotImplementedError(f"TODO: CRUD editor: Add support for primitive type {type(value)}: {value!r}")
-
-    def _selector_view(self, ctx, get_fn, value):
-        selector_model = get_fn.call(ctx, value=value)
-        return self._visualizer(ctx.lcs, ctx, selector_model)
-
-    def _run_init(self, ctx, model, args):
-        fn_ctx = self._helpers.fn_ctx(ctx, model, args)
-        return self._init_action_fn.call(fn_ctx)
 
 
-class CrudHelpers:
+class Crud:
 
-    def __init__(self, canned_ctl_item_factory, system_fn_creg):
+    def __init__(self, canned_ctl_item_factory, system_fn_creg, visualizer, view_reg, selector_reg):
         self._canned_ctl_item_factory = canned_ctl_item_factory
         self._system_fn_creg = system_fn_creg
-
-    # Override context with original elements, canned by args picker.
-    def _canned_kw(self, ctx, args):
-        hook = None
-        kw = {}
-        try:
-            item_piece = args['canned_item_piece']
-        except KeyError:
-            pass
-        else:
-            item = self._canned_ctl_item_factory(item_piece, ctx)
-            kw['hook'] = item.hook
-            kw['widget'] = weakref.ref(item.widget)
-            kw['view'] = item.view
-        return kw
+        self._visualizer = visualizer
+        self._view_reg = view_reg
+        self._selector_reg = selector_reg
 
     def fn_ctx(self, ctx, model, args, **kw):
         if model is not None:
@@ -230,17 +164,104 @@ class CrudHelpers:
             **kw,
             )
 
+    def open_view(
+            self,
+            navigator_rec,
+            ctx,
+            value_t,
+            label,
+            init_action_fn,
+            commit_command_d_ref,
+            commit_action_fn_ref,
+            commit_value_field,
+            model,
+            args,
+            ):
+        try:
+            selector = self._selector_reg[value_t]
+        except KeyError:
+            get_fn = None
+            pick_fn = None
+        else:
+            get_fn = selector.get_fn
+            pick_fn = selector.pick_fn
+        value = self._run_init(ctx, init_action_fn, model, args)
+        if not get_fn:
+            # assert piece.init_action_fn  # Init action fn may be omitted only for selectors.
+            base_view_piece = self._editor_view(value_t, value)
+        else:
+            base_view_piece = self._selector_view(ctx, get_fn, value)
+        new_view_piece = htypes.crud.view(
+            base_view=mosaic.put(base_view_piece),
+            label=label,
+            model=mosaic.put(model),
+            commit_command_d=commit_command_d_ref,
+            args=_args_dict_to_tuple(args),
+            pick_fn=mosaic.put(pick_fn.piece) if pick_fn else None,
+            commit_fn=commit_action_fn_ref,
+            commit_value_field='value',
+            )
+        new_ctx = ctx.clone_with(
+            model=value,
+            )
+        new_view = self._view_reg.animate(new_view_piece, new_ctx)
+        navigator_widget = navigator_rec.widget_wr()
+        if navigator_widget is None:
+            raise RuntimeError("Navigator widget is gone")
+        navigator_rec.view.open(ctx, value, new_view, navigator_widget)
+
+    # Override context with original elements, canned by args picker.
+    def _canned_kw(self, ctx, args):
+        hook = None
+        kw = {}
+        try:
+            item_piece = args['canned_item_piece']
+        except KeyError:
+            pass
+        else:
+            item = self._canned_ctl_item_factory(item_piece, ctx)
+            kw['hook'] = item.hook
+            kw['widget'] = weakref.ref(item.widget)
+            kw['view'] = item.view
+        return kw
+
+    def _editor_view(self, value_t, value):
+        if isinstance(value_t, TPrimitive):
+            return self._primitive_view(value)
+        else:
+            return self._form_view(value_t)
+
+    def _form_view(self, value_t):
+        adapter = htypes.record_adapter.static_record_adapter(
+            record_t=pyobj_creg.actor_to_ref(value_t),
+            )
+        return htypes.form.view(mosaic.put(adapter))
+
+    def _primitive_view(self, value):
+        if type(value) is str:
+            adapter = htypes.str_adapter.static_str_adapter()
+            return htypes.text.edit_view(mosaic.put(adapter))
+        raise NotImplementedError(f"TODO: CRUD editor: Add support for primitive type {type(value)}: {value!r}")
+
+    def _selector_view(self, ctx, get_fn, value):
+        selector_model = get_fn.call(ctx, value=value)
+        return self._visualizer(ctx.lcs, ctx, selector_model)
+
+    def _run_init(self, ctx, init_action_fn, model, args):
+        fn_ctx = self.fn_ctx(ctx, model, args)
+        return init_action_fn.call(fn_ctx)
+
 
 @mark.service
-def crud_helpers(canned_ctl_item_factory, system_fn_creg):
-    return CrudHelpers(canned_ctl_item_factory, system_fn_creg)
+def crud(canned_ctl_item_factory, system_fn_creg, visualizer, view_reg, selector_reg):
+    return Crud(canned_ctl_item_factory, system_fn_creg, visualizer, view_reg, selector_reg)
 
 
 class UnboundCrudCommitCommand(UnboundCommandBase):
 
-    def __init__(self, helpers, d, model, args, pick_fn, commit_fn, commit_value_field):
+    def __init__(self, crud, d, model, args, pick_fn, commit_fn, commit_value_field):
         super().__init__(d)
-        self._helpers = helpers
+        self._crud = crud
         self._model = model
         self._args = args
         self._pick_fn = pick_fn
@@ -257,14 +278,14 @@ class UnboundCrudCommitCommand(UnboundCommandBase):
 
     def bind(self, ctx):
         return BoundCrudCommitCommand(
-            self._helpers, self._d, self.properties, self._model, self._args, self._pick_fn, self._commit_fn, self._commit_value_field, ctx)
+            self._crud, self._d, self.properties, self._model, self._args, self._pick_fn, self._commit_fn, self._commit_value_field, ctx)
 
 
 class BoundCrudCommitCommand(BoundCommandBase):
 
-    def __init__(self, helpers, d, properties, model, args, pick_fn, commit_fn, commit_value_field, ctx):
+    def __init__(self, crud, d, properties, model, args, pick_fn, commit_fn, commit_value_field, ctx):
         super().__init__(d)
-        self._helpers = helpers
+        self._crud = crud
         self._properties = properties
         self._model = model
         self._args = args
@@ -299,7 +320,7 @@ class BoundCrudCommitCommand(BoundCommandBase):
         else:
             value = self._pick_ctx_value(self._ctx)
         log.info("Run CRUD commit command %r: args=%s; %s=%r", self.name, self._args, self._commit_value_field, value)
-        fn_ctx = self._helpers.fn_ctx(
+        fn_ctx = self._crud.fn_ctx(
             self._ctx, self._model, self._args,
             **{self._commit_value_field: value},
             )
@@ -315,7 +336,7 @@ class BoundCrudCommitCommand(BoundCommandBase):
 
 
 @mark.ui_command_enum
-def crud_commit_command_enum(view, lcs, system_fn_creg, view_reg, visualizer, crud_helpers):
+def crud_commit_command_enum(view, lcs, system_fn_creg, view_reg, visualizer):
     model_command = view.unbound_commit_command
     ui_command = wrap_model_command_to_ui_command(view_reg, visualizer, lcs, model_command)
     return [ui_command]
