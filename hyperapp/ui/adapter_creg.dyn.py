8ca591fe6611f3c9b6f13bc89594265d88649fdd
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-02-24 12:53:09 +0300

    adapter_creg: Set ctl to DictListConfigCtl

--- hyperapp/ui/adapter_creg.dyn.py
+++ hyperapp/ui/adapter_creg.dyn.py
@@ -1,7 +1,8 @@
 from .code.mark import mark
 from .code.multi_code_registry import MultiCodeRegistry
+from .code.list_config_ctl import DictListConfigCtl
 
 
-@mark.service
+@mark.service(ctl=DictListConfigCtl())
 def adapter_creg(config):
     return MultiCodeRegistry('adapter_creg', config)
