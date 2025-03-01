a126947159ecddd5ddfc4a7586d6a6cf2f9544a4
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-03-01 13:07:59 +0300

    Remove adapter_creg service

--- hyperapp/ui/adapter_creg.dyn.py
+++
@@ -1,8 +0,0 @@
-from .code.mark import mark
-from .code.multi_code_registry import MultiCodeRegistry
-from .code.list_config_ctl import DictListConfigCtl
-
-
-@mark.service(ctl=DictListConfigCtl())
-def adapter_creg(config):
-    return MultiCodeRegistry('adapter_creg', config)
