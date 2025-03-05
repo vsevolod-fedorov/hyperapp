f6914fd3b810cfc1779ecfa45997395f374e1865
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-03-05 13:00:37 +0300

    Line edit, text views: Call adapter value_changed when text is changed

--- hyperapp/ui/str_adapter.dyn.py
+++ hyperapp/ui/str_adapter.dyn.py
@@ -1,4 +1,8 @@
+import logging
+
 from .code.mark import mark
+
+log = logging.getLogger(__name__)
 
 
 class StaticStrAdapter:
@@ -20,3 +24,8 @@ class StaticStrAdapter:
 
     def text_to_value(self, text):
         return text
+
+    def value_changed(self, new_value):
+        if new_value == self._text:
+            return
+        log.debug("Static str adapter: Ignoring new value: %r", new_value)
