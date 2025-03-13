971efe4b94f56991a2157760efebad14a302faad
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-03-13 11:44:07 +0300

    Add ConfigItemMissingError.__str__ method

--- hyperapp/boot/config_item_missing.py
+++ hyperapp/boot/config_item_missing.py
@@ -4,3 +4,6 @@ class ConfigItemMissingError(KeyError):
         super().__init__(key)
         self.service_name = service_name
         self.key = key
+
+    def __str__(self):
+        return f"ConfigItemMissingError for {self.service_name}: {self.key!r}"
