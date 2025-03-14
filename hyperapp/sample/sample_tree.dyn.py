adf5c5f6a41365cf8c471b329901bd4fa2c87b65
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-03-14 12:51:10 +0300

    Sample tree: Remove feed model, add insert and append commands; add title

--- hyperapp/sample/sample_tree.dyn.py
+++ hyperapp/sample/sample_tree.dyn.py
@@ -15,7 +15,7 @@ def sample_tree_data():
 
 
 def _item(parent_id, idx):
-    item_id = parent_id * 10 + idx
+    item_id = parent_id*10 + idx + 1
     return htypes.sample_tree.item(item_id, f"Item #{item_id}")
 
 
@@ -63,13 +63,52 @@ def _item_path(get_sample_tree_items, item_id):
 async def remove_tree_item(piece, current_item, feed_factory, get_sample_tree_items):
     feed = feed_factory(piece)
     item_id = current_item.id
+    log.info("Sample tree: Remove item #%d", item_id)
     path = _item_path(get_sample_tree_items, item_id)
-    log.info("Sample tree: Remove item #%d: path=%s", item_id, path)
+    log.info("Sample tree: Item #%d path: %s", item_id, path)
     parent_id = item_id // 10
     item_list = get_sample_tree_items(parent_id)
     idx = _index_of(item_list, item_id)
     del item_list[idx]
     diff = TreeDiff.Remove(path)
+    await feed.send(diff)
+
+
+@mark.command
+async def append_tree_item(piece, current_item, feed_factory, get_sample_tree_items):
+    feed = feed_factory(piece)
+    item_id = current_item.id
+    parent_id = item_id // 10
+    log.info("Sample tree: Append item to parent #%d", parent_id)
+    path = _item_path(get_sample_tree_items, parent_id)
+    log.info("Sample tree: Item #%d path: %s", parent_id, path)
+    item_list = get_sample_tree_items(parent_id)
+    idx = len(item_list)
+    if idx >= 9:
+        return
+    new_id = parent_id*10 + idx + 1
+    new_item = htypes.sample_tree.item(new_id, f"New item #{new_id}")
+    item_list.append(new_item)
+    diff = TreeDiff.Append(path, new_item)
+    await feed.send(diff)
+
+
+@mark.command
+async def insert_tree_item(piece, current_item, feed_factory, get_sample_tree_items):
+    feed = feed_factory(piece)
+    item_id = current_item.id
+    log.info("Sample tree: Insert to item #%d", item_id)
+    path = _item_path(get_sample_tree_items, item_id)
+    log.info("Sample tree: Item #%d path: %s", item_id, path)
+    parent_id = item_id // 10
+    item_list = get_sample_tree_items(parent_id)
+    if len(item_list) >= 10:
+        return
+    idx = _index_of(item_list, item_id)
+    new_id = parent_id*10 + idx + 1
+    new_item = htypes.sample_tree.item(new_id, f"New item #{new_id}")
+    item_list.insert(idx, new_item)
+    diff = TreeDiff.Insert(path, new_item)
     await feed.send(diff)
     
 
@@ -78,34 +117,6 @@ async def open_sample_fn_tree():
     return htypes.sample_tree.sample_tree()
 
 
-async def _send_diff(path, base, feed, idx):
-    await asyncio.sleep(1)
-    item = htypes.sample_tree.item(base*10 + idx, f"Sample item #{idx}")
-    await feed.send(TreeDiff.Append(path, item))
-    if idx < 9:
-        asyncio.create_task(_send_diff(path, base, feed, idx + 1))
-
-
-@mark.model
-def feed_sample_tree(piece, parent, feed):
-    loop = asyncio.get_running_loop()
-    if parent:
-        base = parent.id
-    else:
-        base = 0
-    path = []
-    i = base
-    while i:
-        path = [i % 10 - 1, *path]
-        i = i // 10
-    asyncio.create_task(_send_diff(path, base, feed, 4))
-    return [
-        htypes.sample_tree.item(base*10 + 1, "First sample"),
-        htypes.sample_tree.item(base*10 + 2, "Second sample"),
-        htypes.sample_tree.item(base*10 + 3, "Third sample"),
-        ]
-
-
-@mark.global_command
-async def open_feed_sample_fn_tree():
-    return htypes.sample_tree.feed_sample_tree()
+@mark.actor.formatter_creg
+def format_model(piece):
+    return "Sample tree"
