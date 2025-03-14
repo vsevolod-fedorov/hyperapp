374d75db25a474a7aff98e9842fe2afad5737017
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-03-14 12:51:10 +0300

    Sample tree: Remove feed model, add insert and append commands; add title

--- hyperapp/sample/sample_tree.tests.dyn.py
+++ hyperapp/sample/sample_tree.tests.dyn.py
@@ -1,35 +1,47 @@
 import asyncio
 
 from . import htypes
+from .code.mark import mark
 from .fixtures import feed_fixtures
 from .tested.code import sample_tree
 
 
-async def test_remove_item():
-    piece = htypes.sample_tree.sample_tree()
-    current_item = htypes.sample_tree.item(100, "<unused>")
+@mark.fixture
+def piece():
+    return htypes.sample_tree.sample_tree()
+
+
+def test_sample_tree(piece):
+    parent = htypes.sample_tree.item(111, "<unused>")
+    value = sample_tree.sample_tree(piece, parent)
+    assert value
+
+
+async def test_remove_item(feed_factory, piece):
+    feed = feed_factory(piece)
+    current_item = htypes.sample_tree.item(111, "<unused>")
     await sample_tree.remove_tree_item(piece, current_item)
+    await feed.wait_for_diffs(count=1)
+
+
+async def test_append_item(feed_factory, piece):
+    feed = feed_factory(piece)
+    current_item = htypes.sample_tree.item(111, "<unused>")
+    await sample_tree.append_tree_item(piece, current_item)
+    await feed.wait_for_diffs(count=1)
+
+
+async def test_insert_item(feed_factory, piece):
+    feed = feed_factory(piece)
+    current_item = htypes.sample_tree.item(111, "<unused>")
+    await sample_tree.insert_tree_item(piece, current_item)
+    await feed.wait_for_diffs(count=1)
 
 
 async def test_open_sample_fn_tree():
     await sample_tree.open_sample_fn_tree()
 
 
-async def test_open_feed_sample_fn_tree():
-    await sample_tree.open_feed_sample_fn_tree()
-
-
-def test_sample_tree():
-    piece = htypes.sample_tree.sample_tree()
-    parent = htypes.sample_tree.item(100, "Some item")
-    value = sample_tree.sample_tree(piece, parent)
-    assert value
-
-
-async def test_feed_sample_tree(feed_factory):
-    piece = htypes.sample_tree.feed_sample_tree()
-    feed = feed_factory(piece)
-    parent = htypes.sample_tree.item(100, "Some item")
-    value = sample_tree.feed_sample_tree(piece, parent, feed)
-    assert value
-    await feed.wait_for_diffs(count=1)
+def test_format(piece):
+    title = sample_tree.format_model(piece)
+    assert type(title) is str
