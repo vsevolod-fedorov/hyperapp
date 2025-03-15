f90124eed5025347cec1115e7fa6a36d4584dd61
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-03-15 11:44:03 +0300

    CRUD: Pick layout from model_layout_reg

--- hyperapp/ui/crud/args_picker_fn.dyn.py
+++ hyperapp/ui/crud/args_picker_fn.dyn.py
@@ -76,7 +76,7 @@ class ArgsPickerFn:
             value_t=value_t,
             label=f"{value_field} for: {self._name}",
             init_action_fn=self._system_fn_creg.animate(get_default_fn),
-            commit_command_d_ref=mosaic.put(self._commit_command_d),
+            commit_command_d=self._commit_command_d,
             commit_action_fn_ref=self._commit_fn_ref,
             commit_value_field=value_field,
             model=None,
