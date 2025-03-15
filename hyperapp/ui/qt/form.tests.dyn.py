7a755540c74dc511e2d1c4110c80a2fcc7a00a48
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-03-15 10:50:59 +0300

    View ui command registries: Pick commands from base types too

--- hyperapp/ui/qt/form.tests.dyn.py
+++ hyperapp/ui/qt/form.tests.dyn.py
@@ -111,20 +111,3 @@ def view(view_reg, piece, ctx, ctl_hook):
 @mark.fixture
 def widget(view, state, ctx):
     return  view.construct_widget(state, ctx)
-
-
-def test_add_element(qapp, ctx, ctl_hook, view, widget, view_factory):
-    form.add_element(view, widget, view_factory, ctx)
-    ctl_hook.element_inserted.assert_called_once()
-
-
-def test_insert_element(qapp, ctx, ctl_hook, view, widget, view_factory):
-    element_idx = 0
-    form.insert_element(view, widget, element_idx, view_factory, ctx)
-    ctl_hook.element_inserted.assert_called_once()
-
-
-def test_remove_element(qapp, ctl_hook, view, widget):
-    element_idx = 0
-    form.remove_element(view, widget, element_idx)
-    ctl_hook.element_removed.assert_called_once()
