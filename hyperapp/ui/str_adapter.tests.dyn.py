a897718ddf0cb884c06298f60ce52c4925303374
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-03-15 12:04:05 +0300

    Static str adapter: Add custom resource name

--- hyperapp/ui/str_adapter.tests.dyn.py
+++ hyperapp/ui/str_adapter.tests.dyn.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 from . import htypes
 from .code.context import Context
 from .tested.code import str_adapter
@@ -9,3 +11,10 @@ def test_adapter():
     piece = htypes.str_adapter.static_str_adapter()
     adapter = str_adapter.StaticStrAdapter.from_piece(piece, text, ctx)
     assert adapter.get_text() == text
+
+
+def test_resource_name():
+    gen = Mock()
+    piece = htypes.str_adapter.static_str_adapter()
+    name = str_adapter.static_str_adapter_resource_name(piece, gen)
+    assert type(name) is str
