d79a6e3a11862e16d4ec08ceca9c72faeaeaa1e0
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-03-19 13:02:37 +0300

    Data browser: Add view data adapter and view factory

--- hyperapp/models/data_browser_adapter.tests.dyn.py
+++ hyperapp/models/data_browser_adapter.tests.dyn.py
@@ -2,13 +2,27 @@ from . import htypes
 from .services import (
     mosaic,
     )
+from .code.mark import mark
+from .code.context import Context
 from .tested.code import data_browser_adapter
 
 
-def test_data_view_factory():
+@mark.fixture
+def record_view():
     data = "Sample string"
-    piece = htypes.data_browser.record_view(
+    return htypes.data_browser.record_view(
         data=mosaic.put(data),
         )
-    view = data_browser_adapter.data_browser_data_view(piece, adapter=None)
+
+
+def test_adapter(record_view):
+    ctx = Context()
+    model = record_view
+    piece = htypes.data_browser.record_data_adapter()
+    adapter = data_browser_adapter.DataBrowserViewDataAdapter.from_piece(piece, model, ctx)
+    assert type(adapter.get_text()) is str
+
+
+def test_data_view_factory(record_view):
+    view = data_browser_adapter.data_browser_data_view(record_view, adapter=None)
     assert view
