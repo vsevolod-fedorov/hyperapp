89eac1512cf26d3480e5267195b2eca8b77e9d10
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-03-19 13:02:37 +0300

    Data browser: Add view data adapter and view factory

--- hyperapp/models/data_browser_adapter.dyn.py
+++ hyperapp/models/data_browser_adapter.dyn.py
@@ -1,8 +1,36 @@
 from . import htypes
 from .services import (
     mosaic,
+    web,
     )
 from .code.mark import mark
+
+
+class DataBrowserViewDataAdapter:
+
+    @classmethod
+    @mark.actor.ui_adapter_creg
+    def from_piece(cls, piece, model, ctx):
+        data = web.summon(model.data)
+        return cls(data)
+
+    def __init__(self, data):
+        self._data = data
+
+    @property
+    def model(self):
+        return self._data
+
+    def get_text(self):
+        return str(self._data)
+
+    def text_to_value(self, text):
+        return text
+
+    def value_changed(self, new_value):
+        if new_value == self._text:
+            return
+        log.debug("Data browser view data adapter: Ignoring new value: %r", new_value)
 
 
 @mark.view_factory.model_t
