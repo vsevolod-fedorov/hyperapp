9a2c0de6821babbd97353e15347ca95bfe7d028f
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-03-21 11:50:15 +0300

    Server: Set default host to localhost

--- hyperapp/server/server.dyn.py
+++ hyperapp/server/server.dyn.py
@@ -14,14 +14,15 @@ log = logging.getLogger(__name__)
 
 
 DEFAULT_IDENTITY_PATH = Path.home() / '.local/share/hyperapp/server/identity.json'
+DEFAULT_HOST = 'localhost'
 DEFAULT_PORT = 7777
 
 
 def _parse_args(sys_argv):
     parser = argparse.ArgumentParser(description='Hyperapp server')
     parser.add_argument('--identity-path', type=Path, default=DEFAULT_IDENTITY_PATH, help="Path to server identity")
+    parser.add_argument('--host', default=DEFAULT_HOST, help="Bind to host")
     parser.add_argument('--port', type=int, default=DEFAULT_PORT, help="Bind to port")
-    parser.add_argument('--host', default='', help="Bind to host")
     return parser.parse_args(sys_argv)
 
 
