8e532ca9b9840e883de4e1e62f6766b0a19471c4
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-03-23 11:50:38 +0300

    Create SelectorRegistry; Add model_t to Selector

--- hyperapp/ui/selector.dyn.py
+++ hyperapp/ui/selector.dyn.py
@@ -7,7 +7,8 @@ from .code.mark import mark
 
 class Selector:
 
-    def __init__(self, get_fn, pick_fn):
+    def __init__(self, model_t, get_fn, pick_fn):
+        self.model_t = model_t
         self.get_fn = get_fn
         self.pick_fn = pick_fn
 
@@ -19,12 +20,14 @@ class SelectorTemplate:
     def from_piece(cls, piece):
         return cls(
             value_t=pyobj_creg.invite(piece.value_t),
+            model_t=pyobj_creg.invite(piece.model_t),
             get_fn=web.summon(piece.get_fn),
             pick_fn=web.summon(piece.pick_fn),
             )
 
-    def __init__(self, value_t, get_fn, pick_fn):
+    def __init__(self, value_t, model_t, get_fn, pick_fn):
         self._value_t = value_t
+        self._model_t = model_t
         self._get_fn = get_fn
         self._pick_fn = pick_fn
 
@@ -35,11 +38,27 @@ class SelectorTemplate:
     def resolve(self, system, service_name):
         system_fn_creg = system.resolve_service('system_fn_creg')
         return Selector(
+            model_t=self._model_t,
             get_fn=system_fn_creg.animate(self._get_fn),
             pick_fn=system_fn_creg.animate(self._pick_fn),
             )
 
 
+class SelectorRegistry:
+
+    def __init__(self, config):
+        self._config = config
+
+    def __getitem__(self, value_t):
+        return self._config[value_t]
+
+    def by_model_t(self, model_t):
+        for selector in self._config.values():
+            if selector.model_t is model_t:
+                return selector
+        raise KeyError(model_t)
+
+
 @mark.service
 def selector_reg(config):
-    return config
+    return SelectorRegistry(config)
