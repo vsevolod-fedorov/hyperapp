75ca1160e531e6849e97a57bec8894ceacba423b
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-04-04 12:00:34 +0300

    Add test for key list adapter

--- hyperapp/ui/qt/fn_list_adapter.tests.dyn.py
+++ hyperapp/ui/qt/fn_list_adapter.tests.dyn.py
@@ -67,7 +67,7 @@ async def _send_append_diff(feed):
     await feed.send(ListDiff.Append(item))
 
 
-async def test_fn_adapter(feed_factory, model, ctx):
+async def test_index_fn_adapter(feed_factory, model, ctx):
     system_fn = htypes.system_fn.ctx_fn(
         function=pyobj_creg.actor_to_ref(sample_list_fn),
         ctx_params=('piece',),
@@ -95,6 +95,38 @@ async def test_fn_adapter(feed_factory, model, ctx):
     diff = await asyncio.wait_for(queue.get(), timeout=5)
     assert isinstance(diff, ListDiff.Append), repr(diff)
     assert diff.item.id == 44
+
+
+async def test_key_fn_adapter(feed_factory, model, ctx):
+    system_fn = htypes.system_fn.ctx_fn(
+        function=pyobj_creg.actor_to_ref(sample_list_fn),
+        ctx_params=('piece',),
+        service_params=(),
+        )
+    adapter_piece = htypes.list_adapter.key_fn_list_adapter(
+        item_t=mosaic.put(pyobj_creg.actor_to_piece(htypes.list_adapter_tests.item)),
+        system_fn=mosaic.put(system_fn),
+        key_field='id',
+        key_field_t=pyobj_creg.actor_to_ref(htypes.builtin.int),
+        )
+    adapter = fn_list_adapter.FnKeyListAdapter.from_piece(adapter_piece, model, ctx)
+    assert adapter.column_count() == 2
+    assert adapter.column_title(0) == 'id'
+    assert adapter.column_title(1) == 'text'
+    assert adapter.row_count() == 3
+    assert adapter.cell_data(1, 0) == 22
+    assert adapter.cell_data(2, 1) == "third"
+
+    queue = asyncio.Queue()
+    subscriber = Subscriber(queue)
+    adapter.subscribe(subscriber)
+
+    # feed = feed_factory(model)
+    # asyncio.create_task(_send_append_diff(feed))
+
+    # diff = await asyncio.wait_for(queue.get(), timeout=5)
+    # assert isinstance(diff, ListDiff.Append), repr(diff)
+    # assert diff.item.id == 44
 
 
 _sample_fn_is_called = threading.Event()
