2feb66ffbfcbe42bd0b13960d588a1acd464ac72
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-04-06 11:00:29 +0300

    FS sample model

---
+++ hyperapp/sample/fs.tests.dyn.py
@@ -0,0 +1,25 @@
+from . import htypes
+from .code.mark import mark
+from .tested.code import fs
+
+
+@mark.fixture
+def piece():
+    return htypes.fs.model()
+
+
+def test_open():
+    piece = fs.open_fs()
+    assert isinstance(piece, htypes.fs.model)
+
+
+def test_model(piece):
+    parent = htypes.fs.item(
+        name='<unused>',
+        size=None,
+        )
+    path = ['etc']
+    item_list = fs.fs_model(piece, path)
+    assert type(item_list) is list
+    assert item_list
+    assert isinstance(item_list[0], htypes.fs.item)
