ddf868b5f098fed4a1991056c9a2f4805422e760
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-04-07 11:06:05 +0300

    FS sample model: Switch to pathlib, pick size

--- hyperapp/sample/fs.dyn.py
+++ hyperapp/sample/fs.dyn.py
@@ -1,5 +1,4 @@
-import os
-import os.path
+from pathlib import Path
 
 from . import htypes
 from .code.mark import mark
@@ -7,21 +6,27 @@ from .code.mark import mark
 
 @mark.model(key='name')
 def fs_model(piece, current_path):
-    fs_path = '/' + '/'.join(current_path)
-    if not os.path.isdir(fs_path):
+    fs_dir = Path('/').joinpath(*current_path)
+    if not fs_dir.is_dir():
         return []
     try:
-        name_list = os.listdir(fs_path)
+        kid_list = list(fs_dir.iterdir())
     except PermissionError:
         return []
-    return [
-        htypes.fs.item(
-            name=name,
-            size=None,
+    item_list = []
+    for item_path in kid_list:
+        if item_path.name.startswith('.'):
+            continue
+        if item_path.is_file():
+            size = item_path.stat().st_size
+        else:
+            size = None
+        item = htypes.fs.item(
+            name=item_path.name,
+            size=size,
             )
-        for name in name_list
-        if not name.startswith('.')
-        ]
+        item_list.append(item)
+    return item_list
 
 
 @mark.global_command
