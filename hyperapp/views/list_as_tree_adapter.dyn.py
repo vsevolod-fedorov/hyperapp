f2dac320a19e7dc66607f46b6fa357f3fc400c6c
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-04-08 11:09:53 +0300

    Rename class TreeAdapterBase -> TreeAdapter

--- hyperapp/views/list_as_tree_adapter.dyn.py
+++ hyperapp/views/list_as_tree_adapter.dyn.py
@@ -15,12 +15,12 @@ from .code.mark import mark
 from .code.system_fn import ContextFn
 from .code.model_command import model_command_ctx
 from .code.list_adapter import index_list_model_state_t
-from .code.tree_adapter import IndexTreeAdapterMixin, TreeAdapterBase
+from .code.tree_adapter import IndexTreeAdapterMixin, TreeAdapter
 
 log = logging.getLogger(__name__)
 
 
-class ListAsTreeAdapter(TreeAdapterBase, IndexTreeAdapterMixin):
+class ListAsTreeAdapter(TreeAdapter, IndexTreeAdapterMixin):
 
     @dataclass
     class _Layer:
