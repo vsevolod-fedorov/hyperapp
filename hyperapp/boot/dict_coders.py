dad5c572957de4998fd756ad4137909e416bce74
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-04-11 10:59:26 +0300

    Dict coders: Register yaml coders

--- hyperapp/boot/dict_coders.py
+++ hyperapp/boot/dict_coders.py
@@ -1,6 +1,7 @@
 from .htypes.packet_coders import packet_coders
-from .dict_encoder import JsonEncoder
-from .dict_decoder import JsonDecoder
+from .dict_encoder import JsonEncoder, YamlEncoder
+from .dict_decoder import JsonDecoder, YamlDecoder
 
 
 packet_coders.register('json', JsonEncoder, JsonDecoder)
+packet_coders.register('yaml', YamlEncoder, YamlDecoder)
