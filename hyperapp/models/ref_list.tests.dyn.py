6dc8bc7e53b06e99aa76e4a61235f7ca73066019
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-04-11 11:23:08 +0300

    Ref list: model, open command, add folder command

---
+++ hyperapp/models/ref_list.tests.dyn.py
@@ -0,0 +1,79 @@
+import base64
+from unittest.mock import Mock, patch
+
+from . import htypes
+from .services import (
+    mosaic,
+    )
+from .code.mark import mark
+from .tested.code import ref_list
+
+
+def test_open():
+    piece = ref_list.open_ref_list()
+    assert isinstance(piece, htypes.ref_list.model)
+
+
+ref_list_yaml_format = '''
+  folders:
+  - id: folder_1
+    parent_id: null
+    name: Folder A
+  - id: folder_2
+    parent_id: null
+    name: Folder B
+  refs:
+  - id: ref_1
+    parent_id: null
+    ref:
+      hash_algorithm: {ref_1_hash_algorithm}
+      hash: {ref_1_hash}
+'''
+
+
+def format_sample_model(piece):
+    return f'sample-model({piece.id})'
+
+
+@mark.config_fixture('formatter_creg')
+def formatter_creg_config():
+    return {
+        htypes.ref_list_tests.sample_model: format_sample_model,
+        }
+
+
+@mark.fixture
+def root_model():
+    return htypes.ref_list.model(parent_id=None)
+
+
+@mark.fixture
+def mock_storage_path():
+    ref_1_piece = htypes.ref_list_tests.sample_model(id=123)
+    ref_1 = mosaic.put(ref_1_piece)
+    ref_list_yaml_text = ref_list_yaml_format.format(
+        ref_1_hash_algorithm=ref_1.hash_algorithm,
+        ref_1_hash=base64.b64encode(ref_1.hash).decode('ascii'),
+        )
+    path = Mock()
+    path.read_bytes.return_value = ref_list_yaml_text.encode()
+    return path
+
+
+def test_root_model(root_model, mock_storage_path):
+    with patch.object(ref_list, '_STORAGE_PATH', mock_storage_path):
+        item_list = ref_list.ref_list_model(root_model)
+    assert type(item_list) is list
+    assert item_list == [
+        htypes.ref_list.item('folder_1', 'Folder A'),
+        htypes.ref_list.item('folder_2', 'Folder B'),
+        htypes.ref_list.item('ref_1', 'sample-model(123)'),
+        ]
+
+
+def test_add_root_folder(root_model, mock_storage_path):
+    with patch.object(ref_list, '_STORAGE_PATH', mock_storage_path):
+        piece, folder_id = ref_list.add_folder(root_model, 'New folder')
+    assert piece == root_model
+    assert type(folder_id) is str
+    mock_storage_path.write_bytes.assert_called_once()
