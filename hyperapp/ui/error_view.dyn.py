c8ecd5982693605cd8069aaf9e4b0c8c9e5afe79
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-04-12 11:20:24 +0300

    Error view: Add traceback

--- hyperapp/ui/error_view.dyn.py
+++ hyperapp/ui/error_view.dyn.py
@@ -1,9 +1,12 @@
+import traceback
+
 from .code.mark import mark
 
 
 @mark.service
 def error_view(view_reg, visualizer, exception, ctx):
-    model = str(exception)
+    tb_lines = traceback.format_exception(exception)
+    model = str(exception) + '\n\n' + ''.join(tb_lines)
     view_piece = visualizer(ctx, model)
     model_ctx = ctx.clone_with(model=model)
     view = view_reg.animate(view_piece, model_ctx)
