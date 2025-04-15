4a0e2f42a208224ed577e2ee6362b5d74e04b181
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-04-15 11:52:17 +0300

    Details view: Handle errors from details command using error_view

--- hyperapp/views/details.dyn.py
+++ hyperapp/views/details.dyn.py
@@ -9,16 +9,23 @@ from .services import (
 from .code.mark import mark
 from .code.view import Item
 from .code.model_command import model_command_ctx
+from .code.ui_model_command import split_command_result
 from .code.wrapper_view import WrapperView
 
 log = logging.getLogger(__name__)
 
 
 
-async def _run_details_command(ctx, unbound_command, model, model_state):
+async def _run_details_command(error_view, ctx, unbound_command, model, model_state):
     command_ctx = model_command_ctx(ctx, model, model_state)
     bound_command = unbound_command.bind(command_ctx)
-    return await bound_command.run()
+    try:
+        result = await bound_command.run()
+    except Exception as x:
+        log.exception("Error running details command %r", bound_command)
+        result = error_view(x, ctx)
+    model, key = split_command_result(result)
+    return model
 
 
 def _details_context(ctx, details_model):
@@ -32,16 +39,17 @@ class DetailsView(WrapperView):
 
     @classmethod
     @mark.view
-    def from_piece(cls, piece, ctx, view_reg, command_creg, visualizer):
+    def from_piece(cls, piece, ctx, view_reg, error_view, command_creg, visualizer):
         unbound_command = command_creg.invite(piece.command)
         details_model = web.summon(piece.details_model)
         details_ctx = _details_context(ctx, details_model)
         details_view = view_reg.invite(piece.details_view, details_ctx)
-        return cls(view_reg, visualizer, unbound_command, details_model, details_view)
+        return cls(view_reg, error_view, visualizer, unbound_command, details_model, details_view)
 
-    def __init__(self, view_reg, visualizer, unbound_command, details_model, details_view):
+    def __init__(self, view_reg, error_view, visualizer, unbound_command, details_model, details_view):
         super().__init__(details_view)
         self._view_reg = view_reg
+        self._error_view = error_view
         self._visualizer = visualizer
         self._unbound_command = unbound_command
         self._visualizer = visualizer
@@ -71,7 +79,7 @@ class DetailsView(WrapperView):
         return [Item('base', self._base_view, focusable=False)]
 
     async def _update_details_view(self, ctx, model_state, widget):
-        details_model = await _run_details_command(ctx, self._unbound_command, ctx.model, model_state)
+        details_model = await _run_details_command(self._error_view, ctx, self._unbound_command, ctx.model, model_state)
         details_ctx = _details_context(ctx, details_model)
         details_view_piece = self._visualizer(details_ctx, details_model)
         details_view = self._view_reg.animate(details_view_piece, details_ctx)
@@ -116,9 +124,9 @@ def details_command_list(model, model_state, ctx, details_commands):
     return factory_k_list
 
 
-async def details_get(k, model, model_state, ctx, command_creg, visualizer):
+async def details_get(k, model, model_state, ctx, error_view, command_creg, visualizer):
     unbound_command = command_creg.invite(k.command)
-    details_model = await _run_details_command(ctx, unbound_command, model, model_state)
+    details_model = await _run_details_command(error_view, ctx, unbound_command, model, model_state)
     details_ctx = _details_context(ctx, details_model)
     details_view = visualizer(details_ctx, details_model)
     return htypes.details.view(
