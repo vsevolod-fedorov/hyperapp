23bce998480bd962230582f68312285649fc1612
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-04-16 12:27:19 +0300

    Add model and ui command enums

--- hyperapp/ui/ui_command.tests.dyn.py
+++ hyperapp/ui/ui_command.tests.dyn.py
@@ -28,13 +28,44 @@ class PhonyView:
         return 'a-state'
 
 
-def _sample_fn(view, state, sample_service):
+def _sample_command(view, state, sample_service):
     return f'sample-fn: {state}, {sample_service}'
+
+
+@mark.fixture
+def sample_command_fn(rpc_system_call_factory):
+    return ContextFn(
+        rpc_system_call_factory=rpc_system_call_factory,
+        ctx_params=('view', 'state'),
+        service_params=('sample_service',),
+        raw_fn=_sample_command,
+        bound_fn=partial(_sample_command, sample_service='a-service'),
+        )
+
+
+def _sample_command_enum(view, state, sample_service):
+    return []
+
+
+@mark.fixture
+def sample_command_enum_fn():
+    return htypes.command.ui_command_enum_fn(
+        function=pyobj_creg.actor_to_ref(_sample_command_enum),
+        ctx_params=('view', 'state'),
+        service_params=('sample_service',),
+        )
 
 
 @mark.fixture
 def sample_service():
     return 'a-service'
+
+
+def test_command_enum_fn(ctx, sample_command_enum_fn):
+    enum = ui_command.UiCommandEnumFn.from_piece(sample_command_enum_fn)
+    assert enum.piece == sample_command_enum_fn
+    result = enum.call(ctx, view="Sample view", state="Sample state")
+    assert type(result) is tuple
 
 
 @mark.fixture
@@ -49,17 +80,10 @@ def widget():
 
 
 @mark.config_fixture('view_ui_command_reg')
-def view_ui_command_reg_config(rpc_system_call_factory):
-    system_fn = ContextFn(
-        rpc_system_call_factory=rpc_system_call_factory, 
-        ctx_params=('view', 'state'),
-        service_params=('sample_service',),
-        raw_fn=_sample_fn,
-        bound_fn=partial(_sample_fn, sample_service='a-service'),
-        )
+def view_ui_command_reg_config(sample_command_fn):
     command = ui_command.UnboundUiCommand(
         d=htypes.ui_command_tests.sample_command_d(),
-        ctx_fn=system_fn,
+        ctx_fn=sample_command_fn,
         properties=htypes.command.properties(False, False, False),
         groups=set(),
         )
@@ -87,31 +111,21 @@ async def test_view_element_commands(get_view_element_commands, view, ctx):
     assert type(command_list) is list
 
 
-def test_ui_command_from_piece():
+def test_ui_command_from_piece(sample_command_fn):
     d = htypes.ui_command_tests.sample_command_d()
-    system_fn = htypes.system_fn.ctx_fn(
-        function=pyobj_creg.actor_to_ref(_sample_fn),
-        ctx_params=('view', 'state'),
-        service_params=('sample_service',),
-        )
     piece = htypes.command.ui_command(
         d=mosaic.put(d),
         properties=htypes.command.properties(False, False, False),
-        system_fn=mosaic.put(system_fn),
+        system_fn=mosaic.put(sample_command_fn.piece),
         )
     command = ui_command.ui_command_from_piece(piece)
     assert isinstance(command, ui_command.UnboundUiCommand)
     assert command.piece == piece
 
 
-def test_ui_command_enumerator_from_piece():
-    system_fn = htypes.system_fn.ctx_fn(
-        function=pyobj_creg.actor_to_ref(_sample_fn),
-        ctx_params=('view', 'state'),
-        service_params=('sample_service',),
-        )
+def test_ui_command_enumerator_from_piece(sample_command_fn):
     piece = htypes.command.ui_command_enumerator(
-        system_fn=mosaic.put(system_fn),
+        system_fn=mosaic.put(sample_command_fn.piece),
         )
     command = ui_command.ui_command_enumerator_from_piece(piece)
     assert isinstance(command, UnboundCommandEnumerator)
