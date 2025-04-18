691e473a04f193ee0cab0beca37f2a16f0951096
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-04-18 12:56:20 +0300

    Add wiki view

---
+++ hyperapp/ui/qt/wiki.dyn.py
@@ -0,0 +1,74 @@
+import logging
+import re
+
+from PySide6 import QtWidgets
+
+from . import htypes
+from .services import (
+    mosaic,
+    )
+from .code.mark import mark
+from .code.view import View
+
+log = logging.getLogger(__name__)
+
+
+class WikiView(View):
+
+    @classmethod
+    @mark.view
+    def from_piece(cls, piece, model, ctx, ui_adapter_creg):
+        adapter = ui_adapter_creg.invite(piece.adapter, model, ctx)
+        return cls(piece.adapter, adapter)
+
+    def __init__(self, adapter_ref, adapter):
+        super().__init__()
+        self._adapter_ref = adapter_ref
+        self._adapter = adapter
+
+    @property
+    def piece(self):
+        return htypes.wiki.view(
+            adapter=self._adapter_ref,
+            )
+
+    def construct_widget(self, state, ctx):
+        w = QtWidgets.QTextBrowser(
+            openLinks=False,
+            )
+        w.setHtml(self._text_to_html(self._adapter.get_text()))
+        w.anchorClicked.connect(self._on_anchor_clicked)
+        return w
+
+    def widget_state(self, widget):
+        return htypes.wiki.state()
+
+    def primary_parent_context(self, rctx, widget):
+        return rctx.clone_with(
+            model_state=self._model_state(widget),
+            )
+
+    def _model_state(self, widget):
+        return self.get_value(widget)
+
+    def get_plain_text(self, widget):
+        return widget.toPlainText()
+
+    def get_value(self, widget):
+        text = self.get_plain_text(widget)
+        return self._adapter.text_to_value(text)
+
+    def _on_anchor_clicked(self, url):
+        log.info('Wiki view: Anchor clicked: url.path=%r', url.path())
+
+    def _text_to_html(self, text):
+        return re.sub(r'\[([^\]]+)\]', r'<a href="\1">\1</a>', text or '')
+
+
+@mark.view_factory.model_t
+def wiki(piece, adapter=None):
+    if adapter is None:
+        adapter = htypes.str_adapter.static_str_adapter()
+    return htypes.wiki.view(
+        adapter=mosaic.put(adapter),
+        )
