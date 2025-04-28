11bc0a5a789292f58d35788ba346d75264767a67
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-04-28 13:02:32 +0300

    Tree-as-list: Make 'open' command a model command

--- hyperapp/views/tree_as_list.dyn.py
+++ hyperapp/views/tree_as_list.dyn.py
@@ -1,3 +1,5 @@
+import logging
+
 from . import htypes
 from .services import (
     mosaic,
@@ -6,6 +8,8 @@ from .services import (
     )
 from .code.mark import mark
 from .code.wrapper_view import WrapperView
+
+log = logging.getLogger(__name__)
 
 
 class TreeAsListWrapperView(WrapperView):
@@ -41,48 +45,67 @@ class TreeAsListWrapperView(WrapperView):
             current_path=tuple(mosaic.put(elt) for elt in self._current_path),
             )
 
-    def element_view(self, current_elt):
-        return TreeAsListWrapperView(
-            list_view=self._base_view,
-            tree_model_fn=self._tree_model_fn,
-            current_path=[*self._current_path, current_elt],
-            )
+    def element_view(self, view_reg, ctx, list_model, current_elt, current_item):
+        return self._wrapper_view(view_reg, ctx, list_model, [*self._current_path, current_elt], current_item)
 
-    def parent_view(self):
+    def parent_view(self, view_reg):
         if not self._current_path:
             return None
+        return self._wrapper_view(self._current_path[:-1])
+
+    def _wrapper_view(self, view_reg, ctx, list_model, current_path, parent_item):
+        new_list_model = htypes.tree_as_list.list_model(
+            tree_model=list_model.tree_model,
+            tree_model_fn=list_model.tree_model_fn,
+            current_path=tuple(mosaic.put(elt) for elt in current_path),
+            parent_item=mosaic.put(parent_item),
+            )
+        list_ctx = ctx.clone_with(
+            model=new_list_model,
+            piece=new_list_model,
+            )
+        list_view = view_reg.animate(self._base_view.piece, list_ctx)
         return TreeAsListWrapperView(
-            list_view=self._base_view,
+            list_view=list_view,
             tree_model_fn=self._tree_model_fn,
-            current_path=self._current_path[:-1],
+            current_path=current_path,
             )
 
 
-def list_model_fn(piece, current_idx, ctx, system_fn_creg):
+def list_model_fn(piece, ctx, system_fn_creg):
     tree_model = web.summon(piece.tree_model)
     tree_model_fn = system_fn_creg.invite(piece.tree_model_fn)
-    current_path = [web.summon(idx) for idx in piece.current_path]
+    current_path = tuple(web.summon(idx) for idx in piece.current_path)
     parent_item = web.summon_opt(piece.parent_item)
     tree_ctx = ctx.clone_with(
         model=tree_model,
         piece=tree_model,
-        current_path=(*current_path, current_idx),
+        current_path=current_path,
         parent=parent_item,
         )
+    log.info("Tree-as-list model: current_path=%s, parent=%s", current_path, parent_item)
     return tree_model_fn.call(tree_ctx)
 
 
+@mark.command
+def open(piece, current_idx, current_item, view, state, ctx, hook, view_reg):
+    elt_view = view.element_view(view_reg, ctx, piece, current_idx, current_item)
+    if elt_view:
+        hook.replace_view(elt_view, state)
+
+
 @mark.ui_command
-def open(view, state, current_idx, hook):
-    elt_view = view.element_view(current_idx)
-    hook.replace_view(elt_view, state)
+def parent(view, state, model, current_idx, hook, view_reg):
+    elt_view = view.parent_view(view_reg, model, current_idx)
+    if elt_view:
+        hook.replace_view(elt_view, state)
 
 
 @mark.view_factory.ui_t
 def index_tree_as_list_ui_type_layout(piece, system_fn_ref):
     list_fn = htypes.system_fn.ctx_fn(
         function=pyobj_creg.actor_to_ref(list_model_fn),
-        ctx_params=('piece', 'current_idx', 'ctx'),
+        ctx_params=('piece', 'ctx'),
         service_params=('system_fn_creg',),
         )
     list_adapter = htypes.list_adapter.index_fn_list_adapter(
