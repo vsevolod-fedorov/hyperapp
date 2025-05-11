0c16ca7c265637a84dd0d2f91dbae10437d5fbcb
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-05-11 11:41:20 +0300

    Improve sample wiki

--- hyperapp/sample/sample_commands.dyn.py
+++ hyperapp/sample/sample_commands.dyn.py
@@ -17,19 +17,23 @@ async def open_sample_static_text():
 async def open_sample_wiki_text():
     text = textwrap.dedent("""
         Sample wiki text
-        This is `ref#1 <1>`_.
-        And this is `ref#2 <2>`_.
+        ~~~~~~~~~~~~~~~~
+
+        * This is reference to a `Static text <1>`_.
+        * And this one is to a `Static list <2>`_.
+        * Here is `Sample tree model <3>`_.
         """)
     ref_items = tuple([
-        htypes.sample_list.item(1, "First", "First item"),
+        htypes.sample_list.item(1, "First", "First list item for wiki sample"),
         htypes.sample_list.item(2, "Second", "Second item"),
         htypes.sample_list.item(3, "Third", "Third item"),
         ])
     return htypes.wiki.wiki(
         text=text,
         refs=(
-            htypes.wiki.wiki_ref('1', mosaic.put("Ref 1 target text")),
+            htypes.wiki.wiki_ref('1', mosaic.put("Some static text for wiki sample")),
             htypes.wiki.wiki_ref('2', mosaic.put(ref_items)),
+            htypes.wiki.wiki_ref('3', mosaic.put(htypes.sample_tree.model())),
             ),
         )
 
