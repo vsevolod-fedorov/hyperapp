a97b1601bd7acf0409e1ac1e6f7abe034fd457bb
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-05-15 10:41:22 +0300

    Rename service default_model_t_factory -> default_model_factory

--- hyperapp/ui/view/default_view_factory.dyn.py
+++ hyperapp/ui/view/default_view_factory.dyn.py
@@ -2,6 +2,6 @@ from .code.mark import mark
 
 
 @mark.service
-def default_model_t_factory(config, view_factory_reg, model_t):
+def default_model_factory(config, view_factory_reg, model_t):
     k = config[model_t]
     return view_factory_reg[k]
