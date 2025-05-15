80bac7592e372c780dec24659a05e82991db6022
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-05-15 11:02:24 +0300

    Add default_ui_factory service

--- hyperapp/ui/view/default_view_factory.dyn.py
+++ hyperapp/ui/view/default_view_factory.dyn.py
@@ -1,7 +1,17 @@
+from .services import (
+    deduce_t,
+    )
 from .code.mark import mark
 
 
 @mark.service
 def default_model_factory(config, view_factory_reg, model_t):
     k = config[model_t]
+    return view_factory_reg[k]
+
+
+@mark.service
+def default_ui_factory(config, view_factory_reg, ui_t):
+    ui_t_t = deduce_t(ui_t)
+    k = config[ui_t_t]
     return view_factory_reg[k]
