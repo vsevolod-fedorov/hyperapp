6d400e7d5e78c757eef2d200b65c9059586a6780
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-05-23 11:03:30 +0300

    Update error view tests

--- hyperapp/ui/error_view.tests.dyn.py
+++ hyperapp/ui/error_view.tests.dyn.py
@@ -1,22 +1,6 @@
-from . import htypes
-from .services import (
-    mosaic,
-    pyobj_creg,
-    )
-from .code.mark import mark
 from .code.context import Context
+from .fixtures import visualizer_fixtures
 from .tested.code import error_view
-
-
-@mark.config_fixture('model_layout_reg')
-def model_layout_reg_config():
-    def k(t):
-        return htypes.ui.model_layout_k(pyobj_creg.actor_to_ref(t))
-    return {
-        k(htypes.builtin.string): htypes.text.edit_view(
-            adapter=mosaic.put(htypes.str_adapter.static_str_adapter()),
-            ),
-        }
 
 
 async def test_error_view(error_view):
