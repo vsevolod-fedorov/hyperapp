41d7a670f2d572189a319f846c545d298c90f1f3
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-05-27 12:27:31 +0300

    RC: Refactor service params resolving

--- hyperapp/rc/marker_utils.dyn.py
+++ hyperapp/rc/marker_utils.dyn.py
@@ -4,7 +4,18 @@ from collections import namedtuple
 from .code.probe import real_fn
 
 
-class Params:
+
+class ServiceParams:
+
+    def __init__(self, called_class_name, free_names, service_names, values, has_config=False):
+        self._called_class_name = called_class_name
+        self.free_names = free_names
+        self.service_names = service_names
+        self.values = values
+        self.has_config = has_config
+
+
+class ActorParams:
 
     def __init__(self, called_class_name, ctx_names, service_names, values):
         self._called_class_name = called_class_name
@@ -51,21 +62,48 @@ def is_cls_arg(fn, arg):
     return getattr(attr, '__self__', None) is arg
 
 
-def fn_params(fn):
-    return list(inspect.signature(fn).parameters)
-
-
-def split_actor_params(fn, args, kw):
-    fn_names = fn_params(fn)
+def split_service_params(fn, args, kw):
+    fn_params = inspect.signature(fn).parameters
+    param_names = list(fn_params)
     if args and is_cls_arg(fn, args[0]):
         # fn is a classmethod and args[0] is a 'cls' argument.
         called_class_name = args[0].__name__
-        param_ofs = 1
+        ofs = 1
     else:
         called_class_name = None
-        param_ofs = 0
+        ofs = 0
+    has_config = 'config' in fn_params
+    if has_config:
+        if param_names[ofs] != 'config':
+            raise RuntimeError(f"{fn}: 'config' should be first parameter: {', '.join(param_names)}")
+        ofs += 1
+    free_param_count = len(args) + len(kw)
+    service_param_count = len(fn_params) - free_param_count - ofs
+    service_names = param_names[ofs:ofs + service_param_count]
     args_values = {
-        fn_names[idx]: arg
+        param_names[idx]: arg
+        for idx, arg in enumerate(args[ofs + service_param_count:])
+        }
+    values = {
+        **args_values,
+        **kw,
+        }
+    free_names = param_names[ofs + service_param_count:]
+    return ServiceParams(called_class_name, free_names, service_names, values, has_config)
+
+
+def split_actor_params(fn, args, kw):
+    fn_params = inspect.signature(fn).parameters
+    param_names = list(fn_params)
+    if args and is_cls_arg(fn, args[0]):
+        # fn is a classmethod and args[0] is a 'cls' argument.
+        called_class_name = args[0].__name__
+        ofs = 1
+    else:
+        called_class_name = None
+        ofs = 0
+    args_values = {
+        param_names[idx]: arg
         for idx, arg in enumerate(args)
         }
     values = {
@@ -73,14 +111,14 @@ def split_actor_params(fn, args, kw):
         **kw,
         }
     ctx_names = [
-        *fn_names[param_ofs:len(args)],
+        *param_names[ofs:len(args)],
         *kw,
         ]
     service_names = [
-        name for name in fn_names[param_ofs:]
+        name for name in param_names[ofs:]
         if name not in ctx_names
         ]
-    return Params(called_class_name, ctx_names, service_names, values)
+    return ActorParams(called_class_name, ctx_names, service_names, values)
 
 
 def process_awaitable_result(fn, result, *args, **kw):
