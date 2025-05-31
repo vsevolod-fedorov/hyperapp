de01a856d2f50592e9006430379b43ad155207f7
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-05-31 13:14:43 +0300

    Sample record: Add 'apply' command

--- hyperapp/sample/sample_record.tests.dyn.py
+++ hyperapp/sample/sample_record.tests.dyn.py
@@ -11,3 +11,10 @@ def test_sample_record():
 def test_open():
     piece = sample_record.open_sample_fn_record()
     assert piece
+
+
+def test_apply():
+    piece = htypes.sample_record.sample_record()
+    item = htypes.sample_record.item(12345, "Sample title")
+    result = sample_record.apply(piece, item)
+    assert type(result) is str
