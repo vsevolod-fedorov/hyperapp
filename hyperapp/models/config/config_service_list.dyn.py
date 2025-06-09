395b6060d4a496d2425d4eb867824358115ab593
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-06-09 13:04:20 +0300

    Assoc key service; Toggle command for service list

--- hyperapp/models/config/config_service_list.dyn.py
+++ hyperapp/models/config/config_service_list.dyn.py
@@ -1,5 +1,6 @@
 from . import htypes
 from .code.mark import mark
+from .code.list_diff import KeyListDiff
 
 
 def _item_count(config_template):
@@ -9,15 +10,33 @@ def _item_count(config_template):
         return None
 
 
-@mark.model
-def config_service_list(piece, system):
+def _service_item(system, assoc_key, format, service_name):
+    return htypes.config_service_list.item(
+        service_name=service_name,
+        item_count=_item_count(system.get_config_template(service_name)),
+        assoc=format(assoc_key.get(service_name)),
+        )
+
+
+@mark.model(key='service_name')
+def config_service_list(piece, format, assoc_key, system):
     return [
-        htypes.config_service_list.item(
-            service_name=service_name,
-            item_count=_item_count(system.get_config_template(service_name)),
-            )
+        _service_item(system, assoc_key, format, service_name)
         for service_name in sorted(system.service_names)
         ]
+
+
+@mark.command
+async def toggle_assoc(piece, current_key, feed_factory, format, assoc_key, system):
+    service_name = current_key
+    feed = feed_factory(piece)
+    ass = htypes.assoc_key.key_base_association()
+    if service_name in assoc_key:
+        del assoc_key[service_name]
+    else:
+        assoc_key[service_name] = ass
+    item = _service_item(system, assoc_key, format, service_name)
+    await feed.send(KeyListDiff.Replace(service_name, item))
 
 
 @mark.global_command
