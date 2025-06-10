6f0146d93eab561412a93eccb569abc95a371c94
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-06-10 13:44:19 +0300

    Add init hook for assoc_key

--- hyperapp/base/assoc_key.dyn.py
+++ hyperapp/base/assoc_key.dyn.py
@@ -14,6 +14,9 @@ class AssociationKeyRegistry:
         self._assoc_key_creg = assoc_key_creg
         self._config = config
 
+    def init(self, system):
+        system.add_config_hook(self)
+
     def __contains__(self, key):
         return key in self._config
 
@@ -26,9 +29,21 @@ class AssociationKeyRegistry:
     def get(self, key):
         return self._config.get(key)
 
+    # System config hook method
+    def config_item_set(self, service_name, cfg_item):
+        assert 0, (service_name, cfg_item)
+
+    # System config hook method
+    def config_item_removed(self, service_name, cfg_item):
+        assert 0, (service_name, cfg_item)
+
 
 def assoc_key(config, assoc_key_creg, system):
     return AssociationKeyRegistry(system, assoc_key_creg, config)
+
+
+def init_assoc_key(assoc_key, system):
+    assoc_key.init(system)
 
 
 class KeyBaseAssociation:
