45c2143b84930f878603dc2b80910703e34a5757
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-06-11 12:05:27 +0300

    Convert init_hook service to non-builtin service

--- hyperapp/base/subprocess/subprocess_system_main.dyn.py
+++ hyperapp/base/subprocess/subprocess_system_main.dyn.py
@@ -1,5 +1,8 @@
-from .code.system import run_config
+from .code.system import System
 
 
 def system_main(connection, received_refs, system_config_piece, root_name, **kw):
-    run_config(system_config_piece, root_name, connection, received_refs, **kw)
+    system = System()
+    system.load_config(system_config_piece)
+    system['init_hook'].run_hooks()
+    system.run(root_name, connection, received_refs, **kw)
