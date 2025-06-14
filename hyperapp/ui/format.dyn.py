40ea023b21aefaecd757027127ac7f76efacbaf3
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-06-14 10:57:41 +0300

    Formatter service: Add types handling

--- hyperapp/ui/format.dyn.py
+++ hyperapp/ui/format.dyn.py
@@ -1,3 +1,4 @@
+from hyperapp.boot.htypes import Type
 from hyperapp.boot.config_item_missing import ConfigItemMissingError
 
 from .code.mark import mark
@@ -5,6 +6,8 @@ from .code.mark import mark
 
 @mark.service
 def format(formatter_creg, piece):
+    if isinstance(piece, Type):
+        return str(piece)
     try:
         return formatter_creg.animate(piece)
     except ConfigItemMissingError as x:
