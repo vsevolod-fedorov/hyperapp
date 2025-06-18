246e47b03b6e7df0e73c35928d5f386ff31ab448
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-06-18 13:24:07 +0300

    WIP: Split config items to cfg and value resolvers -- Update feed type template

--- hyperapp/ui/feed_type_template.dyn.py
+++ hyperapp/ui/feed_type_template.dyn.py
@@ -1,37 +1,14 @@
-from . import htypes
 from .services import pyobj_creg
 from .code.mark import mark
 
 
-class FeedTypeTemplate:
+@mark.actor.cfg_item_creg
+def resolve_feed_type_cfg_item(piece):
+    model_t = pyobj_creg.invite(piece.model_t)
+    return (model_t, piece)
 
-    @classmethod
-    @mark.actor.cfg_item_creg
-    def from_piece(cls, piece):
-        return cls(
-            model_t=pyobj_creg.invite(piece.model_t),
-            feed_type_ref=piece.feed_type,
-            )
 
-    def __init__(self, model_t, feed_type_ref):
-        self._model_t = model_t
-        self._feed_type_ref = feed_type_ref
-
-    @property
-    def piece(self):
-        return htypes.feed.feed_template(
-            model_t=pyobj_creg.actor_to_ref(self._model_t),
-            feed_type=self._feed_type_ref,
-            )
-
-    @property
-    def key(self):
-        return self._model_t
-
-    def resolve(self, system, service_name):
-        # Should resolve it here manually.
-        # If added as dependency to from_piece, it fails resolving
-        # because cfg_item_creg is a system resource and resolved
-        # before regular resources such as feed_type_creg.
-        feed_type_creg = system.resolve_service('feed_type_creg')
-        return feed_type_creg.invite(self._feed_type_ref)
+@mark.actor.cfg_value_creg
+def resolve_feed_type_cfg_value(piece, key, system, service_name):
+    feed_type_creg = system.resolve_service('feed_type_creg')
+    return feed_type_creg.invite(piece.feed_type)
--- hyperapp/ui/feed_type_template.tests.dyn.py
+++ hyperapp/ui/feed_type_template.tests.dyn.py
@@ -6,14 +6,15 @@ from .services import (
 from .tested.code import feed_type_template
 
 
-def test_feed_type_template():
+def test_feed_type_template(system):
     piece_t = htypes.feed_type_template_tests.sample_view
-    feed_type = htypes.feed.list_feed_type(
+    feed_type_piece = htypes.feed.list_feed_type(
         item_t=pyobj_creg.actor_to_ref(htypes.feed_type_template_tests.sample_item),
         )
     template_piece = htypes.feed.feed_template(
         model_t=pyobj_creg.actor_to_ref(piece_t),
-        feed_type=mosaic.put(feed_type),
+        feed_type=mosaic.put(feed_type_piece),
         )
-    template = feed_type_template.FeedTypeTemplate.from_piece(template_piece)
-    assert template.piece == template_piece
+    key, piece = feed_type_template.resolve_feed_type_cfg_item(template_piece)
+    assert key == piece_t
+    feed_type = feed_type_template.resolve_feed_type_cfg_value(template_piece, key, system, '<unused-service-name>')
