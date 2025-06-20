bdd556e807b56f6ec032b356c302cd924e696154
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-06-20 11:33:00 +0300

    Update view_factory: Catch ConfigItemMissingError instead of KeyError

--- hyperapp/ui/view/view_factory.dyn.py
+++ hyperapp/ui/view/view_factory.dyn.py
@@ -1,5 +1,7 @@
 import inspect
 
+from hyperapp.boot.config_key_error import ConfigItemMissingError
+
 from . import htypes
 from .services import (
     mosaic,
@@ -179,7 +181,7 @@ class ViewFactoryReg:
                 model_t = real_model_t(model)
             try:
                 ui_t, system_fn = self._visualizer_reg(model_t)
-            except KeyError:
+            except ConfigItemMissingError:
                 ui_t = None
                 system_fn = None
         item_list = []
