206663283be0d686181b6bed37d7d33d8d52664b
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-06-26 11:41:21 +0300

    Route table: Store all local routes, not just one

--- hyperapp/base/transport/route_table.dyn.py
+++ hyperapp/base/transport/route_table.dyn.py
@@ -1,4 +1,5 @@
 import logging
+from collections import defaultdict
 
 from .services import (
     web,
@@ -13,25 +14,23 @@ class RouteTable:
         self._service_name = service_name
         self._config = config
         self._route_registry = route_registry
-        self._peer2route = {}
+        self._peer2route_list = defaultdict(list)
 
     def add_route(self, peer_ref, route):
-        self._peer2route[peer_ref] = route
+        self._peer2route_list[peer_ref].append(route)
         if route.piece is None:
             return  # Local route.
         peer_piece = web.summon(peer_ref)
         self._config[peer_piece] = route.piece
 
     def peer_route_list(self, peer_ref):
-        try:
-            route = self._peer2route[peer_ref]
-            return {route}
-        except KeyError:
-            pass
+        route_list = self._peer2route_list.get(peer_ref)
+        if route_list:
+            return route_list
         peer_piece = web.summon(peer_ref)
         route_piece = self._config[peer_piece]
         route = self._route_registry.animate(route_piece)
-        return {route}
+        return [route]
 
 
 def route_table(config, route_registry):
