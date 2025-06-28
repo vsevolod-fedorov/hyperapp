1f85954d1c7b3965f08da53f0b791a1dea7b6971
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-06-28 11:35:23 +0300

    Make cfg_item.typed_cfg_item config builtin

--- hyperapp/base/typed_cfg_item.dyn.py
+++ hyperapp/base/typed_cfg_item.dyn.py
@@ -1,3 +1,4 @@
+from . import htypes
 from .services import (
     pyobj_creg,
     web,
@@ -16,3 +17,15 @@ def resolve_typed_cfg_value(piece, key, system, service_name):
 def resolve_typed_fn_cfg_value(piece, key, system, service_name):
     system_fn_creg = system.resolve_service('system_fn_creg')
     return system_fn_creg.invite(piece.system_fn)
+
+
+def typed_cfg_item_config():
+    return {
+        htypes.cfg_item.typed_cfg_item: resolve_typed_cfg_item,
+        }
+
+
+def typed_cfg_value_config():
+    return {
+        htypes.cfg_item.typed_cfg_item: resolve_typed_cfg_value,
+        }
