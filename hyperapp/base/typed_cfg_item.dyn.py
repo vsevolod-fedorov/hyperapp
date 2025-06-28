1166feefb634708a881c5e2ad3b1fd520472b7d2
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-06-28 13:13:19 +0300

    Cleanup: Remove cfg_item.typed_fn_cfg_item type; cfg item tests, typed_cfg_item value resolver

--- hyperapp/base/typed_cfg_item.dyn.py
+++ hyperapp/base/typed_cfg_item.dyn.py
@@ -11,23 +11,7 @@ def resolve_typed_cfg_item(piece):
     return (t, value)
 
 
-def resolve_typed_cfg_value(piece, key, system, service_name):
-    assert 0, (key, piece, service_name)  # TODO: Remove.
-    return web.summon(piece.value)
-
-
-def resolve_typed_fn_cfg_value(piece, key, system, service_name):
-    system_fn_creg = system.resolve_service('system_fn_creg')
-    return system_fn_creg.invite(piece.system_fn)
-
-
 def typed_cfg_item_config():
     return {
         htypes.cfg_item.typed_cfg_item: resolve_typed_cfg_item,
-        }
-
-
-def typed_cfg_value_config():
-    return {
-        htypes.cfg_item.typed_cfg_item: resolve_typed_cfg_value,
         }
