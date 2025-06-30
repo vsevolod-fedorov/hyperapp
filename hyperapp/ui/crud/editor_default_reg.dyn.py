6b9501efdbf37c1a10aef3c4daaacc42189014f0
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-06-30 11:05:01 +0300

    Editor default reg: Use data value ctl

--- hyperapp/ui/crud/editor_default_reg.dyn.py
+++ hyperapp/ui/crud/editor_default_reg.dyn.py
@@ -1,6 +1,7 @@
 from .code.mark import mark
+from .code.config_ctl import DataValueCtl, DictConfigCtl
 
 
-@mark.service
+@mark.service(ctl=DictConfigCtl(value_ctl=DataValueCtl()))
 def editor_default_reg(config):
     return config
