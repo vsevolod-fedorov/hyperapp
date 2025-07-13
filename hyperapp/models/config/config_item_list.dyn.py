7b663af0e9b0d2bd4bc2b300226cd661a9d8754a
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-07-13 13:03:23 +0300

    Config item list: Separate model for layer items

--- hyperapp/models/config/config_item_list.dyn.py
+++ hyperapp/models/config/config_item_list.dyn.py
@@ -14,19 +14,20 @@ from .code.mark import mark
 from .code.data_browser import data_browser
 
 
+def _enum_items(config):
+    if type(config) in {list, set}:
+        for item in config:
+            yield ('', item)
+    elif type(config) is dict:
+        for key, value in config.items():
+            for _, item in _enum_items(value):
+                yield key, item
+    else:
+        yield ('', config)
+
+
 @mark.model(key='key')
 def config_item_list(piece, system, format):
-
-    def enum_items(config):
-        if type(config) in {list, set}:
-            for item in config:
-                yield ('', item)
-        elif type(config) is dict:
-            for key, value in config.items():
-                for _, item in enum_items(value):
-                    yield key, item
-        else:
-            yield ('', config)
 
     key_to_item_layer = defaultdict(list)
     for layer_name, layer in system.name_to_layer.items():
@@ -34,7 +35,7 @@ def config_item_list(piece, system, format):
             config = layer.config[piece.service_name]
         except KeyError:
             continue
-        for key, value in enum_items(config):
+        for key, value in _enum_items(config):
             key_to_item_layer[key].append((value, layer_name))
 
     def item_layers(key, value):
@@ -44,12 +45,10 @@ def config_item_list(piece, system, format):
                 result.append(layer_name)
         return result
 
-    if piece.layer:
-        config_template = system.get_layer_config_templates(piece.layer).get(piece.service_name)
-    else:
-        config_template = system.get_config_template(piece.service_name)
+    config_template = system.get_config_template(piece.service_name)
+
     item_list = []
-    for key, value in sorted(enum_items(config_template), key=lambda rec: str(rec[0])):
+    for key, value in sorted(_enum_items(config_template), key=lambda rec: str(rec[0])):
         if isinstance(key, Type):
             key_data = pyobj_creg.actor_to_piece(key)
         else:
@@ -72,12 +71,41 @@ def config_item_list(piece, system, format):
     return item_list
 
 
+@mark.model(key='key')
+def config_item_layer_list(piece, system, format):
+    config_template = system.get_layer_config_templates(piece.layer).get(piece.service_name)
+    item_list = []
+    for key, value in sorted(_enum_items(config_template), key=lambda rec: str(rec[0])):
+        if isinstance(key, Type):
+            key_data = pyobj_creg.actor_to_piece(key)
+        else:
+            key_data = key
+        try:
+            deduce_t(value)
+            value_data = value
+        except DeduceTypeError:
+            value_data = None  # TODO: do these cases exists?
+        item = htypes.config_item_list.layer_item(
+            key=mosaic.put(key_data),
+            key_str=format(key),
+            value=mosaic.put_opt(value),
+            value_str=str(value),
+            )
+        item_list.append(item)
+    return item_list
+
+
 @mark.command(preserve_remote=True)
 def open_config_item_list(piece, current_item):
-    return htypes.config_item_list.model(
-        layer=piece.layer,
-        service_name=current_item.service_name,
-        )
+    if piece.layer:
+        return htypes.config_item_list.layer_model(
+            layer=piece.layer,
+            service_name=current_item.service_name,
+            )
+    else:
+        return htypes.config_item_list.model(
+            service_name=current_item.service_name,
+            )
 
 
 @mark.command
@@ -90,6 +118,16 @@ def open_config_key(piece, current_item):
 def open_config_value(piece, current_item):
     value = web.summon(current_item.value)
     return data_browser(value)
+
+
+def _move_item(system, source_layer_name, target_layer_name, service_name, key):
+    source_layer = system.name_to_layer[source_layer_name]
+    target_layer = system.name_to_layer[target_layer_name]
+    key_piece = web.summon(key)
+    service_config = system.get_config_template(service_name)
+    value_template = service_config[key_piece]
+    target_layer.set(service_name, key_piece, value_template)
+    source_layer.remove(service_name, key_piece)
 
 
 @mark.crud.get_layer(commit_action='move')
@@ -107,28 +145,57 @@ def config_item_move_to_another_layer(piece, key, layers, value, system):
     target_layer_name = value.name
     if target_layer_name == source_layer_name:
         return
-    source_layer = system.name_to_layer[source_layer_name]
-    target_layer = system.name_to_layer[target_layer_name]
-    key_piece = web.summon(key)
-    service_config = system.get_config_template(piece.service_name)
-    value_template = service_config[key_piece]
-    target_layer.set(piece.service_name, key_piece, value_template)
-    source_layer.remove(piece.service_name, key_piece)
+    _move_item(system, source_layer_name, target_layer_name, piece.service_name, key)
     return (piece, key)
+
+
+
+@mark.crud.get_layer(commit_action='move')
+def config_layer_item_get_layer(piece):
+    return htypes.config_layer_list.layer(
+        name=piece.layer,
+        )
+
+
+@mark.crud.move
+def config_layer_item_move_to_another_layer(piece, key, value, system):
+    source_layer_name = piece.layer
+    target_layer_name = value.name
+    if target_layer_name == source_layer_name:
+        return
+    _move_item(system, source_layer_name, target_layer_name, piece.service_name, key)
+    target_piece = htypes.config_item_list.layer_model(
+        layer=target_layer_name,
+        service_name=piece.service_name,
+        )
+    return (target_piece, key)
+
+
+def _remove_item(system, layer_name, service_name, key):
+    layer = system.name_to_layer[layer_name]
+    key = web.summon(key)
+    layer.remove(service_name, key)
 
 
 @mark.command.remove
 def remove(piece, current_item, system):
     layer_name = current_item.layers[0]  # Will remove from first layer.
-    layer = system.name_to_layer[layer_name]
-    key = web.summon(current_item.key)
-    layer.remove(piece.service_name, key)
+    _remove_item(system, layer_name, piece.service_name, current_item.key)
+    return True
+
+
+@mark.command.remove
+def layer_remove(piece, current_item, system):
+    layer_name = piece.layer
+    _remove_item(system, layer_name, piece.service_name, current_item.key)
     return True
 
 
 @mark.actor.formatter_creg
 def format_model(piece):
-    if piece.layer:
-        return f"Config: {piece.layer}/{piece.service_name}"
-    else:
-        return f"Config: {piece.service_name}"
+    return f"Config: {piece.service_name}"
+
+
+@mark.actor.formatter_creg
+def format_layer_model(piece):
+    return f"Config: {piece.layer}/{piece.service_name}"
--- hyperapp/models/config/config_item_list.tests.dyn.py
+++ hyperapp/models/config/config_item_list.tests.dyn.py
@@ -11,7 +11,6 @@ from .tested.code import config_item_list
 
 def test_item_list_model():
     piece = htypes.config_item_list.model(
-        layer=None,
         service_name='system',
         )
     item_list = config_item_list.config_item_list(piece)
@@ -19,7 +18,17 @@ def test_item_list_model():
     assert isinstance(item_list[0], htypes.config_item_list.item)
 
 
-def test_open():
+def test_item_list_layer_model(system):
+    piece = htypes.config_item_list.layer_model(
+        layer=system.default_layer_name,
+        service_name='system',
+        )
+    item_list = config_item_list.config_item_layer_list(piece)
+    assert item_list
+    assert isinstance(item_list[0], htypes.config_item_list.layer_item)
+
+
+def test_open_model():
     piece = htypes.config_service_list.model(
         layer=None,
         )
@@ -32,6 +41,32 @@ def test_open():
     assert isinstance(model, htypes.config_item_list.model)
 
 
+def test_open_layer_model(system):
+    piece = htypes.config_service_list.model(
+        layer=system.default_layer_name,
+        )
+    current_item = htypes.config_service_list.item(
+        service_name='sample_service',
+        item_count=0,  # Unused.
+        assoc='',  # Unused
+        )
+    model = config_item_list.open_config_item_list(piece, current_item)
+    assert isinstance(model, htypes.config_item_list.layer_model)
+
+
+def test_open_layer_model(system):
+    piece = htypes.config_service_list.model(
+        layer=system.default_layer_name,
+        )
+    current_item = htypes.config_service_list.item(
+        service_name='sample_service',
+        item_count=0,  # Unused.
+        assoc='',  # Unused
+        )
+    model = config_item_list.open_config_item_list(piece, current_item)
+    assert isinstance(model, htypes.config_item_list.layer_model)
+
+
 @mark.fixture(ctl=DataServiceConfigCtl())
 def sample_service(config):
     return config
@@ -40,7 +75,14 @@ def sample_service(config):
 @mark.fixture
 def piece():
     return htypes.config_item_list.model(
-        layer=None,
+        service_name='sample_service',
+        )
+
+
+@mark.fixture
+def layer_piece(system):
+    return htypes.config_item_list.layer_model(
+        layer=system.default_layer_name,
         service_name='sample_service',
         )
 
@@ -59,6 +101,18 @@ def test_open_key(piece):
     assert browser
 
 
+def test_layer_open_key(layer_piece):
+    key = htypes.config_item_list_tests.sample_key(123)
+    current_item = htypes.config_item_list.layer_item(
+        key=mosaic.put(key),
+        key_str="<unused>",
+        value=None,
+        value_str="<unused>",
+        )
+    browser = config_item_list.open_config_key(layer_piece, current_item)
+    assert browser
+
+
 def test_open_value(piece):
     value = htypes.config_item_list_tests.sample_value("some desc")
     current_item = htypes.config_item_list.item(
@@ -73,11 +127,28 @@ def test_open_value(piece):
     assert browser
 
 
+def test_layer_open_value(layer_piece):
+    value = htypes.config_item_list_tests.sample_value("some desc")
+    current_item = htypes.config_item_list.layer_item(
+        key=mosaic.put("<unused>"),
+        key_str="<unused>",
+        value=mosaic.put(value),
+        value_str="<unused>",
+        )
+    browser = config_item_list.open_config_value(layer_piece, current_item)
+    assert browser
+
+
 def test_get_layer(piece):
     layer = config_item_list.config_item_get_layer(
         piece,
         layers=('source-layer',),
         )
+    assert isinstance(layer, htypes.config_layer_list.layer)
+
+
+def test_layer_get_layer(layer_piece):
+    layer = config_item_list.config_layer_item_get_layer(layer_piece)
     assert isinstance(layer, htypes.config_layer_list.layer)
 
 
@@ -100,6 +171,34 @@ def test_move(system, piece):
         name='target-layer',
         )
     config_item_list.config_item_move_to_another_layer(piece, key_ref, layers, value)
+    source_layer.remove.assert_called_once()
+    target_layer.set.assert_called_once()
+
+
+def test_layer_move(system):
+    piece = htypes.config_item_list.layer_model(
+        layer='source-layer',
+        service_name='sample_service',
+        )
+
+    key = 456
+
+    config = {
+        'sample_service': {
+            key: 'value-456',
+            }
+        }
+    source_layer = Mock(config=config)
+    target_layer = Mock(config={})
+    system.load_config_layer('source-layer', source_layer)
+    system.load_config_layer('target-layer', target_layer)
+
+    key_ref = mosaic.put(key)
+    layers = ('source-layer',)
+    value = htypes.config_layer_list.layer(
+        name='target-layer',
+        )
+    config_item_list.config_layer_item_move_to_another_layer(piece, key_ref, value)
     source_layer.remove.assert_called_once()
     target_layer.set.assert_called_once()
 
@@ -128,6 +227,38 @@ def test_remove(system, piece):
     layer.remove.assert_called_once()
 
 
+def test_layer_remove(system):
+    piece = htypes.config_item_list.layer_model(
+        layer='sample-layer',
+        service_name='sample_service',
+        )
+
+    key = 123
+    config = {
+        'sample_service': {
+            key: 'value-123',
+            }
+        }
+    layer = Mock(config=config)
+    system.load_config_layer('sample-layer', layer)
+
+    key = htypes.config_item_list_tests.sample_key(key)
+    current_item = htypes.config_item_list.layer_item(
+        key=mosaic.put(key),
+        key_str="<unused>",
+        value=None,
+        value_str="<unused>",
+        )
+    result = config_item_list.layer_remove(piece, current_item)
+    assert result is True
+    layer.remove.assert_called_once()
+
+
 def test_format_model(piece):
     title = config_item_list.format_model(piece)
+    assert type(title) is str
+
+
+def test_format_layer_model(layer_piece):
+    title = config_item_list.format_layer_model(layer_piece)
     assert type(title) is str
