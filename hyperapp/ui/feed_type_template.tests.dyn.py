f848cfcaf3c486475d06f202ea225ac0643735bf
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-07-19 11:49:16 +0300

    Improve feed type template tests

--- hyperapp/ui/feed_type_template.tests.dyn.py
+++ hyperapp/ui/feed_type_template.tests.dyn.py
@@ -3,18 +3,31 @@ from .services import (
     mosaic,
     pyobj_creg,
     )
+from .code.mark import mark
 from .tested.code import feed_type_template
 
 
-def test_feed_type_template(system):
-    piece_t = htypes.feed_type_template_tests.sample_view
+@mark.fixture.obj
+def piece_t():
+    return htypes.feed_type_template_tests.sample_model
+
+
+@mark.fixture
+def template_piece(piece_t):
     feed_type_piece = htypes.feed.list_feed_type(
         item_t=pyobj_creg.actor_to_ref(htypes.feed_type_template_tests.sample_item),
         )
-    template_piece = htypes.feed.feed_template(
+    return htypes.feed.feed_template(
         model_t=pyobj_creg.actor_to_ref(piece_t),
         feed_type=mosaic.put(feed_type_piece),
         )
+
+
+def test_cfg_item(system, piece_t, template_piece):
     key, piece = feed_type_template.resolve_feed_type_cfg_item(template_piece)
     assert key == piece_t
+
+
+def test_cfg_value(system, template_piece):
+    key, piece = feed_type_template.resolve_feed_type_cfg_item(template_piece)
     feed_type = feed_type_template.resolve_feed_type_cfg_value(template_piece, key, system, '<unused-service-name>')
