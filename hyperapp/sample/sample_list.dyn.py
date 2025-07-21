5fe96881a4aca9d2c88bcccf974d851b9d0162ee
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-07-21 11:54:06 +0300

    Sample feed list: Add support for server-side (sync)

--- hyperapp/sample/sample_list.dyn.py
+++ hyperapp/sample/sample_list.dyn.py
@@ -1,5 +1,7 @@
 import asyncio
 import logging
+import time
+from concurrent.futures import ThreadPoolExecutor
 
 from . import htypes
 from .code.mark import mark
@@ -23,16 +25,40 @@ async def open_sample_fn_list():
     return htypes.sample_list.sample_list()
 
 
-async def _send_diff(feed):
-    await asyncio.sleep(1)
+def _send_diff(feed):
+    log.info("Sending diff")
     item = htypes.sample_list.item(4, "fourth","Sample item #4")
     feed.send(IndexListDiff.Append(item))
 
 
+def _send_diff_sync(feed):
+    time.sleep(1)
+    _send_diff(feed)
+
+
+@mark.service
+def _sample_thread_pool():
+    pool = ThreadPoolExecutor(max_workers=1)
+    log.info("Sample list: Thread pool created")
+    yield pool
+    pool.shutdown()
+
+
+def _post_diff_task(thread_pool, feed):
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        log.info("Sending diff sync")
+        thread_pool.submit(_send_diff_sync, feed)
+    else:
+        log.info("Sending diff async")
+        loop.call_later(1, _send_diff, feed)
+
+
 @mark.model
-def feed_sample_list(piece, feed_factory):
+def feed_sample_list(piece, _sample_thread_pool, feed_factory):
     feed = feed_factory(piece)
-    asyncio.create_task(_send_diff(feed))
+    _post_diff_task(_sample_thread_pool, feed)
     return [
         htypes.sample_list.item(1, "first", "First sample"),
         htypes.sample_list.item(2, "second", "Second sample"),
