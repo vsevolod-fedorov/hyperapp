25b5831cbe1bdeb867701c0bdfe5380a0350c04d
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-07-23 10:57:44 +0300

    RC: Add config template fixture support

--- hyperapp/rc/config_fixture_marker.dyn.py
+++ hyperapp/rc/config_fixture_marker.dyn.py
@@ -1,6 +1,6 @@
 import inspect
 
-from .code.fixture_ctr import ConfigFixtureCtr
+from .code.fixture_ctr import ConfigFixtureCtr, ConfigTemplateFixtureCtr
 
 
 def config_fixture_marker(service_name, module_name, ctr_collector):
@@ -14,3 +14,17 @@ def config_fixture_marker(service_name, module_name, ctr_collector):
         ctr_collector.add_constructor(ctr)
         return fn
     return _config_fixture_wrapper
+
+
+def config_template_fixture_marker(service_name, module_name, ctr_collector):
+    def _config_template_fixture_wrapper(fn):
+        service_params = tuple(inspect.signature(fn).parameters)
+        ctr = ConfigTemplateFixtureCtr(
+            module_name=module_name,
+            attr_qual_name=fn.__qualname__.split('.'),
+            service_name=service_name,
+            service_params=tuple(inspect.signature(fn).parameters),
+            )
+        ctr_collector.add_constructor(ctr)
+        return fn
+    return _config_template_fixture_wrapper
--- hyperapp/rc/fixture_resource.dyn.py
+++ hyperapp/rc/fixture_resource.dyn.py
@@ -1,12 +1,14 @@
 from . import htypes
 from .services import (
     mosaic,
+    pyobj_creg,
     web,
     )
 from .code.rc_resource import Resource
 from .code.system_probe import ConfigFixture
 
 
+# Config fixtures return resolved configs, which are NOT passed to subprocesses.
 class ConfigFixtureResource(Resource):
 
     @classmethod
@@ -40,3 +42,44 @@ class ConfigFixtureResource(Resource):
     def configure_system(self, system):
         fixture = ConfigFixture(self._function, self._service_params)
         system.add_item_fixtures(self._service_name, [fixture])
+
+
+# Config tempate fixtures return unresolved config templates, with ARE passed to subprocesses.
+class ConfigTemplateFixtureResource(Resource):
+
+    @classmethod
+    def from_piece(cls, piece):
+        return cls(piece.service_name, web.summon(piece.function), piece.service_params)
+
+    def __init__(self, service_name, function, service_params):
+        self._service_name = service_name
+        self._function = function  # piece
+        self._service_params = service_params
+
+    def __eq__(self, rhs):
+        return (
+            self.__class__ is rhs.__class__
+            and self._service_name == rhs._service_name
+            and self._function == rhs._function
+            and self._service_params == rhs._service_params
+            )
+
+    def __hash__(self):
+        return hash(('config-fixture-resource', self._service_name, self._function, self._service_params))
+
+    @property
+    def piece(self):
+        return htypes.fixture_resource.config_template_fixture_resource(
+            service_name=self._service_name,
+            function=mosaic.put(self._function),
+            service_params=tuple(self._service_params),
+            )
+
+    def configure_system(self, system):
+        fn = pyobj_creg.animate(self._function)
+        service_args = [
+            system.resolve_service(name)
+            for name in self._service_params
+            ]
+        config_template = fn(*service_args)
+        system.default_layer.update(self._service_name, config_template)
