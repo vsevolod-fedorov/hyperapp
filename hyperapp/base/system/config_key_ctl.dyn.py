f58cedf3189c59ce0133f115fce5c1e478f01172
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-07-24 11:37:21 +0300

    Config ctl: Add typed key ctl

--- hyperapp/base/system/config_key_ctl.dyn.py
+++ hyperapp/base/system/config_key_ctl.dyn.py
@@ -1,6 +1,11 @@
 from functools import partial
 
 from . import htypes
+from .services import (
+    mosaic,
+    pyobj_creg,
+    web,
+    )
 
 
 # Only items converted from data can be converted to data.
@@ -25,8 +30,30 @@ class OneWayKeyCtl:
         return self._cfg_item_creg.actor_to_piece((key, template))
 
 
+class TypeKeyCtl:
+
+    @classmethod
+    def from_piece(cls, piece):
+        return cls()
+
+    @property
+    def piece(self):
+        return htypes.system.type_key_ctl()
+
+    def data_to_item(self, piece):
+        key = pyobj_creg.invite(piece.t)
+        template = web.summon(piece.value)
+        return (key, template)
+
+    def item_to_data(self, key, template):
+        return htypes.cfg_item.typed_cfg_item(
+            t=pyobj_creg.actor_to_ref(key),
+            value=mosaic.put(template),
+            )
+
 
 def config_key_ctl_creg_config(cfg_item_creg):
     return {
         htypes.system.one_way_key_ctl: partial(OneWayKeyCtl.from_piece, cfg_item_creg=cfg_item_creg),
+        htypes.system.type_key_ctl: TypeKeyCtl.from_piece,
         }
