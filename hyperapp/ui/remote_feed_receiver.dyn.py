324750249d34c2b5d23a9ba00adc4a7c43190e03
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-07-24 14:07:34 +0300

    Improve logging

--- hyperapp/ui/remote_feed_receiver.dyn.py
+++ hyperapp/ui/remote_feed_receiver.dyn.py
@@ -8,6 +8,6 @@ log = logging.getLogger(__name__)
 @mark.service
 def remote_feed_receiver(diff_creg, feed_factory, model, diff):
     diff_obj = diff_creg.animate(diff)
-    log.info("Received remote diff: %s", diff_obj)
+    log.info("Received remote diff for %s: %s", model, diff_obj)
     feed = feed_factory(model)
     feed.send(diff_obj)
