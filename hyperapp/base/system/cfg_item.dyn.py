148abcae87ee58cb9cd4c3299387795c35432d12
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-07-30 11:22:06 +0300

    Add str_cfg_item type and resolver

--- hyperapp/base/system/cfg_item.dyn.py
+++ hyperapp/base/system/cfg_item.dyn.py
@@ -5,6 +5,12 @@ from .services import (
     )
 
 
+def resolve_str_cfg_item(piece):
+    key = piece.key
+    value = web.summon(piece.value)
+    return (key, value)
+
+
 def resolve_typed_cfg_item(piece):
     t = pyobj_creg.invite(piece.t)
     value = web.summon(piece.value)
@@ -13,5 +19,6 @@ def resolve_typed_cfg_item(piece):
 
 def cfg_item_config():
     return {
+        htypes.cfg_item.str_cfg_item: resolve_str_cfg_item,
         htypes.cfg_item.typed_cfg_item: resolve_typed_cfg_item,
         }
