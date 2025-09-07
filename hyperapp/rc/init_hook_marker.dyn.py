70cb80486da4acd0f2120f6d5b6c9606f38996b4
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-09-07 12:09:25 +0300

    RC: Add Init hook probe

--- hyperapp/rc/init_hook_marker.dyn.py
+++ hyperapp/rc/init_hook_marker.dyn.py
@@ -1,13 +1,34 @@
 import inspect
 
+from .code.probe import real_fn
+from .code.marker_utils import split_actor_params
 from .code.init_hook_ctr import InitHookCtr
 
 
-def init_hook_marker(fn, module_name, ctr_collector):
+class InitHookProbe:
+
+    def __init__(self, system_probe, fn):
+        self._system = system_probe
+        self._fn = fn
+
+    @property
+    def real_fn(self):
+        return real_fn(self._fn)
+
+    def __call__(self):
+        params = split_actor_params(self.real_fn, [], {})
+        service_kw = {
+            name: self._system.resolve_service(name)
+            for name in params.service_names
+            }
+        return self._fn(**service_kw)
+
+
+def init_hook_marker(fn, module_name, system, ctr_collector):
     ctr = InitHookCtr(
         module_name=module_name,
         attr_qual_name=fn.__qualname__.split('.'),
         service_params=tuple(inspect.signature(fn).parameters),
         )
     ctr_collector.add_constructor(ctr)
-    return fn
+    return InitHookProbe(system, fn)
