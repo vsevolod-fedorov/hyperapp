9dee11419b4b98d14bf5a091e4563f3149a78da0
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-10-10 11:52:33 +0300

    Create remote peer web source

--- hyperapp/client/local_server_web_source.dyn.py
+++ hyperapp/client/local_server_web_source.dyn.py
@@ -1,7 +1,20 @@
 from .code.mark import mark
+from .services import (
+    web,
+    )
+
+
+class RemotePeerWebSource:
+
+    def __init__(self, identity, peer):
+        self._identity = identity
+        self._peer = peer
+
+    def pull(self, ref):
+        assert 0, (self._identity, self._peer, ref)
 
 
 @mark.init_hook
 def init_local_server_web_source(peer_list_reg, client_identity):
     peer = peer_list_reg.get('localhost')
-    assert 0, client_identity
+    web.add_source(RemotePeerWebSource(client_identity, peer))
