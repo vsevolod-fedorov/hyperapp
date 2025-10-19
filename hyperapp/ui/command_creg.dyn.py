e069ca7bb5fb1543fd1ac4623dfde84facb83099
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-10-19 12:00:05 +0300

    ui_command_creg: Switch to ContextCodeRegistry

--- hyperapp/ui/command_creg.dyn.py
+++ hyperapp/ui/command_creg.dyn.py
@@ -2,11 +2,12 @@ from .services import (
     code_registry_ctr,
     )
 from .code.mark import mark
+from .code.context_code_registry import ContextCodeRegistry
 
 
 @mark.service
 def ui_command_creg(config):
-    return code_registry_ctr('ui_command_creg', config)
+    return ContextCodeRegistry('ui_command_creg', config)
 
 
 @mark.service
