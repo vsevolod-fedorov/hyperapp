345dc889e33e982765ddba7a6258bc356c5f8797
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2025-11-22 12:50:05 +0300

    Add command_group_reg service

--- hyperapp/ui/command_group.dyn.py
+++ hyperapp/ui/command_group.dyn.py
@@ -3,7 +3,16 @@ from .code.mark import mark
 
 
 @mark.service
-def get_command_group(command_key):
+def command_group_reg(config):
+    return config
+
+
+@mark.service
+def get_command_group(command_group_reg, command_key):
+    try:
+        return command_group_reg[command_key]
+    except KeyError:
+        pass
     if isinstance(command_key, htypes.command.model_command_key):
         return 'context'
     if isinstance(command_key, htypes.command.global_model_command_key):
