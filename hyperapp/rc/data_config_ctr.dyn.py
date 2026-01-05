03438871d04ca24733182298dcaf76915e8cb0ec
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2026-01-05 12:37:06 +0300

    RC: DataConfigCtr: Add request

--- hyperapp/rc/data_config_ctr.dyn.py
+++ hyperapp/rc/data_config_ctr.dyn.py
@@ -4,6 +4,7 @@ from .services import (
     )
 from .code.rc_constructor import ModuleCtr
 from .code.config_item_resource import ConfigItemResource
+from .code.cfg_item_req import CfgItemReq
 
 
 class DataConfigCtr(ModuleCtr):
@@ -30,8 +31,9 @@ class DataConfigCtr(ModuleCtr):
 
     def update_resource_targets(self, resource_tgt, target_set):
         resource_tgt.import_tgt.add_test_ctr(self)
+        req = CfgItemReq.from_actor(self._service_name, self._key)
         _, resolved_tgt, _ = target_set.factory.config_items(
-            self._service_name, self._name,
+            self._service_name, self._name, req,
             provider=resource_tgt,
             ctr=self,
             )
