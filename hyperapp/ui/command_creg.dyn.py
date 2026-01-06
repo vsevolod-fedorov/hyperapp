0fc12c7a2587443a0a234a0b6c9aa1d45daa467a
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2026-01-06 11:43:28 +0300

    Add command_enum_creg service

--- hyperapp/ui/command_creg.dyn.py
+++ hyperapp/ui/command_creg.dyn.py
@@ -5,3 +5,8 @@ from .code.context_code_registry import ContextCodeRegistry
 @mark.service
 def command_creg(config):
     return ContextCodeRegistry('command_creg', config)
+
+
+@mark.service
+def command_enum_creg(config):
+    return ContextCodeRegistry('command_enum_creg', config)
