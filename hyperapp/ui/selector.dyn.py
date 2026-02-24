0df80976c9120db5f734b0f6cbb70dafb49cb653
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2026-02-24 13:33:22 +0300

    Selector: Update template value resolver

--- hyperapp/ui/selector.dyn.py
+++ hyperapp/ui/selector.dyn.py
@@ -1,5 +1,6 @@
 from .services import (
     pyobj_creg,
+    web,
     )
 from .code.mark import mark
 from .code.context_code_registry import ContextCodeRegistry
@@ -7,10 +8,10 @@ from .code.context_code_registry import ContextCodeRegistry
 
 class Selector:
 
-    def __init__(self, model_t, get_fn, pick_fn):
+    def __init__(self, model_t, open_action, pick_action):
         self.model_t = model_t
-        self.get_fn = get_fn
-        self.pick_fn = pick_fn
+        self.open_action = open_action
+        self.pick_action = pick_action
 
 
 @mark.actor.cfg_value_creg
@@ -18,8 +19,8 @@ def resolve_selector_cfg_value(piece, key, system, service_name):
     system_fn_creg = system.resolve_service('system_fn_creg')
     return Selector(
         model_t=pyobj_creg.invite(piece.model_t),
-        get_fn=system_fn_creg.invite(piece.get_fn),
-        pick_fn=system_fn_creg.invite(piece.pick_fn),
+        open_action=web.summon(piece.open_action),
+        pick_action=web.summon(piece.pick_action),
         )
 
 
--- hyperapp/ui/selector.tests.dyn.py
+++ hyperapp/ui/selector.tests.dyn.py
@@ -7,21 +7,12 @@ from .code.mark import mark
 from .tested.code import selector as selector_module
 
 
-def _sample_fn(value):
-    return f'sample-fn: {value}'
-
-
 @mark.fixture.obj
 def template_piece():
-    system_fn = htypes.system_fn.ctx_fn(
-        function=pyobj_creg.actor_to_ref(_sample_fn),
-        ctx_params=('value',),
-        service_params=(),
-        )
     return htypes.selector.template(
         model_t=pyobj_creg.actor_to_ref(htypes.selector_tests.sample_model),
-        get_fn=mosaic.put(system_fn),
-        pick_fn=mosaic.put(system_fn),
+        open_action=mosaic.put(htypes.selector_tests.sample_open_action()),
+        pick_action=mosaic.put(htypes.selector_tests.sample_pick_action()),
         )
 
 
