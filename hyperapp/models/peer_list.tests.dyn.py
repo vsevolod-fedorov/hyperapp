dd7e45c23ba08bde31f285444ef4b14e4316177d
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2026-04-19 13:11:46 +0300

    Peer list tests: Rename piece -> model; update run_remotely test

--- hyperapp/models/peer_list.tests.dyn.py
+++ hyperapp/models/peer_list.tests.dyn.py
@@ -17,7 +17,7 @@ from .tested.code import peer_list
 
 
 @mark.fixture
-def piece():
+def model():
     return htypes.peer_list.model()
 
 
@@ -51,37 +51,37 @@ def peer_label_reg():
     return MagicMock()
 
 
-def test_model(piece):
-    item_list = peer_list.peer_list_model(piece)
+def test_model(model):
+    item_list = peer_list.peer_list_model(model)
     assert type(item_list) is list
 
 
-def test_add_localhost(peer_label_reg, piece):
+def test_add_localhost(peer_label_reg, model):
     host = 'localhost'
-    peer_list.add(piece, host)
+    peer_list.add(model, host)
     peer_label_reg.__setitem__.assert_called_once()
 
 
-def test_add_remote(bundler, generate_rsa_identity, peer_label_reg, piece):
+def test_add_remote(bundler, generate_rsa_identity, peer_label_reg, model):
     host = 'example-host'
     identity = generate_rsa_identity(fast=True)
     bundle = bundler([mosaic.put(identity.peer.piece)]).bundle
     data_json = packet_coders.encode('json', bundle, htypes.builtin.bundle)
     with patch.object(peer_list, 'subprocess') as subprocess:
         subprocess.check_output.return_value = data_json
-        peer_list.add(piece, host)
+        peer_list.add(model, host)
     peer_label_reg.__setitem__.assert_called_once()
 
 
 
-async def test_remove(feed_factory, piece, peer_name):
-    feed = feed_factory(piece)
+async def test_remove(feed_factory, model, peer_name):
+    feed = feed_factory(model)
     current_item = htypes.peer_list.item(
         name=peer_name,
         peer=mosaic.put("<unused>"),
         peer_repr="<unused>",
         )
-    await peer_list.remove(piece, current_item)
+    await peer_list.remove(model, current_item)
     await feed.wait_for_diffs(count=1)
 
 
@@ -143,27 +143,27 @@ def ctx(identity):
         )
 
 
-async def _test_run_global_command(ctx, piece, current_item, command_d):
-    command = htypes.global_commands.command_arg(
-        d=mosaic.put(command_d),
+async def _test_run_remotely(ctx, model, current_item, command_d):
+    command = htypes.command.command_arg(
+        command=mosaic.put(htypes.peer_list_tests.sample_command()),
         )
-    result = await peer_list.run_command(piece, current_item, command, ctx)
+    result = await peer_list.run_remotely(model, current_item, command, ctx)
     assert web.summon(result.model) == "Sample result"
 
 
-async def _test_open_model(ctx, piece, current_item, command_d):
+async def _test_open_model(ctx, model, current_item, command_d):
     command = htypes.global_commands.command_arg(
         d=mosaic.put(command_d),
         )
-    remote_model, key = await peer_list.open_model(piece, current_item, command, ctx)
+    remote_model, key = await peer_list.open_model(model, current_item, command, ctx)
     assert isinstance(remote_model, htypes.model.remote_model)
 
 
 def test_open():
-    piece = peer_list.open_peer_list()
-    assert piece
+    model = peer_list.open_peer_list()
+    assert model
 
 
-def test_format_model(piece):
-    title = peer_list.format_model(piece)
+def test_format_model(model):
+    title = peer_list.format_model(model)
     assert type(title) is str
