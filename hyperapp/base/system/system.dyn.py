2e33c7378c89e887a7e2d30619ad20b707077739
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2026-05-10 10:55:16 +0300

    WIP: Start adding services service

--- hyperapp/base/system/system.dyn.py
+++ hyperapp/base/system/system.dyn.py
@@ -15,6 +15,7 @@ from .code.config_ctl import DictConfigCtl, service_pieces_to_config
 # from .code.config_struct_ctl import config_struct_ctl_creg_config
 from .code.config_layer import ProjectConfigLayer, StaticConfigLayer
 from .code.config_item_ctl import StrKeyCtl, config_item_creg_config
+from .code.services import Services
 from .code.service_template import service_template_cfg_item_config, service_template_cfg_value_config
 from .code.actor_template import actor_template_cfg_item_config, actor_template_cfg_value_config
 
