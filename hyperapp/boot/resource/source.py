1b7dbd5c2f2cb4884e78281892d00add190b34a1
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2026-05-17 11:53:39 +0300

    Resource source: Switch to dataclass

--- hyperapp/boot/resource/source.py
+++ hyperapp/boot/resource/source.py
@@ -1,21 +1,6 @@
-from functools import cached_property
+from dataclasses import dataclass
 
 
+@dataclass(frozen=True)
 class ResourceModuleSource:
-
-    def __init__(self, name):
-        self._name = name
-
-    @property
-    def name(self):
-        return self._name
-
-    def __repr__(self):
-        return f"<ResourceModuleSource: {self._name}>"
-
-    def __eq__(self, rhs):
-        return type(rhs) is ResourceModuleSource and rhs._name == self._name
-
-    @cached_property
-    def __hash__(self):
-        return hash(('resource-module-source', self._name))
+    name: str
