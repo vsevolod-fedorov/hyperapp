88cf92f07c17e3c185f629bc253ed65c89540d62
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2026-05-31 13:56:32 +0300

    Update test_resource_type, without reverse resolver tests

--- hyperapp/boot/resource/test/test_resource_type.py
+++ hyperapp/boot/resource/test/test_resource_type.py
@@ -1,9 +1,11 @@
 import logging
 from pathlib import Path
+from unittest.mock import Mock
 
 import pytest
 
-from hyperapp.boot.htypes import tString, TOptional, TList, TRecord
+from hyperapp.boot.htypes import tInt, tString, TOptional, TList, TRecord
+from hyperapp.boot.htypes.meta_type import builtin_mt, optional_mt, list_mt, field_mt, record_mt
 from hyperapp.boot.htypes.partial import partial_param_t, partial_t
 
 log = logging.getLogger(__name__)
@@ -35,16 +37,29 @@ def test_definition_type_partial(resource_type_producer):
         })
 
 
-def test_definition_type_based(resource_type_factory, htypes):
-    resource_t = htypes.test_resources.test_resource_t
-    resource_type = resource_type_factory(resource_t)
+@pytest.fixture
+def a_record_t(pyobj_creg, mosaic):
+    ref_mt = builtin_mt('ref')
+    ref_opt_mt = optional_mt(mosaic.put(ref_mt))
+    base_record_mt = record_mt('a_module', 'base_record', base=None, fields=(
+        field_mt('a_ref_opt', mosaic.put(ref_opt_mt)),
+        ))
+    ref_list_mt = list_mt(mosaic.put(ref_mt))
+    a_record_mt = record_mt('a_module', 'a_record', base=mosaic.put(base_record_mt), fields=(
+        field_mt('a_ref_list', mosaic.put(ref_list_mt)),
+        ))
+    return  pyobj_creg.animate(a_record_mt)
+
+
+def test_definition_type_based(resource_type_factory, a_record_t):
+    resource_type = resource_type_factory(a_record_t)
     log.info("definition_t: %r", resource_type.definition_t)
-    expected_base_t = TRecord('test_resources', 'test_resource_t_base_def', {
-        'value': TOptional(tString),
+    expected_base_t = TRecord('a_module', 'base_record_def', {
+        'a_ref_opt': TOptional(tString),
         })
-    assert resource_type.definition_t == TRecord('test_resources', 'test_resource_t_def', {
-        'value': TOptional(tString),
-        'other_value': TList(tString),
+    assert resource_type.definition_t == TRecord('a_module', 'a_record_def', {
+        'a_ref_opt': TOptional(tString),
+        'a_ref_list': TList(tString),
         }, base=expected_base_t)
 
 
@@ -77,19 +92,18 @@ def test_from_dict_partial(resource_type_producer):
         )
 
 
-def test_from_dict_based(resource_type_factory, htypes):
-    resource_t = htypes.test_resources.test_resource_t
-    resource_type = resource_type_factory(resource_t)
+def test_from_dict_based(resource_type_factory, a_record_t):
+    resource_type = resource_type_factory(a_record_t)
     log.info("definition_t: %r", resource_type.definition_t)
     definition_dict = {
-        'value': 'some value',
-        'other_value': ['some other value 1', 'some other value 2'],
+        'a_ref_opt': 'some-int',
+        'a_ref_list': ['string-1', 'string-2'],
         }
     definition = resource_type.from_dict(definition_dict)
     log.info("definition: %r", definition)
     assert definition == resource_type.definition_t(
-        value='some value',
-        other_value=('some other value 1', 'some other value 2'),
+        a_ref_opt='some-int',
+        a_ref_list=('string-1', 'string-2'),
         )
 
 
@@ -115,18 +129,17 @@ def test_to_dict_partial(resource_type_producer):
         }
 
 
-def test_to_dict_based(resource_type_factory, htypes):
-    resource_t = htypes.test_resources.test_resource_t
-    resource_type = resource_type_factory(resource_t)
+def test_to_dict_based(resource_type_factory, a_record_t):
+    resource_type = resource_type_factory(a_record_t)
     definition = resource_type.definition_t(
-        value='some value',
-        other_value=('some other value 1', 'some other value 2'),
+        a_ref_opt='some-int',
+        a_ref_list=('string-1', 'string-2'),
         )
     definition_dict = resource_type.to_dict(definition)
     log.info("definition dict: %r", definition_dict)
     assert definition_dict == {
-        'value': 'some value',
-        'other_value': ['some other value 1', 'some other value 2'],
+        'a_ref_opt': 'some-int',
+        'a_ref_list': ['string-1', 'string-2'],
         }
 
 
@@ -147,10 +160,11 @@ def test_resolve_definition_partial(mosaic, resource_type_producer):
         'value_2': mosaic.put(222),
         }
 
-    def resolve_name(name):
+    def resolve_to_ref(name):
         return names[name]
 
-    resource = resource_type.resolve(definition, resolve_name, TEST_RESOURCES_DIR)
+    ctx = Mock(resolve_to_ref=resolve_to_ref)
+    resource, sources = resource_type.resolve(definition, ctx)
     log.info('Resolved resource: %r', resource)
     assert resource == resource_t(
         function=names['some_function'],
@@ -161,27 +175,27 @@ def test_resolve_definition_partial(mosaic, resource_type_producer):
     )
 
 
-def test_resolve_definition_based(mosaic, resource_type_factory, htypes):
-    resource_t = htypes.test_resources.test_resource_t
-    resource_type = resource_type_factory(resource_t)
+def test_resolve_definition_based(mosaic, resource_type_factory, a_record_t):
+    resource_type = resource_type_factory(a_record_t)
     definition = resource_type.definition_t(
-        value='some-value',
-        other_value=('other-1', 'other-2'),
+        a_ref_opt='some-int',
+        a_ref_list=('string-1', 'string-2'),
         )
     names = {
-        'some-value': mosaic.put('some value'),
-        'other-1': mosaic.put(111),
-        'other-2': mosaic.put(222),
+        'some-int': mosaic.put(123),
+        'string-1': mosaic.put('some string 1'),
+        'string-2': mosaic.put('some string 2'),
         }
 
-    def resolve_name(name):
+    def resolve_to_ref(name):
         return names[name]
 
-    resource = resource_type.resolve(definition, resolve_name, TEST_RESOURCES_DIR)
+    ctx = Mock(resolve_to_ref=resolve_to_ref)
+    resource, sources = resource_type.resolve(definition, ctx)
     log.info('Resolved resource: %r', resource)
-    assert resource == resource_t(
-        value=names['some-value'],
-        other_value=(names['other-1'], names['other-2']),
+    assert resource == a_record_t(
+        a_ref_opt=names['some-int'],
+        a_ref_list=(names['string-1'], names['string-2']),
     )
 
 
@@ -219,9 +233,8 @@ def test_reverse_resolve_definition_partial(mosaic, resource_type_producer):
     )
 
 
-def test_reverse_resolve_definition_based(mosaic, resource_type_factory, htypes):
-    resource_t = htypes.test_resources.test_resource_t
-    resource_type = resource_type_factory(resource_t)
+def _test_reverse_resolve_definition_based(mosaic, resource_type_factory, a_record_t):
+    resource_type = resource_type_factory(a_record_t)
     names = {
         'some-value': mosaic.put('some value'),
         'other-1': mosaic.put(111),
@@ -231,8 +244,8 @@ def test_reverse_resolve_definition_based(mosaic, resource_type_factory, htypes)
         value: key for key, value in names.items()
         }
     resource = resource_t(
-        value=names['some-value'],
-        other_value=(names['other-1'], names['other-2']),
+        a_ref_opt=names['some-value'],
+        a_ref_list=(names['other-1'], names['other-2']),
         )
 
     def reverse_resolve_name(name):
@@ -241,32 +254,41 @@ def test_reverse_resolve_definition_based(mosaic, resource_type_factory, htypes)
     definition = resource_type.reverse_resolve(resource, reverse_resolve_name, TEST_RESOURCES_DIR)
     log.info('Resolved definition: %r', definition)
     assert definition == resource_type.definition_t(
-        value='some-value',
-        other_value=('other-1', 'other-2'),
+        a_ref_opt='some-value',
+        a_ref_list=('other-1', 'other-2'),
         )
 
 
-# Inherited record result_t should also work.
-def test_resolve_definition_list_service(mosaic, resource_type_factory, htypes):
-    resource_t = htypes.test_resources.test_inherited_2
-    resource_type = resource_type_factory(resource_t)
+def test_resolve_definition_empty_inherited_record(pyobj_creg, mosaic, resource_type_factory):
+    string_mt = builtin_mt('string')
+    ref_mt = builtin_mt('ref')
+    ref_list_mt = list_mt(mosaic.put(ref_mt))
+    base_record_mt = record_mt('a_module', 'base_record', base=None, fields=(
+        field_mt('a_string', mosaic.put(string_mt)),
+        field_mt('a_ref', mosaic.put(ref_mt)),
+        field_mt('a_ref_list', mosaic.put(ref_list_mt)),
+        ))
+    a_record_mt = record_mt('a_module', 'a_record', base=mosaic.put(base_record_mt), fields=())
+    a_record_t = pyobj_creg.animate(a_record_mt)
+    resource_type = resource_type_factory(a_record_t)
     definition = resource_type.definition_t(
-        name='some_name',
-        value='some_value',
-        items=('some_item',),
+        a_string='some string',
+        a_ref='some-value',
+        a_ref_list=('some-item',),
         )
     names = {
-        'some_value': mosaic.put('some_value'),
-        'some_item': mosaic.put('some_item'),
+        'some-value': mosaic.put('some value'),
+        'some-item': mosaic.put('some item'),
         }
 
-    def resolve_name(name):
+    def resolve_to_ref(name):
         return names[name]
 
-    resource = resource_type.resolve(definition, resolve_name, TEST_RESOURCES_DIR)
+    ctx = Mock(resolve_to_ref=resolve_to_ref)
+    resource, sources = resource_type.resolve(definition, ctx)
     log.info('Resolved resource: %r', resource)
-    assert resource == resource_t(
-        name='some_name',
-        value=names['some_value'],
-        items=(names['some_item'],),
+    assert resource == a_record_t(
+        a_string='some string',
+        a_ref=names['some-value'],
+        a_ref_list=(names['some-item'],),
         )
