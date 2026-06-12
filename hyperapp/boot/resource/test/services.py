3fbf8b1ce56f19bb7d7d81c5a5612a8bbc94be8f
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2026-06-12 12:36:57 +0300

    Cleanup: Remove obsolete code from boot

--- hyperapp/boot/resource/test/services.py
+++ hyperapp/boot/resource/test/services.py
@@ -13,11 +13,7 @@ from hyperapp.boot.resource.resource_type import ResourceType
 from hyperapp.boot.resource.resource_type_registry import make_type_to_resource_type
 from hyperapp.boot.resource.pyobj_registry import register_resources_at_pyobj_creg
 from hyperapp.boot.resource.builtin_service import make_builtin_name_to_service
-# from hyperapp.boot.resource.legacy_type import load_legacy_type_resources
 from hyperapp.boot.resource.resource_type_producer import resource_type_producer as resource_type_producer_fn
-# from hyperapp.boot.resource.legacy_type import convert_builtin_types_to_dict
-# from hyperapp.boot.project import BuiltinsProject, Project, load_texts
-# from hyperapp.boot.test.hyper_types_namespace import HyperTypesNamespace
 from hyperapp.boot.register_coders import register_coders
 
 
@@ -57,50 +53,3 @@ def type_to_resource_type():
 @pytest.fixture
 def resource_type_producer(resource_type_factory, type_to_resource_type):
     return partial(resource_type_producer_fn, resource_type_factory, type_to_resource_type)
-
-
-# @pytest.fixture
-# def resource_module_factory(mosaic, resource_type_producer, pyobj_creg):
-#     return partial(ResourceModule, mosaic, resource_type_producer, pyobj_creg)
-
-
-# @pytest.fixture
-# def builtin_types_as_dict(pyobj_creg, builtin_types):
-#     return partial(convert_builtin_types_to_dict, pyobj_creg, builtin_types)
-
-
-# @pytest.fixture
-# def builtin_services(mosaic, web):
-#     return {
-#         'mosaic': mosaic,
-#         'web': web,
-#         }
-
-
-# @pytest.fixture
-# def builtin_service_resource_loader(mosaic, builtin_services):
-#     return partial(make_builtin_service_resource_module, mosaic, builtin_services.keys())
-
-
-# @pytest.fixture
-# def project(
-#         type_module_loader, resource_module_factory, builtin_types_as_dict, builtin_service_resource_loader,
-#         test_resources_dir):
-#     builtin_type_modules = load_legacy_type_resources(builtin_types_as_dict())
-#     builtins_project = BuiltinsProject(builtin_types_as_dict(), builtin_type_modules, builtin_service_resource_loader)
-#     project = Project(
-#         builtins_project, type_module_loader, resource_module_factory,
-#         test_resources_dir, name='test-project')
-#     path_to_text = load_texts(test_resources_dir)
-#     project.load(path_to_text)
-#     return project
-
-
-# @pytest.fixture
-# def resource_registry(project):
-#     return project
-
-
-# @pytest.fixture
-# def htypes(pyobj_creg, project):
-#     return HyperTypesNamespace(pyobj_creg, project.types)
--- hyperapp/boot/test/services.py
+++ hyperapp/boot/test/services.py
@@ -6,25 +6,15 @@ from hyperapp.boot.htypes.builtins import make_builtin_name_to_type
 from hyperapp.boot.mosaic import Mosaic
 from hyperapp.boot.web import Web
 from hyperapp.boot.pyobj_registry import PyObjRegistry
-from hyperapp.boot.association_registry import AssociationRegistry
+# from hyperapp.boot.association_registry import AssociationRegistry
 from hyperapp.boot.python_importer import PythonImporter
-# from hyperapp.boot.htypes import BuiltinTypeRegistry, register_builtin_types
-# from hyperapp.boot.htypes.python_module import python_module_t
-# from hyperapp.boot.htypes.attribute import attribute_t
-# from hyperapp.boot.htypes.partial import partial_t
-# from hyperapp.boot.services import pyobj_config
-# from hyperapp.boot.type_module_loader import TypeModuleLoader
-# from hyperapp.boot.resource.attribute import attribute_pyobj
-# from hyperapp.boot.resource.call import call_pyobj
-# from hyperapp.boot.resource.partial import partial_pyobj
-# from hyperapp.boot.resource.python_module import python_module_pyobj
 
 log = logging.getLogger(__name__)
 
 
-@pytest.fixture
-def association_reg():
-    return AssociationRegistry()
+# @pytest.fixture
+# def association_reg():
+#     return AssociationRegistry()
 
 
 @pytest.fixture
@@ -40,18 +30,6 @@ def mosaic(pyobj_creg):
 @pytest.fixture
 def web(pyobj_creg, mosaic):
     return Web(mosaic, pyobj_creg)
-    # pyobj_creg.init(builtin_types, mosaic, web)
-    # register_builtin_types(builtin_types, pyobj_creg)
-    # pyobj_creg.register_actor(
-    #     python_module_t, python_module_pyobj,
-    #     mosaic=mosaic,
-    #     python_importer=python_importer,
-    #     pyobj_creg=pyobj_creg,
-    #     )
-    # # pyobj_creg.register_actor(builtin_service_t, builtin_service_pyobj, self)
-    # pyobj_creg.register_actor(attribute_t, attribute_pyobj, pyobj_creg=pyobj_creg)
-    # pyobj_creg.register_actor(partial_t, partial_pyobj, pyobj_creg=pyobj_creg)
-    # return (mosaic, web)
 
 
 @pytest.fixture
