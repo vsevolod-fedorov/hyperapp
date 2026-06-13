7a98fb2e962c3cea81991e9cdb4b0977782fe794
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2026-06-13 11:25:51 +0300

    test_loader: Rename resources_root -> resources_dir

--- hyperapp/boot/resource/test/test_loader.py
+++ hyperapp/boot/resource/test/test_loader.py
@@ -17,19 +17,19 @@ pytest_plugins = [
     ]
 
 TEST_DIR = Path(__file__).parent.resolve()
-RESOURCES_ROOT = TEST_DIR / 'resources'
+RESOURCES_DIR = TEST_DIR / 'resources'
 
 
 @pytest.fixture
-def resources_root():
-    return RESOURCES_ROOT
+def resources_dir():
+    return RESOURCES_DIR
 
 
 @pytest.fixture
-def loader(pyobj_creg, mosaic, builtin_name_to_type, builtin_name_to_service, resource_type_producer, resources_root):
+def loader(pyobj_creg, mosaic, builtin_name_to_type, builtin_name_to_service, resource_type_producer, resources_dir):
     def load(project_to_path):
         project_to_files = {
-            name: load_file_tree(resources_root / path)
+            name: load_file_tree(resources_dir / path)
             for name, path in project_to_path.items()
             }
         resources, source_dict = load_resources(
@@ -110,9 +110,9 @@ def test_python_module_builtin_services(pyobj_creg, loader):
     assert result == 'ok'
 
 
-def test_type_module(pyobj_creg, resources_root, loader):
+def test_type_module(pyobj_creg, resources_dir, loader):
     dir = 'type_module'
-    source_text = resources_root.joinpath(dir, 'sample.types').read_text()
+    source_text = resources_dir.joinpath(dir, 'sample.types').read_text()
     resources, sources = loader({'a-project': dir})
     piece = resources['a-project', ('sample.t',), 'sample_record']
     t = pyobj_creg.animate(piece)
@@ -121,9 +121,9 @@ def test_type_module(pyobj_creg, resources_root, loader):
     record = t()  # Should not fail on assertion.
 
 
-def test_type_module_resolve_local(pyobj_creg, resources_root, loader):
+def test_type_module_resolve_local(pyobj_creg, resources_dir, loader):
     dir = 'type_module_resolve_local'
-    source_text = resources_root.joinpath(dir, 'sample.types').read_text()
+    source_text = resources_dir.joinpath(dir, 'sample.types').read_text()
     resources, sources = loader({'a-project': dir})
     outer_mt = resources['a-project', ('sample.t',), 'outer_record']
     outer_t = pyobj_creg.animate(outer_mt)
@@ -135,11 +135,11 @@ def test_type_module_resolve_local(pyobj_creg, resources_root, loader):
     assert sources[inner_mt] == ('a-project', ('sample.t',), TextSource(source_text))
 
 
-def test_type_module_resolve_imports(pyobj_creg, resources_root, loader):
+def test_type_module_resolve_imports(pyobj_creg, resources_dir, loader):
     dir = 'type_module_resolve_imports'
-    source_1_text = resources_root.joinpath(dir, 'sample_1.types').read_text()
-    source_2_text = resources_root.joinpath(dir, 'sample_2.types').read_text()
-    source_3_text = resources_root.joinpath(dir, 'sample_3.types').read_text()
+    source_1_text = resources_dir.joinpath(dir, 'sample_1.types').read_text()
+    source_2_text = resources_dir.joinpath(dir, 'sample_2.types').read_text()
+    source_3_text = resources_dir.joinpath(dir, 'sample_3.types').read_text()
     resources, sources = loader({'a-project': dir})
     outer_mt = resources['a-project', ('sample_3.t',), 'outer_record']
     outer_t = pyobj_creg.animate(outer_mt)
@@ -156,11 +156,11 @@ def test_type_module_resolve_imports(pyobj_creg, resources_root, loader):
     assert sources[outer_mt] == ('a-project', ('sample_3.t',), TextSource(source_3_text))
 
 
-def test_type_module_resolve_nested_imports(pyobj_creg, resources_root, loader):
+def test_type_module_resolve_nested_imports(pyobj_creg, resources_dir, loader):
     dir = 'type_module_resolve_nested_imports'
-    source_1_text = resources_root.joinpath(dir, 'sub_2/sample_1.types').read_text()
-    source_2_text = resources_root.joinpath(dir, 'sub_1/sample_2.types').read_text()
-    source_3_text = resources_root.joinpath(dir, 'sub_1/sub_1_1/sample_3.types').read_text()
+    source_1_text = resources_dir.joinpath(dir, 'sub_2/sample_1.types').read_text()
+    source_2_text = resources_dir.joinpath(dir, 'sub_1/sample_2.types').read_text()
+    source_3_text = resources_dir.joinpath(dir, 'sub_1/sub_1_1/sample_3.types').read_text()
     resources, sources = loader({'a-project': dir})
     outer_mt = resources['a-project', ('sub_1', 'sub_1_1', 'sample_3.t'), 'outer_record']
     outer_t = pyobj_creg.animate(outer_mt)
@@ -177,11 +177,11 @@ def test_type_module_resolve_nested_imports(pyobj_creg, resources_root, loader):
     assert sources[outer_mt] == ('a-project', ('sub_1', 'sub_1_1', 'sample_3.t'), TextSource(source_3_text))
 
 
-def test_type_module_resolve_mixed_imports(pyobj_creg, resources_root, loader):
+def test_type_module_resolve_mixed_imports(pyobj_creg, resources_dir, loader):
     dir = 'type_module_resolve_mixed_imports'
-    source_1_text = resources_root.joinpath(dir, 'sample_1.types').read_text()
-    source_2_text = resources_root.joinpath(dir, 'sample_2.types').read_text()
-    source_3_text = resources_root.joinpath(dir, 'sample_3.types').read_text()
+    source_1_text = resources_dir.joinpath(dir, 'sample_1.types').read_text()
+    source_2_text = resources_dir.joinpath(dir, 'sample_2.types').read_text()
+    source_3_text = resources_dir.joinpath(dir, 'sample_3.types').read_text()
     resources, sources = loader({'a-project': dir})
     outer_mt = resources['a-project', ('sample_3.t',), 'outer_record']
     outer_t = pyobj_creg.animate(outer_mt)
@@ -205,9 +205,9 @@ def test_type_module_resolve_mixed_imports(pyobj_creg, resources_root, loader):
     assert sources[outer_mt] == ('a-project', ('sample_3.t',), TextSource(source_3_text))
 
 
-def test_type_module_resolve_builtin_type(pyobj_creg, resources_root, loader):
+def test_type_module_resolve_builtin_type(pyobj_creg, resources_dir, loader):
     dir = 'type_module_resolve_builtin_type'
-    source_text = resources_root.joinpath(dir, 'sample.types').read_text()
+    source_text = resources_dir.joinpath(dir, 'sample.types').read_text()
     resources, sources = loader({'a-project': dir})
     piece = resources['a-project', ('sample.t',), 'sample_record']
     t = pyobj_creg.animate(piece)
