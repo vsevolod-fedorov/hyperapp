d57872ed6a3df81d6e3f46d11c8e38a89ef445b1
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2026-07-04 11:32:04 +0300

    rc boot: import configs

--- hyperapp/rc/boot.dyn.py
+++ hyperapp/rc/boot.dyn.py
@@ -1,3 +1,8 @@
+from .data.base.config import config as base_config
+from .data.rc.config import config as rc_config
+
 
 def main(args):
     print('rc main:', args)
+    print('base config:', base_config)
+    print('rc config:', rc_config)
