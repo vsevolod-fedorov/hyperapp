843536133ab4bb1ad5224665e6e8b51c58177748
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2026-07-27 13:03:46 +0300

    Change service actor suffix: .actor -> .service-actor

--- hyperapp/base/system/adapter.dyn.py
+++ hyperapp/base/system/adapter.dyn.py
@@ -1,7 +1,8 @@
 
 
 def typed_dict_config_adapter(piece, spiece, fn, service_config_creg):
-    assert 0, (piece, spiece, fn, service_config_creg)
+    config = service_config_creg.animate(spiece)
+    assert 0, (config, piece, spiece, fn)
 
 
 def service_object_adapter(piece, spiece, fn):
