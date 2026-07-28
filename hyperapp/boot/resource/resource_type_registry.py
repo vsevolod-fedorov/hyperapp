3d2485f0a46f4f8a4d72a71ed2aee0816ad70448
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2026-07-28 12:58:18 +0300

    Resource type registry: Fix htypes imports

--- hyperapp/boot/resource/resource_type_registry.py
+++ hyperapp/boot/resource/resource_type_registry.py
@@ -1,8 +1,8 @@
 from ..htypes.meta_type import list_mt, optional_mt, record_mt
-from .python_module import python_module_t
+from ..htypes.python_module import python_module_t
 from ..htypes.file_data import file_data_t
-from .attribute import attribute_t
-from .partial import partial_t
+from ..htypes.attribute import attribute_t
+from ..htypes.partial import partial_t
 from .list_mt_resource_type import ListMtResourceType
 from .optional_mt_resource_type import OptionalMtResourceType
 from .record_mt_resource_type import RecordMtResourceType
