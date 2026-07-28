e7af314acc398e1bca9cf589be3ba6126fd21fca
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2026-07-28 13:16:58 +0300

    Create subprocess_running service

--- hyperapp/rc/main.dyn.py
+++ hyperapp/rc/main.dyn.py
@@ -2,7 +2,8 @@ def compile_resources():
     print("rc compile resources")
 
 
-def main(args, compile_resources, identity_creg):
+def main(args, compile_resources, subprocess_running, identity_creg):
     print("rc main service:", args)
+    print("subprocess_running:", subprocess_running)
     print("identity_creg:", identity_creg)
     compile_resources()
