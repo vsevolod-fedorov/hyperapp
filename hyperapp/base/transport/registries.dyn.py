350ee112a85f4d8cf32e4b13113cf0f2d6c8f38b
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2026-08-05 10:35:13 +0300

    Transport registries: Use ResolvingCodeRegistry

--- hyperapp/base/transport/registries.dyn.py
+++ hyperapp/base/transport/registries.dyn.py
@@ -1,23 +1,25 @@
-from .services import (
-    code_registry_ctr,
-    )
+from .code.resolving_code_registry import ResolvingCodeRegistry
 
 
 def identity_creg(config):
-    return code_registry_ctr('identity_creg', config)
+    return ResolvingCodeRegistry('identity_creg', config)
 
 
 def peer_creg(config):
-    return code_registry_ctr('peer_creg', config)
+    return ResolvingCodeRegistry('peer_creg', config)
 
 
 def signature_creg(config):
-    return code_registry_ctr('signature_creg', config)
+    return ResolvingCodeRegistry('signature_creg', config)
 
 
 def parcel_creg(config):
-    return code_registry_ctr('parcel_creg', config)
+    return ResolvingCodeRegistry('parcel_creg', config)
 
 
 def route_creg(config):
-    return code_registry_ctr('route_creg', config)
+    return ResolvingCodeRegistry('route_creg', config)
+
+
+def message_creg(config):
+    return ResolvingCodeRegistry('message_creg', config)
