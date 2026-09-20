6b45552034f8551aabf31a6de0c935896a210e6f
Vsevolod Fedorov <vsevolod.fedorov@gmail.com>
2026-09-20 12:51:53 +0300

    Cleanup: Remove obsolete top-level runners/booters

--- run_client.pdb.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-import os.path
-
-sys.path.insert(0, os.path.expanduser('~/venv/lib/python2.7/site-packages'))
-exec(compile(open(os.path.join(os.getcwd(), 'run_client.py')).read(), os.path.join(os.getcwd(), 'client.py'), 'exec'))
